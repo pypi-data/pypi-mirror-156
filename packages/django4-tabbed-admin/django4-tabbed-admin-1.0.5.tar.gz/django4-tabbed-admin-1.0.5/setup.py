6c3e07c509904da88607b7564e4d4520340c677d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:41 +0000

    Add package django4-tabbed-admin/1.0.5/django4-tabbed-admin-1.0.5.tar.gz

---
+++ packages/django4-tabbed-admin/django4-tabbed-admin-1.0.5.tar.gz/django4-tabbed-admin-1.0.5/setup.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+import sys
+
+# python2 and python3 support
+try:
+    reload
+except NameError:
+    # py3k has unicode by default
+    pass
+else:
+    reload(sys).setdefaultencoding('utf-8')
+
+
+setup(
+    name='django4-tabbed-admin',
+    version='1.0.5',
+    author='Guillaume Pousseo, Tharushan',
+    author_email='guillaumepousseo@revsquare.com, contact@tharushan.fr',
+    description='Easily add tabs to django admin forms.',
+    long_description=open('README.rst').read(),
+    url='http://www.revsquare.com',
+    license='BSD License',
+    platforms=['OS Independent'],
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
