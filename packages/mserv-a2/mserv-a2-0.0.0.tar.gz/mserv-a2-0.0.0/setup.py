fab5d009639fc6c1b6416e5f63708f39e72f1e10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:17 +0000

    Add package mserv-a2/0.0.0/mserv-a2-0.0.0.tar.gz

---
+++ packages/mserv-a2/mserv-a2-0.0.0.tar.gz/mserv-a2-0.0.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+from os import environ
+
+package_name = environ.get("PACKAGE_NAME", "").strip()
+
+if not package_name:
+    raise RuntimeError("Specify package name as a PACKAGE_NAME env variable!")
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    description=(
+        "This package stands as a placeholder for Ampio packages maintained in private pypi "
+        "repositories. The purpose of such action is to prevent supply chain attacks."
+    ),
+    python_requires=">=99.0",
+    author="Michał Getka",
+    url="https://ampio.com",
+    packages=["placeholder"],
+)
