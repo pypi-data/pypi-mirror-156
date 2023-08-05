275bb7fa5f383cd4919390406655f74141841660
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:21:00 +0000

    Add package parett/1.0.1/parett-1.0.1.tar.gz

---
+++ packages/parett/parett-1.0.1.tar.gz/parett-1.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="parett",
+    version="1.0.1",
+    author="Louis-Stephane Le Clercq",
+    author_email="leclercq.l.s@gmail.com",
+    description="Python Automated Retrieval of TimeTree data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LSLeClercq/PAReTT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+    ],
+    packages=find_packages(),
+    install_requires=[
+        'pandas', 'splinter', 'numpy', 'Bio', 'selenium'],
+    python_requires=">=3.6",
+)
