5979f10d42c4c154197a274de11805ecb9d287dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:12:49 +0000

    Add package wmb/0.0.13/wmb-0.0.13-py3-none-any.whl

---
+++ packages/wmb/wmb-0.0.13-py3-none-any.whl/wmb/annot/broad.py
@@ -0,0 +1,22 @@
+from .annot import CellAnnotation
+
+from ..brain_region import brain
+
+
+class BROADTENXCellAnnotation(CellAnnotation):
+    __slots__ = ()
+
+    def __init__(self, annot_path, metadata):
+        super().__init__(annot_path)
+
+        # add BROAD specific attributes
+        self['sample'] = self.get_index('cell').map(lambda i: i[:-18])
+
+        metadata['MajorRegion'] = metadata['DissectionRegion'].map(
+            brain.map_dissection_region_to_major_region())
+        self['MajorRegion'] = self['sample'].to_pandas().map(metadata['MajorRegion'])
+
+        metadata['SubRegion'] = metadata['DissectionRegion'].map(
+            brain.map_dissection_region_to_sub_region())
+        self['SubRegion'] = self['sample'].to_pandas().map(metadata['SubRegion'])
+        return
