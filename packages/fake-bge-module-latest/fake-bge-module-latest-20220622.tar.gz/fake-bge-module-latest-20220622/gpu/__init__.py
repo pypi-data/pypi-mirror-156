4c569915eee4c1b0e1680475a8232e935f9d2354
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:17:13 +0000

    Add package fake-bge-module-latest/20220622/fake-bge-module-latest-20220622.tar.gz

---
+++ packages/fake-bge-module-latest/fake-bge-module-latest-20220622.tar.gz/fake-bge-module-latest-20220622/gpu/__init__.py
@@ -0,0 +1,10 @@
+import sys
+import typing
+from . import state
+from . import types
+from . import matrix
+from . import platform
+from . import capabilities
+from . import select
+from . import shader
+from . import texture
