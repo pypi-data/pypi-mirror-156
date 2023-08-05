4e56e3125c3b9e98679c73b231c587284db6a3ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:16 +0000

    Add package pattern-clustering/0.3.1/pattern_clustering-0.3.1.tar.gz

---
+++ packages/pattern-clustering/pattern_clustering-0.3.1.tar.gz/pattern_clustering-0.3.1/HISTORY.rst
@@ -0,0 +1,30 @@
+=======
+History
+=======
+
+---------------------------------
+0.1.0 (2022-05-11): First release
+---------------------------------
+
+* First release on PyPI.
+
+---------------------------------
+0.2.0 (2022-06-02): CI 
+---------------------------------
+
+* Updated tox.ini and GitHub actions, work in progress.
+
+-------------------------------------------------
+0.3.0 (2022-06-02): Bug fixes and CI improvements  
+-------------------------------------------------
+
+* Fixed sphinx local build
+* Fixed bumpversion
+* Add experiments notebooks and datasets
+* Improved test suite
+
+-------------------------------------------------
+0.3.1 (2022-06-02): Bug fixes and CI improvements  
+-------------------------------------------------
+
+* Fixed readthedoc build
---
+++ packages/pattern-clustering/pattern_clustering-0.3.1.tar.gz/pattern_clustering-0.3.1/src/pattern_clustering/cli.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# This file is part of the pattern-clustering project.
+# https://github.com/nokia/pattern-clustering
+
+"""Console script for pattern_clustering."""
+
+__author__     = "Marc-Olivier Buob, Maxime Raynal"
+__maintainer__ = "Marc-Olivier Buob, Maxime Raynal"
+__email__      = "marc-olivier.buob@nokia-bell-labs.com, maxime.raynal@nokia.com"
+__copyright__  = "Copyright (C) 2022, Nokia"
+__license__    = "Nokia"
+
+import argparse
+import sys
+
+
+def main():
+    """Console script for pattern_clustering."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('_', nargs='*')
+    args = parser.parse_args()
+
+    print("Arguments: " + str(args._))
+    print("Replace this message by putting your code into "
+          "pattern_clustering.cli.main")
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
