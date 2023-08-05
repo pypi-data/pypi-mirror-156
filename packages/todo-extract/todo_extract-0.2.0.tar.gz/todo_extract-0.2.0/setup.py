30dceadb9f164b9f6babc433003f95abf5d793ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:45 +0000

    Add package todo-extract/0.2.0/todo_extract-0.2.0.tar.gz

---
+++ packages/todo-extract/todo_extract-0.2.0.tar.gz/todo_extract-0.2.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['todo_extract']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'todo-extract',
+    'version': '0.2.0',
+    'description': 'Extract TODO items from the text file',
+    'long_description': None,
+    'author': 'Krystian Safjan',
+    'author_email': 'ksafjan@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
