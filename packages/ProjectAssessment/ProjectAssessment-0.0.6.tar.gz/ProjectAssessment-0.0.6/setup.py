2f811ead2744c628257533470e2c692ce35fea2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:20:15 +0000

    Add package ProjectAssessment/0.0.6/ProjectAssessment-0.0.6.tar.gz

---
+++ packages/ProjectAssessment/ProjectAssessment-0.0.6.tar.gz/ProjectAssessment-0.0.6/setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+
+setup(
+    name='ProjectAssessment',
+    version='0.0.6',
+    packages=['ProjectAssessment',],
+    license='MIT',
+    install_requires=[
+        'numpy',
+        'pandas',
+        'progress',
+        'scipy',
+        'prettytable',
+    ],
+    author='Ben Smith',
+    author_email='bosmith@unomaha.edu',
+    classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Science/Research', 
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.9',
+    ],
+    keywords = ['Assessment', 'Projects', 'Statistics', 'Education', 'Bootstrap'],
+    url = 'https://github.com/tazzben/project-based-assessment',
+    download_url = 'https://github.com/tazzben/project-based-assessment/archive/v0.0.6.tar.gz',  
+    description = 'Package to compute the Project-Based Assessment estimates of student and rubric proficiency.',
+    options={'bdist_wheel':{'universal':True}},
+)
