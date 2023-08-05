411007ae266409476d298e5bf96bdfed3b9dd417
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:10 +0000

    Add package python-office/0.1.4/python-office-0.1.4.tar.gz

---
+++ packages/python-office/python-office-0.1.4.tar.gz/python-office-0.1.4/office/__init__.py
@@ -0,0 +1,15 @@
+from office.api import word
+from office.api import pdf
+from office.api import image
+from office.api import file
+from office.api import video
+from office.api import tools
+from office.api import ppt
+from office.api import excel
+
+print('=' * 30)
+print('【python-office库】，功能持续更新中')
+print( '全部功能文档：https://www.python-office.com/')
+print('使用有问题 or 提交你的功能需求 or 参与项目开发')
+print('请加入【项目交流群】： https://www.python-office.com/api/img-cdn/python-office.jpg')
+print('=' * 30)
