6ce0001d6bfbf15460ec31fa3a3b5bb07ea96a78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:19:30 +0000

    Add package ovos-core/0.0.4a30/ovos_core-0.0.4a30-py3-none-any.whl

---
+++ packages/ovos-core/ovos_core-0.0.4a30-py3-none-any.whl/mycroft/configuration/__init__.py
@@ -0,0 +1,20 @@
+# Copyright 2017 Mycroft AI Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from mycroft.configuration.config import Configuration, LocalConf, RemoteConf
+from mycroft.configuration.locale import set_default_lf_lang, setup_locale, \
+    set_default_tz, set_default_lang, get_default_tz, get_default_lang, \
+    get_config_tz, get_primary_lang_code, load_languages, load_language
+from mycroft.configuration.locations import SYSTEM_CONFIG, USER_CONFIG, DEFAULT_CONFIG
+from ovos_utils.configuration import get_ovos_config, get_xdg_config_locations
---
+++ packages/ovos-core/ovos_core-0.0.4a30-py3-none-any.whl/mycroft/gui/interfaces/smartspeaker.py
@@ -0,0 +1,127 @@
+import platform
+from ovos_utils import network_utils
+from mycroft.messagebus import Message
+from mycroft.version import OVOS_VERSION_STR
+from ovos_utils.gui import GUIInterface
+
+
+class SmartSpeakerExtensionGuiInterface(GUIInterface):
+    def __init__(self, bus, homescreen_manager) -> None:
+        super(SmartSpeakerExtensionGuiInterface, self).__init__(
+            skill_id="SmartSpeakerExtension.GuiInterface")
+        self.bus = bus
+        self.homescreen_manager = homescreen_manager
+
+        # Initiate Bind
+        self.bind()
+
+    def bind(self):
+        super().set_bus(self.bus)
+
+        self.bus.on("mycroft.device.settings", self.handle_device_settings)
+        self.bus.on("ovos.PHAL.dashboard.status.response",
+                    self.update_device_dashboard_status)
+        self.register_handler("mycroft.device.settings",
+                              self.handle_device_settings)
+        self.register_handler(
+            "mycroft.device.settings.homescreen", self.handle_device_homescreen_settings)
+        self.register_handler("mycroft.device.settings.ssh",
+                              self.handle_device_ssh_settings)
+        self.register_handler(
+            "mycroft.device.settings.developer",  self.handle_device_developer_settings)
+        self.register_handler("mycroft.device.enable.dash",
+                              self.handle_device_developer_enable_dash)
+        self.register_handler("mycroft.device.disable.dash",
+                              self.handle_device_developer_disable_dash)
+        self.register_handler("mycroft.device.show.idle",
+                              self.handle_show_homescreen)
+        self.register_handler("mycroft.device.settings.customize",
+                              self.handle_device_customize_settings)
+        self.register_handler("mycroft.device.settings.create.theme",
+                              self.handle_device_create_theme)
+        self.register_handler("mycroft.device.settings.about.page",
+                              self.handle_device_about_page)
+
+    def handle_device_settings(self, message):
+        """ Display device settings page. """
+        self["state"] = "settings/settingspage"
+        self.show_page("SYSTEM_AdditionalSettings.qml", override_idle=True)
+
+    def handle_device_homescreen_settings(self, message):
+        """
+        display homescreen settings page
+        """
+        screens = self.homescreen_manager.homescreens
+        self["idleScreenList"] = {"screenBlob": screens}
+        self["selectedScreen"] = self.homescreen_manager.get_active_homescreen()
+        self["state"] = "settings/homescreen_settings"
+        self.show_page("SYSTEM_AdditionalSettings.qml", override_idle=True)
+
+    def handle_device_ssh_settings(self, message):
+        """
+        display ssh settings page
+        """
+        self["state"] = "settings/ssh_settings"
+        self.show_page("SYSTEM_AdditionalSettings.qml", override_idle=True)
+
+    def handle_set_homescreen(self, message):
+        """
+        Set the homescreen to the selected screen
+        """
+        homescreen_id = message.data.get("homescreen_id", "")
+        if homescreen_id:
+            self.homescreen_manager.set_active_homescreen(homescreen_id)
+
+    def handle_show_homescreen(self, message):
+        self.homescreen_manager.show_homescreen()
+
+    def handle_device_developer_settings(self, message):
+        self['state'] = 'settings/developer_settings'
+        self.handle_get_dash_status()
+
+    def handle_device_developer_enable_dash(self, message):
+        self.bus.emit(Message("ovos.PHAL.dashboard.enable"))
+
+    def handle_device_developer_disable_dash(self, message):
+        self.bus.emit(Message("ovos.PHAL.dashboard.disable"))
+
+    def update_device_dashboard_status(self, message):
+        call_check = message.data.get("status", False)
+        dash_security_pass = message.data.get("password", "")
+        dash_security_user = message.data.get("username", "")
+        dash_url = message.data.get("url", "")
+        if call_check:
+            self["dashboard_enabled"] = call_check
+            self["dashboard_url"] = dash_url
+            self["dashboard_user"] = dash_security_user
+            self["dashboard_password"] = dash_security_pass
+        else:
+            self["dashboard_enabled"] = call_check
+            self["dashboard_url"] = ""
+            self["dashboard_user"] = ""
+            self["dashboard_password"] = ""
+
+    def handle_device_customize_settings(self, message):
+        self['state'] = 'settings/customize_settings'
+        self.show_page("SYSTEM_AdditionalSettings.qml", override_idle=True)
+
+    def handle_device_create_theme(self, message):
+        self['state'] = 'settings/customize_theme'
+        self.show_page("SYSTEM_AdditionalSettings.qml", override_idle=True)
+
+    def handle_device_about_page(self, message):
+        uname_info = platform.uname()
+        system_information = {
+            "uname_os": uname_info[0],
+            "uname_systemversion": uname_info[1],
+            "uname_kernelversion": uname_info[2],
+            "ovos_core_version": OVOS_VERSION_STR,
+            "python_version": platform.python_version(),
+            "local_address": network_utils.get_ip()
+        }
+        self['state'] = 'settings/about_page'
+        self['system_info'] = system_information
+        self.show_page("SYSTEM_AdditionalSettings.qml", override_idle=True)
+
+    def handle_get_dash_status(self):
+        self.bus.emit(Message("ovos.PHAL.dashboard.get.status"))
