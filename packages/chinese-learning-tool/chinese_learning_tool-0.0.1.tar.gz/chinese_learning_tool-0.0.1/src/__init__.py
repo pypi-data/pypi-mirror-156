1e420894f937c6e67baadd935e0db881150de394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:30 +0000

    Add package chinese-learning-tool/0.0.1/chinese_learning_tool-0.0.1.tar.gz

---
+++ packages/chinese-learning-tool/chinese_learning_tool-0.0.1.tar.gz/chinese_learning_tool-0.0.1/src/__init__.py
@@ -0,0 +1,4 @@
+from Chinese_Learning_tool.Chinese_learn import *
+from Chinese_Learning_tool.gif_play import *
+from Chinese_Learning_tool.playsound import playsound
+from Chinese_Learning_tool.QGUI_widget import *
