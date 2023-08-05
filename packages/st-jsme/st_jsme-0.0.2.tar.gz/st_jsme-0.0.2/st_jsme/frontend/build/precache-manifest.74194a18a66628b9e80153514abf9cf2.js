594c759382ed0a1a6d97f47e233e632594f16480
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:15:55 +0000

    Add package st-jsme/0.0.2/st_jsme-0.0.2.tar.gz

---
+++ packages/st-jsme/st_jsme-0.0.2.tar.gz/st_jsme-0.0.2/st_jsme/frontend/build/precache-manifest.74194a18a66628b9e80153514abf9cf2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b77b79cda75ac692237c2a6415057a32",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28ad50f2d911996fc7ce",
+    "url": "./static/js/2.4ea1358c.chunk.js"
+  },
+  {
+    "revision": "c4806c150d49abac1ee67e2a3f6f71b4",
+    "url": "./static/js/2.4ea1358c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "982653f76b599293c73c",
+    "url": "./static/js/main.6ff92f51.chunk.js"
+  },
+  {
+    "revision": "7c417fdcdd265b5366e4",
+    "url": "./static/js/runtime-main.119b752e.js"
+  }
+]);
