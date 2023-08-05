35a700aaceb04c408628b73f4263ae68484e3543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:32 +0000

    Add package egen111222-tmp-fast-project/1.0/egen111222-tmp_fast_project-1.0.tar.gz

---
+++ packages/egen111222-tmp-fast-project/egen111222-tmp_fast_project-1.0.tar.gz/egen111222-tmp_fast_project-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='egen111222-tmp_fast_project',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Kirill',
+      author_email='egen13@ukr.net',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['my_project'],
+     )
