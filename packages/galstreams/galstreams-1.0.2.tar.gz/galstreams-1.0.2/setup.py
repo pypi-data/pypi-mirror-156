5791b31b88fbc89c431f1d840f3fa8712c7bb6b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:21:58 +0000

    Add package galstreams/1.0.2/galstreams-1.0.2.tar.gz

---
+++ packages/galstreams/galstreams-1.0.2.tar.gz/galstreams-1.0.2/setup.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import re
+
+try:
+  from setuptools import setup
+except ImportError:
+  from distutils.core import setup
+
+long_description = ""
+with open('README.md', encoding='utf-8') as file:
+    long_description = file.read()
+
+setup(
+    name='galstreams',
+    version='1.0.2',
+    author='C. Mateu',
+    author_email='cmateu@fisica.edu.uy',
+    packages=['galstreams','galstreams/lib','galstreams/tracks'],
+    package_data={'galstreams/lib':['*.dat',
+                                   '*.log',
+                                   'master*',
+                                   'streams_lib_notes.ipynb',
+                                   'globular_cluster_params.harris2010.tableI.csv'],
+		  'galstreams/tracks':['*.ecsv'],
+		  'galstreams/notebooks':['*ipy*']},
+#    scripts=['bin/'],
+    url='https://github.com/cmateu/galstreams',
+    license='BSD-3-Clause',
+    description='MW stream library toolkit',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+      "numpy",
+      "scipy",
+      "astropy",
+    ],
+    python_requires='>=3.5',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Development Status :: 5 - Production/Stable",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Scientific/Engineering :: Astronomy"
+    ],
+)
