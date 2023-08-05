265517517d6c2b1cbc3a227c461161e601e9d599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:11 +0000

    Add package poetry-demo-thomas-cleary/0.5.2/poetry-demo-thomas-cleary-0.5.2.tar.gz

---
+++ packages/poetry-demo-thomas-cleary/poetry-demo-thomas-cleary-0.5.2.tar.gz/poetry-demo-thomas-cleary-0.5.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['poetry_demo_thomas_cleary']
+
+package_data = \
+{'': ['*'], 'poetry_demo_thomas_cleary': ['data/*']}
+
+install_requires = \
+['requests>=2.28.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['poetry-demo = poetry_demo_thomas_cleary.cli:run']}
+
+setup_kwargs = {
+    'name': 'poetry-demo-thomas-cleary',
+    'version': '0.5.2',
+    'description': 'Example of setting up a python package project with poetry',
+    'long_description': '# Poetry Demo Thomas Cleary\nAn example of setting up a package with poetry',
+    'author': 'Thomas Cleary',
+    'author_email': 'thomasclearydev@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
