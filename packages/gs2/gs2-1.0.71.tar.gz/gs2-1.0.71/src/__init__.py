ed70aa196e0b72e7c5c40ea258883c06aba76823
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:31 +0000

    Add package gs2/1.0.71/gs2-1.0.71.tar.gz

---
+++ packages/gs2/gs2-1.0.71.tar.gz/gs2-1.0.71/src/__init__.py
@@ -0,0 +1,90 @@
+# Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+# Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+import account
+import auth
+import chat
+import core
+import datastore
+import deploy
+import dictionary
+import distributor
+import enhance
+import exchange
+import experience
+import formation
+import friend
+import gateway
+import identifier
+import inbox
+import inventory
+import job_queue
+import key
+import limit
+import lock
+import log
+import lottery
+import matchmaking
+import mission
+import money
+import news
+import project
+import quest
+import ranking
+import realtime
+import schedule
+import script
+import showcase
+import stamina
+import version
+import watch
+
+account = account
+auth = auth
+chat = chat
+core = core
+datastore = datastore
+deploy = deploy
+dictionary = dictionary
+distributor = distributor
+enhance = enhance
+exchange = exchange
+experience = experience
+formation = formation
+friend = friend
+gateway = gateway
+identifier = identifier
+inbox = inbox
+inventory = inventory
+job_queue = job_queue
+key = key
+limit = limit
+lock = lock
+log = log
+lottery = lottery
+matchmaking = matchmaking
+mission = mission
+money = money
+news = news
+project = project
+quest = quest
+ranking = ranking
+realtime = realtime
+schedule = schedule
+script = script
+showcase = showcase
+stamina = stamina
+version = version
+watch = watch
+
