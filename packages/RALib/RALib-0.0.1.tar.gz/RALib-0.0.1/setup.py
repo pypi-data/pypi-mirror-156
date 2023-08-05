a36066e12a7097e7502e4b5cb48a874c5dd50397
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:25 +0000

    Add package RALib/0.0.1/RALib-0.0.1.tar.gz

---
+++ packages/RALib/RALib-0.0.1.tar.gz/RALib-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  name="RALib",
+  version="0.0.1",
+  author="Wangruirui",
+  author_email="buaawr@foxmail.com",
+  description="Rui's astrodynamic lib",
+  long_description="text/markdown",
+  long_description_content_type="text/markdown",
+  url="https://github.com/",
+  packages=setuptools.find_packages(),
+  classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+  ],
+)
