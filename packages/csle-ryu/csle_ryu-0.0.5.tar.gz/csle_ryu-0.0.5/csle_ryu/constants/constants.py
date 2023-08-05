b33357868cac7cbf30cd3e517fd794974d5cb62a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:27 +0000

    Add package csle-ryu/0.0.5/csle_ryu-0.0.5.tar.gz

---
+++ packages/csle-ryu/csle_ryu-0.0.5.tar.gz/csle_ryu-0.0.5/csle_ryu/constants/constants.py
@@ -0,0 +1,17 @@
+
+
+class RYU:
+    """
+    String constants related to RYU
+    """
+    CONTROLLERS_PREFIX = "csle_ryu.controllers."
+    OFCTL_REST_APP = "ryu.app.ofctl_rest"
+    OFCTL_REST_TOPOLOGY = "ryu.app.rest_topology"
+    OFCTL_WS_TOPOLOGY = "ryu.app.ws_topology"
+    OFCTL_GUI_TOPOLOGY = "ryu.app.gui_topology.gui_topology"
+    OBSERVE_LINKS = "--observe-links"
+    APP_LISTS_ARG = "--app-lists"
+    LOG_FILE_ARG = "--log-file"
+    CONTROLLER_PORT_ARG = "--ofp-tcp-listen-port"
+    WEB_APP_PORT_ARG = "--wsapi-port"
+    RYU_MANAGER = "/root/miniconda3/bin/ryu-manager"
---
+++ packages/csle-ryu/csle_ryu-0.0.5.tar.gz/csle_ryu-0.0.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='csle_ryu',
+      version='0.0.5',
+      install_requires=['ryu', 'eventlet==0.30.2'],
+      author='Kim Hammar',
+      author_email='hammar.kim@gmail.com',
+      description='RYU SDN Controllers in CSLE',
+      license='Creative Commons Attribution-ShareAlike 4.0 International',
+      keywords='Reinforcement-Learning Cyber-Security Markov-Games Markov-Decision-Processes',
+      url='https://github.com/Limmen/csle',
+      download_url='https://github.com/Limmen/csle/archive/0.0.1.tar.gz',
+      packages=find_packages(),
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Artificial Intelligence',
+          'Programming Language :: Python :: 3.8'
+      ]
+      )
