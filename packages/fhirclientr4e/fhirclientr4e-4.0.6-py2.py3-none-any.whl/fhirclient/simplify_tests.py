49993d4c5ff1c7ac1ee33add1abd49edb0bc84c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:19 +0000

    Add package fhirclientr4e/4.0.6/fhirclientr4e-4.0.6-py2.py3-none-any.whl

---
+++ packages/fhirclientr4e/fhirclientr4e-4.0.6-py2.py3-none-any.whl/fhirclient/simplify_tests.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from pprint import pprint
+
+from fhirclient.models.patient_tests import PatientTests
+from fhirclient.models.researchstudy_tests import ResearchStudyTests
+
+
+class TestSimplify(unittest.TestCase):
+    """Ensure customizations we've added work."""
+
+    def testResearchStudy1(self):
+        inst = ResearchStudyTests().instantiate_from("researchstudy-example.json")
+        self.assertIsNotNone(inst, "Must have instantiated a ResearchStudy instance")
+        # pprint(inst.as_simplified_json())
+
+    def testPatient10(self):
+        inst = PatientTests().instantiate_from("patient-example-xds.json")
+        self.assertIsNotNone(inst, "Must have instantiated a Patient instance")
+        pprint(inst.as_simplified_json())
