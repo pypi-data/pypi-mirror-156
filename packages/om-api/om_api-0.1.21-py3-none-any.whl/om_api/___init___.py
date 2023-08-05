c914f71beb0482e3a512d28ceddaab9f0a6e244a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:01 +0000

    Add package om-api/0.1.21/om_api-0.1.21-py3-none-any.whl

---
+++ packages/om-api/om_api-0.1.21-py3-none-any.whl/om_api/___init___.py
@@ -0,0 +1,5 @@
+from .om_api import (make_request, DataFrame_to_List, get_properties, get_list, get_parents, get_items, add_item_to_list, change_properties, add_item_with_properties)
+
+__author__ = 'Matney Scherbakov'
+__version__ = '0.1.21'
+__email__ = 'l89851175735@gmail.com'
