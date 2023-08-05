a740daba52e6e2919081d18b8d9a046a47e8b5ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:37 +0000

    Add package py2sambvca/1.3.1/py2sambvca-1.3.1.tar.gz

---
+++ packages/py2sambvca/py2sambvca-1.3.1.tar.gz/py2sambvca-1.3.1/setup.py
@@ -0,0 +1,43 @@
+import os.path
+import codecs
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+cwd = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (cwd / "README.md").read_text()
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+# This call to setup() does all the work
+setup(
+    name="py2sambvca",
+    version=get_version("py2sambvca/__init__.py"),
+    description="Simple thin client to interface python scripts with SambVca catalytic pocket Fortran calculator.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JacksonBurns/py2sambvca",
+    author="Jackson Burns",
+    license="GNU GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    packages=["py2sambvca"],
+    include_package_data=True
+)
