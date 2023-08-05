4965da13c3f1f2858fb6e421ed318ec882d389d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:52 +0000

    Add package database-version-control/0.4.0/database_version_control-0.4.0.tar.gz

---
+++ packages/database-version-control/database_version_control-0.4.0.tar.gz/database_version_control-0.4.0/src/dvc/app/cli/commands/config.py
@@ -0,0 +1,27 @@
+"""
+config subcommand
+"""
+import logging
+
+import typer
+
+from dvc.core.config import ConfigDefault, DatabaseRevisionFilesManager, ConfigFileWriter, ConfigReader
+
+app = typer.Typer()
+
+
+@app.command()
+def init() -> None:
+    """
+    Generate configuration template. Idempotent.
+
+    :return:
+    """
+    # Step 1: Generate config file
+    config_file_writer = ConfigFileWriter(config_file_path=ConfigDefault.VAL__FILE_PATH)
+    config_file_writer.write_to_yaml()
+
+    # Step 2: Generate Datababase Revision SQL folder
+    config_file_reader = ConfigReader(ConfigDefault.VAL__FILE_PATH)
+    db_rv_files_man = DatabaseRevisionFilesManager(config_file_reader)
+    db_rv_files_man.create_database_revision_files_folder()
---
+++ packages/database-version-control/database_version_control-0.4.0.tar.gz/database_version_control-0.4.0/src/dvc/app/cli/main.py
@@ -0,0 +1,38 @@
+'''
+Starting point of the CLI
+'''
+"""
+Define the main commands of the CLI
+"""
+import logging
+import typer
+
+from dvc.version import __version__
+from dvc.app.cli.commands import config, database
+
+# Set default logging to INFO
+logging.root.setLevel(logging.INFO)
+
+DOCUMENTATION_URL = "https://python-database-version-control.readthedocs.io/en/latest/"
+EPILOG_TXT = f"Doc: {DOCUMENTATION_URL}"
+HELP_TXT = "A Database Version Control Tool"
+
+app = typer.Typer(
+    help=HELP_TXT,
+    short_help=HELP_TXT,
+    epilog=EPILOG_TXT,
+)
+app.add_typer(config.app, name='cfg', help="Config related subcommands")
+app.add_typer(database.app, name='db', help="Database related subcommands")
+
+
+@app.command()
+def version():
+    """
+    Show CLI version
+    """
+    typer.echo(__version__)
+
+
+if __name__ == "__main__":
+    app()
