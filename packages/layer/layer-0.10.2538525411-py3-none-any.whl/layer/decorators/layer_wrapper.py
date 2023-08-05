7dfecd005149cf2ea7b3cdc655eef344e30c497e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:14 +0000

    Add package layer/0.10.2538525411/layer-0.10.2538525411-py3-none-any.whl

---
+++ packages/layer/layer-0.10.2538525411-py3-none-any.whl/layer/decorators/layer_wrapper.py
@@ -0,0 +1,64 @@
+from typing import Any, Dict, List, Optional, Tuple, Union
+
+import wrapt  # type: ignore
+
+from layer.contracts.assets import AssetPath, AssetType, BaseAsset
+from layer.contracts.datasets import Dataset
+from layer.contracts.models import Model
+from layer.settings import LayerSettings
+
+
+# See https://wrapt.readthedocs.io/en/latest/wrappers.html#custom-function-wrappers for more.
+class LayerFunctionWrapper(wrapt.FunctionWrapper):
+    def __init__(
+        self,
+        wrapped: Any,
+        wrapper: Any,
+        enabled: Any,
+    ) -> None:
+        super().__init__(wrapped, wrapper, enabled)
+        if not hasattr(wrapped, "layer"):
+            wrapped.layer = LayerSettings()
+        self.layer: LayerSettings = wrapped.layer
+
+    # wrapt doesn't implement this method and based on this https://github.com/GrahamDumpleton/wrapt/issues/102#issuecomment-899937490
+    # we give it a shot and it seems to be working
+    def __reduce_ex__(self, protocol: Any) -> Any:
+        return type(self), (self.__wrapped__, self._self_wrapper, self._self_enabled)
+
+    def __copy__(self) -> None:
+        pass
+
+    def __deepcopy__(self, memo: Dict[int, object]) -> None:
+        pass
+
+    def __reduce__(self) -> Union[str, Tuple[Any, ...]]:
+        pass
+
+
+class LayerAssetFunctionWrapper(LayerFunctionWrapper):
+    def __init__(
+        self,
+        wrapped: Any,
+        wrapper: Any,
+        enabled: Any,
+        asset_type: AssetType,
+        name: str,
+        dependencies: Optional[List[Union[str, Dataset, Model]]],
+    ) -> None:
+        super().__init__(wrapped, wrapper, enabled)
+        self.layer.set_asset_type(asset_type)
+        self.layer.set_asset_name(name)
+
+        paths: List[AssetPath] = []
+        if dependencies is not None:
+            for dependency in dependencies:
+                if isinstance(dependency, str):
+                    paths.append(AssetPath.parse(dependency))
+                elif isinstance(dependency, BaseAsset):
+                    paths.append(dependency._path)
+                else:
+                    raise ValueError(
+                        "Dependencies can only be a string, Dataset or Model."
+                    )
+        self.layer.set_dependencies(paths)
