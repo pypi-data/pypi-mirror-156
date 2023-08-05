9e76f9e2883b836daa41070638bd5c51fe6f4035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:11:10 +0000

    Add package gradio/3.0.20/gradio-3.0.20-py3-none-any.whl

---
+++ packages/gradio/gradio-3.0.20-py3-none-any.whl/gradio/templates/cdn/assets/color.9fae8524.js
@@ -0,0 +1,7 @@
+import { aa as ordered_colors } from './index.baea847c.js';
+
+const get_next_color = (index) => {
+  return ordered_colors[index % ordered_colors.length];
+};
+
+export { get_next_color as g };
