bed6a475c834aba262d9b30b19496a59a1ac4c75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:29 +0000

    Add package OpenFisca-US/0.79.0/OpenFisca_US-0.79.0-py3-none-any.whl

---
+++ packages/OpenFisca-US/OpenFisca_US-0.79.0-py3-none-any.whl/openfisca_us/variables/contrib/ubi_center/basic_income/basic_income.py
@@ -0,0 +1,18 @@
+from openfisca_us.model_api import *
+
+
+class basic_income(Variable):
+    value_type = float
+    entity = Person
+    label = "Basic income"
+    unit = USD
+    documentation = "Total basic income payments for this person. Phase-outs as an equal percentage to all tax unit members."
+    definition_period = YEAR
+
+    def formula(person, period, parameters):
+        basic_income = person("bi_before_phase_out", period)
+        tax_unit = person.tax_unit
+        tax_unit_basic_income = tax_unit.sum(basic_income)
+        tax_unit_phase_out = tax_unit("bi_phase_out", period)
+        percent_reduction = tax_unit_phase_out / tax_unit_basic_income
+        return basic_income * (1 - percent_reduction)
