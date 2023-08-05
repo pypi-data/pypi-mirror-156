dfd58e720caf76dd834765fde3bcb3d9440b5da0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:22 +0000

    Add package stoobly-agent/0.11.5/stoobly_agent-0.11.5-py3-none-any.whl

---
+++ packages/stoobly-agent/stoobly_agent-0.11.5-py3-none-any.whl/stoobly_agent/app/cli/helpers/project_facade.py
@@ -0,0 +1,60 @@
+import pdb
+import requests
+
+from stoobly_agent.lib.api.projects_resource import ProjectsResource
+from stoobly_agent.lib.api.users_resource import UsersResource
+from stoobly_agent.lib.api.interfaces import ProjectCreateParams, ProjectShowResponse, ProjectsIndexResponse, UserProfileResponse
+from stoobly_agent.lib.api.keys import OrganizationKey, ProjectKey
+from stoobly_agent.app.settings import Settings
+
+class ProjectFacade():
+
+  def __init__(self, __settings: Settings):
+    self.__settings = __settings
+    self.__api = ProjectsResource(self.__settings.remote.api_url, self.__settings.remote.api_key)
+
+  def user_profile(self) -> UserProfileResponse:
+    resource = UsersResource(self.__settings.remote.api_url, self.__settings.remote.api_key)
+    res = resource.profile()
+
+    if not res.ok:
+      raise AssertionError(res.content)
+
+    return res.json()
+
+  def create(self, **kwargs: ProjectCreateParams) -> ProjectShowResponse:
+    organization_key: str = OrganizationKey(kwargs.get('organization_key')) 
+
+    res: requests.Response =  self.__api.create(
+      description=kwargs.get('description') or '',
+      name=kwargs.get('name'),
+      organization_id=organization_key.id
+    )
+
+    if not res.ok:
+      raise AssertionError(res.content)
+
+    return res.json()
+
+  def show(self, project_key: str) -> ProjectShowResponse:
+    key = ProjectKey(project_key)
+    res = self.__api.show(key.id)
+
+    if not res.ok:
+      raise AssertionError(res.content)
+
+    return res.json()
+
+  def index(self, kwargs) -> ProjectsIndexResponse:
+    organization_key: str = OrganizationKey(kwargs.get('organization_key')) 
+
+    del kwargs['organization_key']
+
+    res = self.__api.index(**{ 'organization_id': organization_key.id,  **kwargs })
+
+    if not res.ok:
+      raise AssertionError(res.content)
+
+    return res.json()
+
+  
