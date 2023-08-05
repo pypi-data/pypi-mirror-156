9712c67efddedbaf36f3a56a62e08073bf3fecd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:40 +0000

    Add package NEMO-billing/1.20.1/NEMO_billing-1.20.1-py3-none-any.whl

---
+++ packages/NEMO-billing/NEMO_billing-1.20.1-py3-none-any.whl/NEMO_billing/invoices/customization.py
@@ -0,0 +1,34 @@
+from typing import Dict
+
+from NEMO.decorators import customization
+from NEMO.views.customization import CustomizationBase
+from django.core.exceptions import ValidationError
+
+
+@customization(key="invoices", title="Invoices")
+class InvoiceCustomization(CustomizationBase):
+    variables = {"invoice_number_format": "{:04d}", "invoice_number_current": "0"}
+    files = [
+        ("email_send_invoice_subject", ".txt"),
+        ("email_send_invoice_message", ".html"),
+        ("email_send_invoice_reminder_subject", ".txt"),
+        ("email_send_invoice_reminder_message", ".html"),
+    ]
+
+    def context(self) -> Dict:
+        # Adding invoice number formatted to the template
+        customization_context = super().context()
+        try:
+            customization_context["invoice_number_formatted"] = self.get("invoice_number_format").format(
+                int(self.get("invoice_number_current"))
+            )
+        except:
+            pass
+        return customization_context
+
+    def validate(self, name, value):
+        if name == "invoice_number_format":
+            try:
+                value.format(123)
+            except Exception as e:
+                raise ValidationError(str(e))
