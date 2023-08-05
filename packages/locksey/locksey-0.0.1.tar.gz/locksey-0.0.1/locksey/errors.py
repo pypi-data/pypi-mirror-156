134eb3ba8711a6b333571d0d4c174a62ea6e1493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:12 +0000

    Add package locksey/0.0.1/locksey-0.0.1.tar.gz

---
+++ packages/locksey/locksey-0.0.1.tar.gz/locksey-0.0.1/locksey/errors.py
@@ -0,0 +1,14 @@
+class PasswordRequiredError(Exception):
+    pass
+
+
+class PathAlreadyExistsError(Exception):
+    pass
+
+
+class PasswordDoesNotExistError(Exception):
+    pass
+
+
+class PasswordAlreadyExistsError(Exception):
+    pass
