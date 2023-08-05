30306d5f1960321753c81fedcd70a48ec7fe7352
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:16 +0000

    Add package ott-jax/0.2.7/ott_jax-0.2.7-py3-none-any.whl

---
+++ packages/ott-jax/ott_jax-0.2.7-py3-none-any.whl/ott/core/gromov_wasserstein.py
@@ -0,0 +1,400 @@
+# Copyright 2022 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""A Jax version of the regularised GW Solver (Peyre et al. 2016)."""
+import functools
+from typing import Any, Dict, NamedTuple, Optional, Union
+
+import jax
+import jax.numpy as jnp
+
+from ott.core import (
+    fixed_point_loop,
+    problems,
+    quad_problems,
+    sinkhorn,
+    sinkhorn_lr,
+    was_solver,
+)
+from ott.geometry import epsilon_scheduler, geometry, low_rank, pointcloud
+
+LinearOutput = Union[sinkhorn.SinkhornOutput, sinkhorn_lr.LRSinkhornOutput]
+
+
+class GWOutput(NamedTuple):
+  """Holds the output of the Gromov-Wasserstein solver.
+
+  Attributes:
+    costs: Holds the sequence of regularized GW costs seen through the outer
+      loop of the solver.
+    linear_convergence: Holds the sequence of bool convergence flags of the
+      inner Sinkhorn iterations.
+    convergence: Bool convergence flag for the outer GW iterations.
+    errors: Holds sequence of vectors of errors of the Sinkhorn algorithm
+      at each iteration.
+    linear_state: State used to solve and store solutions to the local
+      linearization of GW.
+    geom: The geometry underlying the local linearization.
+    old_transport_mass: Holds total mass of transport at previous iteration.
+    transport: The transport matrix.
+    reg_gw_cost: Regularized optimal transport cost of the linearization.
+  """
+
+  costs: Optional[jnp.ndarray] = None
+  linear_convergence: Optional[jnp.ndarray] = None
+  convergence: bool = False
+  errors: Optional[jnp.ndarray] = None
+  linear_state: Optional[LinearOutput] = None
+  geom: Optional[geometry.Geometry] = None
+  # Intermediate values.
+  old_transport_mass: float = 1.0
+
+  def set(self, **kwargs: Any) -> 'GWOutput':
+    """Return a copy of self, possibly with overwrites."""
+    return self._replace(**kwargs)
+
+  @property
+  def matrix(self) -> jnp.ndarray:
+    """Transport matrix."""
+    rescale_factor = jnp.sqrt(
+        self.old_transport_mass / self.linear_state.transport_mass()
+    )
+    return self.linear_state.matrix * rescale_factor
+
+  @property
+  def reg_gw_cost(self) -> float:
+    return self.linear_state.reg_ot_cost
+
+
+class GWState(NamedTuple):
+  """Holds the state of the Gromov-Wasserstein solver.
+
+  Attributes:
+    costs: Holds the sequence of regularized GW costs seen through the outer
+      loop of the solver.
+    linear_convergence: Holds the sequence of bool convergence flags of the
+      inner Sinkhorn iterations.
+    errors: Holds sequence of vectors of errors of the Sinkhorn algorithm
+      at each iteration.
+    linear_state: State used to solve and store solutions to the local
+      linearization of GW.
+    linear_pb: Local linearization of the quadratic GW problem.
+    old_transport_mass: Intermediary value of the mass of the transport matrix.
+  """
+
+  costs: Optional[jnp.ndarray] = None
+  linear_convergence: Optional[jnp.ndarray] = None
+  errors: Optional[jnp.ndarray] = None
+  linear_state: Optional[LinearOutput] = None
+  linear_pb: Optional[problems.LinearProblem] = None
+  # Intermediate values.
+  old_transport_mass: float = 1.0
+
+  def set(self, **kwargs: Any) -> 'GWState':
+    """Return a copy of self, possibly with overwrites."""
+    return self._replace(**kwargs)
+
+  def update(
+      self, iteration: int, linear_sol: LinearOutput,
+      linear_pb: problems.LinearProblem, store_errors: bool,
+      old_transport_mass: float
+  ) -> 'GWState':
+    costs = self.costs.at[iteration].set(linear_sol.reg_ot_cost)
+    errors = None
+    if store_errors and self.errors is not None:
+      errors = self.errors.at[iteration, :].set(linear_sol.errors)
+    linear_convergence = self.linear_convergence.at[iteration].set(
+        linear_sol.converged
+    )
+    return self.set(
+        linear_state=linear_sol,
+        linear_pb=linear_pb,
+        costs=costs,
+        linear_convergence=linear_convergence,
+        errors=errors,
+        old_transport_mass=old_transport_mass
+    )
+
+
+@jax.tree_util.register_pytree_node_class
+class GromovWasserstein(was_solver.WassersteinSolver):
+  """A Gromov Wasserstein solver, built on generic template."""
+
+  def __call__(self, prob: quad_problems.QuadraticProblem) -> GWOutput:
+    # Consider converting problem first if using low-rank solver
+    if self.is_low_rank:
+      convert = (
+          isinstance(prob.geom_xx, pointcloud.PointCloud) and
+          prob.geom_xx.is_squared_euclidean and
+          isinstance(prob.geom_yy, pointcloud.PointCloud) and
+          prob.geom_yy.is_squared_euclidean
+      )
+      # Consider converting
+      if convert:
+        if not prob.is_fused or isinstance(prob.geom_xy, low_rank.LRCGeometry):
+          prob.geom_xx = prob.geom_xx.to_LRCGeometry()
+          prob.geom_yy = prob.geom_yy.to_LRCGeometry()
+        else:
+          if (
+              isinstance(prob.geom_xy, pointcloud.PointCloud) and
+              prob.geom_xy.is_squared_euclidean
+          ):
+            prob.geom_xy = prob.geom_xy.to_LRCGeometry(prob.fused_penalty)
+            prob.geom_xx = prob.geom_xx.to_LRCGeometry()
+            prob.geom_yy = prob.geom_yy.to_LRCGeometry()
+
+    # Possibly jit iteration functions and run. Closure on rank to
+    # avoid jitting issues, since rank value will be used to branch between
+    # a default entropic GW or a low-rank GW.
+    iterations_fn = functools.partial(iterations, rank=self.rank)
+    gromov_fn = jax.jit(iterations_fn) if self.jit else iterations_fn
+    out = gromov_fn(self, prob)
+    # TODO(lpapaxanthos): remove stop_gradient when using backprop
+    if self.is_low_rank:
+      linearization = prob.update_lr_linearization(
+          jax.lax.stop_gradient(out.linear_state)
+      )
+    else:
+      linearization = prob.update_linearization(
+          jax.lax.stop_gradient(out.linear_state), self.epsilon,
+          jax.lax.stop_gradient(out.old_transport_mass)
+      )
+    linear_state = out.linear_state.set_cost(linearization, True, True)
+    iteration = jnp.sum(out.costs != -1)
+    convergence = jnp.logical_and(
+        iteration < self.max_iterations, jnp.all(out.linear_convergence)
+    )
+    return out.set(linear_state=linear_state, convergence=convergence)
+
+  def init_state(
+      self, prob: quad_problems.QuadraticProblem, rank: int
+  ) -> GWState:
+    """Initialize the state of the Gromov-Wasserstein iterations."""
+    if rank > 0:
+      linearization = prob.init_lr_linearization(rank)
+    else:
+      linearization = prob.init_linearization(self.epsilon)
+
+    linear_state = self.linear_ot_solver(linearization)
+    num_iter = self.max_iterations
+    transport_mass = prob.init_transport_mass()
+    if self.store_inner_errors:
+      errors = -jnp.ones((num_iter, self.linear_ot_solver.outer_iterations))
+    else:
+      errors = None
+    return GWState(
+        -jnp.ones((num_iter,)), -jnp.ones((num_iter,)), errors, linear_state,
+        linearization, transport_mass
+    )
+
+  def output_from_state(self, state: GWState) -> GWOutput:
+    """Create an output from a loop state.
+
+    Arguments:
+      state: A GWState.
+
+    Returns:
+      A GWOutput.
+    """
+    geom = state.linear_pb.geom
+    return GWOutput(
+        costs=state.costs,
+        linear_convergence=state.linear_convergence,
+        errors=state.errors,
+        linear_state=state.linear_state,
+        geom=geom,
+        old_transport_mass=state.old_transport_mass
+    )
+
+
+def iterations(
+    solver: GromovWasserstein, prob: quad_problems.QuadraticProblem, rank: int
+) -> GWOutput:
+  """Jittable Gromov-Wasserstein outer loop."""
+
+  def cond_fn(
+      iteration: int, constants: GromovWasserstein, state: GWState
+  ) -> bool:
+    solver = constants
+    return solver._continue(state, iteration)
+
+  def body_fn(
+      iteration: int, constants: GromovWasserstein, state: GWState,
+      compute_error: bool
+  ) -> GWState:
+    del compute_error  # Always assumed True for outer loop of GW.
+    solver = constants
+    if rank > 0:
+      linear_pb = prob.update_lr_linearization(state.linear_state)
+    else:
+      linear_pb = prob.update_linearization(
+          state.linear_state, solver.epsilon, state.old_transport_mass
+      )
+
+    out = solver.linear_ot_solver(linear_pb)
+    old_transport_mass = jax.lax.stop_gradient(
+        state.linear_state.transport_mass()
+    )
+    return state.update(
+        iteration, out, linear_pb, solver.store_inner_errors, old_transport_mass
+    )
+
+  state = fixed_point_loop.fixpoint_iter(
+      cond_fn=cond_fn,
+      body_fn=body_fn,
+      min_iterations=solver.min_iterations,
+      max_iterations=solver.max_iterations,
+      inner_iterations=1,
+      constants=solver,
+      state=solver.init_state(prob, rank)
+  )
+
+  return solver.output_from_state(state)
+
+
+def make(
+    epsilon: Union[epsilon_scheduler.Epsilon, float] = 1.,
+    rank: int = -1,
+    max_iterations: int = 50,
+    jit: bool = False,
+    warm_start: bool = True,
+    store_inner_errors: bool = False,
+    linear_ot_solver_kwargs: Optional[Dict[str, Any]] = None,
+    threshold: float = 1e-2,
+    min_iterations: int = 1,
+    **kwargs: Any,
+) -> GromovWasserstein:
+  """Create a GromovWasserstein solver.
+
+  Args:
+    epsilon: a regularization parameter or a epsilon_scheduler.Epsilon object.
+    rank: integer used to constrain the rank of GW solutions if >0.
+    max_iterations: the maximum number of outer iterations for
+      Gromov Wasserstein.
+    jit: bool, if True, jits the function.
+    warm_start: deprecated.
+    store_inner_errors: whether or not to return all the errors of the inner
+      Sinkhorn iterations.
+    linear_ot_solver_kwargs: Optionally a dictionary containing the keywords
+      arguments for the linear OT solver (e.g. sinkhorn)
+    threshold: threshold (progress between two iterate costs) used to stop GW.
+    min_iterations: see fixed_point_loop.
+    kwargs: additional kwargs for epsilon.
+
+  Returns:
+    A GromovWasserstein solver.
+  """
+  del warm_start
+  if linear_ot_solver_kwargs is None:
+    linear_ot_solver_kwargs = {}
+
+  if rank == -1:
+    sink = sinkhorn.make(**linear_ot_solver_kwargs)
+  elif rank > 0:
+    # `rank` and `epsilon` are arguments of the `sinkhorn_lr` solver. As we are
+    # passing them to make, we should not pass them in `linear_ot_solver_kwargs`
+    # Therefore, the `rank` or `epsilon` passed to `linear_ot_solver_kwargs` are
+    # deleted.
+    linear_ot_solver_kwargs.pop('rank', None)
+    linear_ot_solver_kwargs.pop('epsilon', None)
+    sink = sinkhorn_lr.make(
+        rank=rank, epsilon=epsilon, **linear_ot_solver_kwargs
+    )
+
+  return GromovWasserstein(
+      epsilon,
+      rank,
+      max_iterations=max_iterations,
+      jit=jit,
+      linear_ot_solver=sink,
+      threshold=threshold,
+      store_inner_errors=store_inner_errors,
+      min_iterations=min_iterations,
+      **kwargs
+  )
+
+
+def gromov_wasserstein(
+    geom_xx: geometry.Geometry,
+    geom_yy: geometry.Geometry,
+    geom_xy: Optional[geometry.Geometry] = None,
+    fused_penalty: float = 1.0,
+    scale_cost: Optional[Union[bool, float, str]] = False,
+    a: Optional[jnp.ndarray] = None,
+    b: Optional[jnp.ndarray] = None,
+    loss: Optional[str] = None,
+    tau_a: Optional[float] = 1.0,
+    tau_b: Optional[float] = 1.0,
+    gw_unbalanced_correction: bool = True,
+    **kwargs: Any,
+) -> GWOutput:
+  """Solve a Gromov Wasserstein problem.
+
+  Wrapper that instantiates a quadratic problem (possibly with linear term
+  if the problem is fused) and calls a solver to output a solution.
+
+  Args:
+    geom_xx: a Geometry object for the first view.
+    geom_yy: a second Geometry object for the second view.
+    geom_xy: a Geometry object representing the linear cost in FGW.
+    fused_penalty: multiplier of the linear term in Fused Gromov Wasserstein,
+      i.e. loss = quadratic_loss + fused_penalty * linear_loss.
+      Ignored if ``geom_xy`` is not specified.
+    scale_cost: option to rescale the cost matrices:
+
+      - if `True`, use the default for each geometry.
+      - if `False`, keep the original scaling in geometries.
+      - if :class:`str`, use a specific method available in
+        :class:`ott.geometry.geometry.Geometry` or
+        :class:`ott.geometry.pointcloud.PointCloud`.
+      - if `None`, do not scale the cost matrices.
+
+    a: jnp.ndarray<float>[num_a,] or jnp.ndarray<float>[batch,num_a] weights.
+    b: jnp.ndarray<float>[num_b,] or jnp.ndarray<float>[batch,num_b] weights.
+    loss: str, None defaults to the square Euclidean distance, can also
+      receive 'kl' to define the GW loss.
+    tau_a: float between 0 and 1.0, parameter that controls the strength of the
+      KL divergence constraint between the weights and marginals of the
+      transport for the first view. If set to 1.0, then it is equivalent to a
+      hard constraint and if smaller to a softer constraint.
+    tau_b: float between 0 and 1.0, parameter that controls the strength of the
+      KL divergence constraint between the weights and marginals of the
+      transport for the second view. If set to 1.0, then it is equivalent to a
+      hard constraint and if smaller to a softer constraint.
+    gw_unbalanced_correction: True (default) if the unbalanced version of
+      Sejourne et al (Neurips 2021) is used, False if tau_a and tau_b
+      only affect the inner Sinhkorn loop.
+    kwargs: keyword arguments to make.
+
+  Returns:
+    A GromovWassersteinState named tuple.
+  """
+  losses = {'kl': quad_problems.make_kl_loss}
+  loss_fn = losses.get(loss, None)
+  prob = quad_problems.QuadraticProblem(
+      geom_xx,
+      geom_yy,
+      geom_xy=geom_xy,
+      fused_penalty=fused_penalty,
+      scale_cost=scale_cost,
+      a=a,
+      b=b,
+      loss=(loss_fn() if loss_fn is not None else None),
+      tau_a=tau_a,
+      tau_b=tau_b,
+      gw_unbalanced_correction=gw_unbalanced_correction
+  )
+  solver = make(**kwargs)
+  return solver(prob)
---
+++ packages/ott-jax/ott_jax-0.2.7-py3-none-any.whl/ott/geometry/costs.py
@@ -0,0 +1,332 @@
+# Copyright 2022 Google LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Lint as: python3
+"""Several cost/norm functions for relevant vector types."""
+import abc
+import functools
+from typing import Any, Callable, Optional, Union
+
+import jax
+import jax.numpy as jnp
+
+from ott.core import fixed_point_loop
+from ott.geometry import matrix_square_root
+
+
+@jax.tree_util.register_pytree_node_class
+class CostFn(abc.ABC):
+  """A generic cost function, taking two vectors as input.
+
+  Cost functions evaluate a function on a pair of inputs. For convenience,
+  that function is split into two norms -- evaluated on each input separately --
+  followed by a pairwise cost that involves both inputs, as in
+
+  c(x,y) = norm(x) + norm(y) + pairwise(x,y)
+
+  If the norm function is not implemented, that value is handled as a 0.
+  """
+
+  # no norm function created by default.
+  norm: Optional[Callable[[jnp.ndarray], Union[float, jnp.ndarray]]] = None
+
+  @abc.abstractmethod
+  def pairwise(self, x: jnp.ndarray, y: jnp.ndarray) -> float:
+    pass
+
+  def barycenter(self, weights: jnp.ndarray, xs: jnp.ndarray) -> float:
+    pass
+
+  def __call__(self, x: jnp.ndarray, y: jnp.ndarray) -> float:
+    return self.pairwise(
+        x, y
+    ) + (0 if self.norm is None else self.norm(x) + self.norm(y))
+
+  def all_pairs(self, x: jnp.ndarray, y: jnp.ndarray) -> jnp.ndarray:
+    """Compute matrix of all costs (including norms) for vectors in x / y.
+
+    Args:
+      x: [num_a, d] jnp.ndarray
+      y: [num_b, d] jnp.ndarray
+    Returns:
+      [num_a, num_b] matrix of cost evaluations.
+    """
+    return jax.vmap(lambda x_: jax.vmap(lambda y_: self(x_, y_))(y))(x)
+
+  def all_pairs_pairwise(self, x: jnp.ndarray, y: jnp.ndarray) -> jnp.ndarray:
+    """Compute matrix of all pairwise-costs (no norms) for vectors in x / y.
+
+    Args:
+      x: [num_a, d] jnp.ndarray
+      y: [num_b, d] jnp.ndarray
+    Returns:
+      [num_a, num_b] matrix of pairwise cost evaluations.
+    """
+    return jax.vmap(lambda x_: jax.vmap(lambda y_: self.pairwise(x_, y_))(y))(x)
+
+  def tree_flatten(self):
+    return (), None
+
+  @classmethod
+  def tree_unflatten(cls, aux_data, children):
+    del aux_data
+    return cls(*children)
+
+
+@jax.tree_util.register_pytree_node_class
+class Euclidean(CostFn):
+  """Squared Euclidean distance CostFn."""
+
+  def norm(self, x: jnp.ndarray) -> Union[float, jnp.ndarray]:
+    return jnp.sum(x ** 2, axis=-1)
+
+  def pairwise(self, x: jnp.ndarray, y: jnp.ndarray) -> float:
+    return -2 * jnp.vdot(x, y)
+
+  def barycenter(self, weights: jnp.ndarray, xs: jnp.ndarray) -> float:
+    return jnp.average(xs, weights=weights, axis=0)
+
+
+@jax.tree_util.register_pytree_node_class
+class Cosine(CostFn):
+  """Cosine distance CostFn."""
+
+  def __init__(self, ridge: float = 1e-8):
+    super().__init__()
+    self._ridge = ridge
+
+  def pairwise(self, x: jnp.ndarray, y: jnp.ndarray) -> float:
+    ridge = self._ridge
+    x_norm = jnp.linalg.norm(x, axis=-1)
+    y_norm = jnp.linalg.norm(y, axis=-1)
+    cosine_similarity = jnp.vdot(x, y) / (x_norm * y_norm + ridge)
+    cosine_distance = 1.0 - cosine_similarity
+    return cosine_distance
+
+
+@jax.tree_util.register_pytree_node_class
+class Bures(CostFn):
+  """Bures distance between a pair of (mean, cov matrix) raveled as vectors."""
+
+  def __init__(self, dimension: int, **kwargs: Any):
+    super().__init__()
+    self._dimension = dimension
+    self._sqrtm_kw = kwargs
+
+  def norm(self, x):
+    mean, cov = self.x_to_means_and_covs(x)
+    norm = jnp.sum(mean ** 2, axis=-1)
+    norm += jnp.trace(cov, axis1=-2, axis2=-1)
+    return norm
+
+  def pairwise(self, x, y):
+    mean_x, cov_x = self.x_to_means_and_covs(x)
+    mean_y, cov_y = self.x_to_means_and_covs(y)
+    mean_dot_prod = jnp.vdot(mean_x, mean_y)
+    sq_x = matrix_square_root.sqrtm(cov_x, self._dimension, **self._sqrtm_kw)[0]
+    sq_x_y_sq_x = jnp.matmul(sq_x, jnp.matmul(cov_y, sq_x))
+    sq__sq_x_y_sq_x = matrix_square_root.sqrtm(
+        sq_x_y_sq_x, self._dimension, **self._sqrtm_kw
+    )[0]
+    return -2 * (mean_dot_prod + jnp.trace(sq__sq_x_y_sq_x, axis1=-2, axis2=-1))
+
+  def x_to_means_and_covs(self, x):
+    x = jnp.atleast_2d(x)
+    means = x[:, 0:self._dimension]
+    covariances = jnp.reshape(
+        x[:, self._dimension:self._dimension + self._dimension ** 2],
+        (-1, self._dimension, self._dimension)
+    )
+    return jnp.squeeze(means), jnp.squeeze(covariances)
+
+  @functools.partial(jax.vmap, in_axes=[None, 0, 0])
+  def means_and_covs_to_x(self, mean, covariance):
+    x = jnp.concatenate(
+        (mean, jnp.reshape(covariance, (self._dimension * self._dimension)))
+    )
+    return x
+
+  @functools.partial(jax.vmap, in_axes=[None, None, 0, 0])
+  def scale_covariances(self, cov_sqrt, cov_i, lambda_i):
+    return lambda_i * matrix_square_root.sqrtm_only(
+        jnp.matmul(jnp.matmul(cov_sqrt, cov_i), cov_sqrt)
+    )
+
+  def relative_diff(self, x: jnp.ndarray, y: jnp.ndarray) -> jnp.ndarray:
+    return jnp.sum(jnp.square(x - y)) / jnp.prod(jnp.array(x.shape))
+
+  def covariance_fixpoint_iter(
+      self,
+      covs: jnp.ndarray,
+      lambdas: jnp.ndarray,
+      rtol: float = 1e-2
+  ) -> jnp.ndarray:
+
+    def cond_fn(iteration, constants, state):
+      _, diff = state
+      return diff > jnp.array(rtol)
+
+    def body_fn(iteration, constants, state, compute_error):
+      del compute_error
+      cov, _ = state
+      cov_sqrt, cov_inv_sqrt, _ = matrix_square_root.sqrtm(cov)
+      scaled_cov = jnp.linalg.matrix_power(
+          jnp.sum(self.scale_covariances(cov_sqrt, covs, lambdas), axis=0), 2
+      )
+      next_cov = jnp.matmul(jnp.matmul(cov_inv_sqrt, scaled_cov), cov_inv_sqrt)
+      diff = self.relative_diff(next_cov, cov)
+      return next_cov, diff
+
+    def init_state():
+      cov_init = jnp.eye(self._dimension)
+      diff = jnp.inf
+      return (cov_init, diff)
+
+    state = fixed_point_loop.fixpoint_iter(
+        cond_fn=cond_fn,
+        body_fn=body_fn,
+        min_iterations=10,
+        max_iterations=500,
+        inner_iterations=1,
+        constants=(),
+        state=init_state()
+    )
+
+    cov, _ = state
+    return cov
+
+  def barycenter(self, weights, xs):
+    """Compute the barycenter.
+
+    Implements the fixed point approach proposed in https://arxiv.org/abs/
+    1511.05355 for the computation of the mean and the covariance of the
+    barycenter.
+
+    Args:
+      weights: The barycentric weights.
+      xs: The points to be used in the computation of the barycenter, where
+        each point is described by a concatenation of the mean and the covariance (raveled).
+
+    Returns:
+      barycenter: A concatenation of the mean and the covariance (raveled) of
+        the barycenter.
+    """
+    # Ensure that barycentric weights sum to 1.
+    weights = weights / jnp.sum(weights)
+    mus, covs = self.x_to_means_and_covs(xs)
+    mu_bary = jnp.sum(weights[:, None] * mus, axis=0)
+    cov_bary = self.covariance_fixpoint_iter(covs=covs, lambdas=weights)
+    barycenter = jnp.concatenate(
+        (mu_bary, jnp.reshape(cov_bary, (self._dimension * self._dimension)))
+    )
+    return barycenter
+
+  def tree_flatten(self):
+    return (), (self._dimension, self._sqrtm_kw)
+
+  @classmethod
+  def tree_unflatten(cls, aux_data, children):
+    del children
+    return cls(aux_data[0], **aux_data[1])
+
+
+@jax.tree_util.register_pytree_node_class
+class UnbalancedBures(CostFn):
+  """Regularized, unbalanced Bures distance between triplets.
+
+  This cost implements the value defined in https://arxiv.org/pdf/2006.02572.pdf
+  Equation 37, 39, 40. We follow their notations. It is assumed inputs are given
+  as triplets (mass, mean, covariance) raveled as vectors, in that order.
+  """
+
+  def __init__(
+      self,
+      dimension: int,
+      gamma: float = 1.0,
+      sigma: float = 1.0,
+      **kwargs: Any,
+  ):
+    super().__init__()
+    self._dimension = dimension
+    self._gamma = gamma
+    self._sigma2 = sigma ** 2
+    self._sqrtm_kw = kwargs
+
+  def norm(self, x: jnp.ndarray) -> Union[float, jnp.ndarray]:
+    return self._gamma * x[0]
+
+  def pairwise(self, x: jnp.ndarray, y: jnp.ndarray) -> float:
+    # Sets a few constants
+    gam = self._gamma
+    sig2 = self._sigma2
+    lam = sig2 + gam / 2
+    tau = gam / (2 * lam)
+
+    # Extracts mass, mean vector, covariance matrices
+    mass_x, mass_y = x[0], y[0]
+    diff_means = x[1:1 + self._dimension] - y[1:1 + self._dimension]
+    x_mat = jnp.reshape(
+        x[1 + self._dimension:], (self._dimension, self._dimension)
+    )
+    y_mat = jnp.reshape(
+        y[1 + self._dimension:], (self._dimension, self._dimension)
+    )
+
+    # Identity matrix of suitable size
+    iden = jnp.eye(self._dimension, dtype=x.dtype)
+
+    # Creates matrices needed in the computation
+    tilde_a = 0.5 * gam * (iden - lam * jnp.linalg.inv(x_mat + lam * iden))
+    tilde_b = 0.5 * gam * (iden - lam * jnp.linalg.inv(y_mat + lam * iden))
+
+    tilde_a_b = jnp.matmul(tilde_a, tilde_b)
+    c_mat = matrix_square_root.sqrtm(
+        1 / tau * tilde_a_b + 0.25 * (sig2 ** 2) * iden, **self._sqrtm_kw
+    )[0]
+    c_mat -= 0.5 * sig2 * iden
+
+    # Computes log determinants (their sign should be >0).
+    sldet_c, ldet_c = jnp.linalg.slogdet(c_mat)
+    sldet_t_ab, ldet_t_ab = jnp.linalg.slogdet(tilde_a_b)
+    sldet_ab, ldet_ab = jnp.linalg.slogdet(jnp.matmul(x_mat, y_mat))
+    sldet_c_ab, ldet_c_ab = jnp.linalg.slogdet(c_mat - 2 * tilde_a_b / gam)
+
+    # Gathers all these results to compute log total mass of transport
+    log_m_pi = (0.5 * self._dimension * sig2 / (gam + sig2)) * jnp.log(sig2)
+
+    log_m_pi += (1 / (tau + 1)) * (
+        jnp.log(mass_x) + jnp.log(mass_y) + ldet_c + 0.5 *
+        (tau * ldet_t_ab - ldet_ab)
+    )
+
+    log_m_pi += -jnp.sum(
+        diff_means * jnp.linalg.solve(x_mat + y_mat + lam * iden, diff_means)
+    ) / (2 * (tau + 1))
+
+    log_m_pi += -0.5 * ldet_c_ab
+
+    # If all logdet signs are 1, output value, nan otherwise.
+    return jnp.where(
+        sldet_c == 1 and sldet_c_ab == 1 and sldet_ab == 1 and sldet_t_ab == 1,
+        2 * sig2 * mass_x * mass_y - 2 * (sig2 + gam) * jnp.exp(log_m_pi),
+        jnp.nan
+    )
+
+  def tree_flatten(self):
+    return (), (self._dimension, self._gamma, self._sigma2, self._sqrtm_kw)
+
+  @classmethod
+  def tree_unflatten(cls, aux_data, children):
+    del children
+    return cls(aux_data[0], aux_data[1], aux_data[2], **aux_data[3])
