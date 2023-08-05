5545c0bcb310afec6b180c4d7b9ab45b429cecff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:35 +0000

    Add package PlexTraktSync/0.20.2/PlexTraktSync-0.20.2-py3-none-any.whl

---
+++ packages/PlexTraktSync/PlexTraktSync-0.20.2-py3-none-any.whl/plextraktsync/decorators/rate_limit.py
@@ -0,0 +1,55 @@
+from functools import wraps
+from time import sleep
+
+from plexapi.exceptions import BadRequest
+from requests import RequestException
+from trakt.errors import (LockedUserAccountException, RateLimitException,
+                          TraktInternalException)
+
+from plextraktsync.logging import logger
+
+
+# https://trakt.docs.apiary.io/#introduction/rate-limiting
+def rate_limit(retries=5):
+    """
+    :param retries: number of retries
+    :return:
+    """
+
+    def decorator(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            retry = 0
+            while True:
+                try:
+                    return fn(*args, **kwargs)
+                except (
+                    BadRequest,
+                    RateLimitException,
+                    RequestException,
+                    TraktInternalException,
+                    LockedUserAccountException,
+                ) as e:
+                    if retry == retries:
+                        logger.error(f"Error: {e}")
+                        logger.error(
+                            "API didn't respond properly, script will abort now. Please try again later."
+                        )
+                        logger.error(
+                            f"Last call: {fn.__module__}.{fn.__name__}({args[1:]}, {kwargs})"
+                        )
+                        exit(1)
+
+                    if isinstance(e, RateLimitException):
+                        seconds = int(e.response.headers.get("Retry-After", 1))
+                    else:
+                        seconds = 1 + retry
+                    retry += 1
+                    logger.warning(
+                        f"{e} for {fn.__module__}.{fn.__name__}(), retrying after {seconds} seconds (try: {retry}/{retries})"
+                    )
+                    sleep(seconds)
+
+        return wrapper
+
+    return decorator
