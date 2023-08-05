c49c84fa1cc0753af782a7e2634a7bfeafd79870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:21 +0000

    Add package wordjc/0.1.15/wordjc-0.1.15.tar.gz

---
+++ packages/wordjc/wordjc-0.1.15.tar.gz/wordjc-0.1.15/setup.py
@@ -0,0 +1,33 @@
+﻿from setuptools import setup, find_packages
+
+VERSION = '0.1.15' 
+DESCRIPTION = 'Scripture (Word) Visualization'
+LONG_DESCRIPTION = 'Package to provide API for the Holy Scripture visualization'
+
+# Setting up
+setup(
+       # the name must match the folder name 'wordjc'
+        name="wordjc", 
+        version=VERSION,
+        author="Johnny Cheng",
+        author_email="<drjohnnycheng@gmail.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=['numpy', 'pandas', 'importlib_resources', 're', 'nltk', \
+			  'matplotlib', 'wordcloud', 'PIL'],
+
+        include_package_data=True,
+        package_dir={"wordjc":  "wordjc"},
+        package_data={"wordjc": ["data/*.*", "images/*.*"]},
+
+        keywords=['word', 'scripture', 'visualization'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
