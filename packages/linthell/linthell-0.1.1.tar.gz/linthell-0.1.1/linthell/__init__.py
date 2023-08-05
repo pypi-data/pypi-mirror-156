2034689e1eece0d35755bb15b2e6077c55c4b6ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:15 +0000

    Add package linthell/0.1.1/linthell-0.1.1.tar.gz

---
+++ packages/linthell/linthell-0.1.1.tar.gz/linthell-0.1.1/linthell/__init__.py
@@ -0,0 +1,111 @@
+import re
+import hashlib
+import sys
+from pathlib import Path
+
+import click
+
+FLAKE8_REGEX = r'(?P<path>[a-zA-Z0-9\._-]+(?:[\\/][a-zA-Z0-9\._-]+)*):(?P<line>\d+):\d+: (?P<message>[^\n]+)'
+PYDOCSTYLE_REGEX = r'(?P<path>[a-zA-Z0-9\._-]+(?:[\\/][a-zA-Z0-9\._-]+)*):(?P<line>\d+).+\n\s+(?P<message>[^\n]+)'
+PYLINT_REGEX = r'(?P<path>[a-zA-Z0-9\._-]+(?:[\\/][a-zA-Z0-9\._-]+)*):(?P<line>\d+):\d+: (?P<message>[^\n]+)'
+
+
+def get_id_line(path: str, line: str, message: str) -> str:
+    lines = Path(path).read_text().splitlines()
+    if not lines:
+        code = ''
+    else:
+        code = lines[int(line) - 1]
+    normalized_path = Path(path).as_posix()
+    return f'{normalized_path}:{code}:{message}'
+
+
+def get_id_lines(lint_output: str, regex: str) -> list[str]:
+    return [
+        get_id_line(
+            match.groupdict()['path'],
+            match.groupdict()['line'],
+            match.groupdict()['message'],
+        )
+        for match
+        in re.finditer(regex, lint_output)
+    ]
+
+
+def id_line_to_digest(id_line: str) -> str:
+    return hashlib.md5(id_line.encode('utf-8')).hexdigest()
+
+
+@click.group()
+def cli() -> None:
+    """Universal flakehell replacement for almost any linter you like.
+
+    Workflow looks like this: at first, create baseline for each linter you use.
+    Then replace calls your linter with piping their results to `linthell lint` command.
+    """
+    pass
+
+
+@cli.command()
+@click.option(
+    '--baseline',
+    '-b',
+    'baseline_file',
+    type=click.Path(),
+    help='Path to baseline file with ignores.',
+)
+@click.option(
+    '--format',
+    '-f',
+    'lint_format',
+    default=FLAKE8_REGEX,
+    help='Regex to parse your linter output.',
+)
+def baseline(baseline_file: str, lint_format: str) -> None:
+    """Create baseline file from your linter output.
+
+    Linter output is provided via stdin.
+    """
+    id_lines = get_id_lines(sys.stdin.read(), lint_format)
+    Path(baseline_file).write_text('\n'.join(sorted(id_lines)))
+
+
+@cli.command()
+@click.option(
+    '--baseline',
+    '-b',
+    'baseline_file',
+    type=click.Path(),
+    help='Path to baseline file with ignores.',
+)
+@click.option(
+    '--format',
+    '-f',
+    'lint_format',
+    default=FLAKE8_REGEX,
+    help='Regex to parse your linter output.',
+)
+def lint(baseline_file: str, lint_format: str) -> None:
+    """Filter your linter output against baseline file.
+
+    Linter output is provided via stdin.
+    """
+    has_errors = False
+    id_lines = Path(baseline_file).read_text().splitlines()
+    digests = {id_line_to_digest(id_line) for id_line in id_lines}
+    for match in re.finditer(lint_format, sys.stdin.read()):
+        path = match.groupdict()['path']
+        line = match.groupdict()['line']
+        message = match.groupdict()['message']
+        lint_message = match.group(0)
+        id_line = get_id_line(path, line, message)
+        digest = id_line_to_digest(id_line)
+        if digest not in digests:
+            print(lint_message)
+            has_errors = True
+    if has_errors:
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    cli()
