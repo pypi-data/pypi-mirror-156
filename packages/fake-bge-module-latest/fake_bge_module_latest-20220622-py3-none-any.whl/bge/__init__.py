83e40407462dadb59c79878406856a79346290c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:17:13 +0000

    Add package fake-bge-module-latest/20220622/fake_bge_module_latest-20220622-py3-none-any.whl

---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20220622-py3-none-any.whl/bge/__init__.py
@@ -0,0 +1,9 @@
+import sys
+import typing
+from . import types
+from . import logic
+from . import constraints
+from . import app
+from . import events
+from . import texture
+from . import render
---
+++ packages/fake-bge-module-latest/fake_bge_module_latest-20220622-py3-none-any.whl/bl_operators/__init__.py
@@ -0,0 +1,46 @@
+import sys
+import typing
+from . import mesh
+from . import object
+from . import anim
+from . import vertexpaint_dirt
+from . import add_mesh_torus
+from . import constraint
+from . import spreadsheet
+from . import view3d
+from . import object_align
+from . import node
+from . import object_quick_effects
+from . import assets
+from . import uvcalc_follow_active
+from . import bmesh
+from . import image
+from . import userpref
+from . import rigidbody
+from . import object_randomize_transform
+from . import clip
+from . import sequencer
+from . import wm
+from . import geometry_nodes
+from . import file
+from . import screen_play_rendered_anim
+from . import console
+from . import presets
+from . import uvcalc_lightmap
+from . import freestyle
+
+
+def register():
+    ''' 
+
+    '''
+
+    pass
+
+
+def unregister():
+    ''' 
+
+    '''
+
+    pass
