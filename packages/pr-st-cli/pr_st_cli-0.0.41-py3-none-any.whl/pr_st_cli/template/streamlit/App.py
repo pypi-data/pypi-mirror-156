147011b5fea55e8123209de6b115dc7d7f2eb06a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:49 +0000

    Add package pr-st-cli/0.0.41/pr_st_cli-0.0.41-py3-none-any.whl

---
+++ packages/pr-st-cli/pr_st_cli-0.0.41-py3-none-any.whl/pr_st_cli/template/streamlit/App.py
@@ -0,0 +1,8 @@
+"""This page is the main page of the streamlit app. Is uses PascalCase for naming convention."""
+
+import streamlit as st
+
+{{AZURE_KEYVAULT_CONTENT}}
+{{pr_st_cli_TEMPLATE_CONTENT}}
+{{MULTIPAGE_CONTENT}}
+{{HYDRALIT_CONTENT}}
