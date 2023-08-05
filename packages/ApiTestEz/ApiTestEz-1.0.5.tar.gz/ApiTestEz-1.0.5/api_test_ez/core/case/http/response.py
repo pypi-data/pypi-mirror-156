a0fe250a2cbdadbc2c7eb95ca86b7cc8585d883f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:09:05 +0000

    Add package ApiTestEz/1.0.5/ApiTestEz-1.0.5.tar.gz

---
+++ packages/ApiTestEz/ApiTestEz-1.0.5.tar.gz/ApiTestEz-1.0.5/api_test_ez/core/case/http/response.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+# @Time    : 2022/5/12 17:54
+# @Author  : bruce
+# @desc    :
+"""
+import logging
+from json import JSONDecodeError
+
+from requests import Response
+
+from api_test_ez.ez.decorator.jsonbean import json_bean
+from api_test_ez.ez.serialize.errors import ValidationError
+from api_test_ez.ez.serialize.models import ValidatorModel
+from marshmallow import Schema
+
+
+class EzResponse(Response):
+
+    __slots__ = ("owner", "response")
+
+    def __init__(self, response: Response = None):
+        super().__init__()
+        self.response = response
+
+    def __getattribute__(self, item):
+        if item not in ("owner", "response") and not item.startswith('__') and hasattr(self.response, item):
+            return self.response.__getattribute__(item)
+        else:
+            return super(EzResponse, self).__getattribute__(item)
+
+    def set(self, response: Response):
+        self.response = response
+
+    @property
+    @json_bean
+    def bean(self):
+        return self.json()
+
+    def pair(self, model: ValidatorModel, full_repr=False):
+        validate_result = model.validate(self.response.json(), full_repr)
+        if 'ValidationError' in str(validate_result):
+            raise ValidationError(f'[{self.owner} {validate_result}]')
+
+    def validate(self, schema: Schema):
+        """Validate from marshmallow."""
+        return schema.load(self.response.json())
+
+    def __str__(self):
+        if self.response:
+            return f"<{self.__class__.__name__}> {self.owner}:\n" \
+                   f"{self.response.text!r}"
+        else:
+            return f"<{self.__class__.__name__}> {self.owner}: response is None, " \
+                   f"maybe you didn't send the request?"
+
+    __repr__ = __str__
