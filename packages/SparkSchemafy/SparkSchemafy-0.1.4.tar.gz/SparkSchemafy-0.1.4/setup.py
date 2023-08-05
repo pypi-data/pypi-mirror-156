affb65b336047180f24118406df8f34ebc43923a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:39 +0000

    Add package SparkSchemafy/0.1.4/SparkSchemafy-0.1.4.tar.gz

---
+++ packages/SparkSchemafy/SparkSchemafy-0.1.4.tar.gz/SparkSchemafy-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="SparkSchemafy",
+    version="0.1.4",
+    description="Formats spark schema output into a schema definition",
+    py_modules=["schemafy"],
+    package_dir={'':'src'},
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires = ["pyspark >= 3.0.0 ",
+                        "pandas >= 1.2.0"],
+    extras_require = {
+        "dev": [
+            "pytest>=3.7",
+        ],
+    },
+    url="https://github.com/MichaelShoemaker/schemafy",
+    author="Michael Shoemaker",
+    author_email="mikeshoemaker2000@yahoo.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"]
+)
