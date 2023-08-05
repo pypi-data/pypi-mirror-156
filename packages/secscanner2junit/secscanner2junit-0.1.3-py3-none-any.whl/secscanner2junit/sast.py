4e312072c712f2874bd68f78b1c90bc3e8e6d787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:18 +0000

    Add package secscanner2junit/0.1.3/secscanner2junit-0.1.3-py3-none-any.whl

---
+++ packages/secscanner2junit/secscanner2junit-0.1.3-py3-none-any.whl/secscanner2junit/sast.py
@@ -0,0 +1,40 @@
+from .parser import Parser
+from junit_xml import TestSuite, TestCase
+
+
+class SastParser(Parser):
+    def __init__(self, report, ts_name):
+        super().__init__(report, ts_name)
+        self.p_type = "SAST"
+
+    def parse_findings(self, finding, time):
+        print(finding)
+        name = finding['message']
+        message = finding['message']
+        location_file = finding['location']['file']
+        
+        try:
+            location_line = finding['location']['start_line']
+        except KeyError:
+            location_line = None
+            
+        try:
+            url = finding['identifiers'][0]['url']
+        except KeyError:
+            url = message
+           
+        f_type = finding['identifiers'][0]['name']
+        tc = TestCase(name=name, classname=self.p_type, file=location_file, elapsed_sec=time, line=location_line)
+        tc.add_failure_info(message=message, output=url, failure_type=f_type)
+        return tc
+
+    def parse(self):
+        timing = 0
+        findings = self.report['vulnerabilities']
+        scanners = [f['scanner']['name'] for f in findings]
+        testsuites = []
+        for scanner in scanners:
+            rel_find = filter(lambda x: x['scanner']['name'] == scanner, findings)
+            testcases = [self.parse_findings(finding, timing) for finding in rel_find]
+            testsuites.append(TestSuite(name=self.ts_name + scanner.replace(' ', '-'), test_cases=testcases))
+        return testsuites
