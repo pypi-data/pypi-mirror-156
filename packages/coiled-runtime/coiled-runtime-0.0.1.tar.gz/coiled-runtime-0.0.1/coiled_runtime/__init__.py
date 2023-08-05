247d29c8e8fbf6a5c9b4544fcf92460dc1b46427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:04 +0000

    Add package coiled-runtime/0.0.1/coiled-runtime-0.0.1.tar.gz

---
+++ packages/coiled-runtime/coiled-runtime-0.0.1.tar.gz/coiled-runtime-0.0.1/coiled_runtime/__init__.py
@@ -0,0 +1,5 @@
+raise RuntimeError(
+    "`coiled-runtime` is not currently supported on PyPI. "
+    "Please install using conda instead:\n\n\t"
+    "conda install -c conda-forge coiled-runtime"
+)
---
+++ packages/coiled-runtime/coiled-runtime-0.0.1.tar.gz/coiled-runtime-0.0.1/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="coiled-runtime",
+    version="0.0.1",
+    description="Simple and fast way to get started with Dask",
+    url="https://github.com/coiled/coiled-runtime",
+    license="BSD",
+    packages=["coiled_runtime"],
+)
