0d21fcb6fb38ebd24a3d5872d33e3294acc397a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:34 +0000

    Add package topdf/1.0/topdf-1.0.tar.gz

---
+++ packages/topdf/topdf-1.0.tar.gz/topdf-1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='topdf',
+	version='1.0',
+	packages=find_packages(),
+	author='503',
+	author_email='shuxing503@163.com',
+	keywords='',
+	description='一个批量将文件转化为PDF的库',
+	license='',
+	url='',
+	include_package_data=True,
+	install_requires=[],
+	)
