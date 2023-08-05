c2a543200dc4ab6000169c78cce4db0bb6eb615c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:45 +0000

    Add package pakkanpdf/0.1.3/pakkanpdf-0.1.3-py3-none-any.whl

---
+++ packages/pakkanpdf/pakkanpdf-0.1.3-py3-none-any.whl/pakkanpdf/__init__.py
@@ -0,0 +1,3 @@
+from .core import PdfExtractor
+
+__version__ = "0.1.3"
---
+++ packages/pakkanpdf/pakkanpdf-0.1.3-py3-none-any.whl/pakkanpdf/exceptions.py
@@ -0,0 +1,10 @@
+class NotFoundPdfException(Exception):
+    pass
+
+
+class NotFoundWorkingDirectoryError(Exception):
+    pass
+
+
+class DuplicatedWorkingImageDirectoryError(Exception):
+    pass
