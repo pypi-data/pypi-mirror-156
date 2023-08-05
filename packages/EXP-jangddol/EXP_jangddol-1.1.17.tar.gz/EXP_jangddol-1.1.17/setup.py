f77bf9ab1f63a31ab8f32cc81e4f4a858a138e9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:20:17 +0000

    Add package EXP-jangddol/1.1.17/EXP_jangddol-1.1.17.tar.gz

---
+++ packages/EXP-jangddol/EXP_jangddol-1.1.17.tar.gz/EXP_jangddol-1.1.17/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+import setuptools
+
+setup(
+      name='EXP_jangddol',
+      version='1.1.17',
+      url='https://github.com/jangddol/EXP_jangddol_pip.git',
+      description='kawaii experiment data analysis tool',
+      author='jangddol',
+      author_email='j991005@korea.ac.kr',
+      packages=setuptools.find_packages(),
+      install_requires=[
+          'uncertainties==3.1.6',
+          'matplotlib==3.5.0',
+          'numpy==1.21.0'
+      ],
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent"
+      ],
+)
