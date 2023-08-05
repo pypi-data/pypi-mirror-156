be46248d28d1ce66d7fbcb8dafc6911b3c18286a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:48 +0000

    Add package minimist/0.0.2/minimist-0.0.2.tar.gz

---
+++ packages/minimist/minimist-0.0.2.tar.gz/minimist-0.0.2/setup.py
@@ -0,0 +1,18 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+import pathlib
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+# Arguments marked as "Required" below must be included for upload to PyPI.
+# Fields marked as "Optional" may be commented out.
+
+setup(
+    name="minimist",
+    version="0.0.2",
+    license='MIT',
+    description = 'python minimist',
+)
---
+++ packages/minimist/minimist-0.0.2.tar.gz/minimist-0.0.2/src/minimist/example.py
@@ -0,0 +1,2 @@
+def minimist():
+    print("minimist")
