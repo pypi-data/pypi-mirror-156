0935eca6805f4467f692d561c2706210046d70a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:12 +0000

    Add package st-card-alert-2/0.0.1/st_card_alert_2-0.0.1.tar.gz

---
+++ packages/st-card-alert-2/st_card_alert_2-0.0.1.tar.gz/st_card_alert_2-0.0.1/st_card_alert_2/frontend/build/precache-manifest.97c96c1c7639b135f5d990a6dcf2ae4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7da3dfe574715742c94db912aeea1226",
+    "url": "./index.html"
+  },
+  {
+    "revision": "83dd466699c81e68af69",
+    "url": "./static/js/2.0dc3e5ca.chunk.js"
+  },
+  {
+    "revision": "babaef3c9a135655dd47fd2ad2fd982f",
+    "url": "./static/js/2.0dc3e5ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3e743f68f64b9a1a6de",
+    "url": "./static/js/main.91ba057a.chunk.js"
+  },
+  {
+    "revision": "7c26bca7e16783d14d15",
+    "url": "./static/js/runtime-main.11ec9aca.js"
+  }
+]);
