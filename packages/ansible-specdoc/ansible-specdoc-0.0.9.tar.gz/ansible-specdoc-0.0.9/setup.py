3cc8bd77c9f7e222849a48d9ecaf21896d874d99
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:08:03 +0000

    Add package ansible-specdoc/0.0.9/ansible-specdoc-0.0.9.tar.gz

---
+++ packages/ansible-specdoc/ansible-specdoc-0.0.9.tar.gz/ansible-specdoc-0.0.9/setup.py
@@ -0,0 +1,25 @@
+import os
+import setuptools
+
+setuptools.setup(
+    name="ansible-specdoc",
+    version="0.0.9",
+    author="Lena Garber",
+    author_email="lbgarber2@gmail.com",
+    description=("A simple tool for generating Ansible collection documentation from module spec."),
+    license="GNU General Public License v3.0",
+    keywords="ansible",
+    url="http://packages.python.org/ansible-specdoc",
+    packages=['ansible_specdoc'],
+    install_requires=[
+        'PyYAML==5.4.1',
+        'Jinja2==3.0.1',
+        'redbaron==0.9.2'
+    ],
+    setup_requires=['setupext_janitor'],
+    python_requires='>=3',
+    entry_points={
+        'console_scripts': ['ansible-specdoc=ansible_specdoc.cli:main'],
+        'distutils.commands': ['clean = setupext_janitor.janitor:CleanCommand']
+    }
+)
