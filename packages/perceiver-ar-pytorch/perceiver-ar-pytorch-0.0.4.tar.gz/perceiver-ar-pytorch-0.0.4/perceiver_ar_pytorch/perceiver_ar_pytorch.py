8eab0aedc7bc3f480b36086a63f8fc5c791ce4d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:55 +0000

    Add package perceiver-ar-pytorch/0.0.4/perceiver-ar-pytorch-0.0.4.tar.gz

---
+++ packages/perceiver-ar-pytorch/perceiver-ar-pytorch-0.0.4.tar.gz/perceiver-ar-pytorch-0.0.4/perceiver_ar_pytorch/perceiver_ar_pytorch.py
@@ -0,0 +1,239 @@
+import torch
+import torch.nn.functional as F
+from torch import nn, einsum
+
+from einops import rearrange
+
+# helper functions
+
+def exists(val):
+    return val is not None
+
+# feedforward
+
+def FeedForward(dim, mult = 4, dropout = 0.):
+    hidden_dim = int(dim * mult)
+    return nn.Sequential(
+        nn.LayerNorm(dim),
+        nn.Linear(dim, hidden_dim, bias = False),
+        nn.GELU(),
+        nn.Dropout(dropout),
+        nn.Linear(hidden_dim, dim, bias = False)
+    )
+
+# rotary positional embedding
+# https://arxiv.org/abs/2104.09864
+
+class RotaryEmbedding(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        inv_freq = 1.0 / (10000 ** (torch.arange(0, dim, 2).float() / dim))
+        self.register_buffer("inv_freq", inv_freq)
+
+    def forward(self, max_seq_len, *, device):
+        seq = torch.arange(max_seq_len, device = device, dtype = self.inv_freq.dtype)
+        freqs = einsum("i , j -> i j", seq, self.inv_freq)
+        return torch.cat((freqs, freqs), dim = -1)
+
+
+def rotate_half(x):
+    x = rearrange(x, "... (j d) -> ... j d", j = 2)
+    x1, x2 = x.unbind(dim = -2)
+    return torch.cat((-x2, x1), dim = -1)
+
+
+def apply_rotary_pos_emb(pos, t):
+    seq_len, rotate_dim = t.shape[-2], pos.shape[-1]
+    pos = pos[..., -seq_len:, :]
+    t, t_pass = t[..., :rotate_dim], t[..., rotate_dim:]
+    t = (t * pos.cos()) + (rotate_half(t) * pos.sin())
+    return torch.cat((t, t_pass), dim = -1)
+
+# attention
+
+class CausalAttention(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        dim_head = 64,
+        heads = 8,
+        dropout = 0.
+    ):
+        super().__init__()
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        inner_dim = heads * dim_head
+
+        self.norm = nn.LayerNorm(dim)
+        self.to_qkv = nn.Linear(dim, inner_dim * 3, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim, bias = False)
+
+    def forward(self, x, rotary_pos_emb = None):
+        x = self.norm(x)
+
+        q, k, v = self.to_qkv(x).chunk(3, dim = -1)
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), (q, k, v))
+
+        q = q * self.scale
+
+        if exists(rotary_pos_emb):
+            q = apply_rotary_pos_emb(rotary_pos_emb, q)
+            k = apply_rotary_pos_emb(rotary_pos_emb, k)
+
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        i, j = sim.shape[-2:]
+        causal_mask = torch.ones((i, j), device = x.device, dtype = torch.bool).triu(j - i + 1)
+        sim = sim.masked_fill(causal_mask, -torch.finfo(sim.dtype).max)
+
+        attn = sim.softmax(dim = -1)
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+        return self.to_out(out)
+
+class CausalPrefixAttention(nn.Module):
+    def __init__(
+        self,
+        *,
+        dim,
+        dim_head = 64,
+        heads = 8,
+        dropout = 0.
+    ):
+        super().__init__()
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        inner_dim = heads * dim_head
+
+        self.norm = nn.LayerNorm(dim)
+        self.to_q = nn.Linear(dim, inner_dim, bias = False)
+        self.to_kv = nn.Linear(dim, inner_dim * 2, bias = False)
+        self.to_out = nn.Linear(inner_dim, dim)
+
+    def forward(self, x, context, context_mask = None, rotary_pos_emb = None):
+        x = self.norm(x)
+        q = self.to_q(x)
+
+        k_input, v_input = self.to_kv(x).chunk(2, dim = -1)
+        k_context, v_context = self.to_kv(context).chunk(2, dim = -1)
+
+        k = torch.cat((k_context, k_input), dim = 1)
+        v = torch.cat((v_context, v_input), dim = 1)
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.heads), (q, k, v))
+
+        q = q * self.scale
+
+        if exists(rotary_pos_emb):
+            q = apply_rotary_pos_emb(rotary_pos_emb, q)
+            k = apply_rotary_pos_emb(rotary_pos_emb, k)
+
+        sim = einsum('b h i d, b h j d -> b h i j', q, k)
+
+        i, j = sim.shape[-2:]
+
+        mask_value = -torch.finfo(sim.dtype).max
+
+        if exists(context_mask):
+            mask_len = context_mask.shape[-1]
+            context_mask = F.pad(context_mask, (0, max(j - mask_len, 0)), value = True)
+            context_mask = rearrange(context_mask, 'b j -> b 1 1 j')
+            sim = sim.masked_fill(~context_mask, mask_value)
+
+        causal_mask = torch.ones((i, j), device = x.device, dtype = torch.bool).triu(j - i + 1)
+        sim = sim.masked_fill(causal_mask, mask_value)
+
+        attn = sim.softmax(dim = -1)
+        out = einsum('b h i j, b h j d -> b h i d', attn, v)
+
+        out = rearrange(out, 'b h n d -> b n (h d)')
+
+        return self.to_out(out)
+
+class PerceiverAR(nn.Module):
+    def __init__(
+        self,
+        *,
+        num_tokens,
+        dim,
+        depth,
+        max_seq_len,
+        cross_attn_seq_len,
+        dim_head = 64,
+        heads = 8,
+        dropout = 0.,
+        ff_mult = 4,
+        perceive_depth = 1
+    ):
+        super().__init__()
+        assert max_seq_len > cross_attn_seq_len, 'max_seq_len must be greater than cross_attn_seq_len, the length of the sequence for which to cross attend to "perceiver" style'
+        self.max_seq_len = max_seq_len
+        self.cross_attn_seq_len = cross_attn_seq_len
+
+        self.token_emb = nn.Embedding(num_tokens, dim)
+        self.pos_emb = nn.Embedding(max_seq_len, dim)
+
+        self.rotary_pos_emb = RotaryEmbedding(dim = max(32, dim_head // 2))
+
+        self.perceive_layers  = nn.ModuleList([])
+
+        for _ in range(perceive_depth):
+            self.perceive_layers.append(nn.ModuleList([
+                CausalPrefixAttention(dim = dim, dim_head = dim_head, heads = heads, dropout = dropout),
+                FeedForward(dim, mult = ff_mult, dropout = dropout)
+            ]))
+
+        self.layers = nn.ModuleList([])
+        for _ in range(depth):
+            self.layers.append(nn.ModuleList([
+                CausalAttention(dim = dim, dim_head = dim_head, heads = heads),
+                FeedForward(dim, mult = ff_mult, dropout = dropout),
+            ]))
+
+        self.to_logits = nn.Linear(dim, num_tokens, bias = False)
+
+    def forward(
+        self,
+        x,
+        prefix_mask = None,
+        labels = None
+    ):
+        seq_len, device = x.shape[1], x.device
+        assert self.cross_attn_seq_len < seq_len <= self.max_seq_len
+
+        x = self.token_emb(x)
+        x = x + self.pos_emb(torch.arange(seq_len, device = device))
+
+        # rotary positional embedding
+
+        rotary_pos_emb = self.rotary_pos_emb(seq_len, device = device)
+
+        # divide into prefix to cross attend to and sequence to self attend to
+
+        prefix, x = x[:, :self.cross_attn_seq_len], x[:, self.cross_attn_seq_len:]
+
+        # initial perceiver attention and feedforward (one cross attention)
+
+        for cross_attn, ff in self.perceive_layers:
+            x = cross_attn(x, prefix, context_mask = prefix_mask, rotary_pos_emb = rotary_pos_emb) + x
+            x = ff(x) + x
+
+        # layers
+
+        for attn, ff in self.layers:
+            x = attn(x, rotary_pos_emb = rotary_pos_emb) + x
+            x = ff(x) + x
+
+        # to logits
+
+        logits = self.to_logits(x)
+
+        # take care of cross entropy loss if labels are provided
+
+        if not exists(labels):
+            return logits
+
+        labels = labels[:, self.cross_attn_seq_len:]
+        return F.cross_entropy(rearrange(logits, 'b n c -> b c n'), labels, ignore_index = 0)
