c71732aa7769be85aa77cadb5a3956d409f185c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:22 +0000

    Add package chlt/0.0.6/chlt-0.0.6.tar.gz

---
+++ packages/chlt/chlt-0.0.6.tar.gz/chlt-0.0.6/CHLT/__init__.py
@@ -0,0 +1,9 @@
+print('import ChLT.Chinese_learn')
+from CHLT.Chinese_learn import *
+print('import ChLT.gif_play')
+from CHLT.gif_play import *
+print('import ChLT.playsound')
+from CHLT.playsound import playsound
+print('import ChLT.QGUI_widget')
+from CHLT.QGUI_widget import *
+import tkinter as tk
