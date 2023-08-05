50d6f743c0000f2d78c2141f9052093f58b5c434
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:06:07 +0000

    Add package data-connectors/0.20.7/data_connectors-0.20.7-py3-none-any.whl

---
+++ packages/data-connectors/data_connectors-0.20.7-py3-none-any.whl/data_connectors/mssql.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+load_dotenv()
+
+class SQLServer:
+    """
+    Pass in a conn string to generate the engine for querying
+    Example: pd.read_sql(query, SQLServer(SERVER="EXAMPLE_SERVER_CONN_STRING").engine)
+    
+    SQLAlchemy EXAMPLE_SERVER_CONN_STRING Format:
+    mssql+pyodbc://user:password@server-ip/database?driver=ODBC+Driver
+    """
+    def __init__(self, SQLSERVER_CONN_STR):
+        self.engine = create_engine(os.getenv(SQLSERVER_CONN_STR))
