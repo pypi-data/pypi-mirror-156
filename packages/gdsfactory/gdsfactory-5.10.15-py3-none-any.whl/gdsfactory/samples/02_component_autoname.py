3202a7b86ec677f098b3bb132a95a4bc9115a062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:34 +0000

    Add package gdsfactory/5.10.15/gdsfactory-5.10.15-py3-none-any.whl

---
+++ packages/gdsfactory/gdsfactory-5.10.15-py3-none-any.whl/gdsfactory/samples/02_component_autoname.py
@@ -0,0 +1,43 @@
+"""When you create components you have to make sure they have unique names.
+
+the cell decorator gives unique names to components that depend on their parameters
+"""
+
+import gdsfactory as gf
+from gdsfactory.component import Component
+
+
+@gf.cell
+def straight(width: float = 10.0, height: float = 1.0) -> Component:
+    """Returns straight with automatic name."""
+    wg = gf.Component("ThisNameWillBeOverwriten")
+    wg.add_polygon([(0, 0), (width, 0), (width, height), (0, height)])
+
+    layer = (1, 0)
+    wg.add_port(
+        name="o1", midpoint=(0, height / 2), width=height, orientation=180, layer=layer
+    )
+    wg.add_port(
+        name="o2",
+        midpoint=(width, height / 2),
+        width=height,
+        orientation=0,
+        layer=layer,
+    )
+    return wg
+
+
+def test_autoname() -> None:
+    c = straight()
+    assert c.name.split("_")[0] == "straight"
+
+
+if __name__ == "__main__":
+    c1 = straight()
+    print(c1)
+
+    c2 = straight(width=0.5)
+    print(c2)
+
+    c3 = straight(0.5)
+    assert c2.name == c3.name
