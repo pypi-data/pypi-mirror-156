ea5b04040bdadb671cf14ff45d966e4cf0899cb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:48 +0000

    Add package hcai-nova-server-nightly/0.1.0.dev202206221351/hcai-nova-server-nightly-0.1.0.dev202206221351.tar.gz

---
+++ packages/hcai-nova-server-nightly/hcai-nova-server-nightly-0.1.0.dev202206221351.tar.gz/hcai-nova-server-nightly-0.1.0.dev202206221351/nova_server/nova_backend.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from nova_server.route.train import train
+from nova_server.route.predict import predict
+from nova_server.route.extract import extract
+from nova_server.route.status import status
+from nova_server.route.ui import ui
+from nova_server.tests.test_route import test
+
+
+def create_app():
+    print("Starting nova-backend server")
+    app = Flask(__name__)
+    app.register_blueprint(train)
+    app.register_blueprint(predict)
+    app.register_blueprint(extract)
+    app.register_blueprint(status)
+    app.register_blueprint(ui)
+    app.register_blueprint(test)
+    return app
+
+
+if __name__ == "__main__":
+    from waitress import serve
+
+    app = create_app()
+    serve(app, host="127.0.0.1", port=8080)
+
+# TODO: Init database access
+# app.config['MONGODB_SETTINGS'] = {
+# 'host': 'mongodb://localhost/movie-bag'
+# }
