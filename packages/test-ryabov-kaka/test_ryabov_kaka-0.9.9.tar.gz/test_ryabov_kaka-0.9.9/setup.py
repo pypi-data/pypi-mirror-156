c729d43edbb5e0c737a1bd7b5bab860621d4b7f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:13 +0000

    Add package test-ryabov-kaka/0.9.9/test_ryabov_kaka-0.9.9.tar.gz

---
+++ packages/test-ryabov-kaka/test_ryabov_kaka-0.9.9.tar.gz/test_ryabov_kaka-0.9.9/setup.py
@@ -0,0 +1,21 @@
+from io import open
+from setuptools import setup
+
+version = '0.9.9'
+
+long_description = 'We will hope that this will help us'
+
+setup(
+    name = 'test_ryabov_kaka',
+    version = version,
+
+    author = 'NuatStanskiy',
+    author_email = 'smiril13@mail.ru',
+
+    description = ('dangerous'),
+    long_description = long_description,
+
+    license = 'Apache License, Version 2.0, see LICENSE file',
+
+    packages = ['test_ryabov_kaka']
+)
---
+++ packages/test-ryabov-kaka/test_ryabov_kaka-0.9.9.tar.gz/test_ryabov_kaka-0.9.9/test_ryabov_kaka/__init__.py
@@ -0,0 +1,7 @@
+"""
+:authors: NuatStanskiy
+:license: Apache License, Version 2.0, see LICENSE file
+
+:copyright: (c) 2022 NuatStanskiy
+"""
+from .comanda_daunov import proverka
