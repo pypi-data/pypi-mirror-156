a311a4a918608697b1a1afa9c017d5bbdc0884e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:55 +0000

    Add package watchman-agent/1.1.9/watchman-agent-1.1.9.tar.gz

---
+++ packages/watchman-agent/watchman-agent-1.1.9.tar.gz/watchman-agent-1.1.9/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name="watchman-agent",
+    version="1.1.9",
+    author = "Watchman" ,
+    author_email = "support@watchman.bj",
+    # description = "Watchman Agent 1.0.0",
+    # packages=["watchman_agent","watchman_agent/commands"],
+
+    package_data={
+        "watchman_agent":["commands/dist/main","commands/dist/main.exe","commands/dist/.env"]
+    },
+
+    # entry_points={  # Optional
+    #     "console_scripts": [
+    #         "watchman-agent=watchman_agent.__main__:main",
+    #     ],
+        
+    #   },
+
+)
