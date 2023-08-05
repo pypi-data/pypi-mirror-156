0fc04289691d18cfb17060f6ad12f5b6c5a10800
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:28 +0000

    Add package gs2/1.0.70/gs2-1.0.70.tar.gz

---
+++ packages/gs2/gs2-1.0.70.tar.gz/gs2-1.0.70/src/__init__.py
@@ -0,0 +1,53 @@
+# Copyright 2016 Game Server Services, Inc. or its affiliates. All Rights
+# Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License").
+# You may not use this file except in compliance with the License.
+# A copy of the License is located at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is distributed
+# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing
+# permissions and limitations under the License.
+
+from account import * as account
+from auth import * as auth
+from chat import * as chat
+from core import * as core
+from datastore import * as datastore
+from deploy import * as deploy
+from dictionary import * as dictionary
+from distributor import * as distributor
+from enhance import * as enhance
+from exchange import * as exchange
+from formation import * as formation
+from experience import * as experience
+from formation import * as formation
+from friend import * as friend
+from gateway import * as gateway
+from identifier import * as identifier
+from inbox import * as inbox
+from inventory import * as inventory
+from job_queue import * as job_queue
+from key import * as key
+from limit import * as limit
+from lock import * as lock
+from log import * as log
+from lottery import * as lottery
+from matchmaking import * as matchmaking
+from mission import * as mission
+from money import * as money
+from news import * as news
+from project import * as project
+from quest import * as quest
+from ranking import * as ranking
+from realtime import * as realtime
+from schedule import * as schedule
+from script import * as script
+from showcase import * as showcase
+from stamina import * as stamina
+from version import * as version
+from watch import * as watch
+
