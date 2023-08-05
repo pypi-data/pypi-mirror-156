03df0d062b893d481bda700b73b52651d2f89502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:31 +0000

    Add package AGParser/0.3.9/AGParser-0.3.9.tar.gz

---
+++ packages/AGParser/AGParser-0.3.9.tar.gz/AGParser-0.3.9/setup.py
@@ -0,0 +1,25 @@
+import setuptools 
+
+setuptools.setup(
+    name="AGParser",
+    version="0.3.9",
+    description="Parsing library",
+    long_description_content_type="text/markdown",
+    author="Alexandr Grikurov",
+    author_email="grikurovaa@email.com",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages=setuptools.find_packages(include=["AG_Parser"]),
+    include_package_data=True,
+    install_requires=["pandas", "beautifulsoup4 ", "requests", "tqdm"]
+)
