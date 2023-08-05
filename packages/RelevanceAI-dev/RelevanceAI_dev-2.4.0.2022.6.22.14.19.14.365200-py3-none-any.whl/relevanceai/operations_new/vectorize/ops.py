2b3564509ac6d662739de9c112d5298ced02e905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:15 +0000

    Add package RelevanceAI-dev/2.4.0.2022.6.22.14.19.14.365200/RelevanceAI_dev-2.4.0.2022.6.22.14.19.14.365200-py3-none-any.whl

---
+++ packages/RelevanceAI-dev/RelevanceAI_dev-2.4.0.2022.6.22.14.19.14.365200-py3-none-any.whl/relevanceai/operations_new/vectorize/ops.py
@@ -0,0 +1,7 @@
+from relevanceai.operations_new.apibase import OperationAPIBase
+from relevanceai.operations_new.vectorize.base import VectorizeBase
+
+
+class VectorizeOps(VectorizeBase, OperationAPIBase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
