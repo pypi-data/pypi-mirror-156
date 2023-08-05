33adeb4f6ccf9fe2c4dd40e60e5fcf28bd07d56e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:10 +0000

    Add package ALLCools/0.2.12/allcools-0.2.12.tar.gz

---
+++ packages/ALLCools/allcools-0.2.12.tar.gz/allcools-0.2.12/ALLCools/integration/__init__.py
@@ -0,0 +1,2 @@
+from .confusion import calculate_direct_confusion
+from .seurat import find_anchor, integrate
---
+++ packages/ALLCools/allcools-0.2.12.tar.gz/allcools-0.2.12/README.md
@@ -0,0 +1,3 @@
+# ALLCools
+
+See documentation https://lhqing.github.io/ALLCools/intro.html
