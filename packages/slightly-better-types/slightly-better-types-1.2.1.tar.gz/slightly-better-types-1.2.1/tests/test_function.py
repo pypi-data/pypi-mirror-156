4ce9e603dcb28b7617034ebdd0924a0cc05fdde2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:11 +0000

    Add package slightly-better-types/1.2.1/slightly-better-types-1.2.1.tar.gz

---
+++ packages/slightly-better-types/slightly-better-types-1.2.1.tar.gz/slightly-better-types-1.2.1/tests/test_function.py
@@ -0,0 +1,116 @@
+import unittest
+from abc import ABC
+from typing import Dict  # noqa
+from typing import Optional  # noqa
+from typing import TYPE_CHECKING
+from typing import Union  # noqa
+
+from slightly_better_types.function import Function
+
+if TYPE_CHECKING:
+    from typing import Generator
+
+
+class AbstractBar(ABC):
+    pass
+
+
+class Bar(AbstractBar):
+    pass
+
+
+class Foo:
+    def __init__(self):
+        pass
+
+
+class TestSlightlyBetterFunction(unittest.TestCase):
+    integer: int = 1
+    string: str = 'string'
+    bar: Bar
+    foo: Foo
+
+    def true_values(self) -> "Generator":
+        self.bar = Bar()
+
+        # Arg test
+        yield ['', [], {}]
+
+        yield ['a: int', [self.integer], {}]
+        yield ['a: str', [self.string], {}]
+        yield ['*args: str', [self.string, self.string], {}]
+
+        yield ['a: int, b: Union[int, str]', [self.integer, self.integer], {}]
+        yield ['a: int, b: Union[int, str]', [self.integer, self.string], {}]
+
+        yield ['a: Optional[int], b: Optional[int]', [None, None], {}]
+        yield ['a: Optional[int], b: Optional[str]', [None, self.string], {}]
+
+        yield ['a: dict', [{self.string: self.integer}], {}]
+
+        # Kwarg Test
+        yield [
+            'a: int, b: Union[int, str], c: int = 2, *args, keyword_only: str, **kwargs',
+            [],
+            {'a': self.integer, 'b': self.string, 'keyword_only': 'word'}
+        ]
+        yield [
+            'a: int, b: Union[int, str], c: int = 2, *args, keyword_only: str, **kwargs: str',
+            [],
+            {'a': self.integer, 'b': self.string, 'keyword_only': self.string, 'another_kwarg': self.string}
+        ]
+
+        # Arg and Kwarg Test
+        yield [
+            'a: int, b: Union[int, str], c: int = 2, *args, keyword_only: str, **kwargs',
+            [self.integer],
+            {'b': self.string, 'keyword_only': 'word'}
+        ]
+
+        yield [
+            'a: int, b: Union[int, str], c: int = 2, *args, keyword_only: str, **kwargs: str',
+            [self.integer, self.string],
+            {'keyword_only': self.string, 'another_kwarg': self.string}
+        ]
+
+        # Inheritance
+        yield ['bar: Bar, bar_2: AbstractBar', [self.bar, self.bar], {}]
+
+    def false_values(self) -> "Generator":
+        self.foo = Foo()
+
+        yield ['a: str', [self.integer], {}]
+
+        # Too many values
+        yield ['', [self.string], {}]
+        yield ['a: str', [self.string, self.string], {}]
+
+        # Too few values
+        yield ['a: str, b: str', [self.string], {}]
+
+        # Wrong Args type
+        yield ['*args: str', [self.integer, self.integer], {}]
+
+        # Wrong kwargs type
+        yield ['**args: str', [], {'keyword_arg': self.integer}]
+
+        # Inheritance
+        yield ['bar: Bar', [self.foo], {}]
+
+    def test_true_values(self):
+        for definition, args, kwargs in self.true_values():
+            f = self._make_method(definition)
+            assert f.accepts(*args, **kwargs)
+
+    def test_false_values(self):
+        for definition, args, kwargs in self.false_values():
+            f = self._make_method(definition)
+            assert not f.accepts(*args, *kwargs)
+
+    def _make_method(self, definition: str) -> Function:
+        exec(f"class TestClass:\n\tdef test(self,{definition}):\n\t\tpass")
+        return Function(eval('TestClass.test'))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ packages/slightly-better-types/slightly-better-types-1.2.1.tar.gz/slightly-better-types-1.2.1/tests/test_handlers.py
@@ -0,0 +1,72 @@
+import unittest
+from typing import Dict
+
+from slightly_better_types.handlers import Handlers
+
+
+class Bar:
+
+    def accepts_string(self, a: str):
+        pass
+
+    def accepts_string_too(self, b: str):
+        pass
+
+    def accepts_int(self, c: int):
+        pass
+
+    def _protected(self, a: Dict):
+        pass
+
+    def __private(self, a: Dict):
+        pass
+
+    @staticmethod
+    def static(a: float):
+        pass
+
+
+class TestHandlers(unittest.TestCase):
+
+    def test_find(self):
+        handlers = Handlers(Bar)
+
+        assert ['accepts_string', 'accepts_string_too'] == list(handlers.accepts('string').all().keys())
+        assert ['accepts_string_too'] == list(handlers.accepts(b='string').all().keys())
+        print(list(handlers.accepts(1).all().keys()))
+        assert ['accepts_int', 'static'] == list(handlers.accepts(1).all().keys())
+
+        class TestClass:
+            pass
+
+        assert [] == list(handlers.accepts(TestClass()).all().keys())
+
+    def test_first(self):
+        handlers = Handlers(Bar)
+        assert 'accepts_string' == handlers.accepts('string').first().get_name()
+        assert 'accepts_string_too' == handlers.accepts(b='string').first().get_name()
+
+        class TestClass:
+            pass
+
+        assert None is handlers.accepts(TestClass()).first()
+
+    def test_visibility(self):
+        assert None is Handlers.new(Bar).public().accepts([]).first()
+        assert None is Handlers.new(Bar).protected().accepts([]).first()
+        assert Handlers.new(Bar).private().accepts({}).first() is not None
+        assert Handlers.new(Bar).public().protected().accepts({}).first() is not None
+
+    def test_all(self):
+        assert 1 == len(Handlers.new(Bar).private().all())
+        assert 4 == len(Handlers.new(Bar).public().all())
+        assert 6 == len(Handlers.new(Bar).all())
+        assert 1 == len(Handlers.new(Bar).filter(lambda x: x.is_static()).all())
+
+    def test_filter(self):
+        assert Handlers.new(Bar).filter(lambda x: not x.is_static()).accepts(2.1).first() is None
+        assert Handlers.new(Bar).filter(lambda x: x.is_static()).accepts(1.2).first() is not None
+
+
+if __name__ == '__main__':
+    unittest.main()
