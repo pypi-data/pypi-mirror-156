22a485ecfcaeac0812795fb553c2870fbf7ac394
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:13:33 +0000

    Add package nester_ht7714/2.3.2/nester_ht7714-2.3.2.tar.gz

---
+++ packages/nester_ht7714/nester_ht7714-2.3.2.tar.gz/nester_ht7714-2.3.2/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='nester_ht7714',
+    version='2.3.2',
+    py_modules=['nester'],
+    author='hfpython',
+    author_email='hfpython@headfirstlabs.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists,更新了写入文件',
+
+)
