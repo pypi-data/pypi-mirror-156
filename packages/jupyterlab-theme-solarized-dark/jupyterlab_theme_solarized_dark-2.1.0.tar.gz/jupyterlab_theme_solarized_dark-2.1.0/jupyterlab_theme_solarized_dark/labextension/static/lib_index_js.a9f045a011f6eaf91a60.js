8bd0224483452f68eb06a4790ab245c02ccf26fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:21:19 +0000

    Add package jupyterlab-theme-solarized-dark/2.1.0/jupyterlab_theme_solarized_dark-2.1.0.tar.gz

---
+++ packages/jupyterlab-theme-solarized-dark/jupyterlab_theme_solarized_dark-2.1.0.tar.gz/jupyterlab_theme_solarized_dark-2.1.0/jupyterlab_theme_solarized_dark/labextension/static/lib_index_js.a9f045a011f6eaf91a60.js
@@ -0,0 +1,41 @@
+"use strict";
+(self["webpackChunkjupyterlab_theme_solarized_dark"] = self["webpackChunkjupyterlab_theme_solarized_dark"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @jupyterlab/apputils */ "webpack/sharing/consume/default/@jupyterlab/apputils");
+/* harmony import */ var _jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__);
+
+/**
+ * A plugin for jupyterlab-theme-solarized-dark
+ */
+const plugin = {
+    id: 'jupyterlab-theme-solarized-dark:plugin',
+    requires: [_jupyterlab_apputils__WEBPACK_IMPORTED_MODULE_0__.IThemeManager],
+    activate: function (app, manager) {
+        const style = 'jupyterlab-theme-solarized-dark/index.css';
+        manager.register({
+            name: 'JupyterLab Solarized Dark',
+            isLight: false,
+            themeScrollbars: true,
+            load: () => manager.loadCSS(style),
+            unload: () => Promise.resolve(undefined)
+        });
+    },
+    autoStart: true
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (plugin);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.a9f045a011f6eaf91a60.js.map
