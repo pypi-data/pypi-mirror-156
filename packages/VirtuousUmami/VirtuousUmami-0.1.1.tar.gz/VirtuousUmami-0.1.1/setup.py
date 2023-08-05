6654cd7029dade1a869d5c33b33ec73a7e614267
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:20:08 +0000

    Add package VirtuousUmami/0.1.1/VirtuousUmami-0.1.1.tar.gz

---
+++ packages/VirtuousUmami/VirtuousUmami-0.1.1.tar.gz/VirtuousUmami-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='VirtuousUmami',
+      version='0.1.1',
+      description='The Virtuous Umami tool predict the umami/non-umami taste of query molecules based on their molecular structures.',
+      author='Lorenzo Pallante, Aigli Korfiati, Lampros Androutsos',
+      author_email='lorenzo.pallante@polito.it, a.korfiati@insybio.com, l.androutsos@insybio.com',
+      packages=find_packages(),
+     )
