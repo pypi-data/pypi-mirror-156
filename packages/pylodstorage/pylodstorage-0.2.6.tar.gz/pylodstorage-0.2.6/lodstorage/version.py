715f3a04365973a91ddf1f0c753af2c3b6f9cdd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:08:43 +0000

    Add package pylodstorage/0.2.6/pylodstorage-0.2.6.tar.gz

---
+++ packages/pylodstorage/pylodstorage-0.2.6.tar.gz/pylodstorage-0.2.6/lodstorage/version.py
@@ -0,0 +1,14 @@
+'''
+Created on 2022-03-06
+
+@author: wf
+'''
+
+class Version(object):
+    '''
+    Version handling for pyLoDStorage
+    '''
+    name="pylodstorage"
+    version='0.2.6'
+    date = '2020-09-10'
+    updated = '2022-06-22'
