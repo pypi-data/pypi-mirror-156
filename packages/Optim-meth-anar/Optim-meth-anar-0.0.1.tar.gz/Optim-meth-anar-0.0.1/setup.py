4346a01d222e64ac84e0587c78d6dc8a8d6745ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:14 +0000

    Add package Optim-meth-anar/0.0.1/Optim-meth-anar-0.0.1.tar.gz

---
+++ packages/Optim-meth-anar/Optim-meth-anar-0.0.1.tar.gz/Optim-meth-anar-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(version='0.0.1',
+      name='Optim-meth-anar',
+      description='abc',
+      packages=['Optim-meth-anar'],
+      author_email='anariem@mail.ru',
+      zip_safe=False)
