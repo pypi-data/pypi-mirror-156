e79714ccab8cb9e21aa3180f65c1892730ebf1dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:12 +0000

    Add package expressmoney-service/0.5.1/expressmoney-service-0.5.1.tar.gz

---
+++ packages/expressmoney-service/expressmoney-service-0.5.1.tar.gz/expressmoney-service-0.5.1/setup.py
@@ -0,0 +1,16 @@
+"""
+py setup.py sdist
+twine upload dist/expressmoney-service-0.5.1.tar.gz
+"""
+import setuptools
+
+setuptools.setup(
+    name='expressmoney-service',
+    packages=setuptools.find_packages(),
+    version='0.5.1',
+    description='Remote services',
+    author='Development team',
+    author_email='dev@expressmoney.com',
+    install_requires=('expressmoney',),
+    python_requires='>=3.7',
+)
