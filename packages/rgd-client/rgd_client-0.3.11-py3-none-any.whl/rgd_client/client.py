67f06cc1a307695f95fa16439fb729548a05a1f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:22 +0000

    Add package rgd-client/0.3.11/rgd_client-0.3.11-py3-none-any.whl

---
+++ packages/rgd-client/rgd_client-0.3.11-py3-none-any.whl/rgd_client/client.py
@@ -0,0 +1,136 @@
+import getpass
+import logging
+import os
+from typing import List, Optional, Type
+
+import requests
+
+from .plugin import CorePlugin
+from .session import RgdClientSession, clone_session
+from .utils import API_KEY_DIR_PATH, API_KEY_FILE_NAME, DEFAULT_RGD_API
+
+logger = logging.getLogger(__name__)
+
+
+class RgdClient:
+    def __init__(
+        self,
+        api_url: str = DEFAULT_RGD_API,
+        username: Optional[str] = None,
+        password: Optional[str] = None,
+        save: Optional[bool] = True,
+    ) -> None:
+        """
+        Initialize the base RGD Client.
+
+        Args:
+            api_url: The base url of the RGD API instance.
+            username: The username to authenticate to the instance with, if any.
+            password: The password associated with the provided username. If None, a prompt will be provided.
+            save: Whether or not to save the logged-in user's API key to disk for future use.
+
+        Returns:
+            A base RgdClient instance.
+        """
+        # Look for an API key in the environment. If it's not there, check username/password
+        api_key = _read_api_key(api_url=api_url, username=username, password=password)
+        if api_key is None:
+            if username is not None and password is None:
+                password = getpass.getpass()
+
+            # Get an API key for this user and save it to disk
+            if username and password:
+                api_key = _get_api_key(api_url, username, password, save)
+                if api_key is None:
+                    logger.error(
+                        'Failed to retrieve API key; are your username and password correct?'
+                    )
+
+        self.session = RgdClientSession(base_url=api_url, auth_token=api_key)
+        self.rgd = CorePlugin(clone_session(self.session))
+
+    def clear_token(self):
+        """Delete a locally-stored API key."""
+        (API_KEY_DIR_PATH / API_KEY_FILE_NAME).unlink(missing_ok=True)
+
+
+def _get_api_key(api_url: str, username: str, password: str, save: bool) -> Optional[str]:
+    """Get an RGD API Key for the given user from the server, and save it if requested."""
+    resp = requests.post(f'{api_url}/api-token-auth', {'username': username, 'password': password})
+    token = resp.json().get('token')
+    if token is None:
+        return None
+    if save:
+        API_KEY_DIR_PATH.mkdir(parents=True, exist_ok=True)
+        with open(API_KEY_DIR_PATH / API_KEY_FILE_NAME, 'w') as fd:
+            fd.write(token)
+    return token
+
+
+def _read_api_key(api_url: str, username: str = None, password: str = None) -> Optional[str]:
+    """
+    Retrieve an RGD API Key from the users environment.
+
+    This function checks for an environment variable named RGD_API_TOKEN and returns it if it exists.
+    If it does not exist, it looks for a file located at ~/.rgd/token and returns its contents.
+    """
+    api_key = os.getenv('RGD_API_TOKEN', None)
+    save = False
+    if api_key is None:
+        try:
+            # read the first line of the text file at ~/.rgd/token
+            with open(API_KEY_DIR_PATH / API_KEY_FILE_NAME, 'r') as fd:
+                api_key = fd.readline().strip()
+                save = True  # save any new api key to disk
+        except FileNotFoundError:
+            return None
+
+    # Make sure API key works by hitting a protected endpoint
+    resp = requests.get(f'{api_url}/rgd/collection', headers={'Authorization': f'Token {api_key}'})
+
+    # If it doesn't, try to get a new one and save it to ~/.rgd/token, as the current one is corrupted
+    if resp.status_code == 401:
+        logger.error('API key is invalid.')
+        # If username + password were provided, try to get a new API key with them
+        # Note we only do this if `save` is `True`, i.e. if the user originally attempted to
+        # instantiate the client with an API key located on disk. If they instead provided an env
+        # var, do not assume that they want a key saved and do not attempt to fetch a new one.
+        if save and username is not None and password is not None:
+            logger.info('Attempting to fetch a new API key...')
+            api_key = _get_api_key(api_url, username, password, save=True)
+            if api_key is not None:
+                logger.info('Succeeded.')
+            return api_key
+        else:
+            logger.error('Provide your username and password next time to fetch a new one.')
+            return None
+
+    # If the response failed with an error status other than 401, raise an exception
+    resp.raise_for_status()
+
+    return api_key
+
+
+def create_rgd_client(
+    api_url: str = DEFAULT_RGD_API,
+    username: Optional[str] = None,
+    password: Optional[str] = None,
+    save: Optional[bool] = True,
+    extra_plugins: Optional[List[Type]] = None,
+):
+    # Avoid circular import
+    from ._plugin_utils import _inject_plugin_deps, _plugin_classes, _plugin_instances
+
+    # Strip trailing slash
+    if api_url.endswith('/'):
+        api_url = api_url.rstrip('/')
+
+    # Create initial client
+    client = RgdClient(api_url, username, password, save)
+
+    # Perform plugin initialization
+    plugin_classes = _plugin_classes(extra_plugins=extra_plugins)
+    plugin_instances = _plugin_instances(client, plugin_classes)
+    _inject_plugin_deps(plugin_instances)
+
+    return client
---
+++ packages/rgd-client/rgd_client-0.3.11-py3-none-any.whl/rgd_client/plugin.py
@@ -0,0 +1,207 @@
+from pathlib import Path
+from typing import Dict, List, Optional, Tuple, Union
+
+import validators
+
+from .session import RgdClientSession
+from .types import DATETIME_OR_STR_TUPLE, SEARCH_PREDICATE_CHOICE
+from .utils import download_checksum_file_to_path, spatial_search_params
+
+
+class RgdPlugin:
+    """The base plugin that all other plugins must inherit from."""
+
+    def __init__(self, session: RgdClientSession):
+        self.session = session
+
+
+class CorePlugin(RgdPlugin):
+    """The core django-rgd client plugin."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.session.base_url += 'rgd/'
+
+    def get_collection(self, pk: int):
+        """Get Collection by primary key."""
+        r = self.session.get(f'collection/{pk}')
+        r.raise_for_status()
+        return r.json()
+
+    def get_collection_item(self, pk: Union[int, dict], index: int):
+        """Get Collection by primary key."""
+        if isinstance(pk, dict):
+            pk = pk['id']
+        r = self.session.get(f'collection/{pk}/item/{index}')
+        r.raise_for_status()
+        return r.json()
+
+    def get_collection_by_name(self, name: str):
+        """Get collection by name."""
+        payload = {'name': name}
+        data = self.session.get('collection', params=payload).json()
+        try:
+            if isinstance(data, list) and data:
+                # Test env returns list
+                return data[0]
+            elif isinstance(data, dict) and data['results']:
+                # User env returns dict
+                return data['results'][0]
+        except (IndexError, KeyError):
+            pass
+        raise ValueError(f'Collection ({name}) cannot be found.')
+
+    def search(
+        self,
+        query: Optional[Union[Dict, str]] = None,
+        predicate: Optional[SEARCH_PREDICATE_CHOICE] = None,
+        relates: Optional[str] = None,
+        distance: Optional[Tuple[float, float]] = None,
+        acquired: Optional[DATETIME_OR_STR_TUPLE] = None,
+        instrumentation: Optional[str] = None,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+        time_of_day: Optional[DATETIME_OR_STR_TUPLE] = None,
+        collections: Optional[List[Union[str, int]]] = None,
+    ) -> List[Dict]:
+        """
+        Search for geospatial entries based on various criteria.
+
+        For Ranges (Tuples), an entry of `None` means that side of the range is unbounded.
+        E.g. a range of (2, None) is 2 or more, (None, 5) is at most 5, (2, 5) is between 2 and 5.
+
+        Args:
+            query: Either a WKT GeoJSON representation, a GeoJSON string, or a GeoJSON dict.
+            predicate: A named spatial predicate based on the DE-9IM. This spatial predicate will
+                be used to filter data such that predicate(a, b) where b is the queried geometry.
+            relates: Specify exactly how the queried geometry should relate to the data using a
+                DE-9IM string code.
+            distance: The min/max distance around the queried geometry in meters.
+            acquired: The min/max date and time (ISO 8601) when data was acquired.
+            instrumentation: The instrumentation used to acquire at least one of these data.
+            limit: The maximum number of results to return.
+            offset: The number of results to skip.
+            collection: The collection ID or name
+
+        Returns:
+            A list of Spatial Entries.
+        """
+        if collections is not None:
+            for i, collection in enumerate(collections):
+                if isinstance(collection, str):
+                    collections[i] = self.get_collection_by_name(collection)['id']
+
+        params = spatial_search_params(
+            query=query,
+            predicate=predicate,
+            relates=relates,
+            distance=distance,
+            acquired=acquired,
+            instrumentation=instrumentation,
+            limit=limit,
+            offset=offset,
+            time_of_day=time_of_day,
+            collections=collections,
+        )
+
+        r = self.session.get('search', params=params)
+
+        r.raise_for_status()
+
+        return r.json()
+
+    def create_collection(self, name: str):
+        """Get or create collection by name."""
+        try:
+            return self.get_collection_by_name(name)
+        except ValueError:
+            r = self.session.post('collection', json={'name': name})
+            r.raise_for_status()
+            return r.json()
+
+    def create_file_from_url(
+        self,
+        url: str,
+        name: Optional[str] = None,
+        collection: Optional[int] = None,
+        description: Optional[str] = None,
+    ) -> Dict:
+        """
+        Get or create a ChecksumFile from a URL.
+
+        Args:
+            url: The URL to retrieve the file from
+            name: The name of the file
+            collection: The integer collection ID to associate this ChecksumFile with
+            description: The description of the file
+        """
+        # Verify that url is valid in shape, will raise error on failure
+        validators.url(url)
+
+        if isinstance(collection, str):
+            collection = self.create_collection(collection)['id']
+
+        # Check if url/collection combination already exists, and return it
+        payload = {'url': url}
+        if collection is not None:
+            payload['collection'] = collection
+        data = self.session.get('checksum_file', params=payload).json()
+        # TODO: This is absolutely stumping me...
+        if isinstance(data, list) and data:
+            # Test env returns list
+            return data[0]
+        elif isinstance(data, dict) and data['results']:
+            # User env returns dict
+            return data['results'][0]
+
+        # Create new checksum file
+        # Construct payload, leaving out empty arguments
+        payload['type'] = 2
+        if name is not None:
+            payload['name'] = name
+        if collection is not None:
+            payload['collection'] = collection
+        if description is not None:
+            payload['description'] = description
+
+        r = self.session.post('checksum_file', json=payload)
+
+        r.raise_for_status()
+        return r.json()
+
+    def file_tree_search(self, path: str = ''):
+        """
+        Search files in a hierarchical format, from a provided folder path.
+
+        This endpoint returns all files and folders that are "within" the specified "folder" (the path argument).
+        An example is
+
+        Args:
+            path: The path to apply to the search. This can be thought of as the folder path that you'd like to search.
+
+        Returns:
+            A dictionary, containing all direct subfolders (`folders`), and files (`files`) under the specified path.
+        """
+        return self.session.get('checksum_file/tree', params={'path_prefix': path}).json()
+
+    def download_checksum_file_to_path(
+        self, id: int, path: Optional[Path] = None, keep_existing=False, use_id=False
+    ):
+        """
+        Download a RGD ChecksumFile to a given path.
+
+        Args:
+            id: The id of the RGD ChecksumFile to download.
+            path: The root path to download this file to.
+            keep_existing: If False, replace files existing on disk.
+            use_id: If True, save this file to disk using it's ID, rather than it's name.
+
+        Returns:
+            The path on disk the file was downloaded to.
+        """
+        r = self.session.get(f'checksum_file/{id}')
+        r.raise_for_status()
+
+        return download_checksum_file_to_path(
+            r.json(), path, keep_existing=keep_existing, use_id=use_id
+        )
