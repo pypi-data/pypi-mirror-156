51d1755b989319460973a4ce95e38052f2b57b20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:51 +0000

    Add package pyglider/0.0.3/pyglider-0.0.3.tar.gz

---
+++ packages/pyglider/pyglider-0.0.3.tar.gz/pyglider-0.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+# Get the version from versioneer
+__version__ = "0.0.3"
+
+setup(name="pyglider",
+      version=__version__,
+      description="Glider data to netCDF translation in python",
+      author="Jody Klymak",
+      author_email="jklymak@gmail.com",
+      url="https://pyglider.readthedocs.io",
+      packages=find_packages(exclude=['tests']),
+      python_requires='>=3.6',
+      install_requires=[
+        "xarray",
+        "dask",
+        "netcdf4",
+        "gsw",
+        "scipy",
+        "bitstring"
+        "pooch"
+      ],
+      license='Apache',
+      extras_require={
+        "code_style": ["flake8<3.8.0,>=3.7.0", "black", "pre-commit==1.17.0"],
+        "testing": ["pytest"],
+        "docs": ["pydata-sphinx-theme", "numpydoc", "autoapi", "myst-parser",
+                 "sphinx"]
+      },
+      zip_safe=True
+      )
