896a7cdaba7609a80fa1334c814cbb8015198dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 20:10:23 +0000

    Add package checkov/2.0.1227/checkov-2.0.1227-py3-none-any.whl

---
+++ packages/checkov/checkov-2.0.1227-py3-none-any.whl/checkov/cloudformation/checks/resource/aws/LambdaEnvironmentCredentials.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from typing import Any
+
+from checkov.cloudformation.checks.resource.base_resource_check import BaseResourceCheck
+from checkov.common.models.enums import CheckResult, CheckCategories
+from checkov.common.util.secrets import string_has_secrets, AWS, GENERAL
+
+
+class LambdaEnvironmentCredentials(BaseResourceCheck):
+    def __init__(self) -> None:
+        name = "Ensure no hard-coded secrets exist in lambda environment"
+        id = "CKV_AWS_45"
+        supported_resources = ("AWS::Lambda::Function", "AWS::Serverless::Function")
+        categories = (CheckCategories.SECRETS,)
+        super().__init__(name=name, id=id, categories=categories, supported_resources=supported_resources)
+
+    def scan_resource_conf(self, conf: dict[str, Any]) -> CheckResult:
+        self.evaluated_keys = ["Properties/Environment/Variables"]
+        properties = conf.get("Properties")
+        if isinstance(properties, dict):
+            environment = properties.get("Environment")
+            if environment and isinstance(environment, dict):
+                variables = environment.get("Variables")
+                if variables and isinstance(variables, dict):
+                    for var_name, value in variables.items():
+                        if string_has_secrets(str(value), AWS, GENERAL):
+                            self.evaluated_keys = [f"Properties/Environment/Variables/{var_name}"]
+                            return CheckResult.FAILED
+
+        return CheckResult.PASSED
+
+
+check = LambdaEnvironmentCredentials()
