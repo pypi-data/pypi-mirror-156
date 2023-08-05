ddee9061c43c60c451da5336aaf68cecec4f5d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:36 +0000

    Add package nodejs-bin/16.15.1a2/nodejs_bin-16.15.1a2-py3-none-macosx_10_9_x86_64.whl

---
+++ packages/nodejs-bin/nodejs_bin-16.15.1a2-py3-none-macosx_10_9_x86_64.whl/nodejs/npm.py
@@ -0,0 +1,26 @@
+import os, sys
+from . import node
+
+def call(args, **kwargs):
+    return node.call([
+        os.path.join(os.path.dirname(__file__), "lib/node_modules/npm/bin/npm-cli.js"),
+        *args
+    ], **kwargs)
+
+def run(args, **kwargs):
+    return node.run([
+        os.path.join(os.path.dirname(__file__), "lib/node_modules/npm/bin/npm-cli.js"),
+        *args
+    ], **kwargs)
+
+def Popen(args, **kwargs):
+    return node.Popen([
+        os.path.join(os.path.dirname(__file__), "lib/node_modules/npm/bin/npm-cli.js"),
+        *args
+    ], **kwargs)
+
+def main():
+    sys.exit(call(sys.argv[1:]))
+
+if __name__ == '__main__':
+    main()
