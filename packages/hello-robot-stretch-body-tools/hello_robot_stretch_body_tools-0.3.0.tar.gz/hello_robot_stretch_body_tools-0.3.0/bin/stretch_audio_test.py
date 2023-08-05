bc499b6ee88b7afbded009a9e26c82171c881bb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:10:00 +0000

    Add package hello-robot-stretch-body-tools/0.3.0/hello_robot_stretch_body_tools-0.3.0.tar.gz

---
+++ packages/hello-robot-stretch-body-tools/hello_robot_stretch_body_tools-0.3.0.tar.gz/hello_robot_stretch_body_tools-0.3.0/bin/stretch_audio_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import os
+import stretch_body.hello_utils as hu
+hu.print_stretch_re_use()
+
+os.system('pactl set-default-sink alsa_output.pci-0000_00_1f.3.analog-stereo')
+os.system('amixer set Master 80%')
+os.system('paplay --device=alsa_output.pci-0000_00_1f.3.analog-stereo /usr/share/sounds/ubuntu/stereo/desktop-login.ogg')
+
