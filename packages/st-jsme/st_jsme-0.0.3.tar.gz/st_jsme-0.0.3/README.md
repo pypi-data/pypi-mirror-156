325f72d7991201ffb4ea89a67ead74a0030d7ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:57 +0000

    Add package st-jsme/0.0.3/st_jsme-0.0.3.tar.gz

---
+++ packages/st-jsme/st_jsme-0.0.3.tar.gz/st_jsme-0.0.3/README.md
@@ -0,0 +1,9 @@
+# st-jsme
+
+Usage:
+
+1. cd selfiepath_editor_app\st_jsme\frontend
+2. npm install
+3. npm run build 
+4. in the directory with app.py
+    streamlit run app.py
---
+++ packages/st-jsme/st_jsme-0.0.3.tar.gz/st_jsme-0.0.3/st_jsme/frontend/build/precache-manifest.aaf86aa453d677732e1207b534dc6876.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e9a252c9f4f2f5f1e0f42b6a3b83f87",
+    "url": "./index.html"
+  },
+  {
+    "revision": "28ad50f2d911996fc7ce",
+    "url": "./static/js/2.4ea1358c.chunk.js"
+  },
+  {
+    "revision": "c4806c150d49abac1ee67e2a3f6f71b4",
+    "url": "./static/js/2.4ea1358c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1924d2a9c7bee1d552a3",
+    "url": "./static/js/main.edee5c72.chunk.js"
+  },
+  {
+    "revision": "7c417fdcdd265b5366e4",
+    "url": "./static/js/runtime-main.119b752e.js"
+  }
+]);
