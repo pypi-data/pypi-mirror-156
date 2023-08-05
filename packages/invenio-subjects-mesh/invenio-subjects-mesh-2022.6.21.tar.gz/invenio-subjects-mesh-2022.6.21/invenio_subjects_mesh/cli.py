23643c233cfd414fe51b7f17394b70d1a9290528
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:55 +0000

    Add package invenio-subjects-mesh/2022.6.21/invenio-subjects-mesh-2022.6.21.tar.gz

---
+++ packages/invenio-subjects-mesh/invenio-subjects-mesh-2022.6.21.tar.gz/invenio-subjects-mesh-2022.6.21/invenio_subjects_mesh/cli.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2021 Northwestern University.
+#
+# invenio-subjects-mesh is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Command line tool."""
+
+from pathlib import Path
+
+import click
+
+from .converter import MeSHConverter
+from .downloader import MeSHDownloader
+from .reader import MeSHReader, topic_filter
+from .writer import write_jsonl
+
+
+@click.command()
+def main():
+    """Generate new subjects_mesh.yaml file."""
+    downloads_dir = Path(__file__).parent / "downloads"
+    files = MeSHDownloader(downloads_dir)
+    files.download()
+
+    topics_reader = MeSHReader(files.topics_filepath, filter=topic_filter)
+    qualifiers_reader = MeSHReader(files.qualifiers_filepath)
+
+    converter = MeSHConverter(topics_reader, qualifiers_reader)
+
+    filepath = write_jsonl(converter)
+
+    print(f"MeSH terms written here {filepath}")
