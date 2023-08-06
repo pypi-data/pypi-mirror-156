0acf99ca4f2b56294e30e6c8e5d3ddb0d778a0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:27 +0000

    Add package webknossos/0.10.6/webknossos-0.10.6-py3-none-any.whl

---
+++ packages/webknossos/webknossos-0.10.6-py3-none-any.whl/webknossos/dataset/_utils/infer_bounding_box_existing_files.py
@@ -0,0 +1,13 @@
+from functools import reduce
+
+from webknossos.dataset.mag_view import MagView
+from webknossos.geometry import BoundingBox
+
+
+def infer_bounding_box_existing_files(mag_view: MagView) -> BoundingBox:
+    """Since volume annotation layers are only a single layer, they do not contain a datasource-properties.json.
+    Therefore, the bounding box needs to be inferred when working with those."""
+
+    return reduce(
+        lambda acc, bbox: acc.extended_by(bbox), mag_view._array.list_bounding_boxes()
+    )
