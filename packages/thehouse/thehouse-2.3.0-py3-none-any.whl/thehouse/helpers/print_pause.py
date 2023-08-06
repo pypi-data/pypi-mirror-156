3f2d8fef86999530d145f09eda28fa1a78e1e843
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:47 +0000

    Add package thehouse/2.3.0/thehouse-2.3.0-py3-none-any.whl

---
+++ packages/thehouse/thehouse-2.3.0-py3-none-any.whl/thehouse/helpers/print_pause.py
@@ -0,0 +1,19 @@
+"""print_pause.
+
+This function will print a message and then it will pause the terminal
+"""
+
+import time
+
+
+def print_pause(string, sleep=1):
+    """Print a message and pause the terminal.
+
+    :param sleep: the amount of seconds the terminal should sleep as integer.
+    """
+    print(string)
+
+    try:
+        time.sleep(sleep)
+    except TypeError:
+        time.sleep(1)
