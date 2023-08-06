a5181320fb45fb0a8d11a41faf7fe85fa3d8a7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:50 +0000

    Add package planekit/0.1.705/planekit-0.1.705.tar.gz

---
+++ packages/planekit/planekit-0.1.705.tar.gz/planekit-0.1.705/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import setuptools
+
+
+VERSION = "0.1.704"  # BURAYI AKLINIZDA TUTUN (Değiştirebilirsiniz)
+long_description = ""
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+setup(
+    name='planekit',
+    packages=setuptools.find_packages(),
+    install_requires=['pymavlink==2.4.29', 'pyserial==3.5'],
+    version='0.1.705',
+    license='MIT',
+    description='ASD',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Musa Şen',
+    author_email='m42@gmail.com',
+    url='https://github.com/msasen/planekit',
+
+    download_url=f'https://github.com/msasen/planekit{VERSION}.tar.gz',
+    keywords=['Alakalı', 'kelimeler'],
+)
