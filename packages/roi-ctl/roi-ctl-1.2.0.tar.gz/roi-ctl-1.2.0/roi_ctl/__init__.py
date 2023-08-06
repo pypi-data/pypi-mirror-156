a1d74eecbe1b10e60a5ad7b7f8f9147a0b7d4edf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:28:41 +0000

    Add package roi-ctl/1.2.0/roi-ctl-1.2.0.tar.gz

---
+++ packages/roi-ctl/roi-ctl-1.2.0.tar.gz/roi-ctl-1.2.0/roi_ctl/__init__.py
@@ -0,0 +1,12 @@
+import importlib
+import os
+import sys
+
+from .roi_ctl import *
+
+# MODULE REGISTER
+globals().update(importlib.import_module('roi_ctl').__dict__)
+
+# STATIC PATH
+if sys.platform.startswith('linux'):
+	os.environ['ROI_CTL_STATIC'] = os.path.dirname(os.path.abspath(__file__))
---
+++ packages/roi-ctl/roi-ctl-1.2.0.tar.gz/roi-ctl-1.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+setup(
+name='roi-ctl',
+version='1.2.0',
+description='Change Module Infomation',
+author_email='ops@smartahc.com',
+author='roictl',
+license='smartahc',
+keywords=['roi_ctl'],
+packages=find_packages(),
+include_package_data=True,
+install_requires=['click==8.0.3', 'twine==3.5.0', 'nuitka==0.7.6', 'requests==2.25.1', 'pytest==6.2.5'],
+python_requires='>=3.8',
+entry_points="""
+[console_scripts]
+roictl=roi_ctl:cli
+"""
+)
