853a7d582b54b1c189939047e3f34fdcca6d0de1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:29:41 +0000

    Add package solidlab-perftest-agent/0.1.1/solidlab-perftest-agent-0.1.1.tar.gz

---
+++ packages/solidlab-perftest-agent/solidlab-perftest-agent-0.1.1.tar.gz/solidlab-perftest-agent-0.1.1/solidlab_perftest_agent/config.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class Config:
+    api_endpoint: str
+    machine_id: str
+    test_env_id: int
+    auth_token: str
+    # Below: Config depending on the test environment this agent is running on.
+    #        Probably fixed values in the ESpec that sets up the test environment.
+    css_config_file: Optional[str]
+    css_host: Optional[str]
+    css_port: Optional[str]
