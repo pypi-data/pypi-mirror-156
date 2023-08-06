6c80e1a2b72a0f1e4370adc579d42da97132de35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:03 +0000

    Add package wei/1.10/wei-1.10-py3-none-any.whl

---
+++ packages/wei/wei-1.10-py3-none-any.whl/wei/__init__.py
@@ -0,0 +1,10 @@
+from socket import gethostname
+from getpass import getuser
+
+def signature(sep = None):
+    sig = f"{getuser()}@{gethostname()}"
+    if sep:
+        return '\n\n%s\n%s\n' % (sep * len(sig), sig)
+    return sig
+
+### wei/__init__.py ends here
