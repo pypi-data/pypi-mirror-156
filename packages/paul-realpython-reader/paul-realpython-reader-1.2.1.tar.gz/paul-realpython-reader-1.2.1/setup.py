2248e908c09854ff3c2e23332d0066a55f6ed14e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:15 +0000

    Add package paul-realpython-reader/1.2.1/paul-realpython-reader-1.2.1.tar.gz

---
+++ packages/paul-realpython-reader/paul-realpython-reader-1.2.1.tar.gz/paul-realpython-reader-1.2.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    # author='...',
+    # description='...',
+    # download_url='...',
+    # license='...',
+    # long_description=open('README.md', 'r').read(),
+    # long_description_content_type='text/markdown',
+    # name='...',
+    # packages=['...'],
+    # url='...',
+    # version='...'
+
+)
