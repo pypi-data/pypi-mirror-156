6090a55158b9a4741a57325f112777aa382f8e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:14 +0000

    Add package hkw/1.1.9/hkw-1.1.9.tar.gz

---
+++ packages/hkw/hkw-1.1.9.tar.gz/hkw-1.1.9/src/rdata_/data_.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+def hkw() :
+	ppd = pd.read_csv('exam.csv')
+	lat= np.loadtxt('exam.csv', delimiter = ',', skiprows = 1, dtype = 'str')
+	print(lat)
+	print(ppd)
