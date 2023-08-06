59ce5ce36f8d7b36ab3a7b46edf5aa5e843bcb14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:44 +0000

    Add package dwclib/2022.6.25/dwclib-2022.6.25-py3-none-any.whl

---
+++ packages/dwclib/dwclib-2022.6.25-py3-none-any.whl/dwclib/dask/waves.py
@@ -0,0 +1,32 @@
+from typing import List, Optional
+
+from dwclib.common.meta import waves_meta
+from dwclib.common.waves import run_waves_query
+from dwclib.dask.generic import read_data
+
+from .waves_convert import convert_dataframe
+
+
+def read_waves(
+    patientid,
+    dtbegin,
+    dtend,
+    labels: Optional[List[str]] = None,
+    interval=None,
+    npartitions=None,
+    uri=None,
+):
+    if labels is None:
+        labels = []
+    ddf = read_data(
+        runner=run_waves_query,
+        meta=waves_meta,
+        dtbegin=dtbegin,
+        dtend=dtend,
+        uri=uri,
+        interval=interval,
+        patientid=patientid,
+        labels=labels,
+        naive_datetime=True,
+    )
+    return convert_dataframe(ddf, labels, npartitions)
