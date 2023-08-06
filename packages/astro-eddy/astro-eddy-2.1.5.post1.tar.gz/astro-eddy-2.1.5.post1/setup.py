4020861ae97b990ee40e61cf19465c5a4ab9cdc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:02 +0000

    Add package astro-eddy/2.1.5.post1/astro-eddy-2.1.5.post1.tar.gz

---
+++ packages/astro-eddy/astro-eddy-2.1.5.post1.tar.gz/astro-eddy-2.1.5.post1/setup.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="astro-eddy",
+    version="2.1.5-1",
+    author="Richard Teague",
+    author_email="richard.d.teague@cfa.harvard.edu",
+    description=("Tools to recover expectionally precise rotation curves from "
+                 "spatially resolved spectra."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/richteague/eddy",
+    packages=["eddy"],
+    license="MIT",
+    install_requires=[
+        "scipy>=1",
+        "numpy",
+        "matplotlib>=3",
+        "emcee>=3",
+        "corner>=2",
+        "zeus-mcmc",
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
