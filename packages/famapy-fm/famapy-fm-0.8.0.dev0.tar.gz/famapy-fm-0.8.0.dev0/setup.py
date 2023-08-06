0da7ce5d0112ef9afb20c31cb22c8fd136be0a4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:39 +0000

    Add package famapy-fm/0.8.0.dev0/famapy-fm-0.8.0.dev0.tar.gz

---
+++ packages/famapy-fm/famapy-fm-0.8.0.dev0.tar.gz/famapy-fm-0.8.0.dev0/setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="famapy-fm",
+    version="0.8.0.dev0",
+    author="Víctor Ramírez de la Corte",
+    author_email="me@virako.es",
+    description="famapy-fm is a plugin to FaMaPy module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FaMaPy/fm_metamodel",
+    packages=setuptools.find_namespace_packages(include=['famapy.*']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+    install_requires=[
+        'famapy>=0.9.0',
+        'uvlparser==0.9.0.dev0',
+        'afmparser>=0.2.0.dev1',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-mock',
+            'prospector',
+            'mypy',
+            'coverage',
+        ]
+    },
+    dependency_links=[
+        'famapy>=0.9.0'
+    ],
+)
