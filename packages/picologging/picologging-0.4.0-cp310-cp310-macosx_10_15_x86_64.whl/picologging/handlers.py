216ab6d8de86f79001316420848ec26f6dc0c246
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:30:14 +0000

    Add package picologging/0.4.0/picologging-0.4.0-cp310-cp310-macosx_10_15_x86_64.whl

---
+++ packages/picologging/picologging-0.4.0-cp310-cp310-macosx_10_15_x86_64.whl/picologging/handlers.py
@@ -0,0 +1,145 @@
+import os
+
+import picologging
+
+
+class WatchedFileHandler(picologging.FileHandler):
+    """
+    A handler for logging to a file, which watches the file
+    to see if it has changed while in use. This can happen because of
+    usage of programs such as newsyslog and logrotate which perform
+    log file rotation. This handler, intended for use under Unix,
+    watches the file to see if it has changed since the last emit.
+    (A file has changed if its device or inode have changed.)
+    If it has changed, the old file stream is closed, and the file
+    opened to get a new stream.
+
+    This handler is not appropriate for use under Windows, because
+    under Windows open files cannot be moved or renamed - logging
+    opens the files with exclusive locks - and so there is no need
+    for such a handler. Furthermore, ST_INO is not supported under
+    Windows; stat always returns zero for this value.
+    """
+    def __init__(self, filename, mode="a", encoding=None, delay=False):
+        picologging.FileHandler.__init__(self, filename, mode=mode,
+                                        encoding=encoding, delay=delay)
+        self.dev, self.ino = -1, -1
+        self._statstream()
+
+    def _statstream(self):
+        if self.stream:
+            sres = os.fstat(self.stream.fileno())
+            self.dev, self.ino = sres.st_dev, sres.st_ino
+
+    def reopenIfNeeded(self):
+        """
+        Reopen log file if needed.
+
+        Checks if the underlying file has changed, and if it
+        has, close the old stream and reopen the file to get the
+        current stream.
+        """
+        try:
+            sres = os.stat(self.baseFilename)
+        except FileNotFoundError:
+            sres = None
+
+        if not sres or sres.st_dev != self.dev or sres.st_ino != self.ino:
+            if self.stream is not None:
+                self.stream.flush()
+                self.stream.close()
+                self.stream = None  # See Issue #21742: _open () might fail.
+                self.stream = self._open()
+                self._statstream()
+
+    def emit(self, record):
+        """
+        Emit a record.
+
+        If underlying file has changed, reopen the file before emitting the
+        record to it.
+        """
+        self.reopenIfNeeded()
+        picologging.FileHandler.emit(self, record)
+
+
+class BaseRotatingHandler(picologging.FileHandler):
+    """
+    Base class for handlers that rotate log files at a certain point.
+    Not meant to be instantiated directly.  Instead, use RotatingFileHandler
+    or TimedRotatingFileHandler.
+    """
+    namer = None
+    rotator = None
+
+    def __init__(self, filename, mode, encoding=None, delay=False):
+        """
+        Use the specified filename for streamed logging
+        """
+        picologging.FileHandler.__init__(self, filename, mode=mode,
+                                        encoding=encoding, delay=delay)
+        self.mode = mode
+        self.encoding = encoding
+
+    def shouldRollover(self, record):
+        """
+        Determine if rollover should occur.
+        Should be implemented in inherited classes.
+        """
+        raise NotImplementedError()
+
+    def doRollover(self, record):
+        """
+        Do a rollover.
+        Should be implemented in inherited classes.
+        """
+        raise NotImplementedError()
+
+    def emit(self, record):
+        """
+        Emit a record.
+        Output the record to the file, catering for rollover as described
+        in doRollover().
+        """
+        try:
+            if self.shouldRollover(record):
+                self.doRollover()
+            picologging.FileHandler.emit(self, record)
+        except Exception:
+            self.handleError(record)
+
+    def rotation_filename(self, default_name):
+        """
+        Modify the filename of a log file when rotating.
+        This is provided so that a custom filename can be provided.
+        The default implementation calls the 'namer' attribute of the
+        handler, if it's callable, passing the default name to
+        it. If the attribute isn't callable (the default is None), the name
+        is returned unchanged.
+        :param default_name: The default name for the log file.
+        """
+        if not callable(self.namer):
+            result = default_name
+        else:
+            result = self.namer(default_name)
+        return result
+
+    def rotate(self, source, dest):
+        """
+        When rotating, rotate the current log.
+        The default implementation calls the 'rotator' attribute of the
+        handler, if it's callable, passing the source and dest arguments to
+        it. If the attribute isn't callable (the default is None), the source
+        is simply renamed to the destination.
+        :param source: The source filename. This is normally the base
+                       filename, e.g. 'test.log'
+        :param dest:   The destination filename. This is normally
+                       what the source is rotated to, e.g. 'test.log.1'.
+        """
+        if not callable(self.rotator):
+            # Issue: https://bugs.python.org/issue18940
+            # A file may not have been created if delay is True.
+            if os.path.exists(source):
+                os.rename(source, dest)
+        else:
+            self.rotator(source, dest)
