54d274f73fb8fe7d206da6b62e1efae9176daaaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:28 +0000

    Add package OpenFisca-US/0.82.0/OpenFisca-US-0.82.0.tar.gz

---
+++ packages/OpenFisca-US/OpenFisca-US-0.82.0.tar.gz/OpenFisca-US-0.82.0/openfisca_us/variables/household/income/spm_unit/spm_unit_benefits.py
@@ -0,0 +1,27 @@
+from openfisca_us.model_api import *
+
+
+class spm_unit_benefits(Variable):
+    value_type = float
+    entity = SPMUnit
+    label = "Benefits"
+    definition_period = YEAR
+    unit = USD
+
+    formula = sum_of_variables(
+        [
+            "social_security",
+            "ssi",
+            "ca_cvrp",  # California Clean Vehicle Rebate Project.
+            "snap",
+            "wic",
+            "free_school_meals",
+            "reduced_price_school_meals",
+            "lifeline",
+            "acp",
+            "ebb",
+            "basic_income",
+            "spm_unit_capped_housing_subsidy",
+            "tanf",
+        ]
+    )
