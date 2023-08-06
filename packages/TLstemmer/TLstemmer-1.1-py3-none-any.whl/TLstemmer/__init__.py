f11cc5dd6c11e0ea7792af0713e0e7a4ff97c5fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:46 +0000

    Add package TLstemmer/1.1/TLstemmer-1.1-py3-none-any.whl

---
+++ packages/TLstemmer/TLstemmer-1.1-py3-none-any.whl/TLstemmer/__init__.py
@@ -0,0 +1,23 @@
+def TLstemmer(inputW, root):
+        stem = list()
+        str1 = ''
+        str2 = '' 
+        inputletter = ''
+        rootletter = ''
+        for x in inputW:
+            for y in x['inputT']: 
+                inputletter += y
+                for r in root:
+                    if inputletter == r['stem']:
+                        rootletter = r['stem']
+               
+            str1 = inputletter
+            str2 = rootletter
+            suffix = str1.replace(str2, "")
+            if len(rootletter) > 0:
+                stem.append("Input Word->"+inputletter+'; Root word->'+rootletter+"; suffix->"+suffix)
+            else:
+                stem.append('Invalid word!')
+            inputletter = ''
+            rootletter = ''
+        return stem 
