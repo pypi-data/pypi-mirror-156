01f1b716fc07ac4b22767f66291bf7c5de90f06f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:26 +0000

    Add package pyvial/1.0.0/pyvial-1.0.0.tar.gz

---
+++ packages/pyvial/pyvial-1.0.0.tar.gz/pyvial-1.0.0/vial/loggers.py
@@ -0,0 +1,21 @@
+import logging
+from logging import Formatter, Handler, Logger, StreamHandler
+
+
+class LoggerFactory:
+
+    DEFAULT_FORMAT = "[%(levelname)s] %(asctime)s %(filename)s.%(funcName)s: %(message)s"
+
+    @classmethod
+    def get(cls, name: str) -> Logger:
+        log = logging.getLogger(name)
+        log.setLevel(logging.DEBUG)
+        log.propagate = False
+        log.addHandler(cls.get_handler())
+        return log
+
+    @classmethod
+    def get_handler(cls) -> Handler:
+        handler = StreamHandler()
+        handler.setFormatter(Formatter(cls.DEFAULT_FORMAT))
+        return handler
