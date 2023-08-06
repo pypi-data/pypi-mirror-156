dc9fa5e05297aa578fa94ba8ad492d487d942413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:37 +0000

    Add package asyncify-python/1.2/asyncify-python-1.2.tar.gz

---
+++ packages/asyncify-python/asyncify-python-1.2.tar.gz/asyncify-python-1.2/asyncify/_sentinel.py
@@ -0,0 +1,18 @@
+import functools
+from typing import Any,  NoReturn, Tuple, Type
+
+
+class RaisingSentinel:
+    def __init__(self, **error_on: Tuple[Type[Exception], str]):
+        for name, (exc_type, exc_msg) in error_on.items():
+            old = getattr(self, name, None)
+
+            def raiser(*args: Any, **kwargs: Any) -> NoReturn:
+                raise exc_type(exc_msg)
+
+            if old:
+                raiser = functools.wraps(old)  # type: ignore
+            else:
+                raiser.__name__ = name
+
+            setattr(self, name, raiser)
