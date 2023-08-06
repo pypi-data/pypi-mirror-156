3611c1aa6905cbf8670df43e09252146f45df65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:25 +0000

    Add package je-load-density-dev/0.0.10/je_load_density_dev-0.0.10.tar.gz

---
+++ packages/je-load-density-dev/je_load_density_dev-0.0.10.tar.gz/je_load_density_dev-0.0.10/je_load_density/utils/exception/exception.py
@@ -0,0 +1,22 @@
+class LocustNotFoundException(Exception):
+    pass
+
+
+class JELoadingTestException(Exception):
+    pass
+
+
+class JELoadingTestJsonException(JELoadingTestException):
+    pass
+
+
+class JELoadingTestExecuteException(JELoadingTestException):
+    pass
+
+
+class JELoadingAssertException(JELoadingTestException):
+    pass
+
+
+class HTMLException(JELoadingTestException):
+    pass
