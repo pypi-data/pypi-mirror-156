2ed6b8c9e9f56067d9acd05e2349df699eff16a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:34 +0000

    Add package phrydy/3.1.0/phrydy-3.1.0.tar.gz

---
+++ packages/phrydy/phrydy-3.1.0.tar.gz/phrydy-3.1.0/phrydy/__init__.py
@@ -0,0 +1,22 @@
+from . import field_docs  # noqa: F401
+from . import doc_generator  # noqa: F401
+from . import mediafile_extended  # noqa: F401
+
+from ._version import get_versions
+
+from .mediafile_extended import \
+    MediaFile, \
+    MediaFileExtended  # noqa: F401
+
+from .field_docs import \
+    fields, \
+    FieldDocCollection  # noqa: F401
+
+from .doc_generator import \
+    get_max_field_length, \
+    format_fields_as_txt, \
+    merge_fields, \
+    print_debug  # noqa: F401
+
+__version__ = get_versions()['version']
+del get_versions
