d55a139fd861458459b58d061d26dd2855135c8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:03 +0000

    Add package deepprojection/1.0.0a0/DeepProjection-1.0.0a0.tar.gz

---
+++ packages/deepprojection/DeepProjection-1.0.0a0.tar.gz/DeepProjection-1.0.0a0/deepprojection/__init__.py
@@ -0,0 +1,12 @@
+from .project import *
+from .CARE_projection import CARE_projection
+from .data import DataProcess
+from .predict import PredictStack
+from .postprocess import *
+from .train import Trainer
+from .utils import *
+
+# import models
+from .ProjNet import ProjNet
+
+
