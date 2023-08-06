34b9b11dfcb91ec5ada1b1a6e7e597d7b6302fef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:12 +0000

    Add package notetiktok/0.1.11/notetiktok-0.1.11-py3-none-any.whl

---
+++ packages/notetiktok/notetiktok-0.1.11-py3-none-any.whl/notetiktok/server/merge.py
@@ -0,0 +1,5 @@
+from .data_service import FavoriteService, FollowService, ResourceService
+
+favorite_service = FavoriteService()
+follow_service = FollowService()
+resource_service = ResourceService()
