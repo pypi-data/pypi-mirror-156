123a6f6fa2b76f40473867fc2056a11e7da1c23e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:32 +0000

    Add package bugsplat/0.2.0/bugsplat-0.2.0-py3.10.egg

---
+++ packages/bugsplat/bugsplat-0.2.0-py3.10.egg/bugsplat/__init__.py
@@ -0,0 +1,11 @@
+'''
+bugsplat
+
+A BugSplat integration for reporting Unhandled Exceptions in Python.
+'''
+
+__version__ = '0.2.0'
+__author__ = '@bobbyg603'
+__credits__ = 'BugSplat'
+
+from .bugsplat import BugSplat
