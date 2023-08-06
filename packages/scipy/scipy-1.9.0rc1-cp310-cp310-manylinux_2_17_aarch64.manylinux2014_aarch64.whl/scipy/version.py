c1f70d72bcee2c4b011cd595655aed5fdbfa8b57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:07 +0000

    Add package scipy/1.9.0rc1/scipy-1.9.0rc1-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl

---
+++ packages/scipy/scipy-1.9.0rc1-cp310-cp310-manylinux_2_17_aarch64.manylinux2014_aarch64.whl/scipy/version.py
@@ -0,0 +1,12 @@
+# THIS FILE IS GENERATED DURING THE SCIPY BUILD
+# See tools/version_utils.py for details
+
+short_version = '1.9.0rc1'
+version = '1.9.0rc1'
+full_version = '1.9.0rc1'
+git_revision = '72a5f46'
+commit_count = '0'
+release = True
+
+if not release:
+    version = full_version
