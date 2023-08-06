1b44e67f4dc45a6789614e4686b7712a3041ad64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:00 +0000

    Add package sdss-mangadap/4.0.4/sdss_mangadap-4.0.4-py3-none-any.whl

---
+++ packages/sdss-mangadap/sdss_mangadap-4.0.4-py3-none-any.whl/mangadap/tests/test_emissionlinedb.py
@@ -0,0 +1,17 @@
+from IPython import embed
+
+from mangadap.par.emissionlinedb import EmissionLineDB
+
+
+def test_read():
+    dbs = EmissionLineDB.available_databases()
+    assert len(dbs) > 0, 'No emission-line databases available'
+    for key in dbs.keys():
+        emldb = EmissionLineDB.from_key(key)
+
+
+def test_mpl11():
+    emldb = EmissionLineDB.from_key('ELPMPL11')
+    assert len(emldb) == 35, 'Incorrect number of emission lines'
+    assert 'ArIII' in emldb['name'], 'Does not contain ArIII in list'
+
