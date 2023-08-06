736daacbaffdf81607cd56bf8332b48dfb49a66a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:54 +0000

    Add package easy-predictor/0.6.0/easy_predictor-0.6.0.tar.gz

---
+++ packages/easy-predictor/easy_predictor-0.6.0.tar.gz/easy_predictor-0.6.0/src/easy_predictor/classification.py
@@ -0,0 +1,46 @@
+from sklearn import svm
+from sklearn.feature_extraction.text import CountVectorizer
+import pandas as pd 
+
+def predict(data: str,data_type: str,x: str, y: str,value: str):
+    
+    ERROR_LIST = {
+        'null_value': "'value' Input can't be null",
+        'null_dataset': "'data' can't be null"
+    }
+
+    value_list = [value]
+
+    if data_type == "xlsx":
+        if len(value_list) != 0:
+            data = pd.read_excel(data)
+
+            train_x = data[[x]].values
+            y_output = data[[y]].values
+
+            vectorizer = CountVectorizer(binary=True)
+            vectors = vectorizer.fit_transform(train_x.ravel())
+
+            model = svm.SVC(kernel='linear')
+            model.fit(vectors,y_output.ravel())
+            input = vectorizer.transform([value])
+            print(model.predict(input))
+        else:
+            print(ERROR_LIST['null_value'])
+
+    if data_type == 'csv':
+        if len(value_list) != 0:
+            data = pd.read_csv(data)
+
+            train_x = data[[x]].values
+            y_output = data[[y]].values
+
+            vectorizer = CountVectorizer(binary=True)
+            vectors = vectorizer.fit_transform(train_x.ravel())
+
+            model = svm.SVC(kernel='linear')
+            model.fit(vectors,y_output.ravel())
+            input = vectorizer.transform([value])
+            print(model.predict(input))
+        else:
+            print(ERROR_LIST['null_value'])
