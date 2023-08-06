c1074001f0ca9a46e7eb504bb440c9eebba33729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:39 +0000

    Add package djangoCRUDframework/0.2.22/djangoCRUDframework-0.2.22.tar.gz

---
+++ packages/djangoCRUDframework/djangoCRUDframework-0.2.22.tar.gz/djangoCRUDframework-0.2.22/crud_framework/schemas/user_aware.py
@@ -0,0 +1,34 @@
+from crud_framework.errors import Error, HttpStatus
+from crud_framework.schemas import CrudSchema
+
+
+class UserAwareCrudSchema(CrudSchema):
+    USER_FIELD = None
+
+    def __init__(self, user, *args, **kwargs):
+        if not self.USER_FIELD:
+            raise Error(
+                code=-1, status=HttpStatus.HTTP_405_METHOD_NOT_ALLOWED, field_name='USER_FIELD',
+                message='USER_FIELD missing', description='USER_FIELD missing')
+
+        self.user = user
+        super(UserAwareCrudSchema, self).__init__(*args, **kwargs)
+        print(f'USER: {self.user}')
+
+        # if not isinstance(self.MODEL_CLASS, BaseTrackedModel): TODO
+        #     raise Error(
+        #         code=-1, status=HttpStatus.HTTP_405_METHOD_NOT_ALLOWED, field_name='MODEL_CLASS',
+        #         message='Class must be of type BaseTrackedModel', description='Class must be of type BaseTrackedModel')
+
+    def set_queryset(self, filters):
+        filters[self.USER_FIELD] = self.user
+        return super(UserAwareCrudSchema, self).set_queryset(filters)
+
+    def post(self, **data):
+        return super(UserAwareCrudSchema, self).post(creator=self.user, editor=self.user, **data)
+
+    def put(self, **data):
+        return super(UserAwareCrudSchema, self).put(editor=self.user, **data)
+
+    def delete(self, **data):
+        return super(UserAwareCrudSchema, self).delete(editor=self.user, **data)
---
+++ packages/djangoCRUDframework/djangoCRUDframework-0.2.22.tar.gz/djangoCRUDframework-0.2.22/crud_framework/views/oauth.py
@@ -0,0 +1,49 @@
+from oauth2_provider.views.mixins import ProtectedResourceMixin, OAuthLibMixin
+from .base import view_catch_error, BaseCrudView, BaseView
+from .lookups import ChoicesView
+
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
+
+# def set_user(f): TODO
+#     def wrap(self, request, *args, **kwargs):
+#         self.user = request.resource_owner
+#         return f(self=self, request=request, *args, **kwargs)
+#
+#     wrap.__doc__ = f.__doc__
+#     wrap.__name__ = f.__name__
+#     return wrap
+
+
+class _SetUserCrudView(BaseCrudView):
+    def get(self, request, *args, **kwargs):
+        return super(_SetUserCrudView, self).get(
+            request=request, initkwargs=dict(user=request.resource_owner), *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return super(_SetUserCrudView, self).post(
+            request=request, initkwargs=dict(user=request.resource_owner), *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return super(_SetUserCrudView, self).put(
+            request=request, initkwargs=dict(user=request.resource_owner), *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return super(_SetUserCrudView, self).delete(
+            request=request, initkwargs=dict(user=request.resource_owner), *args, **kwargs)
+
+
+@method_decorator([csrf_exempt, view_catch_error], name='dispatch')
+class OauthChoiceView(ProtectedResourceMixin, OAuthLibMixin, ChoicesView):
+    pass
+
+
+@method_decorator([csrf_exempt, view_catch_error], name='dispatch')
+class OauthCrudView(ProtectedResourceMixin, OAuthLibMixin, _SetUserCrudView):
+    pass
+
+
+@method_decorator([csrf_exempt, view_catch_error], name='dispatch')
+class OauthFunctionalView(ProtectedResourceMixin, OAuthLibMixin, BaseView):
+    pass
