49475cb2a943804a7aa0ca23f335af2d63beb3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:43 +0000

    Add package creart-graia/0.1.2/creart_graia-0.1.2-py3-none-any.whl

---
+++ packages/creart-graia/creart_graia-0.1.2-py3-none-any.whl/graia/creart/broadcast.py
@@ -0,0 +1,65 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from creart import AbstractCreator, CreateTargetInfo, exists_module, it, mixin
+
+if TYPE_CHECKING:
+    from graia.broadcast import Broadcast
+    from graia.broadcast.interrupt import InterruptControl
+    from graia.saya.builtins.broadcast.behaviour import BroadcastBehaviour
+
+
+class BroadcastCreator(AbstractCreator):
+    targets = (
+        CreateTargetInfo(
+            module="graia.broadcast",
+            identify="Broadcast",
+            humanized_name="Broadcast Control",
+            description="<common,graia,broadcast> a high performance, highly customizable, elegantly designed event system based on asyncio",
+            author=["GraiaProject@github"]
+        ),
+        CreateTargetInfo(
+            module="graia.broadcast.interrupt",
+            identify="InterruptControl",
+            humanized_name="Interrupt",
+            description="<common,graia,broadcast,interrupt> Interrupt feature for broadcast control.",
+            author=["GraiaProject@github"]
+        ),
+    )
+
+    @staticmethod
+    def available() -> bool:
+        return exists_module("graia.broadcast")
+
+    @staticmethod
+    def create(
+        create_type: type[Broadcast | InterruptControl],
+    ) -> Broadcast | InterruptControl:
+        from graia.broadcast import Broadcast
+        from graia.broadcast.interrupt import InterruptControl
+
+        if issubclass(create_type, Broadcast):
+            import asyncio
+
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return create_type(loop=loop)
+        elif issubclass(create_type, InterruptControl):
+            return create_type(it(Broadcast))
+
+
+class BroadcastBehaviourCreator(AbstractCreator):
+    targets = (
+        CreateTargetInfo("graia.saya.builtins.broadcast.behaviour", "Broadcast"),
+    )
+
+    @staticmethod
+    @mixin(BroadcastCreator)
+    def available() -> bool:
+        return exists_module("graia.saya")
+
+    @staticmethod
+    def create(create_type: type[BroadcastBehaviour]) -> BroadcastBehaviour:
+        broadcast = it(Broadcast)
+        return create_type(broadcast)
