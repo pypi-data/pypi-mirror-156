dd9068388afdbfe66077d0d263b823951c6766a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:47 +0000

    Add package spiderpen/0.0.12/spiderpen-0.0.12.tar.gz

---
+++ packages/spiderpen/spiderpen-0.0.12.tar.gz/spiderpen-0.0.12/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name = 'spiderpen',
+    packages = ['spiderpen'],
+    version = '0.0.12',
+    description = 'Python library for controlling a plotter model complementary to the LEGO Boost 17101 set',
+    long_description = '',
+    author = 'Valentin Burillier',
+    author_email = '',
+    url = 'https://github.com/valentin-burillier/spiderpen',
+    keywords = ['LEGO', 'ROBOTICS'],
+    requires=['pylgbst'],
+)
