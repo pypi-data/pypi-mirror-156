338cb11904eabc27cf4e9e429aeeef8983006692
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:18 +0000

    Add package setux-repl/0.22.25.8/setux_repl-0.22.25.8-py3-none-any.whl

---
+++ packages/setux-repl/setux_repl-0.22.25.8-py3-none-any.whl/setux/commands/edit.py
@@ -0,0 +1,42 @@
+from os import environ
+from pathlib import Path
+from subprocess import call
+
+from pybrary.command import Command, Param
+
+
+class Remote(Param):
+    '''File to edit.
+    '''
+    name = 'path'
+    positional = True
+    mandatory = True
+
+
+class Edit(Command):
+    '''Edit a file.
+    '''
+    Params = [
+        Remote,
+    ]
+
+    def run(self):
+        target = self.get('target')
+        remote = self.get('path')
+        editor = environ.get('EDITOR','vim')
+        dest = Path('/tmp/setux')
+        dest.mkdir(exist_ok=True)
+        path = Path(remote)
+        local = f'{dest}/{path.name}'
+        ok = target.fetch(remote, local, quiet=True)
+        if ok:
+            original = open(local).read()
+        else:
+            original = ''
+            with open(local, 'w'): pass
+        call([editor, local])
+        edited = open(local).read()
+        if edited!=original:
+            ok = target.send(local, remote)
+            status = '.' if ok else 'X'
+            print(f'write {remote} {status}')
