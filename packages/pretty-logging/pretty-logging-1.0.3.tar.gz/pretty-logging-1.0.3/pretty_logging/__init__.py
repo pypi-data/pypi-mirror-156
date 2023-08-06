2097b3f460ec10fd4712afd84a4a4721d001422c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:48 +0000

    Add package pretty-logging/1.0.3/pretty-logging-1.0.3.tar.gz

---
+++ packages/pretty-logging/pretty-logging-1.0.3.tar.gz/pretty-logging-1.0.3/pretty_logging/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*-
+
+from .log import enable_pretty_logging
+import logging
+
+pretty_logger = logging.getLogger('pretty_logger')
+
+enable_pretty_logging(logger=pretty_logger)
+
+pretty_logger.setLevel(logging.DEBUG)
+pretty_logger.propagate = False
