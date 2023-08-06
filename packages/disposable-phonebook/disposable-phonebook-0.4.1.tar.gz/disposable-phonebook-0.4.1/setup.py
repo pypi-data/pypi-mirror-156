96b6af232afe2a4d39df086b9aed0243f6b1ceee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:49 +0000

    Add package disposable-phonebook/0.4.1/disposable-phonebook-0.4.1.tar.gz

---
+++ packages/disposable-phonebook/disposable-phonebook-0.4.1.tar.gz/disposable-phonebook-0.4.1/setup.py
@@ -0,0 +1,47 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='disposable-phonebook',
+    description='Scraper for disposable phone number services',
+    long_description=open('README.md').read(),
+    url='https://github.com/anroots/disposable-phonebook',
+    project_urls={
+        'Documentation': 'https://anroots.github.io/disposable-phonebook/',
+        'Source Code': 'https://github.com/anroots/disposable-phonebook'
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Topic :: Communications :: Telephony'
+    ],
+    setup_requires='setuptools',
+    license='Apache 2.0',
+    packages=find_packages(exclude=['tests*']),
+    package_data={
+        'dphonebook': ['disposable-phonebook.yml', 'vendor/user-agents/*']
+    },
+    version='0.4.1',
+    author='Ando Roots',
+    author_email='ando@sqroot.eu',
+    install_requires=[
+        'requests',
+        'click',
+        'setuptools',
+        'beautifulsoup4',
+        'phonenumbers',
+        'pyyaml',
+        'tqdm'
+    ],
+    entry_points={
+        'console_scripts': [
+            'dphonebook = dphonebook.cli:main',
+        ],
+    },
+)
