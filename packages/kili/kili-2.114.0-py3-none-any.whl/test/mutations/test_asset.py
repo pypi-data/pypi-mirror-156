07c01de231990f2ecf6df5d1fa168282eb82de7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:58 +0000

    Add package kili/2.114.0/kili-2.114.0-py3-none-any.whl

---
+++ packages/kili/kili-2.114.0-py3-none-any.whl/test/mutations/test_asset.py
@@ -0,0 +1,261 @@
+"""
+Test mutations with pytest
+"""
+import os
+import json
+import time
+import shutil
+import tempfile
+from unittest import TestCase
+import unittest
+import uuid
+
+import pytest
+from kili.client import Kili
+from kili.mutations.asset.helpers import get_file_mimetype, process_append_many_to_dataset_parameters, process_content
+from kili.mutations.asset.queries import GQL_APPEND_MANY_FRAMES_TO_DATASET
+import requests
+
+api_key = os.getenv('KILI_USER_API_KEY')
+api_endpoint = os.getenv('KILI_API_ENDPOINT')
+kili = Kili(api_key=api_key, api_endpoint=api_endpoint)
+
+
+tester = TestCase()
+
+@pytest.fixture
+def create_video_project():
+    """
+    Create a video project
+    """
+    with open('./test/mutations/interface.json', 'r') as file:
+        interface = json.load(file)
+    project = kili.create_project(
+        input_type='FRAME', json_interface=interface, title='Test project | video import')
+    return project['id']
+
+class LocalDownloader():
+
+    def __init__(self, directory):
+        self.directory = directory
+
+    def __call__(self, url):
+        content = requests.get(url)
+        name = os.path.basename(url)
+        path = os.path.join(self.directory, f'{str(uuid.uuid4())}-{name}')
+        with open(path, 'wb') as file:
+            file.write(content.content)
+        return path
+
+
+TIMEOUT = 10
+RETRY = 1
+BASE_TEST_CASES = [
+    {
+        'case': 'uploading a video with a url',
+        'content': ['https://storage.googleapis.com/label-public-staging/presales/industry_small.mp4'],
+        'expected_processing_parameters': {
+            'shouldKeepNativeFrameRate': True,
+            'framesPlayedPerSecond': 25,
+            'shouldUseNativeVideo': True
+        }
+    },
+    {
+        'case': 'uploading a video as a list of image urls',
+        'json_content': [["https://storage.googleapis.com/label-public-staging/video1/video1-img000001.jpg",
+                            "https://storage.googleapis.com/label-public-staging/video1/video1-img000002.jpg"]],
+        'expected_processing_parameters': {
+            'shouldKeepNativeFrameRate': False,
+            'framesPlayedPerSecond': 30,
+            'shouldUseNativeVideo': False
+        }
+    },
+    {
+        'case': 'uploading a video into frames from url',
+        'content': ['https://storage.googleapis.com/label-public-staging/presales/industry_small.mp4'],
+        'expected_processing_parameters': {
+            'shouldKeepNativeFrameRate': True,
+            'shouldUseNativeVideo': False,
+            'framesPlayedPerSecond': 25,
+        },
+        'json_metadata': [{
+            'processingParameters': {
+                'shouldKeepNativeFrameRate': True,
+                'shouldUseNativeVideo': False,
+            }
+        }],
+    },
+    
+]
+CUSTOM_VALUE_CASES = [
+    {
+        'case': 'uploading a video with a url',
+        'content': ['https://storage.googleapis.com/label-public-staging/presales/industry_small.mp4'],
+        'expected_processing_parameters': {
+            'shouldKeepNativeFrameRate': False,
+            'framesPlayedPerSecond': 40,
+            'shouldUseNativeVideo': True
+        },
+        'json_metadata': [{
+            'processingParameters': {
+                'shouldKeepNativeFrameRate': False,
+                'framesPlayedPerSecond': 40,
+            }
+        }]
+    },
+]
+TEST_CASES = BASE_TEST_CASES
+TEST_CASES.extend([{**x, 'local': True} for x in BASE_TEST_CASES])
+TEST_CASES.extend(CUSTOM_VALUE_CASES)
+
+
+def test_upload_video(create_video_project, tmpdir):
+    """
+    Test upload of video in different ways
+    """
+    project_id = create_video_project
+    assert project_id != '', 'Project not successfully created'
+
+    downloader = LocalDownloader(tmpdir)
+
+    for i, test_case in enumerate(TEST_CASES):
+        external_id = f'video_case_{i+1}'
+        content = test_case.get('content', None)
+        expected_parameters = test_case.get(
+            'expected_processing_parameters', {})
+        json_metadata = test_case.get('json_metadata', None)
+        json_content = test_case.get('json_content', None)
+        local = test_case.get('local', False)
+        case = test_case.get('case', 'Unknown case') + f' | {"local" if local else "cloud"}'
+        print('case', case)
+        if local:
+            if content is not None:
+                content = list(map(downloader, content))
+            else:
+                json_content = list(map(lambda jc: [downloader(x) for x in jc], json_content))
+
+
+        kili.append_many_to_dataset(
+            project_id=project_id,
+            content_array=content,
+            json_metadata_array=json_metadata,
+            json_content_array=json_content,
+            external_id_array=[external_id]
+        )
+
+        tstart = time.time()
+        n_assets = kili.count_assets(project_id=project_id)
+        while not n_assets == i+1 and time.time() - tstart < TIMEOUT:
+            time.sleep(RETRY)
+            n_assets = kili.count_assets(project_id=project_id)
+        assert n_assets == i+1, 'Asset upload failed'
+
+        asset_uploaded = kili.assets(
+            project_id=project_id, external_id_contains=[external_id])
+        processed_parameters = asset_uploaded[0]['jsonMetadata']['processingParameters']
+        tester.assertDictEqual(expected_parameters, processed_parameters), f'{case}, got {processed_parameters}, expected {expected_parameters}'
+
+class TestMimeType():
+    """
+    Tests if the mime type is the correct one
+    """
+
+    def should_have_right_mimetype(self, content_array, json_content_array, expected_mimetype):
+        mimetype = get_file_mimetype(content_array, json_content_array)
+        assert mimetype == expected_mimetype, f'Bad mimetype {mimetype}'
+
+    def test_contents_empty(self):
+        content_array = None
+        json_content_array = None
+        self.should_have_right_mimetype(content_array, json_content_array, None)
+
+    def test_mimetype_url(self, tmpdir):
+        url = 'https://storage.googleapis.com/label-public-staging/car/car_1.jpg'
+        content_array = [url]
+        json_content_array = None
+        self.should_have_right_mimetype(content_array, json_content_array, None)
+
+    def test_mimetype_image(self, tmpdir):
+        url = 'https://storage.googleapis.com/label-public-staging/car/car_1.jpg'
+        downloader = LocalDownloader(tmpdir)
+        path = downloader(url)
+        content_array = [path]
+        json_content_array = None
+        self.should_have_right_mimetype(content_array, json_content_array, 'image/jpeg')
+
+    def test_mimetype_geotiff(self, tmpdir):
+        url = 'https://storage.googleapis.com/label-public-staging/geotiffs/bogota.tif'
+        downloader = LocalDownloader(tmpdir)
+        path = downloader(url)
+        content_array = [path]
+        json_content_array = None
+        self.should_have_right_mimetype(content_array, json_content_array, 'image/tiff')
+    
+    def test_cannot_upload_mp4_to_image_project(self):
+        path = './test.mp4'
+        content_array = [path]
+        json_content_array = None
+        processed_content = process_content('IMAGE', content_array, json_content_array)
+        assert(processed_content==[None])
+
+    def test_cannot_upload_png_to_frame_project(self):
+        path = './test.png'
+        content_array = [path]
+        json_content_array = None
+        processed_content = process_content('FRAME', content_array, json_content_array)
+        assert(processed_content==[None])
+
+    def test_cannot_upload_text_to_pdf_project(self):
+        path = 'Hello world'
+        content_array = [path]
+        json_content_array = None
+        processed_content = process_content('PDF', content_array, json_content_array)
+        assert(processed_content==[None])
+
+    @pytest.mark.xfail(raises=FileNotFoundError)
+    def test_can_upload_png_to_image_project(self):
+        path = './test.png'
+        content_array = [path]
+        json_content_array = None
+        process_content('IMAGE', content_array, json_content_array)
+
+
+class TestUploadTiff(unittest.TestCase):
+    """
+    Test functions for uploading geotiff
+    """
+
+    def setUp(self):
+        # Create a temporary directory
+        self.test_dir = tempfile.mkdtemp()
+
+    def tearDown(self):
+        # Remove the directory after the test
+        shutil.rmtree(self.test_dir)
+
+    def test_geotiff_upload_properties(self):
+        url = 'https://storage.googleapis.com/label-public-staging/geotiffs/bogota.tif'
+        downloader = LocalDownloader(self.test_dir)
+        path = downloader(url)
+        input_type = 'IMAGE'
+        content_array = [path]
+        external_id_array = ['bogota']
+        is_honeypot_array = None
+        status_array = None
+        json_content_array = None
+        json_metadata_array = None
+        properties, upload_type, request = process_append_many_to_dataset_parameters(
+            input_type,
+            content_array,
+            external_id_array,
+            is_honeypot_array,
+            status_array,
+            json_content_array,
+            json_metadata_array,
+        )
+        assert properties['json_metadata_array'] == ['{}']
+        assert properties['content_array'][0].startswith(
+            'data:image/tiff;base64,SUkqAAgABABre')
+        assert properties['external_id_array'] == ['bogota']
+        assert upload_type == 'GEO_SATELLITE', 'uploadType do not match'
+        assert request == GQL_APPEND_MANY_FRAMES_TO_DATASET, 'Requests do not match'
---
+++ packages/kili/kili-2.114.0-py3-none-any.whl/test/test_cli.py
@@ -0,0 +1,204 @@
+"""Tests the Kili CLI"""
+
+import os
+from kili.cli import describe_project, import_assets, list_project, create_project
+from click.testing import CliRunner
+
+from .utils import debug_subprocess_pytest
+
+
+def mocked__projects(project_id=None, **_):
+    if project_id == 'text_project':
+        return [{'id': 'text_project', 'inputType': 'TEXT'}]
+    if project_id == 'image_project':
+        return [{'id': 'image_project', 'inputType': 'IMAGE'}]
+    if project_id == 'frame_project':
+        return [{'id': 'frame_project', 'inputType': 'FRAME'}]
+    if project_id == None:
+        return [{'id': 'text_project', 'title': 'text_project', 'description': ' a project with text',
+                 'numberOfAssets': 10, 'numberOfRemainingAssets': 10},
+                {'id': 'image_project', 'title': 'image_project', 'description': ' a project with image',
+                 'numberOfAssets': 0, 'numberOfRemainingAssets': 0},
+                {'id': 'frame_project', 'title': 'frame_project', 'description': ' a project with frame',
+                 'numberOfAssets': 10, 'numberOfRemainingAssets': 0}]
+
+
+def test_list(mocker):
+    mocker.patch("kili.client.Kili.__init__", return_value=None)
+    mock__projects = mocker.patch("kili.client.Kili.projects",
+                                  side_effect=mocked__projects)
+
+    runner = CliRunner()
+    result = runner.invoke(list_project)
+    assert ((result.exit_code == 0) and
+            (result.output.count("100.0%") == 1) and
+            (result.output.count("0.0%") == 2) and
+            (result.output.count("nan") == 1))
+
+
+def test_create_project(mocker):
+    mocker.patch("kili.client.Kili.__init__", return_value=None)
+    mocker.patch("kili.client.Kili.create_project")
+    runner = CliRunner()
+    runner.invoke(create_project,
+                  ['--interface',
+                   "test/fixtures/image_interface.json",
+                   '--title',
+                   'Test project',
+                   '--description',
+                   'description',
+                   '--input-type',
+                   'IMAGE'])
+
+
+def test_import(mocker):
+    mocker.patch("kili.client.Kili.__init__", return_value=None)
+    mocked__append_many_to_dataset = mocker.patch(
+        "kili.client.Kili.append_many_to_dataset")
+    mocker.patch("kili.client.Kili.projects",
+                 side_effect=mocked__projects)
+
+    TEST_CASES = [{
+        'case_name': 'AAU, when I import a list of file to an image project, I see a success',
+        'files': ['test_tree/image1.png', 'test_tree/leaf/image3.png'],
+        'options': {
+            'project-id': 'image_project',
+        },
+        'expected_mutation_payload': {
+            'project_id': 'image_project',
+            'content_array': ['test_tree/image1.png', 'test_tree/leaf/image3.png'],
+            'external_id_array': ['image1.png', 'image3.png'],
+            'json_metadata_array': None
+        }
+    },
+        {
+        'case_name': 'AAU, when I import a list of folder and files with excluded files to an image project, I see a success',
+        'files': ['test_tree/', 'test_tree/leaf'],
+        'options': {
+            'project-id': 'image_project',
+            'exclude': 'test_tree/image1.png'},
+        'expected_mutation_payload': {
+            'project_id': 'image_project',
+            'content_array': ['test_tree/image2.jpg', 'test_tree/leaf/image3.png', 'test_tree/leaf/image4.jpg'],
+            'external_id_array': ['image2.jpg', 'image3.png', 'image4.jpg'],
+            'json_metadata_array': None
+        }
+    },
+        {
+            'case_name': 'AAU, when I import files with stars, I see a success',
+            'files': ['test_tree/**.jpg', 'test_tree/leaf/**.jpg'],
+            'options': {
+                'project-id': 'image_project',
+            },
+            'expected_mutation_payload': {
+                'project_id': 'image_project',
+                'content_array': ['test_tree/image2.jpg', 'test_tree/leaf/image4.jpg'],
+                'external_id_array': ['image2.jpg', 'image4.jpg'],
+                'json_metadata_array': None
+            }
+    },
+        {
+        'case_name': 'AAU, when I import a files to a text project, I see a success',
+        'files': ['test_tree/', 'test_tree/leaf'],
+        'options': {
+            'project-id': 'text_project',
+            'exclude': 'test_tree/image1.png'},
+        'expected_mutation_payload': {
+            'project_id': 'text_project',
+            'content_array': ['test_tree/leaf/texte2.txt', 'test_tree/texte1.txt'],
+            'external_id_array': ['texte2.txt', 'texte1.txt'],
+            'json_metadata_array': None
+        }
+    },
+        {
+        'case_name': 'AAU, when I import videos to a frame project, as native by changing the fps, I see a success',
+        'files': ['test_tree/'],
+        'options': {
+            'project-id': 'frame_project',
+            'fps': '10',
+        },
+        'expected_mutation_payload': {
+            'project_id': 'frame_project',
+            'content_array': ['test_tree/video1.mp4', 'test_tree/video2.mp4'],
+            'external_id_array': ['video1.mp4', 'video2.mp4'],
+            'json_metadata_array': [
+                {'processingParameters': {
+                    'shouldKeepNativeFrameRate': False,
+                    'framesPlayedPerSecond': 10,
+                    'shouldUseNativeVideo': True}
+                 }
+            ] * 2
+        }
+    },
+        {
+        'case_name': 'AAU, when I import videos to a frame project, as frames with the native frame rate, I see a success',
+        'files': ['test_tree/'],
+        'options': {
+            'project-id': 'frame_project',
+        },
+        'flags': ['frames'],
+        'expected_mutation_payload': {
+            'project_id': 'frame_project',
+            'content_array': ['test_tree/video1.mp4', 'test_tree/video2.mp4'],
+            'external_id_array': ['video1.mp4', 'video2.mp4'],
+            'json_metadata_array': [
+                {'processingParameters': {
+                    'shouldKeepNativeFrameRate': True,
+                    'framesPlayedPerSecond': None,
+                    'shouldUseNativeVideo': False}
+                 }
+            ] * 2
+        }
+    }
+    ]
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        os.mkdir('test_tree')
+        # pylint: disable=unspecified-encoding
+        open('test_tree/image1.png', 'w')
+        open('test_tree/image2.jpg', 'w')
+        open('test_tree/texte1.txt', 'w')
+        open('test_tree/video1.mp4', 'w')
+        open('test_tree/video2.mp4', 'w')
+        os.mkdir('test_tree/leaf')
+        open('test_tree/leaf/image3.png', 'w')
+        open('test_tree/leaf/image4.jpg', 'w')
+        open('test_tree/leaf/texte2.txt', 'w')
+
+        for test_case in TEST_CASES:
+            arguments = test_case['files']
+            for k, v in test_case['options'].items():
+                arguments.append('--'+k)
+                arguments.append(v)
+            if test_case.get('flags'):
+                arguments.extend(['--'+flag for flag in test_case['flags']])
+            result = runner.invoke(import_assets, arguments)
+            debug_subprocess_pytest(result)
+            mocked__append_many_to_dataset.assert_called_with(
+                **test_case['expected_mutation_payload'])
+
+
+def test_describe_project(mocker):
+    mocker.patch("kili.client.Kili.__init__", return_value=None)
+    mocker.patch("kili.client.Kili.projects",
+                 side_effect=lambda **_: [{'title': 'project title',
+                                          'id': 'project_id',
+                                           'description': 'description test',
+                                           'numberOfAssets': 49,
+                                           'numberOfRemainingAssets': 29,
+                                           'numberOfReviewedAssets': 0,
+                                           'numberOfAssetsWithSkippedLabels': 0,
+                                           'numberOfOpenIssues': 3,
+                                           'numberOfSolvedIssues': 2,
+                                           'numberOfOpenQuestions': 0,
+                                           'numberOfSolvedQuestions': 2,
+                                           'honeypotMark': None,
+                                           'consensusMark': None}])
+    runner = CliRunner()
+    result = runner.invoke(describe_project, ['--project-id', "project_id"])
+    debug_subprocess_pytest(result)
+    assert (result.output.count('40.8%') == 1) and (
+        result.output.count('N/A') == 2) and (
+        result.output.count('49') == 1) and (
+        result.output.count('project title') == 1
+    )
