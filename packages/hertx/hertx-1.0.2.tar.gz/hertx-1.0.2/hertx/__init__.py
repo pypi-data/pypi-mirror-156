b65710f9ca415071c9589cd6d7c25dd8d9489c50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:26 +0000

    Add package hertx/1.0.2/hertx-1.0.2.tar.gz

---
+++ packages/hertx/hertx-1.0.2.tar.gz/hertx-1.0.2/hertx/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/12/14 8:29
+# @Author  : hertx
+# @Software: PyCharm
+# @File    : hertx.py
+from .httpx import http
+from .httpx import Http
+from .httpx import func_run_times
+from .httpx import random_chrome_version
+
+__version__ = "1.0.2"
