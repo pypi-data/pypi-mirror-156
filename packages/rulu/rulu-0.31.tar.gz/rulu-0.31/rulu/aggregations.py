55a2482d4f972a82a5043ea662db4def932fd266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:55:02 +0000

    Add package rulu/0.31/rulu-0.31.tar.gz

---
+++ packages/rulu/rulu-0.31.tar.gz/rulu-0.31/rulu/aggregations.py
@@ -0,0 +1,66 @@
+"""
+This module implements aggregators, in which facts are collected during 
+the rule engine execution and then aggregated in Python code afterwards. 
+"""
+
+
+from collections import defaultdict
+from munch import Munch
+
+from .expr import FieldExpr
+from .fact import FactReference
+from .utils import RuleEngineError
+
+
+class Aggregator:
+    def __init__(self, engine, template):
+        self.engine = engine
+        self.template = template
+        self.finalized = False
+        
+    def init(self):
+        """ Runs before rule engine execution """
+        pass
+    
+    def finalize(self, assert_):
+        """ Runs after rule engine execution """
+        self.finalized = True
+    
+    def process_one(self, **kwargs):
+        """ Runs on each fact to be aggregated """
+        raise NotImplementedError
+
+
+def grouping_aggregator(keys, func):
+    """ 
+    'Groupby' aggregator. Facts are collected to lists grouped by
+    (zero or more) keys and then each group is processed separately
+    by the given 'func'
+    """ 
+    keys = tuple(_make_key(key) for key in keys)
+    class GroupAggregator(Aggregator):
+        def init(self): 
+            self.data = defaultdict(lambda : [])
+            
+        def process_one(self, **kwargs):
+            if self.finalized:
+                raise RuleEngineError('Aggregation failed')
+            key = tuple(key(**kwargs) for key in keys)
+            value = list(kwargs.values())[0] if len(kwargs) == 1 else Munch(kwargs)
+            self.data[key].append(value)
+            
+        def finalize(self, assert_):
+            super(GroupAggregator, self).finalize(assert_)
+            for key, group in self.data.items():
+                func(group, assert_, *key)
+        
+    return GroupAggregator
+
+
+def _make_key(key):
+    if isinstance(key, FieldExpr):
+        return lambda **kwargs : getattr(_make_key(key.container)(**kwargs), key.name)
+    elif isinstance(key, FactReference):
+        return lambda **kwargs : key._from_python_params(kwargs)
+    else:
+        raise TypeError('Invalid group key: {}'.format(type(key)))
---
+++ packages/rulu/rulu-0.31.tar.gz/rulu-0.31/rulu/typedefs.py
@@ -0,0 +1,129 @@
+"""
+This module defines the possible types in the rule engine, and provides 
+conversions from and to Python types.
+"""
+
+from calendar import timegm
+from collections.abc import Iterable
+from datetime import datetime
+import clips
+
+
+class RuleEngineType(object):
+    """
+    Base class for a possible type
+    """
+    PYTHON_TYPE = None
+    PYTHON_TYPE_CHECK = None
+    CLIPS_TYPE = None
+    DEFAULT = None
+
+    @classmethod
+    def _isinstance(cls, x):
+        """ Check if argument is of the current type """
+        return isinstance(x, cls.PYTHON_TYPE_CHECK)
+    
+    @classmethod
+    def _from_clips_value(cls, x):
+        """ Convert from CLIPS value to Python value """
+        return cls.DEFAULT if x is None else cls.PYTHON_TYPE(x)
+    
+    @classmethod
+    def _to_clips_value(cls, x):
+        """ Convert from Python value to CLIPS value """
+        return x
+
+
+class Integer(RuleEngineType):
+    PYTHON_TYPE = int
+    PYTHON_TYPE_CHECK = int
+    CLIPS_TYPE = 'INTEGER'
+    DEFAULT = 0
+
+
+class Boolean(Integer):
+    PYTHON_TYPE = bool
+    PYTHON_TYPE_CHECK = bool
+    CLIPS_TYPE = 'SYMBOL'
+    TRUE = clips.Symbol('TRUE')
+    FALSE = clips.Symbol('FALSE')
+    DEFAULT = FALSE
+
+    @classmethod
+    def _to_clips_value(cls, x):
+        if isinstance(x, clips.Symbol):
+            return x
+        elif x:
+            return cls.TRUE
+        else:
+            return cls.FALSE
+    
+    @classmethod
+    def _from_clips_value(cls, x):
+        return str(x) == 'TRUE'
+
+
+class Number(RuleEngineType):
+    PYTHON_TYPE = float
+    PYTHON_TYPE_CHECK = (int, float)
+    CLIPS_TYPE = 'NUMBER'
+    DEFAULT = 0.0
+
+
+class String(RuleEngineType):
+    PYTHON_TYPE = str
+    PYTHON_TYPE_CHECK = str
+    CLIPS_TYPE = 'STRING'
+    DEFAULT = ''
+
+
+class Unicode(String):
+    pass
+
+
+class Symbol(String):
+    CLIPS_TYPE = 'SYMBOL'
+    DEFAULT = ''
+
+
+class DateTime(RuleEngineType):
+    """
+    Date-time object. 
+    There is no date object in CLIPS, we use an integer representation to
+    allow for comparisons and arithmetic.
+    """
+    PYTHON_TYPE = datetime
+    PYTHON_TYPE_CHECK = datetime
+    CLIPS_TYPE = 'INTEGER'
+    DEFAULT = datetime.fromtimestamp(0)
+    
+    @classmethod
+    def _to_clips_value(cls, x):
+        return timegm(x.utctimetuple())
+
+    @classmethod
+    def _from_clips_value(cls, x):
+        return datetime.utcfromtimestamp(x)
+    
+
+class Multifield(RuleEngineType):
+    """
+    Multifield type. Maps to Python list.
+    """
+    PYTHON_TYPE = list
+    PYTHON_TYPE_CHECK = Iterable
+    CLIPS_TYPE = 'MULTIFIELD'
+    DEFAULT = ()
+
+    @classmethod
+    def _from_clips_value(cls, x):
+        if isinstance(x, str) or not isinstance(x, Iterable):
+            return x,
+        else:
+            return tuple(x)
+
+
+class FactIndexType(RuleEngineType): pass
+
+
+TYPE_MAP = {t.PYTHON_TYPE: t for t in (Integer, Number, String, Boolean, DateTime)}
