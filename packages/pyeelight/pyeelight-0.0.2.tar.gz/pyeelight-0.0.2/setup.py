fa4027836fbacce981bc6192b785e50e91ee1c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:37 +0000

    Add package pyeelight/0.0.2/pyeelight-0.0.2.tar.gz

---
+++ packages/pyeelight/pyeelight-0.0.2.tar.gz/pyeelight-0.0.2/setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='pyeelight',
+    version='0.0.2',
+    license='MIT',
+    author="Mohamed El Yousfi",
+    author_email='pip@mootje.be',
+    packages=find_packages(),
+    url='https://github.com/Mohagames205/pyeelight-api',
+    keywords=["yeelight", "api", "smart home"],
+
+)
