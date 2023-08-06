c7c24a8140b297a7e3dd7b87ee1d8f4775dbbd38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:08 +0000

    Add package qtneat/0.0.3/qtneat-0.0.3.tar.gz

---
+++ packages/qtneat/qtneat-0.0.3.tar.gz/qtneat-0.0.3/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name='qtneat',
+    version='0.0.3',
+    author='Jung Gyu Yoon',
+    author_email='yjg30737@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    description='Set the Qt frameless titlebar/theme easily',
+    url='https://github.com/yjg30737/qtneat.git',
+    long_description_content_type='text/markdown',
+    long_description=long_description,
+    install_requires=[
+        'pyqt-custom-titlebar-setter>=0.0.1',
+        'qtsasstheme>=0.0.1'
+        'qtpy>=0.0.1'
+    ]
+)
