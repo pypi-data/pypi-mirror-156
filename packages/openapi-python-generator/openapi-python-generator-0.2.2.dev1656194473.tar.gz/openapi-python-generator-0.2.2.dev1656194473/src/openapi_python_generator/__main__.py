6df4dfb5ac8ccff03f6691b832d6d7fa292c82f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:09 +0000

    Add package openapi-python-generator/0.2.2.dev1656194473/openapi-python-generator-0.2.2.dev1656194473.tar.gz

---
+++ packages/openapi-python-generator/openapi-python-generator-0.2.2.dev1656194473.tar.gz/openapi-python-generator-0.2.2.dev1656194473/src/openapi_python_generator/__main__.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+import typer
+
+from openapi_python_generator.common import HTTPLibrary
+from openapi_python_generator.generate_data import generate_data
+
+app = typer.Typer()
+
+
+@app.command()
+def main(
+    file_name: str, output: str, library: Optional[HTTPLibrary] = HTTPLibrary.httpx
+) -> None:
+    """
+    Generate Python code from an OpenAPI 3.0 specification.
+    """
+    generate_data(file_name, output, library)
+
+
+if __name__ == "__main__":  # pragma: no cover
+    app()
