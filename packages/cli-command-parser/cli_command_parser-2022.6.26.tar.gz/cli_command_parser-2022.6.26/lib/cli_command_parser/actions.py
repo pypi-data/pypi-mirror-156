4b2fbae2714428c02d4f5879ed81d069cfd46c1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:35 +0000

    Add package cli-command-parser/2022.6.26/cli_command_parser-2022.6.26.tar.gz

---
+++ packages/cli-command-parser/cli_command_parser-2022.6.26.tar.gz/cli_command_parser-2022.6.26/lib/cli_command_parser/actions.py
@@ -0,0 +1,16 @@
+"""
+Common actions.
+
+:author: Doug Skrypa
+"""
+
+from .exceptions import ParserExit
+from .parameters.options import action_flag
+
+
+@action_flag('--help', '-h', order=float('-inf'), always_available=True, help='Show this help message and exit')
+def help_action(self):
+    """The ``--help`` / ``-h`` action.  Prints help text, then exits."""
+    cls = self.__class__
+    print(cls.__class__.params(cls).formatter.format_help())
+    raise ParserExit
