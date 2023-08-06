365b039375b9674235833bd72af7e3d47d2cc09a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:32:27 +0000

    Add package shared/0.0.15/shared-0.0.15-py3-none-any.whl

---
+++ packages/shared/shared-0.0.15-py3-none-any.whl/shared/dto/__init__.py
@@ -0,0 +1,10 @@
+"""Data Transfer Object"""
+from collections import namedtuple
+
+Target = namedtuple("Target", ["name", "directory", "pathname", "tempdir"])
+
+DossierEntryInfo = namedtuple("DossierEntryInfo", ["name", "container", "filename"])
+
+ColumnInfo = namedtuple("ColumnInfo", ["index", "name", "type", "not_null", "default", "primary_key"])
+
+QueryResult = namedtuple("QueryResult", ["columns", "data"])
