2e6e0789bd7cf3f1b9876aa099ac9db91a0276be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:37 +0000

    Add package forge-tailwind/0.2.0/forge-tailwind-0.2.0.tar.gz

---
+++ packages/forge-tailwind/forge-tailwind-0.2.0.tar.gz/forge-tailwind-0.2.0/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['forgetailwind', 'forgetailwind.management.commands']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.0,<9.0.0', 'forge-core>=0.2.0,<0.3.0', 'requests>=2.28.0,<3.0.0']
+
+entry_points = \
+{'console_scripts': ['forge-tailwind = forgetailwind:cli']}
+
+setup_kwargs = {
+    'name': 'forge-tailwind',
+    'version': '0.2.0',
+    'description': 'Work library for Forge',
+    'long_description': '# forge-tailwind\n',
+    'author': 'Dave Gaeddert',
+    'author_email': 'dave.gaeddert@dropseed.dev',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://www.forgepackages.com/',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
