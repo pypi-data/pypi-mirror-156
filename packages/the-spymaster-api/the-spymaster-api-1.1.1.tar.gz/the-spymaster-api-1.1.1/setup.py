afdaa78629e8deecd9a27ea1ee3ee4df7b6fa4d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:32 +0000

    Add package the-spymaster-api/1.1.1/the-spymaster-api-1.1.1.tar.gz

---
+++ packages/the-spymaster-api/the-spymaster-api-1.1.1.tar.gz/the-spymaster-api-1.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name="the-spymaster-api",
+    version="1.1.1",
+    description="Python client implementation for The Spymaster HTTP backend.",
+    author="Asaf Kali",
+    author_email="akali93@gmail.com",
+    url="https://github.com/asaf-kali/the-spymaster-backend",
+    packages=["the_spymaster_api", "the_spymaster_api.structs"],
+    install_requires=[
+        "codenames~=1.1",
+        "the_spymaster_util~=1.10",
+        "the_spymaster_solvers_client~=1.0",
+        "pydantic~=1.9",
+        "requests~=2.28",
+    ],
+    include_package_data=True,
+)
