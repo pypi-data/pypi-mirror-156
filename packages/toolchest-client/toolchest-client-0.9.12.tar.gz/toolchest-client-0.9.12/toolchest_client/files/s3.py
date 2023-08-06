e73c292cbcd12b4fe168d6d2943a1acbf46206cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:16 +0000

    Add package toolchest-client/0.9.12/toolchest-client-0.9.12.tar.gz

---
+++ packages/toolchest-client/toolchest-client-0.9.12.tar.gz/toolchest-client-0.9.12/toolchest_client/files/s3.py
@@ -0,0 +1,101 @@
+"""
+toolchest_client.files.s3
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+Functions for handling files in AWS S3 buckets.
+"""
+import sys
+
+import requests
+from requests.exceptions import HTTPError
+
+from toolchest_client.api.auth import get_headers
+from toolchest_client.api.exceptions import ToolchestS3AccessError
+from toolchest_client.api.urls import get_s3_metadata_url
+
+
+def assert_accessible_s3(uri):
+    """Raises an error if the given S3 URI is not accessible by a worker node.
+
+    :param uri: An S3 URI.
+    """
+    try:
+        get_s3_file_size(uri)
+    except ToolchestS3AccessError as err:
+        raise err from None
+
+
+def get_s3_file_size(uri):
+    """Returns the size (in bytes) of a file in S3 that is accessible
+    from the worker node.
+
+    :param uri: An S3 URI.
+    """
+    params = get_params_from_s3_uri(uri)
+    response = requests.post(
+        get_s3_metadata_url(),
+        headers=get_headers(),
+        json=params,
+    )
+    try:
+        response.raise_for_status()
+    except HTTPError:
+        error_message = "Given S3 input cannot be accessed by Toolchest."
+        print(error_message, file=sys.stderr)
+        raise ToolchestS3AccessError(error_message) from None
+
+    return response.json()["file_size"]
+
+
+def get_params_from_s3_uri(uri):
+    """Gets the bucket name, key name/path, and ARN of an S3 object from an S3 URI.
+    Returns this info in a dict.
+
+    .. note: Assumes S3 bucket is located in a generic AWS region,
+      as opposed to China (`aws-cn`) or GovCloud (`aws-us-gov`).
+
+    :param uri: An S3 URI.
+    """
+
+    # Index of S3 bucket name and initial key directory in the URI (when split by slashes).
+    S3_BUCKET_INDEX = 2
+    S3_KEY_INITIAL_INDEX = S3_BUCKET_INDEX + 1
+    uri_split = uri.split(sep="/")
+
+    arn = "arn:aws:s3:::" + "/".join(uri_split[S3_BUCKET_INDEX:])
+    bucket = uri_split[S3_BUCKET_INDEX]
+    key_initial = uri_split[S3_KEY_INITIAL_INDEX]
+    key_final = uri_split[-1]
+    key = "/".join(uri_split[S3_KEY_INITIAL_INDEX:])
+
+    params = {
+        "arn": arn,
+        "bucket": bucket,
+        "key": key,
+        "key_initial": key_initial,
+        "key_final": key_final,
+    }
+
+    return params
+
+
+def path_is_s3_uri(path):
+    """Returns whether the given path is an S3 URI.
+
+    :param path: An input path.
+    """
+    # Note: this is just a prefix check and does not validate whether
+    #       a file exists at the given URI.
+    S3_PREFIX = "s3://"
+    return path and path.startswith(S3_PREFIX)
+
+
+def inputs_are_in_s3(input_paths):
+    """Returns a list of booleans describing which of the input files are S3 URIs.
+
+    :param input_paths: An input path or list of input paths (strings).
+    """
+    if isinstance(input_paths, str):
+        input_paths = [input_paths]
+
+    return [path_is_s3_uri(file_path) for file_path in input_paths]
---
+++ packages/toolchest-client/toolchest-client-0.9.12.tar.gz/toolchest-client-0.9.12/toolchest_client/tools/star.py
@@ -0,0 +1,49 @@
+"""
+toolchest_client.tools.STAR
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+This is the STAR implementation of the Tool class.
+
+Note: This tool is named STARInstance to differentiate it from
+the STAR function called by the user, which is given in all caps
+to be in line with the command-line argument.
+"""
+from . import Tool
+from toolchest_client.files import merge_sam_files, OutputType
+
+
+class STARInstance(Tool):
+    """
+    The STAR implementation of the Tool class.
+    """
+    def __init__(self, tool_args, output_name, inputs, input_prefix_mapping,
+                 output_path, database_name, database_version, parallelize, **kwargs):
+        super().__init__(
+            tool_name="STAR",
+            tool_version="2.7.9a",
+            tool_args=tool_args,
+            output_name=output_name,
+            output_path=output_path,
+            inputs=inputs,
+            input_prefix_mapping=input_prefix_mapping,
+            min_inputs=1,
+            max_inputs=2,
+            database_name=database_name,
+            database_version=database_version,
+            parallel_enabled=False,  # True if parallelize else False
+            max_input_bytes_per_file=128 * 1024 * 1024 * 1024,
+            max_input_bytes_per_file_parallel=4.5 * 1024 * 1024 * 1024,
+            output_type=OutputType.SAM_FILE if parallelize else OutputType.GZ_TAR,
+            output_is_directory=not parallelize,
+            output_names=[
+                "Aligned.out.sam",
+                "Log.final.out",
+                "Log.out",
+                "Log.progress.out",
+                "SJ.out.tab",
+            ],
+            **kwargs,
+        )
+
+    def _merge_outputs(self, output_file_paths):
+        merge_sam_files(output_file_paths, self.output_path)
