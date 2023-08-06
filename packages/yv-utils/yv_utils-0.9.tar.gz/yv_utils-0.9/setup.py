eebb0365331cd64edf3e199100762f6761659125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:42 +0000

    Add package yv-utils/0.9/yv_utils-0.9.tar.gz

---
+++ packages/yv-utils/yv_utils-0.9.tar.gz/yv_utils-0.9/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='yv_utils',
+    version='0.9',
+    license='MIT',
+    author="Yerram Varun",
+    author_email='yerram.varun@gmail.com',
+    packages=find_packages('src', exclude=['.git', '.gitignore']),
+    package_dir={'': 'src'},
+    url='https://github.com/Varun221/yv_utils',
+    keywords='utils package',
+    install_requires=[
+          'tqdm',
+          'pandas'
+      ],
+
+)
