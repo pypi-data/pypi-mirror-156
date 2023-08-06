9de596cdbc8ffeabe338ad2b1cd270ea0ad4c8af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:44 +0000

    Add package AvWeather/1/AvWeather-1.tar.gz

---
+++ packages/AvWeather/AvWeather-1.tar.gz/AvWeather-1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+import setuptools
+
+setup(
+  name = 'AvWeather',         # How you named your package folder (MyLib)
+  packages = ['AvWeather'],   # Chose the same as "name"
+  version = 'V1',      # Start with a small number and increase it with every change you make
+  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Aviation METAR and TAF Reports',   # Give a short description about your library
+  author = 'Siddharth Tewari',                   # Type in your name
+  url = 'https://github.com/siddharth-tewari/AvWeather',   # Provide either the link to your github or to your website
+  download_url = 'https://github.com/siddharth-tewari/AvWeather/archive/refs/tags/0.1.tar.gz',    # I explain this later on
+  keywords = ['Aviation', 'Aviation Weather', 'Metar', 'TAF'],   # Keywords that define your package best
+)
