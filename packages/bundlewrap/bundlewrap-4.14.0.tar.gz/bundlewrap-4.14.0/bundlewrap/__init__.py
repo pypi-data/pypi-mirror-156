2443cf5ea3094392f2107f611621d1a6f0ebe285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:34 +0000

    Add package bundlewrap/4.14.0/bundlewrap-4.14.0.tar.gz

---
+++ packages/bundlewrap/bundlewrap-4.14.0.tar.gz/bundlewrap-4.14.0/bundlewrap/__init__.py
@@ -0,0 +1,2 @@
+VERSION = (4, 14, 0)
+VERSION_STRING = ".".join([str(v) for v in VERSION])
---
+++ packages/bundlewrap/bundlewrap-4.14.0.tar.gz/bundlewrap-4.14.0/bundlewrap/cmdline/groups.py
@@ -0,0 +1,34 @@
+from ..group import GROUP_ATTR_DEFAULTS
+from ..utils.text import bold, mark_for_translation as _
+from ..utils.ui import io
+from .nodes import attribute_table, attrs_for_entities
+
+
+GROUP_ATTRS = sorted(list(GROUP_ATTR_DEFAULTS) + ['nodes', 'node_count'])
+
+
+def bw_groups(repo, args):
+    if not args['groups']:
+        for group in sorted(repo.groups):
+            io.stdout(group.name)
+    else:
+        groups = {repo.get_group(group.strip()) for group in args['groups']}
+        if not args['attrs']:
+            subgroups = groups.copy()
+            for group in groups:
+                subgroups.update(group.subgroups)
+            for subgroup in sorted(subgroups):
+                io.stdout(subgroup.name)
+        else:
+            results = attrs_for_entities(
+                groups,
+                args['attrs'],
+                1,  # groups don't have dynamic attrs like nodes
+            )
+            attribute_table(
+                results,
+                bold(_("group")),
+                args['attrs'],
+                GROUP_ATTRS,
+                args['inline'],
+            )
