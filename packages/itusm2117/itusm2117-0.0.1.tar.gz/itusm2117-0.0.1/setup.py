cd4ab0ac182fa28c8acbe914008cbae038948fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:02 +0000

    Add package itusm2117/0.0.1/itusm2117-0.0.1.tar.gz

---
+++ packages/itusm2117/itusm2117-0.0.1.tar.gz/itusm2117-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(   
+    install_requires=[
+        'numpy>=1.19.5', 
+        'h5py>=3.1.0',
+        'Cerberus>=1.3.4',
+        'python_dateutil>=2.8.2',
+    ]
+)
