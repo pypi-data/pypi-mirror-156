a942b42d84bb53b5ba1ac28edc1b8c3488dd7b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:30 +0000

    Add package btmod/1.0.2/btmod-1.0.2.tar.gz

---
+++ packages/btmod/btmod-1.0.2.tar.gz/btmod-1.0.2/btm/collections.py
@@ -0,0 +1,12 @@
+
+"""
+Collections of useful modifications for the python built-in types.
+for now this no have any modificator for the python built-in types,
+you can suggest new modificators for future releases
+in the issues section of the GitHub page of this project (see btm.URL).
+"""
+
+import btm
+
+__all__ = []
+
