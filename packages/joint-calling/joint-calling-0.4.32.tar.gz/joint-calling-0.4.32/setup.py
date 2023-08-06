01d74ab2187bcbf972f8416a87cf2440a3d3037a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:18 +0000

    Add package joint-calling/0.4.32/joint-calling-0.4.32.tar.gz

---
+++ packages/joint-calling/joint-calling-0.4.32.tar.gz/joint-calling-0.4.32/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+from os.path import join
+import setuptools
+
+
+setuptools.setup(
+    name='joint-calling',
+    version='0.4.32',
+    description='Pipeline for joint calling, sample and variant QC for WGS germline '
+    'variant calling data',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/populationgenomics/joint-calling',
+    license='MIT',
+    packages=['joint_calling'],
+    package_data={'joint_calling': ['filter_cutoffs.yaml']},
+    include_package_data=True,
+    zip_safe=False,
+    scripts=[join('scripts', fp) for fp in os.listdir('scripts') if fp.endswith('.py')],
+    keywords='bioinformatics',
+    install_requires=[
+        'cpg-utils',
+        'pandas',
+        'hail>=0.2.91',
+        'click',
+        'google-cloud-storage',
+        'google-cloud-secret-manager',
+        'gnomad',
+        'analysis-runner',
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+    ],
+)
