33410003b229c84d0195459c8e4d778452e23726
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:31:54 +0000

    Add package odd-collector-sdk/0.2.6/odd-collector-sdk-0.2.6.tar.gz

---
+++ packages/odd-collector-sdk/odd-collector-sdk-0.2.6.tar.gz/odd-collector-sdk-0.2.6/odd_collector_sdk/domain/collector_config_loader.py
@@ -0,0 +1,25 @@
+import logging
+import pydantic
+
+from typing import List
+
+from .collector_config import CollectorConfig
+from pyaml_env import parse_config
+
+
+class CollectorConfigLoader:
+    def __init__(self, config_path: str, plugins_union_type) -> None:
+        self.union = plugins_union_type
+        self.path = config_path
+
+    def load(self) -> CollectorConfig:
+        try:
+            parsed = parse_config(self.path)
+            model = pydantic.create_model(
+                "DynamicModel",
+                __base__=CollectorConfig,
+                plugins=(List[self.union], ...),
+            )
+            return model.parse_obj(parsed)
+        except Exception as e:
+            logging.error(e)
