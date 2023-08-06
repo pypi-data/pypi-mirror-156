05bf8bfb2990b3a292d78c6d73b925ab07ec80ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:26 +0000

    Add package funfunc/0.0.4/funfunc-0.0.4-py3-none-any.whl

---
+++ packages/funfunc/funfunc-0.0.4-py3-none-any.whl/funfunc/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# AUTHOR  : wjia
+# TIME    : 2022/6/21 16:02
+# FILE    : __init__
+# PROJECT : funfunc
+# IDE     : PyCharm
+__VERSION__ = "0.0.4"
+__AUTHOR__ = "Wei Jia"
+__DESCRIPTION__ = "Make Py Development Much Easier & Fun"
+__all__ = ['cv', 'dl', 'ml', 'nlp', 'tool', 'tornado_helper']
