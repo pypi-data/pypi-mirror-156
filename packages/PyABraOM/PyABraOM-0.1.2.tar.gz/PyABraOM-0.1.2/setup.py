d76e9f0780c4651703288f43cb7135aeefc66298
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:29 +0000

    Add package PyABraOM/0.1.2/PyABraOM-0.1.2.tar.gz

---
+++ packages/PyABraOM/PyABraOM-0.1.2.tar.gz/PyABraOM-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+setup(
+  name= "PyABraOM",
+  packages = ['pyabraom'],
+  version = '0.1.2',
+  description= "A Python API to communicate with ABraOM database",
+  author ="Paola Carneiro and Felipe Colombeli",
+  author_email= "bioinfo@hcpa.edu.br",
+  url="https://github.com/bioinfo-hcpa/pyABraOM",
+  license='GNU General Public License v3.0',
+  install_requires=["requests>=2.23.0",
+                    "pandas>=1.1.5",
+                    "times"
+                    ],
+  py_modules=["Search_gene",
+              "Search_Region",
+              "Variant_ID", 
+              'Searches'],
+    classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
