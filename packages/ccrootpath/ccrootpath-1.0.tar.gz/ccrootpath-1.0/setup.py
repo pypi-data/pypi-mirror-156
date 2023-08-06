788ad52ad22feab4913118cf4d7d9a7047d53870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:57 +0000

    Add package ccrootpath/1.0/ccrootpath-1.0.tar.gz

---
+++ packages/ccrootpath/ccrootpath-1.0.tar.gz/ccrootpath-1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='ccrootpath',
+      version='1.0',
+      description='Set project root path for importing local modules',
+      url='https://github.com/OpenFibers/CCRootPath',
+      author='OpenFibers',
+      author_email='openfibers@gmail.com',
+      license='MIT',
+      packages=['ccrootpath'],
+      zip_safe=False)
