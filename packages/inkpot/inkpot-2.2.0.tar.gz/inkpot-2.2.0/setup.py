c7fa37321ad94a0637ba188d25b1e2233a27d25c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:58 +0000

    Add package inkpot/2.2.0/inkpot-2.2.0.tar.gz

---
+++ packages/inkpot/inkpot-2.2.0.tar.gz/inkpot-2.2.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='inkpot',
+    version='2.2.0',
+    description='a small simple library for generating documentation from docstrings',
+    url='https://github.com/AxelGard/inkpot',
+    author='Axel Gard',
+    author_email='axel.gard@tutanota.com',
+    license='MIT',
+    packages=['inkpot'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    install_requires=["astunparse"],
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Documentation",
+        "Topic :: Software Development :: Documentation",
+        "License :: OSI Approved :: MIT License",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
