7f6730cd5ce6b8e7b1f1fe0152c7b6b7f981d1d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:16:55 +0000

    Add package fidesctl/1.7.0/fidesctl-1.7.0.tar.gz

---
+++ packages/fidesctl/fidesctl-1.7.0.tar.gz/fidesctl-1.7.0/src/fidesapi/main.py
@@ -0,0 +1,200 @@
+"""
+Contains the code that sets up the API.
+"""
+
+
+from datetime import datetime
+from enum import Enum
+from logging import WARNING
+from pathlib import Path
+from typing import Callable, Dict
+
+from fastapi import FastAPI, HTTPException, Request, Response, status
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from loguru import logger as log
+from uvicorn import Config, Server
+
+import fidesctl
+from fidesapi import view
+from fidesapi.database import database
+from fidesapi.database.database import get_db_health
+from fidesapi.routes import crud, datamap, generate, validate, visualize
+from fidesapi.routes.util import API_PREFIX, WEBAPP_DIRECTORY, WEBAPP_INDEX
+from fidesapi.utils.errors import get_full_exception_name
+from fidesapi.utils.logger import setup as setup_logging
+from fidesctl.core.config import FidesctlConfig, get_config
+
+app = FastAPI(title="fidesctl")
+CONFIG: FidesctlConfig = get_config()
+
+
+def configure_routes() -> None:
+    "Include all of the routers not defined in this module."
+    routers = crud.routers + visualize.routers
+    for router in routers:
+        log.debug(f'Adding router to fidesctl: {" ".join(router.tags)}')
+        app.include_router(router)
+
+    app.include_router(view.router)
+    app.include_router(generate.router)
+    app.include_router(datamap.router)
+    app.include_router(validate.router)
+
+
+# Configure the routes here so we can generate the openapi json file
+configure_routes()
+
+
+async def configure_db(database_url: str) -> None:
+    "Set up the db to be used by the app."
+    try:
+        database.create_db_if_not_exists(database_url)
+        await database.init_db(database_url)
+    except Exception as error:  # pylint: disable=broad-except
+        error_type = get_full_exception_name(error)
+        log.error(f"Unable to configure database: {error_type}: {error}")
+
+
+@app.on_event("startup")
+async def create_webapp_dir_if_not_exists() -> None:
+    """Creates the webapp directory if it doesn't exist."""
+
+    if not WEBAPP_INDEX.is_file():
+        WEBAPP_DIRECTORY.mkdir(parents=True, exist_ok=True)
+        with open(WEBAPP_DIRECTORY / "index.html", "w") as index_file:
+            index_file.write("<h1>Privacy is a Human Right!</h1>")
+
+    app.mount("/static", StaticFiles(directory=WEBAPP_DIRECTORY), name="static")
+
+
+@app.on_event("startup")
+async def setup_server() -> None:
+    "Run all of the required setup steps for the webserver."
+    setup_logging(
+        CONFIG.api.log_level,
+        serialize=CONFIG.api.log_serialization,
+        desination=CONFIG.api.log_destination,
+    )
+
+    log.bind(api_config=CONFIG.api.json()).debug("Configuration options in use")
+    await configure_db(CONFIG.api.sync_database_url)
+
+
+@app.middleware("http")
+async def log_request(request: Request, call_next: Callable) -> Response:
+    "Log basic information about every request handled by the server."
+    start = datetime.now()
+    response = await call_next(request)
+    handler_time = datetime.now() - start
+    log.bind(
+        method=request.method,
+        status_code=response.status_code,
+        handler_time=f"{handler_time.microseconds * 0.001}ms",
+        path=request.url.path,
+    ).info("Request received")
+    return response
+
+
+@app.get(
+    f"{API_PREFIX}/health",
+    response_model=Dict[str, str],
+    responses={
+        status.HTTP_200_OK: {
+            "content": {
+                "application/json": {
+                    "example": {
+                        "status": "healthy",
+                        "version": "1.0.0",
+                        "database": "healthy",
+                    }
+                }
+            }
+        },
+        status.HTTP_503_SERVICE_UNAVAILABLE: {
+            "content": {
+                "application/json": {
+                    "example": {
+                        "detail": {
+                            "status": "healthy",
+                            "version": "1.0.0",
+                            "database": "unhealthy",
+                        }
+                    }
+                }
+            }
+        },
+    },
+    tags=["Health"],
+)
+async def health() -> Dict:
+    "Confirm that the API is running and healthy."
+    database_health = get_db_health(CONFIG.api.sync_database_url)
+    response = {
+        "status": "healthy",
+        "version": str(fidesctl.__version__),
+        "database": database_health,
+    }
+
+    for key in response:
+        if response[key] == "unhealthy":
+            raise HTTPException(
+                status_code=status.HTTP_503_SERVICE_UNAVAILABLE, detail=response
+            )
+
+    return response
+
+
+class DBActions(str, Enum):
+    "The available path parameters for the `/admin/db/{action}` endpoint."
+    init = "init"
+    reset = "reset"
+
+
+@app.post(API_PREFIX + "/admin/db/{action}", tags=["Admin"])
+async def db_action(action: DBActions) -> Dict:
+    """
+    Initiate one of the enumerated DBActions.
+    """
+    action_text = "initialized"
+    if action == DBActions.reset:
+        database.reset_db(CONFIG.api.sync_database_url)
+        action_text = DBActions.reset
+    await configure_db(CONFIG.api.sync_database_url)
+    return {"data": {"message": f"Fidesctl database {action_text}"}}
+
+
+# Configure the static file paths last since otherwise it will take over all paths
+@app.get("/", tags=["Default"])
+def read_index() -> Response:
+    """
+    Return an index.html at the root path
+    """
+    return FileResponse(WEBAPP_INDEX)
+
+
+@app.get("/{catchall:path}", response_class=FileResponse, tags=["Default"])
+def read_other_paths(request: Request) -> FileResponse:
+    """
+    Return related frontend files. Adapted from https://github.com/tiangolo/fastapi/issues/130
+    """
+    # check first if requested file exists (for frontend assets)
+    path = request.path_params["catchall"]
+    file = WEBAPP_DIRECTORY / Path(path)
+    if file.exists():
+        return FileResponse(file)
+
+    # raise 404 for anything that should be backend endpoint but we can't find it
+    if path.startswith(API_PREFIX[1:]):
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="Item not found"
+        )
+
+    # otherwise return the index
+    return FileResponse(WEBAPP_INDEX)
+
+
+def start_webserver() -> None:
+    "Run the webserver."
+    server = Server(Config(app, host="0.0.0.0", port=8080, log_level=WARNING))
+    server.run()
---
+++ packages/fidesctl/fidesctl-1.7.0.tar.gz/fidesctl-1.7.0/src/fidesapi/routes/generate.py
@@ -0,0 +1,153 @@
+"""
+Contains all of the endpoints required to manage generating resources.
+"""
+from enum import Enum
+from typing import Dict, List, Optional, Union
+
+from fastapi import APIRouter, HTTPException, Response, status
+from fideslang.models import Dataset, Organization, System
+from loguru import logger as log
+from pydantic import BaseModel, root_validator
+
+from fidesapi.routes.crud import get_resource
+from fidesapi.routes.util import (
+    API_PREFIX,
+    route_requires_aws_connector,
+    unobscure_aws_config,
+)
+from fidesapi.sql_models import sql_model_map
+from fidesctl.connectors.models import (
+    AWSConfig,
+    ConnectorAuthFailureException,
+    OktaConfig,
+)
+from fidesctl.core.system import generate_aws_systems
+
+
+class ValidTargets(str, Enum):
+    """
+    Validation of targets attempted to generate resources from
+    """
+
+    AWS = "aws"
+
+
+class GenerateTypes(str, Enum):
+    """
+    Generate Type Enum to capture the discrete possible values
+    for a valid type of resource to generate.
+    """
+
+    SYSTEM = "systems"
+    DATASET = "datasets"
+
+
+class Generate(BaseModel):
+    """
+    Defines attributes for generating resources included in a request.
+    """
+
+    config: Union[AWSConfig, OktaConfig]
+    target: ValidTargets
+    type: GenerateTypes
+
+    @root_validator()
+    @classmethod
+    def target_matches_type(cls, values: Dict) -> Dict:
+        """
+        Ensures that both of the target and type attributes are a valid
+        pair (returning an error on an ('aws', 'dataset') as an example).
+        """
+        target_type = (values.get("target"), values.get("type"))
+        valid_target_types = [("aws", "systems")]
+        if target_type not in valid_target_types:
+            raise ValueError("Target and Type are not a valid match")
+        return values
+
+
+class GenerateRequestPayload(BaseModel):
+    """
+    The model for the request body housing generate information.
+    """
+
+    organization_key: str
+    generate: Generate
+
+
+class GenerateResponse(BaseModel):
+    """
+    The model to house the response for generated infrastructure.
+    """
+
+    generate_results: Optional[List[Union[Dataset, System]]]
+
+
+router = APIRouter(tags=["Generate"], prefix=f"{API_PREFIX}/generate")
+
+
+@router.post(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_model=GenerateResponse,
+    responses={
+        status.HTTP_400_BAD_REQUEST: {
+            "content": {
+                "application/json": {
+                    "example": {"detail": "Unable to generate resources."}
+                }
+            }
+        },
+    },
+)
+async def generate(
+    generate_request_payload: GenerateRequestPayload, response: Response
+) -> GenerateResponse:
+    """
+    A multi-purpose endpoint to generate Fides resources based on existing
+    infrastructure
+
+    Currently generates Fides resources for the following:
+    * AWS: Systems
+
+    In the future, this will include options for other Systems & Datasets,
+    examples include:
+    * Okta: Systems
+    * Snowflake: Datasets
+
+    All config secrets should be encoded as a minor security precaution, using the
+    `obscure_string` function in `fidesapi.routes.util`
+
+    All production deployments should implement HTTPS for security purposes
+    """
+    organization = await get_resource(
+        sql_model_map["organization"], generate_request_payload.organization_key
+    )
+    if generate_request_payload.generate.target.lower() == "aws":
+        generated_systems = generate_aws(
+            aws_config=AWSConfig(**generate_request_payload.generate.config.dict()),
+            organization=organization,
+        )
+    return GenerateResponse(generate_results=generated_systems)
+
+
+@route_requires_aws_connector
+def generate_aws(
+    aws_config: AWSConfig, organization: Organization
+) -> List[Dict[str, str]]:
+    """
+    Returns a list of Systems found in AWS.
+    """
+    log.info("Setting config for AWS")
+    unobscured_config = unobscure_aws_config(aws_config=aws_config)
+    try:
+        log.info("Generating systems from AWS")
+        aws_systems = generate_aws_systems(
+            organization=organization, aws_config=unobscured_config
+        )
+    except ConnectorAuthFailureException as error:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail=str(error),
+        )
+
+    return [i.dict(exclude_none=True) for i in aws_systems]
