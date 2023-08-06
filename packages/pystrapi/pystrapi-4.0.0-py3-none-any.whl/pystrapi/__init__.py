a0fc943f784ab71b54c02e681b5991aaf5c67d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:14 +0000

    Add package pystrapi/4.0.0/pystrapi-4.0.0-py3-none-any.whl

---
+++ packages/pystrapi/pystrapi-4.0.0-py3-none-any.whl/pystrapi/__init__.py
@@ -0,0 +1,4 @@
+from .strapi_client import StrapiClient
+from .strapi_client_sync import StrapiClientSync
+from .helpers import process_response, process_data
+__all__ = ['StrapiClient', 'StrapiClientSync', 'process_response', 'process_data']
---
+++ packages/pystrapi/pystrapi-4.0.0-py3-none-any.whl/pystrapi/strapi_connector_sync.py
@@ -0,0 +1,63 @@
+from abc import abstractmethod
+from typing import Any, Protocol
+
+import requests
+
+from ._utils import getattrs_safe
+from .types import RequestKwargs
+
+
+class StrapiConnectorSync(Protocol):
+    api_url: str
+
+    @abstractmethod
+    def get(self, endpoint: str, *, reqargs: RequestKwargs = None, session: requests.Session = None) -> Any:
+        """Send HTTP GET request and load response text as json"""
+
+    @abstractmethod
+    def post(self, endpoint: str, *, reqargs: RequestKwargs = None, session: requests.Session = None) -> Any:
+        """Send HTTP POST request and load response text as json"""
+
+    @abstractmethod
+    def put(self, endpoint: str, *, reqargs: RequestKwargs = None, session: requests.Session = None) -> Any:
+        """Send HTTP PUT request and load response text as json"""
+
+    @abstractmethod
+    def delete(self, endpoint: str, *, reqargs: RequestKwargs = None, session: requests.Session = None) -> Any:
+        """Send HTTP DELETE request and load response text as json"""
+
+
+class DefaultStrapiConnectorSync(StrapiConnectorSync):
+    """Default strapi connector. Used if no custom connector was given"""
+
+    def __init__(self, api_url: str):
+        self.api_url = api_url
+
+    def _request(
+        self, method: str, endpoint: str, *, reqargs: RequestKwargs = None, session: requests.Session = None
+    ) -> requests.Response:
+        reqargs = reqargs or {}
+        url = self.api_url + endpoint
+        try:
+            if session:
+                response = session.request(method=method, url=url, **reqargs)
+            else:
+                response = requests.request(method=method, url=url, **reqargs)
+        except Exception as e:
+            raise Exception(f"Unable to {method}, error: {e})") from e
+        if not response.ok:
+            status_code, text = getattrs_safe(response, "status_code", "text")
+            raise Exception(f"Unable to {method}, status code: {status_code}, text: {text}")
+        return response
+
+    def get(self, endpoint: str, *, reqargs: RequestKwargs = None, session: requests.Session = None) -> Any:
+        return self._request("GET", endpoint, reqargs=reqargs).json()
+
+    def post(self, endpoint: str, *, reqargs: RequestKwargs = None, session: requests.Session = None) -> Any:
+        return self._request("POST", endpoint, reqargs=reqargs).json()
+
+    def put(self, endpoint: str, *, reqargs: RequestKwargs = None, session: requests.Session = None) -> Any:
+        return self._request("PUT", endpoint, reqargs=reqargs).json()
+
+    def delete(self, endpoint: str, *, reqargs: RequestKwargs = None, session: requests.Session = None) -> Any:
+        return self._request("DELETE", endpoint, reqargs=reqargs).json()
