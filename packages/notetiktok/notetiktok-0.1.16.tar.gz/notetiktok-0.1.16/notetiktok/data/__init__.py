9a3ad31f3758fb828b466b9fcc724a5f5ac2fa57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:15 +0000

    Add package notetiktok/0.1.16/notetiktok-0.1.16.tar.gz

---
+++ packages/notetiktok/notetiktok-0.1.16.tar.gz/notetiktok-0.1.16/notetiktok/data/__init__.py
@@ -0,0 +1,5 @@
+from .base import AuthorDetail, ResourceDetail, ResourceType, SourceDetail
+from .core import (add_author, add_favorite, add_follow, add_image,
+                   add_resource, add_source, add_video, author_db, favorite_db,
+                   follow_db, resource_db, resource_decrypt, resource_encrypt,
+                   source_db)
