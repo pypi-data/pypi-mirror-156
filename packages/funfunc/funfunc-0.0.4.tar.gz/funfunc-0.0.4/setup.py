efd6edc3b07fffbdc281169c8f52ed3a9a9fd1e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:26 +0000

    Add package funfunc/0.0.4/funfunc-0.0.4.tar.gz

---
+++ packages/funfunc/funfunc-0.0.4.tar.gz/funfunc-0.0.4/setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages, setup
+
+setup(name="funfunc",
+      version="0.0.4",
+      description="Make Py Development Much Easier & Fun",
+      author="Wei Jia",
+      author_email="437160499@163.com",
+      packages=find_packages(),
+      platforms=["all"],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Operating System :: OS Independent',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3']
+      )
