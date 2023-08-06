fb35c48045d282f30a9b803dfdcacca13dd7a92c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:57 +0000

    Add package babel-heureka-code/0.1.1/babel_heureka_code-0.1.1-py2.py3-none-any.whl

---
+++ packages/babel-heureka-code/babel_heureka_code-0.1.1-py2.py3-none-any.whl/Babel_heureka_code/_sprach_config.py
@@ -0,0 +1,35 @@
+import configparser
+from configparser import ConfigParser
+
+from ._babel_basis_sprach_version import BasisBabelSprachVersion
+
+
+class SprachConfig(BasisBabelSprachVersion):
+    """ Verwaltet eine Sprachkonfiguration aus einer Datei """
+    def __init__(self, datei, bereich: str = None):
+        """
+        Verwaltet eine Sprachkonfiguration aus einer Datei
+
+        :param datei: Die zu lesende Datei
+        :param bereich: Der Bereich, in dem die Codes stehen
+        """
+        super(SprachConfig, self).__init__()
+        self._configparser = ConfigParser()
+        self.__datei = datei
+        self._configparser.read(datei, encoding="utf8")
+        self.__bereich = bereich if bereich else "codes"
+
+    def wert_fuer_code(self, code: str) -> str:
+        """ Ermittelt die passende Uebersetzung """
+        try:
+            return self._configparser.get(self.__bereich, code)
+        except configparser.NoOptionError:
+            pass
+        except configparser.NoSectionError:
+            pass
+
+    def __str__(self):
+        """ Darstellung einer Sprachkonfigurationsdatei """
+        return f"<{self.__class__.__name__} {self.__datei}>"
+
+    pass
