026123cac841e7e731d63eff19ff78a6b478004f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:12 +0000

    Add package academylibr/0.0.1/academylibr-0.0.1.tar.gz

---
+++ packages/academylibr/academylibr-0.0.1.tar.gz/academylibr-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name='academylibr',
+    version='0.0.1',
+    description='Academy 4 Summer Course',
+    py_modules=["academylib"],
+    package_dir={'':'src'},
+)
