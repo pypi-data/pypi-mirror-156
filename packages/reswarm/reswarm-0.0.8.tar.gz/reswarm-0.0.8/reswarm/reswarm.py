456ed2ebd32745c219f1ec08949fa068e58aa86f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:54 +0000

    Add package reswarm/0.0.8/reswarm-0.0.8.tar.gz

---
+++ packages/reswarm/reswarm-0.0.8.tar.gz/reswarm-0.0.8/reswarm/reswarm.py
@@ -0,0 +1,34 @@
+import asyncio
+import os
+from autobahn.wamp.types import PublishOptions
+from .crossbar import CrossbarConnection
+
+class Reswarm():
+    def __init__(self, **options):
+        self.quiet = options.get("quiet", False)
+        self.serial_number = os.environ.get('DEVICE_SERIAL_NUMBER', options.get("serial_number"))
+        if self.serial_number == None:
+            raise Exception("serial number is missing")
+
+        self.cb_connection = CrossbarConnection(self.serial_number, self.quiet)
+        self.cb_connection.start()
+
+    def publish(self, topic: str, *args: list):
+        device_name = os.environ.get("DEVICE_NAME")
+
+        session = self.cb_connection.getSession()
+        threadedLoop = self.cb_connection.getEventLoop()
+
+        extra = {
+            "DEVICE_SERIAL_NUMBER": self.serial_number,
+            "options": PublishOptions(acknowledge=True)
+        }
+
+        if device_name:
+            extra["DEVICE_NAME"] = device_name
+
+        async def publishFunc():
+            return await session.publish(topic, *args, **extra)
+
+        concurrentFuture = asyncio.run_coroutine_threadsafe(publishFunc(), threadedLoop)
+        return asyncio.wrap_future(concurrentFuture)
