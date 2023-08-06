d7ac5c9ba4b0a002b8484de7e4ad532d69d9ccb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:32:53 +0000

    Add package toncli/0.0.38/toncli-0.0.38-py3-none-any.whl

---
+++ packages/toncli/toncli-0.0.38-py3-none-any.whl/toncli/modules/utils/check_hash.py
@@ -0,0 +1,36 @@
+import glob
+import hashlib
+import os
+import pathlib
+
+from appdirs import user_config_dir
+
+from toncli.modules.utils.system.conf import project_root
+
+
+def get_dir_hashes(path):
+    ans = []
+    for path, subdirs, files in os.walk(path):
+        for name in list(sorted(files)):
+            fname = os.path.join(path, name)
+            with open(fname, 'rb') as file:
+                data = file.read()
+                _hash = hashlib.md5(data).hexdigest()
+
+                ans.append(_hash)
+    return ans
+
+
+def check_2_libs_actual():
+    global_path, local_path = get_libs_paths()
+
+    global_fift_hashes = get_dir_hashes(os.path.abspath(f"{global_path}/fift-libs"))
+    local_fift_hashes = get_dir_hashes(os.path.abspath(f"{local_path}/fift-libs"))
+    global_func_hashes = get_dir_hashes(os.path.abspath(f"{global_path}/func-libs"))
+    local_func_hashes = get_dir_hashes(os.path.abspath(f"{local_path}/func-libs"))
+
+    return global_fift_hashes == local_fift_hashes and global_func_hashes == local_func_hashes
+
+
+def get_libs_paths():
+    return os.path.abspath(f"{project_root}/lib"), user_config_dir('toncli')
---
+++ packages/toncli/toncli-0.0.38-py3-none-any.whl/toncli/modules/utils/func/commands.py
@@ -0,0 +1,68 @@
+import os
+import subprocess
+import sys
+from typing import Optional, List
+
+import yaml
+from colorama import Fore, Style
+
+from toncli.modules.utils.system.conf import config_folder, executable, getcwd
+from toncli.modules.utils.system.log import logger
+from toncli.modules.utils.system.project import migrate_project_struction
+from toncli.modules.utils.system.project_conf import ProjectConf, TonProjectConfig
+
+bl = Fore.CYAN
+gr = Fore.GREEN
+rs = Style.RESET_ALL
+
+
+def build(project_root: str,
+          cwd: Optional[str] = None,
+          func_args: List[str] = None,
+          contracts: List[TonProjectConfig] = None,
+          use_tests_lib: bool = False) -> Optional[str]:
+    """
+    Build func file(s) and save result fift file to location
+
+    :param contracts: contracts to build
+    :param func_args: add arguments to func
+    :param project_root: Files to build in needed order
+    :param cwd: If you need to change root of running script pass it here
+    :param use_tests_lib: Use stdlib-tests.func
+    :return:
+    """
+    if not contracts:
+        project_config = ProjectConf(project_root)
+        contracts = project_config.contracts
+
+    if not func_args:
+        func_args = []
+
+    output = []
+    for contract in contracts:
+        output.append(
+            build_files(contract.func_files_locations, contract.to_save_location, func_args, cwd,
+                        use_tests_lib=use_tests_lib))
+
+        if len(contract.func_tests_files_locations) and use_tests_lib:
+            output.append(
+                build_files([f"{config_folder}/func-libs/tests-helpers.func", *contract.func_tests_files_locations],
+                            contract.to_save_tests_location, [], cwd,
+                            use_tests_lib=True))
+
+    return "\n".join(list(map(str, output)))
+
+
+def build_files(func_files_locations: List[str], to_save_location: str, func_args: List[str] = None,
+                cwd: Optional[str] = None, use_tests_lib: bool = False):
+    build_command = [os.path.abspath(executable['func']), *func_args, "-o",
+                     os.path.abspath(to_save_location), "-SPA",
+                     os.path.abspath(
+                         f"{config_folder}/func-libs/stdlib.func") if not use_tests_lib else os.path.abspath(
+                         f"{config_folder}/func-libs/stdlib-tests.func"),
+                     *[os.path.abspath(i) for i in func_files_locations]]
+
+    get_output = subprocess.check_output(build_command, cwd=getcwd() if not cwd else os.path.abspath(cwd))
+
+    if get_output:
+        return get_output.decode()
