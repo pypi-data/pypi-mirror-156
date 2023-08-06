2d2cbf40b1337efe5b37365ce7606bffcc5f367f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:24 +0000

    Add package reflekt/0.1.25/reflekt-0.1.25.tar.gz

---
+++ packages/reflekt/reflekt-0.1.25.tar.gz/reflekt-0.1.25/reflekt/logger.py
@@ -0,0 +1,29 @@
+# SPDX-FileCopyrightText: 2022 Gregory Clunies <greg@reflekt-ci.com>
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import sys
+
+from reflekt.project import ReflektProject
+
+if ReflektProject().exists:
+    sink = (
+        str(ReflektProject().project_dir)
+        + "/.reflekt/logs/reflekt_{time:YYYY-MM-DD-HHmmss}.log"
+    )
+else:
+    sink = "./.reflekt/logs/reflekt_{time:YYYY-MM-DD-HHmmss}.log"
+
+
+logger_config = {
+    "handlers": [
+        {
+            "sink": sys.stdout,
+            "format": "{time:HH:mm:ss} {message}",
+        },
+        {
+            "sink": sink,
+            "serialize": True,
+        },
+    ],
+}
