6adb02300ff67a356197dff4a4d4fadf68525cc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:43:16 +0000

    Add package triplerecovery/0.0.7/triplerecovery-0.0.7.tar.gz

---
+++ packages/triplerecovery/triplerecovery-0.0.7.tar.gz/triplerecovery-0.0.7/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name='triplerecovery',
+    version='0.0.7',
+    description='Triple Recovery paper implementation in python',
+    py_modules=['triplerecovery'],
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where="src"),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    url="https://github.com/crackaf/triple-recovery",
+    author="Hunzlah Malik",
+    author_email="hunzlahmalik@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+    install_requires=[
+        "numpy>=1.22.4",
+        "opencv-python>=4.6.0.66",
+    ],
+    extras_require={
+        "test": [
+            "pytest>=7.1.2",
+        ]
+    }
+)
