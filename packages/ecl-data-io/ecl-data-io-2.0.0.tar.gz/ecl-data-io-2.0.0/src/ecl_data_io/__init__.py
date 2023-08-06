e5947c199a60cd4b333c598ee7c1b4b325d007a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:51 +0000

    Add package ecl-data-io/2.0.0/ecl-data-io-2.0.0.tar.gz

---
+++ packages/ecl-data-io/ecl-data-io-2.0.0.tar.gz/ecl-data-io-2.0.0/src/ecl_data_io/__init__.py
@@ -0,0 +1,13 @@
+import ecl_data_io.version
+
+from .format import Format
+from .read import lazy_read, read
+from .types import MESS
+from .write import write
+
+__author__ = "Equinor"
+__email__ = "fg_sib-scout@equinor.com"
+
+__version__ = ecl_data_io.version.version
+
+__all__ = ["read", "lazy_read", "write", "Format", "MESS"]
