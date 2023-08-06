24d70922bde164e0326c8f54dda5743d80cf9471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:22:43 +0000

    Add package quickpython-mvc/0.1.3/quickpython-mvc-0.1.3.tar.gz

---
+++ packages/quickpython-mvc/quickpython-mvc-0.1.3.tar.gz/quickpython-mvc-0.1.3/README.md
@@ -0,0 +1,17 @@
+# quickpython-mvc
+`MVC` `fast` `CRUD` `ORM` `database` `chained`
+
+
+```
+这是一款简单的、方便的、快速的ORM框架。
+
+PS: 让CRUD回归简单 by lo106258
+```
+
+# 开始
+
+# 其他
+```
+你可以关注我更多产品：
+[`quickpython`](https://github.com/quickpython)
+```
---
+++ packages/quickpython-mvc/quickpython-mvc-0.1.3.tar.gz/quickpython-mvc-0.1.3/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="quickpython-mvc",
+    version="0.1.3",
+    keywords=["quickpython-mvc", "quickpython", "quick", "WEB", "database", "MVC"],  # 关键字
+    description="Python rapid development framework .",
+    long_description="Python rapid development framework ",
+    license="MIT Licence",
+
+    url="https://github.com/quickpython",
+    author="lo106258",
+    author_email="lo106258@gmail.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=['./boot.py', './config.py', './README.md', './requirements.txt'],
+    platforms="any",
+    install_requires=["tornado", "pymysql"],
+    extras_require={':python_version <= "3.6"': ['enum34', 'future']}
+)
