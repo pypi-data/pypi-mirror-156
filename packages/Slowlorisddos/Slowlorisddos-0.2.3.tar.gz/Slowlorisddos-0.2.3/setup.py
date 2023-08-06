9af8d472c358e953abc6011dd945f221c330bcad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:07 +0000

    Add package Slowlorisddos/0.2.3/Slowlorisddos-0.2.3.tar.gz

---
+++ packages/Slowlorisddos/Slowlorisddos-0.2.3.tar.gz/Slowlorisddos-0.2.3/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name="Slowlorisddos",
+    py_modules=["slowloris"],
+    entry_points={"console_scripts": ["slowloris=slowloris:main"]},
+    version="0.2.3",
+    description="Low bandwidth DoS tool. Slowloris rewrite in Python.",
+    author="Nahid Alam",
+    author_email="nahidalam1603@gmail.com",
+    url="https://github.com/gkbrk/slowloris",
+    keywords=["dos", "http", "slowloris"],
+    license="MIT",
+)
