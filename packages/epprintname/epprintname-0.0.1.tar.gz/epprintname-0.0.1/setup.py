dd5e9814925fb57ea72e7447ec0d31bd88d6404c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:32:51 +0000

    Add package epprintname/0.0.1/epprintname-0.0.1.tar.gz

---
+++ packages/epprintname/epprintname-0.0.1.tar.gz/epprintname-0.0.1/setup.py
@@ -0,0 +1,18 @@
+#This is setup.py file to set all details of the package here
+
+from setuptools import setup
+
+__project__ = "epprintname"
+__version__ = "0.0.1"
+__description__ = "This is test package to learn distribution on open source"
+__packages__ = ["printName"]
+__author__ = "EP"
+
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+    author = __author__,
+    )
