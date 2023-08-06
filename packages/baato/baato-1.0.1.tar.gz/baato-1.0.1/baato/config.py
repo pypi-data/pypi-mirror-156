a700000eeed3d3c05bf5df5d8ffbe478153ab74e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:03:28 +0000

    Add package baato/1.0.1/baato-1.0.1.tar.gz

---
+++ packages/baato/baato-1.0.1.tar.gz/baato-1.0.1/baato/config.py
@@ -0,0 +1,8 @@
+"""Check the latest version at https://pypi.org/project/baato/"""
+__version__ = "1.0.1"
+__pypi_username__ = "baato"
+__pypi_packagename__ = "baato"
+__github_username__ = "baato"
+__github_reponame__ = "python-client"
+__baato_endpoint__ = "https://api.baato.io/api"
+__baato_version__ = "v1"
