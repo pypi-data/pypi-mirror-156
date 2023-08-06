7c2a89b7962f330fec9388d5cf50b2a66958a4dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:48 +0000

    Add package paf-sapgui-eltrans/1.0.8/paf_sapgui_eltrans-1.0.8-py3-none-any.whl

---
+++ packages/paf-sapgui-eltrans/paf_sapgui_eltrans-1.0.8-py3-none-any.whl/paf_sapgui_eltrans/al11.py
@@ -0,0 +1,7 @@
+﻿from paf_sapgui import field
+
+
+class Table(field.Field):
+    def table(self):
+        self._path = "wnd[0]/usr/cntlGRID1/shellcont/shell"
+        return self
---
+++ packages/paf-sapgui-eltrans/paf_sapgui_eltrans-1.0.8-py3-none-any.whl/paf_sapgui_eltrans/zjvr_rjvnetch.py
@@ -0,0 +1,48 @@
+﻿//namespace AS.Auto.Web.Sap.Gui.Elemente.Vx.Programme
+//{
+//  using OpenQA.Selenium;
+//  using aliasElements = As.It.Ses.Frameworks.Selenium.Sap.Gui.Selection.Elements;
+//  using aliasButtons = As.It.Ses.Frameworks.Selenium.Sap.Gui.Selection.Buttons;
+
+//  public static class Zjvr_Rjvnetch
+//  {
+//    // Schaltflächen
+//    public static class Schaltflaechen { }
+//    public static class Felder
+//    {
+//      public static IWebElement Auftragsnummer_von()
+//      {
+//        return aliasElements.CreateElement("M0:46:::1:34");
+//      }
+//      public static IWebElement Auftragsnummer_bis()
+//      {
+//        return aliasElements.CreateElement("M0:46:::1:59");
+//      }
+//      public static IWebElement Testlauf()
+//      {
+//        return aliasElements.CreateElement("M0:46:::2:0-txt");
+//      }
+//    }
+//    public static class Ablaeufe
+//    {
+//      public static bool Fehler_beheben(string VBELN)
+//      {
+//        Felder.Auftragsnummer_von().Clear();
+//        Felder.Auftragsnummer_bis().Clear();
+
+//        Felder.Auftragsnummer_von().SendKeys(VBELN);
+//        Felder.Auftragsnummer_bis().SendKeys(VBELN);
+
+//        Felder.Testlauf().Click();
+//        aliasButtons.Execute();
+
+//        if (aliasElements.Exists("M0:46:::0:0_l"))
+//        {
+//          return true;
+//        }
+//        return false;
+//      }
+//    }
+
+//  }
+//}
