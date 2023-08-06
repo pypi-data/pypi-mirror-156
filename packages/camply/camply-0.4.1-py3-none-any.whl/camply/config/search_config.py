d936773be0ac83e75a1165de5a9a946b98cd0c36
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:30:37 +0000

    Add package camply/0.4.1/camply-0.4.1-py3-none-any.whl

---
+++ packages/camply/camply-0.4.1-py3-none-any.whl/camply/config/search_config.py
@@ -0,0 +1,15 @@
+"""
+Project Configuration for Yellowstone Variables
+"""
+
+
+class SearchConfig:
+    """
+    File Path Storage Class
+    """
+
+    POLLING_INTERVAL_MINIMUM: int = 5  # 5 MINUTES
+    RECOMMENDED_POLLING_INTERVAL: int = 10  # 10 MINUTES
+    ERROR_MESSAGE: str = "No search days configured. Exiting"
+    MINIMUM_CAMPSITES_FIRST_NOTIFY: int = 5
+    MAXIMUM_NOTIFICATION_BATCH_SIZE: int = 20
