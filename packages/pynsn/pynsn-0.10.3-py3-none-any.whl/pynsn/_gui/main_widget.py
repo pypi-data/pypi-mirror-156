ce3804dec843b18eb7168ab4ee95c5982a02bd70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:44 +0000

    Add package pynsn/0.10.3/pynsn-0.10.3-py3-none-any.whl

---
+++ packages/pynsn/pynsn-0.10.3-py3-none-any.whl/pynsn/_gui/main_widget.py
@@ -0,0 +1,105 @@
+"""
+"""
+from PyQt5.QtGui import QFont
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout, QWidget, QLabel, \
+    QTextBrowser, QScrollBar, QPushButton
+
+from . dialogs import SettingsDialog
+from .misc import heading, LabeledNumberInput, LabeledNumberInputTwoValues, \
+    LabeledInput
+from ..factory import DotArraySpecs
+from .._lib import distributions as distr
+
+
+class MainWidget(QWidget):
+
+    def __init__(self, parent, settings, number, da_specs):
+        assert isinstance(settings, SettingsDialog)
+        super(MainWidget, self).__init__(parent)
+        self.settings = settings
+
+        self.initUI(number, da_specs)
+
+    def initUI(self, number, da_specs):
+        assert isinstance(da_specs, DotArraySpecs)
+        assert isinstance(da_specs.diameter_distr, (distr.Beta, distr.Normal))
+
+        self.btn_generate = QPushButton("Generate new array")
+
+        self.number = LabeledNumberInput("Number", number)
+        self.number2 = LabeledNumberInput("Number 2", 5)
+        self.target_array_radius = LabeledNumberInput("Max radius", da_specs.target_array_radius)
+        self.item_diameter_mean = LabeledNumberInput("Mean diameter", da_specs.diameter_distr.mu)
+        self.item_diameter_std = LabeledNumberInput("Diameter range std", da_specs.diameter_distr.sigma)
+        self.item_diameter_range = LabeledNumberInputTwoValues("Diameter range from",
+                                                               value1=da_specs.diameter_distr.min_max[0],
+                                                               value2=da_specs.diameter_distr.min_max[1])
+
+        self.minimum_gap = LabeledNumberInput("Minimum gap", da_specs.minimum_gap)
+
+        self.dot_colour = LabeledInput("Colour", text=self.settings.default_object_colour,
+                                       case_sensitive=False)
+        self.dot_colour2 = LabeledInput("Colour 2", text="expyriment_orange", case_sensitive=False)
+
+        ctrl = QVBoxLayout()
+        ctrl.addWidget(self.btn_generate)
+        ctrl.addSpacing(10)
+        ctrl.addLayout(self.number.layout())
+        ctrl.addLayout(self.number2.layout())
+
+        ctrl.addWidget(heading("Dot"))
+        ctrl.addLayout(self.item_diameter_mean.layout())
+        ctrl.addLayout(self.item_diameter_range.layout())
+        ctrl.addLayout(self.item_diameter_std.layout())
+        ctrl.addLayout(self.dot_colour.layout())
+        ctrl.addLayout(self.dot_colour2.layout())
+
+        ctrl.addWidget(heading("Array"))
+        ctrl.addLayout(self.target_array_radius.layout())
+        ctrl.addLayout(self.minimum_gap.layout())
+        ctrl.addStretch(1)
+
+        # Add text field
+        fields = QVBoxLayout()
+        self._text_field = QTextBrowser(self)
+        self._text_field.setFont(QFont("Courier New", 8))
+        self._text_field.setReadOnly(True)
+        self._text_field.setWordWrapMode(False)
+        self._text_field.setVerticalScrollBar(QScrollBar())
+        self._text_field.setHorizontalScrollBar(QScrollBar())
+        self.picture_field = QLabel(self)
+        self.resize_fields(400)
+        fields.addWidget(self.picture_field)
+        fields.addWidget(self._text_field)
+
+        hlayout = QHBoxLayout()
+        hlayout.addLayout(ctrl)
+        hlayout.addLayout(fields)
+        self.setLayout(hlayout)
+
+        self.updateUI()
+
+    def updateUI(self):
+        self.dot_colour2.setVisible(self.settings.bicoloured.isChecked())
+        self.number2.setVisible(self.settings.bicoloured.isChecked())
+
+
+    def resize_fields(self, width, text_height=150, minium_text_width=300):
+        self.picture_field.setFixedSize(width, width)
+        if width < minium_text_width:
+            width = minium_text_width
+        self._text_field.setMinimumSize(width, text_height)
+
+    def text_clear(self):
+        self._text_field.clear()
+
+    def text_out(self, txt):
+        self._text_field.append(txt)
+
+    def text_error_feedback(self, error):
+        self.text_clear()
+        self.text_out("ERROR\n=====\n")
+        txt = '<span style=\" color: #ff0000;\">%s</span>' % str(error)
+        self.text_out(txt)
+        self.updateUI()
+
---
+++ packages/pynsn/pynsn-0.10.3-py3-none-any.whl/pynsn/_lib/coordinate2D.py
@@ -0,0 +1,105 @@
+__author__ = 'Oliver Lindemann <lindemann@cognitive-psychology.eu>'
+
+import math
+
+class Coordinate2D(object):
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return "Coordinate2D(xy={})".format(self.xy)
+
+    def __add__(self, other):
+        return Coordinate2D(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Coordinate2D(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other):
+        return Coordinate2D(self.x * other, self.y * other)
+
+    def __div__(self, other):
+        return Coordinate2D(self.x / other if other else self.x,
+                            self.y / other if other else self.y)
+
+    def __iadd__(self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __isub__(self, other):
+        self.x -= other.x
+        self.y -= other.y
+        return self
+
+    def __imul__(self, other):
+        self.x *= other
+        self.y *= other
+        return self
+
+    def __idiv__(self, other):
+        self.x /= other
+        self.y /= other
+        return self
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other):
+        return self.x != other.x or self.y != other.y
+
+    @property
+    def xy(self):
+        return self.x, self.y
+
+    @xy.setter
+    def xy(self, xy_tuple):
+        self.x = xy_tuple[0]
+        self.y = xy_tuple[1]
+
+    @property
+    def polar_radius(self):
+        return math.hypot(self.x, self.y)
+
+    @polar_radius.setter
+    def polar_radius(self, value):
+        self.polar = (value, self.polar_angle)
+
+    @property
+    def polar_angle(self):
+        return math.atan2(self.y, self.x)
+
+    @polar_angle.setter
+    def polar_angle(self, value):
+        self.polar = (self.polar_radius, value)
+
+    @property
+    def polar(self):
+        """polar coordinate (radius, pos_angle) """
+        return self.polar_radius, self.polar_angle
+
+    @polar.setter
+    def polar(self, rad_ang):
+        """polar coordinate (radius, angle) """
+
+        self.x = rad_ang[0] * math.cos(rad_ang[1])
+        self.y = rad_ang[0] * math.sin(rad_ang[1])
+
+
+    def distance(self, d):
+        """Returns Euclidean distance to the another Coordinate. The function
+        does not takes the size of an object into account.
+
+        Parameters
+        ----------
+        d : Coordinate2D
+
+        Returns
+        -------
+        distance : float
+
+        """
+
+        return math.hypot(self.x - d.x, self.y - d.y)
