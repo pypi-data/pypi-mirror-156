ead7d818e92ba598bdbeda57d8f78edbc72b51e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:14 +0000

    Add package django-power-admin/0.1.30/django_power_admin-0.1.30-py3-none-any.whl

---
+++ packages/django-power-admin/django_power_admin-0.1.30-py3-none-any.whl/django_power_admin/__init__.py
@@ -0,0 +1,12 @@
+default_app_config = "django_power_admin.apps.DjangoPowerAdminConfig"
+
+app_requires = [
+    "mptt",
+    "django_static_fontawesome",
+    "django_static_jquery_ui",
+    "django_middleware_global_request",
+]
+
+app_middleware_requires = [
+    "django_middleware_global_request.middleware.GlobalRequestMiddleware",
+]
