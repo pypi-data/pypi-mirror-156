d2f3a8d4f6eac1de8f0df5ee2adce72260425ed4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:21 +0000

    Add package pymatdet/1.0.0/pymatdet-1.0.0.tar.gz

---
+++ packages/pymatdet/pymatdet-1.0.0.tar.gz/pymatdet-1.0.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='pymatdet',
+    version='1.0.0',
+    license='MIT',
+    author="Viren Tanti",
+    author_email='virentanti16@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/Virentanti/MatrixObject',
+    keywords=['matrix','MATRIX','matrix-object','matrix_object','matrixobject','MatrixObject', 'mat', "determinant","det"],
+
+
+)
