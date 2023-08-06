7f35e29158e7764508345aa64fffb7248c3fd5fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:58 +0000

    Add package Gdader/0.0.3/Gdader-0.0.3.tar.gz

---
+++ packages/Gdader/Gdader-0.0.3.tar.gz/Gdader-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name="Gdader",
+    version="0.0.3",
+    description="Must have a helper with you",
+    author="Coder100",
+    author_email="jacobhason86@gmail.com"
+)
