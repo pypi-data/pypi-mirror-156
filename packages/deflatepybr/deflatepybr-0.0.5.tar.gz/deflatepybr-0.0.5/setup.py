15c07107d48c85c1a956a7bbabd82b4c401c9b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:45 +0000

    Add package deflatepybr/0.0.5/deflatepybr-0.0.5.tar.gz

---
+++ packages/deflatepybr/deflatepybr-0.0.5.tar.gz/deflatepybr-0.0.5/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+long_description = open("README.md").read()
+
+project_urls = {"Github": "https://github.com/eugeniothiago/deflatepy"}
+
+setuptools.setup(
+    name="deflatepybr",
+    version="0.0.5",
+    author="Thiago Eugênio",
+    description="A simple package to deflate BR currency values using IPCA's historical yearly and monthly indexes",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls=project_urls,
+    packages=["deflator"],
+    readme="README.md",
+    install_requires=["requests", "pandas"],
+)
