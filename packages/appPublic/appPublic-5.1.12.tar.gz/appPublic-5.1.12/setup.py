ac36741738b9fbe7ad013331dbc5d371568b17e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:30:25 +0000

    Add package appPublic/5.1.12/appPublic-5.1.12.tar.gz

---
+++ packages/appPublic/appPublic-5.1.12.tar.gz/appPublic-5.1.12/setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+from  appPublic.version import __version__
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+# usage:
+# python setup.py bdist_wininst generate a window executable file
+# python setup.py bdist_egg generate a egg file
+# Release information about eway
+
+version = __version__
+name = "appPublic"
+description = "appPublic"
+author = "yumoqing"
+email = "yumoqing@gmail.com"
+
+package_data = {}
+
+setup(
+	name="appPublic",
+	version=version,
+	
+	# uncomment the following lines if you fill them out in release.py
+	description=description,
+	author=author,
+	author_email=email,
+   	platforms='any',
+	install_requires=[
+	],
+	packages=[
+		"appPublic"
+	],
+	package_data=package_data,
+	keywords = [
+	],
+	url="https://github.com/yumoqing/appPublic",
+	long_description=description,
+	long_description_content_type="text/markdown",
+	classifiers = [
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+		'License :: OSI Approved :: MIT License',
+	],
+)
