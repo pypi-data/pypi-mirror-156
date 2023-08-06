8bf8f5afb3ddd0f1ea17559853965ea676d5a33a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:44 +0000

    Add package antidote/1.4.2/antidote-1.4.2-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl

---
+++ packages/antidote/antidote-1.4.2-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl/antidote/core/__init__.py
@@ -0,0 +1,30 @@
+from .annotations import From, FromArg, Get, Inject, Provide
+from .container import Container, DependencyValue, Scope
+from .injection import Arg, DEPENDENCIES_TYPE, inject
+from .provider import does_not_freeze, Provider, StatelessProvider
+from .typing import Dependency, Source
+from .utils import DependencyDebug
+from .wiring import wire, Wiring, WithWiringMixin
+
+__all__ = [
+    "Provide",
+    "Inject",
+    "Get",
+    "From",
+    "FromArg",
+    "Container",
+    "DependencyValue",
+    "Scope",
+    "inject",
+    "Arg",
+    "DEPENDENCIES_TYPE",
+    "does_not_freeze",
+    "Provider",
+    "StatelessProvider",
+    "DependencyDebug",
+    "wire",
+    "Wiring",
+    "WithWiringMixin",
+    "Source",
+    "Dependency",
+]
---
+++ packages/antidote/antidote-1.4.2-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.manylinux_2_24_x86_64.whl/antidote/core/utils.py
@@ -0,0 +1,57 @@
+from dataclasses import dataclass
+from typing import Optional, Sequence
+
+from typing_extensions import final
+
+from .container import Scope
+from .._internal import API
+from .._internal.utils import FinalImmutable
+
+
+@API.private
+@dataclass
+class DebugInfoPrefix:
+    prefix: str
+    dependency: object
+
+
+@API.public
+@final
+class DependencyDebug(FinalImmutable):
+    """
+    Debug information on a dependency. Used by :py:mod:`.world.debug` to provide runtime
+    information for debugging.
+    """
+
+    __slots__ = ("info", "scope", "wired", "dependencies")
+    info: str
+    scope: Optional[Scope]
+    wired: Sequence[object]
+    dependencies: Sequence[object]
+
+    def __init__(
+        self,
+        __info: str,
+        *,
+        scope: Optional[Scope] = None,
+        wired: Sequence[object] = tuple(),
+        dependencies: Sequence[object] = tuple(),
+    ):
+        """
+        Args:
+            __info: Short and concise information on the dependency, just enough to
+                identify clearly which one it is.
+            scope: Scope of the dependency.
+            wired: Every class or function that may have been wired for this dependency.
+            dependencies: Dependencies of the dependency itself. Ordering is kept.
+        """
+        super().__init__(__info, scope, wired, dependencies)
+
+    def __eq__(self, other: object) -> bool:
+        return (
+            isinstance(other, DependencyDebug)
+            and self.info == other.info
+            and self.scope == other.scope
+            and self.wired == other.wired
+            and self.dependencies == other.dependencies
+        )  # noqa: E126
