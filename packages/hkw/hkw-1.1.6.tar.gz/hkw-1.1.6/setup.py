263c032b7f16d1a21854d7be8dcff3fb62af9894
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:20 +0000

    Add package hkw/1.1.6/hkw-1.1.6.tar.gz

---
+++ packages/hkw/hkw-1.1.6.tar.gz/hkw-1.1.6/setup.py
@@ -0,0 +1,17 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+from setuptools import find_namespace_packages
+setup(name='hkw',
+      version='1.1.6',
+      description = 'hkwarray.',
+      author='hkw',
+      author_email='kkhwer@gmail.com',
+      packages=find_namespace_packages(where='src'),
+      package_dir = {"": "src"},
+	package_data={
+		"rdata": ["*.*"],
+	}
+	
+      )
+
+
