d35807880e540392ef15d766f35ab3c5c84b53da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:34:46 +0000

    Add package ep2/22.1.2/ep2-22.1.2.tar.gz

---
+++ packages/ep2/ep2-22.1.2.tar.gz/ep2-22.1.2/printName/__init__.py
@@ -0,0 +1,1 @@
+from .pN import NamePrint
---
+++ packages/ep2/ep2-22.1.2.tar.gz/ep2-22.1.2/printName/pN.py
@@ -0,0 +1,16 @@
+#This programe is written for understand package distribution on pip for open source
+'''Code written by EP on 23/6/22
+on the importing of the package it will ask for enter name and once valid input given then return a short msg
+package name = printName
+main code file name = pN.py
+
+'''
+
+
+def NamePrint():
+    def PassName(x):
+        Result = "Hello {}! happy to see you here"
+        print(Result.format(x))
+
+    x = input ("Enter your Name\t")
+    PassName(x)
