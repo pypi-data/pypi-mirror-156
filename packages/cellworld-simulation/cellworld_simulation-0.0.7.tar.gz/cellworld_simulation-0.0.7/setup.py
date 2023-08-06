5d3cc9782189c2795cd25d14b642e00a7ca27b6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:23:40 +0000

    Add package cellworld-simulation/0.0.7/cellworld_simulation-0.0.7.tar.gz

---
+++ packages/cellworld-simulation/cellworld_simulation-0.0.7.tar.gz/cellworld_simulation-0.0.7/setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup
+
+setup(name='cellworld_simulation',author='German Espinosa',author_email='germanespinosa@gmail.com',packages=['cellworld_simulation'],install_requires=['cellworld'],license='MIT',version='0.0.7',zip_safe=False)
