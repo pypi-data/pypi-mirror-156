af8d351cd7f0f2d14f4d96693d25ed132a78c242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:29 +0000

    Add package gprmc/0.0.3/GPRMC-0.0.3.tar.gz

---
+++ packages/gprmc/GPRMC-0.0.3.tar.gz/GPRMC-0.0.3/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("C:\\Users\\ak080\\desktop\\gprmc\\README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+    print(long_description)
+
+setuptools.setup(
+    name = "GPRMC",
+    version = "0.0.3",
+    author = "arunkumar",
+    author_email = "ak080495@gmail.com",
+    description = "convert gprmc data",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://gitlab.com/ak080495/gprmc",
+    project_urls = {
+        "Bug Tracker": "https://gitlab.com/ak080495/gprmc/-/issues",
+        
+    },
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.6"
+)
