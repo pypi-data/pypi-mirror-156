948d7d542f61d80f64e17ea188df9697b54436f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:56 +0000

    Add package kivyblocks/0.2.13/kivyblocks-0.2.13.tar.gz

---
+++ packages/kivyblocks/kivyblocks-0.2.13.tar.gz/kivyblocks-0.2.13/kivyblocks/version.py
@@ -0,0 +1,12 @@
+# __version__ = '0.2.8'
+# add a new class DefaultImage, if the source load failed, source set to default_source
+#__version__ = '0.2.9'
+
+# fix bug in ffpyplayer_video.py
+#__version__ = '0.2.10'
+# fix bug in __init__.py, can not import kivy here
+# __version__ = '0.2.11'
+# move all the source using opencv to a independent module
+# __version__ = '0.2.12'
+# move code using pyaudio to a independent module
+__version__ = '0.2.13'
