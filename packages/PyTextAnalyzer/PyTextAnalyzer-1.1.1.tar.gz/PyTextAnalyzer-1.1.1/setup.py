dc5a1e925af1438270af2e4e4ae656c7c5f96997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:26:59 +0000

    Add package PyTextAnalyzer/1.1.1/PyTextAnalyzer-1.1.1.tar.gz

---
+++ packages/PyTextAnalyzer/PyTextAnalyzer-1.1.1.tar.gz/PyTextAnalyzer-1.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = ['Development Status :: 5 - Production/Stable',
+'Intended Audience :: Education',
+'Operating System :: Microsoft :: Windows :: Windows 10',
+'License :: OSI Approved :: MIT License',
+'Programming Language :: Python :: 3']
+
+
+
+
+
+setup(name="PyTextAnalyzer",
+version="1.1.1",
+description="Python Module with functions helping with analyzing strings.",
+author="Fallen Cat",
+author_email="fallencat.user@gmail.com",
+packages=find_packages(),
+license='MIT',
+classifiers=classifiers,
+keywords='String Analyzer',
+url='https://github.com/TheFallen-Cat/PyTextAnalyzer',
+download_url='https://github.com/TheFallen-Cat/PyTextAnalyzer/archive/refs/tags/1.1.1.tar.gz')
