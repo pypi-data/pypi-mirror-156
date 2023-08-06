b20be6ddd9486f5781ddb992f6d91e34c9b9b838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:14:34 +0000

    Add package devcloudcli/1.3.2/devcloudcli-1.3.2.tar.gz

---
+++ packages/devcloudcli/devcloudcli-1.3.2.tar.gz/devcloudcli-1.3.2/dc_cli/src/scripts/dl-model-services/inference-runtimes/intel-optimized-tensorflow/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Copyright (C) 2018-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+
+docker pull intel/intel-optimized-tensorflow
+echo -e "\e[1;32mImage is pulled successfully\e[0m"
+echo -e "\e[1;36mTo view the images--> docker images\e[0m"
