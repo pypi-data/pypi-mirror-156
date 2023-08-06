9145e961765ef279e96889e53e5ea40eeb619b04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:00:46 +0000

    Add package fbgem-gpu-cpu/100.0.5/fbgem-gpu-cpu-100.0.5.tar.gz

---
+++ packages/fbgem-gpu-cpu/fbgem-gpu-cpu-100.0.5.tar.gz/fbgem-gpu-cpu-100.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="fbgem-gpu-cpu",
+    version="100.0.5",
+    author="Alon walker",
+    author_email="alonwalker@facebook.com",
+    description="fbgem-gpu-cpu of fbgem-gpu-cpu of fbgem-gpu-cpu",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
