07f341748a85284ecb0e11d5d9c4894dc7a25736
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:25 +0000

    Add package wasd/1.0.99/wasd-1.0.99.tar.gz

---
+++ packages/wasd/wasd-1.0.99.tar.gz/wasd-1.0.99/wasd/support/expected_conditions.py
@@ -0,0 +1,44 @@
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+
+
+def _element_if_visible(element, visibility=True):
+    return element if element.is_displayed() == visibility else False
+
+
+class visibility_of(object):
+    def __init__(self, element):
+        self.element = element
+
+    def __call__(self, browser):
+        try:
+            return _element_if_visible(browser.find(self.element))
+        except StaleElementReferenceException:
+            return False
+
+
+class invisibility_of(object):
+    def __init__(self, element):
+        self.element = element
+
+    def __call__(self, browser):
+        try:
+            return _element_if_visible(
+                browser.find(self.element),
+                visibility=False,
+            )
+        except (NoSuchElementException, StaleElementReferenceException):
+            return True
+
+
+class staleness_of(object):
+    def __init__(self, element):
+        self.element = element
+
+    def __call__(self, browser):
+        try:
+            # Calling any method forces a staleness check
+            browser.find(self.element).is_enabled()
+            return False
+        except StaleElementReferenceException:
+            return True
