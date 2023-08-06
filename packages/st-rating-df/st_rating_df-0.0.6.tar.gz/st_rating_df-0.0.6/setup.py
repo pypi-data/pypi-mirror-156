379489c2815072eb13239d8f18e28ea419db0399
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:30 +0000

    Add package st-rating-df/0.0.6/st_rating_df-0.0.6.tar.gz

---
+++ packages/st-rating-df/st_rating_df-0.0.6.tar.gz/st_rating_df-0.0.6/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="st_rating_df",
+    version="0.0.6",
+    author="Anton Buch",
+    author_email="anton.buch@bayer.com",
+    description="",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.6",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 0.63",
+        "pandas >= 1.3",
+        "numpy >= 1.20",
+    ],
+)
