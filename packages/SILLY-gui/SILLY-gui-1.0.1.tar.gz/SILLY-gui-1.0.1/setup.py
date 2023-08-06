6baa408d8aba643fc04577f71fb1037e2031ea89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:16 +0000

    Add package SILLY-gui/1.0.1/SILLY-gui-1.0.1.tar.gz

---
+++ packages/SILLY-gui/SILLY-gui-1.0.1.tar.gz/SILLY-gui-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+import numpy
+
+setup(
+    name='SILLY-gui',
+    version='1.0.1',
+    include_package_data = True,
+    include_dirs=[numpy.get_include()],
+    packages=find_packages()
+)
