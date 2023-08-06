0954f180bd61e60aaf81fbd7c68b1197f941d041
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:22:28 +0000

    Add package devcloudcli/1.3.0/devcloudcli-1.3.0.tar.gz

---
+++ packages/devcloudcli/devcloudcli-1.3.0.tar.gz/devcloudcli-1.3.0/dc_cli/src/scripts/dl-model-training-services/data-engineering-tools/datumaro/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright (C) 2018-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+if [[ $(pip3 --version) ]]; then
+         echo -e "\e[1;32mpip3 is installed\e[0m"
+     else
+         echo -e "\e[1;32mInstalling pip3\e[0m"
+         sudo apt-get update
+         sudo apt-get -y install python3-pip
+fi
+
+if [[ $(pip3 show datumaro) ]]; then
+         echo -e "\e[1;32mdatumaro is already installed in the system\e[0m"
+     else
+         echo -e "\e[1;32mInstalling datumaro....\e[0m"
+         sudo pip3 install datumaro[default]
+fi
+
+
