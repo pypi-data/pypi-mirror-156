81de79819bf7a89d2b6951678f59685c3934d798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:12 +0000

    Add package npt/0.7.1/npt-0.7.1.tar.gz

---
+++ packages/npt/npt-0.7.1.tar.gz/npt-0.7.1/npt/_log.py
@@ -0,0 +1,7 @@
+import logging
+# fmt="%(levelname)s:%(funcName)s():%(lineno)i: %(message)s"
+fmt="%(levelname)s:%(module)s.%(funcName)s(): %(message)s"
+logging.basicConfig(level=logging.INFO, format=fmt)
+log = logging.getLogger('npt')
+log.setLevel('INFO')
+log.set_level = log.setLevel
