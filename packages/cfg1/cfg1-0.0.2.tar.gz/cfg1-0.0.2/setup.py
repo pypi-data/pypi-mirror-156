bd188e80726237a0b4366183780016bdf08f9fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:10 +0000

    Add package cfg1/0.0.2/cfg1-0.0.2.tar.gz

---
+++ packages/cfg1/cfg1-0.0.2.tar.gz/cfg1-0.0.2/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['cfg1']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pyaml>=21.10.1,<22.0.0', 'pytest>=7.1.2,<8.0.0', 'python-box>=6.0.2,<7.0.0']
+
+setup_kwargs = {
+    'name': 'cfg1',
+    'version': '0.0.2',
+    'description': 'A configuration library for Python3 projects',
+    'long_description': None,
+    'author': 'Dan Sikes',
+    'author_email': 'dansikes7@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
