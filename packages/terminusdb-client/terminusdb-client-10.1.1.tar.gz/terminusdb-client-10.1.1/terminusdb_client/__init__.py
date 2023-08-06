fa74c74f2f28760ad9fbf644e8a507e81ddd7766
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:35 +0000

    Add package terminusdb-client/10.1.1/terminusdb-client-10.1.1.tar.gz

---
+++ packages/terminusdb-client/terminusdb-client-10.1.1.tar.gz/terminusdb-client-10.1.1/terminusdb_client/__init__.py
@@ -0,0 +1,5 @@
+from .woqlclient import Patch, WOQLClient  # noqa
+from .woqldataframe import woqlDataframe as WOQLDataFrame  # noqa
+from .woqlquery import WOQLQuery  # noqa
+from .woqlschema import *  # noqa
+from .woqlview import WOQLView  # noqa
