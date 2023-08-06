04128ebfa18506ab1929d1f488bcebf795549b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:35 +0000

    Add package bruco/0.2.1/bruco-0.2.1-py3-none-any.whl

---
+++ packages/bruco/bruco-0.2.1-py3-none-any.whl/bruco/__init__.py
@@ -0,0 +1,10 @@
+# Brute force coherence (Gabriele Vajente, 2022-06-16)
+
+"""
+Brute-force coherence analysis of LIGO data
+"""
+
+__version__ = '0.2.1'
+__date__    = '2022-06-16' 
+
+from .__main__ import bruco
