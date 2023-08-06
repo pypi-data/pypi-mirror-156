c1c95a6ce4a4a01d5c0e1696cb468d5b50af9db7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:35 +0000

    Add package vinca/2.4.5/vinca-2.4.5.tar.gz

---
+++ packages/vinca/vinca-2.4.5.tar.gz/vinca-2.4.5/vinca/_lib/julianday.py
@@ -0,0 +1,23 @@
+import datetime
+
+def unixepoch_local():
+        return (datetime.datetime.now() - datetime.datetime(year=1970, month=1, day=1)).total_seconds()
+
+def now():
+        return unixepoch_local() / 86400
+
+def today():
+        today = int(now())
+        assert today == (datetime.date.today() - datetime.date(year=1970,month=1,day=1)).days
+        return today
+
+class JulianDate(float):
+        
+        def __str__(self):
+            date = datetime.date(year=1970,month=1,day=1) + datetime.timedelta(days = 1) * int(self)
+            return f'{date}'
+
+        @property
+        def isoformat(self):
+            return str(self)
+
