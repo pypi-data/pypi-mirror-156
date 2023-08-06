62b751821c5ace5cddb0c77be3b9bbfa5c6994e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:42:20 +0000

    Add package layer/0.10.2560785391/layer-0.10.2560785391-py3-none-any.whl

---
+++ packages/layer/layer-0.10.2560785391-py3-none-any.whl/layer/contracts/logged_data.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+from enum import Enum, unique
+from typing import Dict
+
+
+@unique
+class LoggedDataType(Enum):
+    INVALID = 0
+    TEXT = 1
+    TABLE = 2
+    BLOB = 3
+    NUMBER = 4
+    BOOLEAN = 5
+    IMAGE = 6
+    VIDEO = 7
+    MARKDOWN = 8
+
+
+@dataclass(frozen=True)
+class LoggedData:
+    logged_data_type: LoggedDataType
+    tag: str
+    data: str
+    epoched_data: Dict[int, str]
+
+
+@dataclass(frozen=True)
+class ModelMetricPoint:
+    epoch: int
+    value: float
+
+
+@dataclass(frozen=True)
+class Markdown:
+    """
+    Encapsulates a markdown text to be displayed correctly in Layer UI as part of logging.
+
+    .. code-block:: python
+
+        layer.log(layer.Markdown("## Hello world!"))
+
+    """
+
+    data: str
