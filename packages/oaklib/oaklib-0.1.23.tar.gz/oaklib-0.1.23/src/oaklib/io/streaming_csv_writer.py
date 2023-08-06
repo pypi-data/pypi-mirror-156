66e17adbc0a2f61461e75b83c105fa07b45ea4d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:46:29 +0000

    Add package oaklib/0.1.23/oaklib-0.1.23.tar.gz

---
+++ packages/oaklib/oaklib-0.1.23.tar.gz/oaklib-0.1.23/src/oaklib/io/streaming_csv_writer.py
@@ -0,0 +1,40 @@
+import csv
+from dataclasses import dataclass
+from typing import Any, Dict, Union
+
+from linkml_runtime import CurieNamespace
+from linkml_runtime.utils.yamlutils import YAMLRoot
+
+from oaklib.io.streaming_writer import StreamingWriter
+
+
+def _keyval(x: Any) -> str:
+    if isinstance(x, CurieNamespace):
+        return str(x.curie())
+    # if isinstance(x, EnumDefinitionImpl):
+    #    if x.curie:
+    #        return str(x.curie)
+    return str(x)
+
+
+@dataclass
+class StreamingCsvWriter(StreamingWriter):
+    """
+    A writer that streams CSV/TSV output
+    """
+
+    header_emitted: bool = None
+    delimiter: str = "\t"
+    writer: csv.DictWriter = None
+
+    def emit(self, obj: Union[YAMLRoot, Dict]):
+        if isinstance(obj, dict):
+            obj_as_dict = obj
+        else:
+            obj_as_dict = vars(obj)
+        if self.writer is None:
+            self.writer = csv.DictWriter(
+                self.file, delimiter=self.delimiter, fieldnames=list(obj_as_dict)
+            )
+            self.writer.writeheader()
+        self.writer.writerow({k: _keyval(v) for k, v in obj_as_dict.items()})
