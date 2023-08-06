e1419447a84aadeb3587f13f82f2a8144c4f48d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:13 +0000

    Add package theartisans-shared-python/0.1.0/theartisans-shared_python-0.1.0.tar.gz

---
+++ packages/theartisans-shared-python/theartisans-shared_python-0.1.0.tar.gz/theartisans-shared_python-0.1.0/src/middleware/current_user.py
@@ -0,0 +1,35 @@
+import os
+from typing import Any, List, Optional, TypedDict, Callable
+import jwt
+from src.types import UserRoleValues, SubscriptionTypeValues
+# from functools import wraps
+from flask import request
+
+class Subscription(TypedDict):
+    id: str
+    subscriptionType: SubscriptionTypeValues
+    expiryDate: str
+
+class UserPayload(TypedDict):
+    id: str
+    email: str
+    roles: List[UserRoleValues]
+    firstName: Optional[str]
+    lastName: Optional[str]
+    active: bool
+    banned: bool
+    subscription: Optional[Subscription]
+    billingId: Optional[str]
+    mobileNumber: Optional[str]
+    loginCount: int
+    lastLogin: str
+    profileImageUrl: Optional[str]
+
+def current_user():    
+    try:
+        userJWT = request.environ['session']['jwt']
+        current_user = jwt.decode(userJWT, os.environ.get('JWT_KEY'), ['HS256']) 
+    except:
+        current_user = None
+    setattr(request, 'current_user', current_user)
+        
