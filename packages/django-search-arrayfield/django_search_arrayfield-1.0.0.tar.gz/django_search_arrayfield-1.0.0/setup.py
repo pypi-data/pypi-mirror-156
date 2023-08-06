3eb1fe70694406721d2a34ae86e398e9b3295c0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:48 +0000

    Add package django-search-arrayfield/1.0.0/django_search_arrayfield-1.0.0.tar.gz

---
+++ packages/django-search-arrayfield/django_search_arrayfield-1.0.0.tar.gz/django_search_arrayfield-1.0.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.0.0'
+DESCRIPTION = 'Dynamic Search in Array Field in Django Admin'
+LONG_DESCRIPTION = 'A package that allows Dynamic Search in Array Field in Django Admin'
+
+# Setting up
+setup(
+    name="django_search_arrayfield",
+    version=VERSION,
+    author="Arshan Ahmad , Shivaank Tripathi",
+    author_email="<arshan@thetarzanway.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'django_better_admin', 'dynamic_search', 'dynamic-search', 'django-admin'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
