bbcfcdafa597fc57c8bb78a7b66d23b7681c79ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:46:09 +0000

    Add package xontrib-back2dir/0.1.2/xontrib-back2dir-0.1.2.tar.gz

---
+++ packages/xontrib-back2dir/xontrib-back2dir-0.1.2.tar.gz/xontrib-back2dir-0.1.2/xontrib/back2dir.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+from xonsh import dirstack
+
+_file = Path(__xonsh__.env.get('XDG_CACHE_HOME', '~/.cache')).expanduser() / 'xontrib-back2dir' / 'latest.path'
+
+@events.on_post_init
+def on_post_init(**_):
+    if Path(__xonsh__.env["PWD"]) == Path(__xonsh__.env["HOME"]).resolve() and _file.exists():
+        d = Path(_file.read_text().strip())
+        nd = d
+        while not nd.exists():
+            nd = nd.parent
+        __xonsh__.subproc_captured_stdout(['cd', str(nd)])
+
+@events.on_chdir
+def _xontrib_back2dir(olddir, newdir, **kwargs):
+    if not _file.parent.exists():
+        _file.parent.mkdir(parents=True, exist_ok=True)
+    with open(_file, 'w') as f:
+        print(newdir, end='', file=f)
