28aeeefb0aa9e57df1d3e93da9fed80ad0c3a71f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:35 +0000

    Add package pondos/0.0.2/pondos-0.0.2.tar.gz

---
+++ packages/pondos/pondos-0.0.2.tar.gz/pondos-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = ''
+LONG_DESCRIPTION = ''
+
+# Setting up
+setup(
+    name="pondos",
+    version=VERSION,
+    author="dscestudent",
+    author_email="<smf9092@newsote.com>",
+    description=DESCRIPTION,
+    keywords=[],
+    include_package_data=True,
+    packages=['pondos'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
+
