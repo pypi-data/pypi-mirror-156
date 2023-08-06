0a367f02f74dca9d0b481847d76c316ad7ca8455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:15 +0000

    Add package geihdanepy/0.1.15/geihdanepy-0.1.15-py3-none-any.whl

---
+++ packages/geihdanepy/geihdanepy-0.1.15-py3-none-any.whl/geihdanepy/utils.py
@@ -0,0 +1,21 @@
+def __referenciador_modulo():
+    ref_modulo = {'Vivienda':'v',
+                   'Caracteristicas':'c',
+                   'Fuerza':'f',
+                   'Ocupados':'o',
+                   'Desocupados':'d',
+                   'Inactivos':'i',
+                   'Otras actividades':'o_a',
+                   'Otros ingresos':'o_i'}
+    return ref_modulo 
+
+def __referenciador_zona():
+    ref_zona = {'Cabecera':'C',
+                "Area":"A",
+                "Resto":"R"}
+    return ref_zona
+
+def meses():
+    return ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+
