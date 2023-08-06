5c003dd034a30177ec7bdbda1674eb7fbac4a737
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:43 +0000

    Add package graphysio/2022.6.26/graphysio-2022.6.26-py3-none-any.whl

---
+++ packages/graphysio/graphysio-2022.6.26-py3-none-any.whl/graphysio/plotwidgets/__init__.py
@@ -0,0 +1,14 @@
+from graphysio.plotwidgets.plotwidget import PlotWidget, TimeAxisItem
+from graphysio.plotwidgets.poiselector import POISelectorWidget
+from graphysio.plotwidgets.puplot import LoopWidget
+from graphysio.plotwidgets.spectrogram import SpectrogramWidget
+from graphysio.plotwidgets.tsplot import TSWidget
+
+all = [
+    PlotWidget,
+    TimeAxisItem,
+    POISelectorWidget,
+    LoopWidget,
+    SpectrogramWidget,
+    TSWidget,
+]
