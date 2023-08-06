fecaee8cf28506d1921be312a3dac443d3800891
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:10 +0000

    Add package jupyter-datasette-proxy/1.1/jupyter-datasette-proxy-1.1.tar.gz

---
+++ packages/jupyter-datasette-proxy/jupyter-datasette-proxy-1.1.tar.gz/jupyter-datasette-proxy-1.1/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", encoding="utf8") as f:
+    readme = f.read()
+
+setuptools.setup(
+  name="jupyter-datasette-proxy",
+  version="1.1",
+  # py_modules rather than packages, since we only have 1 file
+  py_modules=['jupyter-datasette-proxy'],
+  project_urls={
+      "Source":"https://github.com/psychemedia/jupyter-datasette-proxy",
+  },
+  install_requires=[
+    'datasette',
+    'jupyter-server-proxy'
+  ],
+  description="Run datasette on JupyterHub with jupyter-server-proxy",
+  long_description=readme,
+  long_description_content_type="text/markdown",
+  entry_points={
+      'jupyter_serverproxy_servers': [
+          #path = module:entry_function
+          'datasette = jupyter_datasette_proxy:setup_datasette',
+      ]
+  },
+)
