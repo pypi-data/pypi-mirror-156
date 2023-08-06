b1019118a1e9c2d3902882cc9872832dae4670df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:03 +0000

    Add package vedirect-m8/1.2.2/vedirect_m8-1.2.2.tar.gz

---
+++ packages/vedirect-m8/vedirect_m8-1.2.2.tar.gz/vedirect_m8-1.2.2/test/test_serutils.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+SerialUtils unittest class.
+
+Use pytest package.
+"""
+from vedirect_m8.serutils import SerialUtils as Ut
+
+__author__ = "Eli Serra"
+__copyright__ = "Copyright 2020, Eli Serra"
+__deprecated__ = False
+__license__ = "MIT"
+__status__ = "Production"
+__version__ = "1.0.0"
+
+
+class TestSerialUtils:
+
+    def test_is_key_pattern(self):
+        """"""
+        datas = [
+            '_hello', 'hel lo', "#hj_58Hyui#",  # false
+            "hj_58Hyui"  # true
+        ]
+        tests = [x for x in datas if Ut.is_key_pattern(x)]
+        assert len(tests) == 1
+
+    def test_is_serial_key_pattern(self):
+        """"""
+        datas = [
+            '_hello', 'hel lo',  # false
+            "hj_58Hyui", "#hj_58Hyui#"  # true
+        ]
+        tests = [x for x in datas if Ut.is_serial_key_pattern(x)]
+        assert len(tests) == 2
+
+    def test_is_unix_serial_port_pattern(self):
+        """"""
+        datas = [
+            '/etc/ttyUSB1', "/dev/tty", "/dev/tty1",  # false
+            "/dev/ttyACM1", "/dev/ttyUSB3"  # true
+        ]
+        tests = [x for x in datas if Ut.is_unix_serial_port_pattern(x)]
+        assert len(tests) == 2
+
+    def test_is_win_serial_port_pattern(self):
+        """"""
+        datas = [
+            '/etc/ttyUSB1', "/dev/COM3", "/COM3", "COM"  # false
+            "COM255", "COM3", "COM0"  # true
+        ]
+        tests = [x for x in datas if Ut.is_win_serial_port_pattern(x)]
+        assert len(tests) == 2
