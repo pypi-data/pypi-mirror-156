13a67b8bb8a5e7f7df1272e2343aa5045176bace
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:45 +0000

    Add package FRAPP/1.1.1/FRAPP-1.1.1.tar.gz

---
+++ packages/FRAPP/FRAPP-1.1.1.tar.gz/FRAPP-1.1.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import os
+import re
+
+
+# auto-updating version code stolen from orbitize
+def get_property(prop, project):
+    result = re.search(r'{}\s*=\s*[\'"]([^\'"]*)[\'"]'.format(prop),
+                       open(project + '/__init__.py').read())
+    return result.group(1)
+
+# auto-updating version code stolen from orbitize
+def get_requires():
+    reqs = []
+    for line in open('requirements.txt', 'r').readlines():
+        reqs.append(line)
+    return reqs
+
+setup(
+    name='FRAPP',
+    version=get_property('__version__', 'FRAPP'),
+    packages=find_packages(),
+    description='FRAPP (Font-Facilitated Reading for Arxiv PDF Publications)',
+    url='https://github.com/mpgalleg/FRAPP',
+    author='MGG, QH, PP',
+    keywords='ArXiv Reading Accessibility'
+    #install_requires=get_requires()
+)
