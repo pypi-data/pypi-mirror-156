0937614b4d2e154573230c3aa0c55e6a097b08b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:04:13 +0000

    Add package feature-store-bundle/2.5.0b3/feature_store_bundle-2.5.0b3-py3-none-any.whl

---
+++ packages/feature-store-bundle/feature_store_bundle-2.5.0b3-py3-none-any.whl/featurestorebundle/feature/Feature.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass
+from typing import Any, Optional
+from datetime import datetime
+
+from featurestorebundle.feature.FeatureTemplate import FeatureTemplate
+
+
+@dataclass(frozen=True)
+class Feature:
+    name_template: str
+    description_template: str
+    fillna_with: Any
+    type: Optional[str] = None
+
+    def create_template(
+        self,
+        location: str,
+        backend: str,
+        notebook: str,
+        category: Optional[str],
+        owner: Optional[str],
+        start_date: Optional[datetime],
+        frequency: Optional[str],
+        last_compute_date: Optional[datetime],
+    ) -> FeatureTemplate:
+        return FeatureTemplate(
+            name_template=self.name_template,
+            description_template=self.description_template,
+            fillna_value=self.fillna_with,
+            fillna_value_type=type(self.fillna_with).__name__,
+            location=location,
+            backend=backend,
+            notebook=notebook,
+            type=self.type,
+            category=category,
+            owner=owner,
+            start_date=start_date,
+            frequency=frequency,
+            last_compute_date=last_compute_date,
+        )
