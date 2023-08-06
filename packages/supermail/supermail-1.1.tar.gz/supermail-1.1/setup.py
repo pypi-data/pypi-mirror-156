c1159206440ee68c42f985a4cacd6d49362ff60e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:01 +0000

    Add package supermail/1.1/supermail-1.1.tar.gz

---
+++ packages/supermail/supermail-1.1.tar.gz/supermail-1.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+	name='supermail',
+	version='1.1',
+	author="Flavio Piccoli",
+	author_email="dros1986@gmail.com",
+	description="An email client for reading and sending emails.",
+	# long_description="An email client for reading and sending emails.",
+	# long_description_content_type="text/markdown",
+	url="https://github.com/dros1986/EmailClient",
+	packages=setuptools.find_packages('.'),
+	#packages=setuptools.find_packages(include=['src']),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	install_requires=[
+          'imapclient'
+    ],
+	python_requires='>=3',
+ )
