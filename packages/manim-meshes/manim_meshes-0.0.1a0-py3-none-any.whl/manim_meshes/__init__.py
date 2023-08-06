2cdd6b39979fdfd76d522555585b656acc014779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:07:59 +0000

    Add package manim-meshes/0.0.1a0/manim_meshes-0.0.1a0-py3-none-any.whl

---
+++ packages/manim-meshes/manim_meshes-0.0.1a0-py3-none-any.whl/manim_meshes/__init__.py
@@ -0,0 +1,13 @@
+"""
+Initialization of basic manim plugin structure
+Authorship information according to Python Coding Guidelines
+"""
+# third-party imports
+# import trimesh
+
+__author__ = "Martin Steinborn"
+__license__ = "MIT"
+__status__ = "Development"
+
+# attack trimesh logger to print debug messages if necessary
+# trimesh.util.attach_to_log()
