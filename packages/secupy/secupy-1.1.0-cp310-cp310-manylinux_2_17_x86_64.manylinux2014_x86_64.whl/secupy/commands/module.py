bdbfaf2cefc32f9fc5c045605f3385829785c423
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:52 +0000

    Add package secupy/1.1.0/secupy-1.1.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/secupy/secupy-1.1.0-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/secupy/commands/module.py
@@ -0,0 +1,15 @@
+import os
+import sys
+from runpy import _run_module_as_main
+
+
+def main():
+    sys.path.insert(0, os.getcwd())
+
+    # Run the module specified as the next command line argument
+    if len(sys.argv) < 3:
+        print("No module specified for execution", file=sys.stderr)
+    else:
+        del sys.argv[0]  # Make the requested module sys.argv[0]
+        del sys.argv[0]  # Make the requested module sys.argv[0]
+        _run_module_as_main(sys.argv[0])
