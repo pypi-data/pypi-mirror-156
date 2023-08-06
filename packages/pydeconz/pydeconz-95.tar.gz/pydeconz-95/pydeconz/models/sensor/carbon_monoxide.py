f94eb8d9ca779a3d8f2c946480db45cbafa216cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:41 +0000

    Add package pydeconz/95/pydeconz-95.tar.gz

---
+++ packages/pydeconz/pydeconz-95.tar.gz/pydeconz-95/pydeconz/models/sensor/carbon_monoxide.py
@@ -0,0 +1,30 @@
+"""Python library to connect deCONZ and Home Assistant to work together."""
+
+from typing import TypedDict
+
+from . import SensorBase
+
+
+class TypedCarbonMonoxideState(TypedDict):
+    """Carbon monoxide state type definition."""
+
+    carbonmonoxide: bool
+
+
+class TypedCarbonMonoxide(TypedDict):
+    """Carbon monoxide type definition."""
+
+    state: TypedCarbonMonoxideState
+
+
+class CarbonMonoxide(SensorBase):
+    """Carbon monoxide sensor."""
+
+    ZHATYPE = ("ZHACarbonMonoxide",)
+
+    raw: TypedCarbonMonoxide
+
+    @property
+    def carbon_monoxide(self) -> bool:
+        """Carbon monoxide detected."""
+        return self.raw["state"]["carbonmonoxide"]
---
+++ packages/pydeconz/pydeconz-95.tar.gz/pydeconz-95/pydeconz/models/sensor/consumption.py
@@ -0,0 +1,43 @@
+"""Python library to connect deCONZ and Home Assistant to work together."""
+
+from __future__ import annotations
+
+from typing import TypedDict
+
+from . import SensorBase
+
+
+class TypedConsumptionState(TypedDict):
+    """Consumption state type definition."""
+
+    consumption: int
+    power: int
+
+
+class TypedConsumption(TypedDict):
+    """Consumption type definition."""
+
+    state: TypedConsumptionState
+
+
+class Consumption(SensorBase):
+    """Power consumption sensor."""
+
+    ZHATYPE = ("ZHAConsumption",)
+
+    raw: TypedConsumption
+
+    @property
+    def consumption(self) -> int:
+        """Consumption."""
+        return self.raw["state"]["consumption"]
+
+    @property
+    def scaled_consumption(self) -> float:
+        """State of sensor."""
+        return self.consumption / 1000
+
+    @property
+    def power(self) -> int | None:
+        """Power."""
+        return self.raw["state"].get("power")
