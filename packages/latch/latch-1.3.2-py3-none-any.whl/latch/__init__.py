5f73e4d089977058f8545f2f86663113158364ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:36:26 +0000

    Add package latch/1.3.2/latch-1.3.2-py3-none-any.whl

---
+++ packages/latch/latch-1.3.2-py3-none-any.whl/latch/__init__.py
@@ -0,0 +1,16 @@
+"""The Latch SDK
+
+A commandline toolchain to define and register serverless workflows with the
+Latch platform.
+"""
+
+from latch.resources.conditional import create_conditional_section
+from latch.resources.map_tasks import map_task
+from latch.resources.tasks import (
+    large_gpu_task,
+    large_task,
+    medium_task,
+    small_gpu_task,
+    small_task,
+)
+from latch.resources.workflow import workflow
