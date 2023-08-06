4bdb6a4c5d9e24bff52d97b0054e9782e96c9e9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:27:28 +0000

    Add package code-loader/0.2.25/code-loader-0.2.25.tar.gz

---
+++ packages/code-loader/code-loader-0.2.25.tar.gz/code-loader-0.2.25/code_loader/contract/visualizer_classes.py
@@ -0,0 +1,55 @@
+from typing import List
+
+import numpy as np
+import numpy.typing as npt
+from dataclasses import dataclass
+
+from code_loader.contract.enums import LeapDataType
+
+
+@dataclass
+class LeapImage:
+    data: npt.NDArray[np.float32]
+    type: LeapDataType = LeapDataType.Image
+
+
+@dataclass
+class LeapImageWithBBox:
+    data: npt.NDArray[np.float32]
+    bbox: npt.NDArray[np.float32]
+    type: LeapDataType = LeapDataType.ImageWithBBox
+
+
+@dataclass
+class LeapGraph:
+    data: npt.NDArray[np.float32]
+    type: LeapDataType = LeapDataType.Graph
+
+
+@dataclass
+class LeapText:
+    data: List[str]
+    type: LeapDataType = LeapDataType.Text
+
+
+@dataclass
+class LeapHorizontalBar:
+    body: npt.NDArray[np.float32]
+    labels: List[str]
+    type: LeapDataType = LeapDataType.HorizontalBar
+
+
+@dataclass
+class LeapImageMask:
+    mask: npt.NDArray[np.float32]
+    image: npt.NDArray[np.float32]
+    labels: List[str]
+    type: LeapDataType = LeapDataType.ImageMask
+
+
+@dataclass
+class LeapTextMask:
+    mask: npt.NDArray[np.float32]
+    text_array: npt.NDArray[np.float32]
+    labels: List[str]
+    type: LeapDataType = LeapDataType.TextMask
