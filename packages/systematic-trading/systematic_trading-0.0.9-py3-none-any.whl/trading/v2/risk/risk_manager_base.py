fb0b6e68a52ecd47fbcbc235c5e83b15dc205d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:33:09 +0000

    Add package systematic-trading/0.0.9/systematic_trading-0.0.9-py3-none-any.whl

---
+++ packages/systematic-trading/systematic_trading-0.0.9-py3-none-any.whl/trading/v2/risk/risk_manager_base.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from abc import ABCMeta, abstractmethod
+
+
+class RiskManagerBase(metaclass=ABCMeta):
+    """
+    RiskManager base class
+    """
+
+    @abstractmethod
+    def order_in_compliance(self, o, strategy_manager=None):
+        raise NotImplementedError("order_in_compliance should be implemented")
