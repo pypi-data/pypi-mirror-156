4b83fe1257142e4c20555c175f767e3342c1edc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:23 +0000

    Add package proset/0.2.1/proset-0.2.1.tar.gz

---
+++ packages/proset/proset-0.2.1.tar.gz/proset-0.2.1/proset/__init__.py
@@ -0,0 +1,10 @@
+"""Turn proset into a module.
+
+Copyright by Nikolaus Ruf
+Released under the MIT license - see LICENSE file for details
+"""
+
+__version__ = "0.2.1"
+
+
+from proset.model import ClassifierModel
