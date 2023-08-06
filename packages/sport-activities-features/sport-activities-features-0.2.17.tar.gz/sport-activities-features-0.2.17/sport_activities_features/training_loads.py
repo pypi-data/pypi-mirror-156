814f9ec942f5a19d199b9bcaff289b66c88d49f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:32:12 +0000

    Add package sport-activities-features/0.2.17/sport-activities-features-0.2.17.tar.gz

---
+++ packages/sport-activities-features/sport-activities-features-0.2.17.tar.gz/sport-activities-features-0.2.17/sport_activities_features/training_loads.py
@@ -0,0 +1,28 @@
+class BanisterTRIMP(object):
+    """
+    Class for calculation of Banisters TRIMP.\n
+    Args:
+        duration (float):
+            total duration in minutes
+        avg_hr (float):
+            average heart rate in bpm
+    """
+    def __init__(self, duration: float, avg_hr: float) -> None:
+        """
+        Initialisation method for BanisterTRIMP class.\n
+        Args:
+            duration (float):
+                total duration in minutes
+            avg_hr (float):
+                average heart rate in bpm
+        """
+        self.duration = duration
+        self.avg_hr = avg_hr
+
+    def calculate_TRIMP(self) -> float:
+        """
+        Method for the calculation of TRIMP.\n
+        Returns:
+            float: TRIMP value
+        """
+        return self.duration * self.avg_hr
