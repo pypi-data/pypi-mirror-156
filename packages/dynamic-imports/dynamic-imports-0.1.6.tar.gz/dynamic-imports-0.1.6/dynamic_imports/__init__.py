8caad8437e2801e42b559e419afcb82bef5948eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:46:31 +0000

    Add package dynamic-imports/0.1.6/dynamic-imports-0.1.6.tar.gz

---
+++ packages/dynamic-imports/dynamic-imports-0.1.6.tar.gz/dynamic-imports-0.1.6/dynamic_imports/__init__.py
@@ -0,0 +1,8 @@
+from dynamic_imports.discover import (
+    class_impls_from_module,
+    class_impls_from_package,
+    class_instances_from_module,
+    class_instances_from_package,
+    get_modules,
+)
+from dynamic_imports.importers import import_module, import_module_attr
