ecbc35c632b65e2c066f038c2dda34defcae2d6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:03:17 +0000

    Add package arbejdstimer/2022.6.27/arbejdstimer-2022.6.27-py2.py3-none-any.whl

---
+++ packages/arbejdstimer/arbejdstimer-2022.6.27-py2.py3-none-any.whl/arbejdstimer/api.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned,line-too-long
+"""Working hours (Danish arbejdstimer) or not? API model."""
+from __future__ import annotations
+
+from datetime import date
+from enum import Enum
+from typing import Annotated, List, Optional, no_type_check
+
+from pydantic import BaseModel, Field, validator
+
+
+class Application(Enum):
+    arbejdstimer = 'arbejdstimer'
+
+
+class Operator(Enum):
+    and_ = 'and'
+    or_ = 'or'
+    xor = 'xor'
+
+
+class Hour(BaseModel):
+    __root__: Annotated[int, Field(ge=0.0, le=23.0)]
+
+
+class WorkingHours(BaseModel):
+    __root__: Annotated[
+        List[Hour],
+        Field(
+            description='Inclusive range of 24 hour start and end integer values.',
+            max_items=2,
+            min_items=2,
+            title='Working Hours',
+        ),
+    ]
+
+
+class Dates(BaseModel):
+    __root__: Annotated[
+        List[date],
+        Field(
+            description='Two dates are an inclusive range and 1, 3, or more dates represent a set of dates.',
+            min_items=1,
+            title='Dates - Range or Set',
+        ),
+    ]
+
+    @no_type_check
+    @validator('__root__')
+    def is_unique(cls, v):
+        if v and 1 < len(v) != len(set(v)):
+            raise ValueError('dates must be unique')
+        return v
+
+
+class Holiday(BaseModel):
+    label: Annotated[Optional[str], Field(examples=['public holiday'])] = ''
+    at: Dates
+
+
+class Holidays(BaseModel):
+    __root__: Annotated[
+        List[Holiday],
+        Field(
+            description='Optionally labeled dates of non-working days.',
+            min_items=0,
+            title='Holidays',
+        ),
+    ]
+
+
+class Arbejdstimer(BaseModel):
+    api: Annotated[
+        Optional[int],
+        Field(
+            description='API version of the application this configuration targets.',
+            title='API Version',
+        ),
+    ] = 1
+    application: Annotated[
+        Optional[Application],
+        Field(
+            description='Name of the application this configuration targets.',
+            title='Application Name',
+        ),
+    ] = Application.arbejdstimer
+    operator: Annotated[
+        Operator,
+        Field(
+            description='Logic combining the given specific values with the application defaults.',
+            title='Logic for Combination with Default Values',
+        ),
+    ]
+    holidays: Optional[Holidays] = None
+    working_hours: Optional[WorkingHours] = None
---
+++ packages/arbejdstimer/arbejdstimer-2022.6.27-py2.py3-none-any.whl/tests/test_api.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=line-too-long,missing-docstring,reimported,unused-import,unused-variable
+import datetime as dti
+
+import pytest
+from pydantic.error_wrappers import ValidationError
+
+import arbejdstimer.api as api
+import arbejdstimer.arbejdstimer as at
+import tests.conftest as fix
+
+
+def _subs(count: int, what: str) -> str:
+    """DRY."""
+    return f'{count} validation error{"" if count == 1 else "s"} for {what}'
+
+
+def test_api_hour_noon():
+    twelve = api.Hour(__root__='12')  # type: ignore
+    assert twelve.__root__ == 12
+
+
+def test_api_hour_42():
+    with pytest.raises(ValidationError, match=_subs(1, 'Hour')) as err:
+        _ = api.Hour(__root__='42')  # type: ignore
+    assert '\n__root__\n  ensure this value is less than or equal to 23' in str(err.value)
+
+
+def test_api_hour_negative():
+    with pytest.raises(ValidationError, match=_subs(1, 'Hour')) as err:
+        _ = api.Hour(__root__='-1')  # type: ignore
+    assert '\n__root__\n  ensure this value is greater than or equal to 0' in str(err.value)
+
+
+def test_api_hour_no_digit():
+    with pytest.raises(ValidationError, match=_subs(1, 'Hour')) as err:
+        _ = api.Hour(__root__='_')  # type: ignore
+    assert '\n__root__\n  value is not a valid integer' in str(err.value)
+
+
+def test_api_date_range_wun():
+    wun = api.Dates(__root__=['2021-12-31'])  # type: ignore
+    assert wun.__root__ == [dti.date(2021, 12, 31)]
+
+
+def test_api_date_range_two():
+    two = api.Dates(__root__=['2021-12-31', '2022-01-01'])  # type: ignore
+    assert two.__root__ == [dti.date(2021, 12, 31), dti.date(2022, 1, 1)]
+
+
+def test_api_date_range_three():
+    three = api.Dates(__root__=['2021-12-31', '2022-01-01', '2022-01-02'])  # type: ignore
+    assert three.__root__ == [dti.date(2021, 12, 31), dti.date(2022, 1, 1), dti.date(2022, 1, 2)]
+
+
+def test_api_date_range_duplicate():
+    with pytest.raises(ValidationError, match=_subs(1, 'Dates')) as err:
+        _ = api.Dates(__root__=['2021-12-31', '2021-12-31'])  # type: ignore
+    assert '\n__root__\n  dates must be unique' in str(err.value)
+
+
+def test_api_working_hours_nine_to_five():
+    nine = api.Hour(__root__='9')  # type: ignore
+    five = api.Hour(__root__='17')  # type: ignore
+    assert api.WorkingHours(__root__=[nine, five]).__root__ == [nine, five]
+
+
+def test_api_working_hours_empty_list():
+    with pytest.raises(ValidationError, match=_subs(1, 'WorkingHours')) as err:
+        _ = api.WorkingHours(__root__=[])  # type: ignore
+    assert '\n__root__\n  ensure this value has at least 2 items' in str(err.value)
+
+
+def test_api_working_hours_nine_single_value():
+    nine = api.Hour(__root__='9')  # type: ignore
+    with pytest.raises(ValidationError, match=_subs(1, 'WorkingHours')) as err:
+        _ = api.WorkingHours(__root__=[nine])  # type: ignore
+    assert '\n__root__\n  ensure this value has at least 2 items' in str(err.value)
+
+
+def test_api_working_hours_nine_nine_five_triplet():
+    nine = api.Hour(__root__='9')  # type: ignore
+    five = api.Hour(__root__='17')  # type: ignore
+    with pytest.raises(ValidationError, match=_subs(1, 'WorkingHours')) as err:
+        _ = api.WorkingHours(__root__=[nine, nine, five])  # type: ignore
+    assert '\n__root__\n  ensure this value has at most 2 items' in str(err.value)
+
+
+def test_api_holiday_wun():
+    wun = api.Dates(__root__=['2021-12-31'])  # type: ignore
+    holiday = api.Holiday(at=wun)
+    assert holiday.at.__root__ == [dti.date(2021, 12, 31)]
+    assert holiday.json() == '{"label": "", "at": ["2021-12-31"]}'
+    data = {'label': '', 'at': ['2021-12-31']}
+    # noinspection Pydantic
+    another_holiday = api.Holiday(**data)  # type: ignore
+    assert holiday == another_holiday
+
+
+def test_api_today():
+    # noinspection Pydantic
+    cfg = api.Arbejdstimer(**fix.CFG_PY_TODAY_HOLIDAY)
+    today_rep = fix.TODAY.strftime(at.DATE_FMT)
+    expected = (
+        '{"api": 1, "application": "arbejdstimer", "operator": "or",'
+        f' "holidays": [{{"label": "", "at": ["{today_rep}"]}}], "working_hours": [8, 17]}}'
+    )
+    assert cfg.json() == expected
