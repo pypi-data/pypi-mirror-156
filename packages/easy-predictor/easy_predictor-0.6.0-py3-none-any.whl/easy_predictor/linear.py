43bbc4541c45dbfa26117af9b76f667b95f4d374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:54 +0000

    Add package easy-predictor/0.6.0/easy_predictor-0.6.0-py3-none-any.whl

---
+++ packages/easy-predictor/easy_predictor-0.6.0-py3-none-any.whl/easy_predictor/linear.py
@@ -0,0 +1,37 @@
+from sklearn.linear_model import LinearRegression
+import pandas as pd 
+
+def predict(data: str,data_type: str,x: str, y: str,value: int):
+
+    ERROR_LIST = {
+        'null_value': "'value' Input can't be null",
+        'null_dataset': "'data' can't be null"
+    }
+
+    value_list = [value]
+
+    if data_type == 'xlsx':
+        if len(value_list) != 0:
+            data = pd.read_excel(data)
+
+            train = data[[x]].values
+            predict_value = data[[y]].values
+            
+            lr = LinearRegression()
+            lr.fit(train,predict_value) 
+            print(lr.predict([[value]]))
+        else:
+            print(ERROR_LIST['null_value'])
+
+    if data_type == 'csv':   
+        if len(value_list) != 0:
+            data = pd.read_csv(data)
+
+            train = data[[x]].values
+            predict_value = data[[y]].values
+            
+            lr = LinearRegression()
+            lr.fit(train,predict_value) 
+            print(lr.predict([[value]]))
+        else:
+            print(ERROR_LIST['null_value'])
