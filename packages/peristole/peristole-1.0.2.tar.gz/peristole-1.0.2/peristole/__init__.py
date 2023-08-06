edac7dcf81cf4385956b228c07e7c872ec7b7401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:41 +0000

    Add package peristole/1.0.2/peristole-1.0.2.tar.gz

---
+++ packages/peristole/peristole-1.0.2.tar.gz/peristole-1.0.2/peristole/__init__.py
@@ -0,0 +1,8 @@
+import os
+
+print("PERISTOLE : PackagE that geneRates tIme delay plotS due To gravitatiOnaL lEnsing")
+
+from import amp.py
+from import geo_grav_td.py
+from import lat_td.py
+from import rot_td.py
