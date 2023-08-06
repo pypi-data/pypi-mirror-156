b0faae3be816e86951b7732ae8b295205a54d75d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:54 +0000

    Add package messenger-server-from-nadezhda/0.3.0/messenger_server_from_nadezhda-0.3.0-py3-none-any.whl

---
+++ packages/messenger-server-from-nadezhda/messenger_server_from_nadezhda-0.3.0-py3-none-any.whl/server/common/utils.py
@@ -0,0 +1,38 @@
+""" Функции отправки и приема сообщений."""
+import json
+import sys
+
+from variables import MAX_PACKAGE_LENGTH, ENCODING
+
+sys.path.append('../')
+
+
+# @log
+def get_message(client):
+    """
+    Функция приёма сообщений от удалённых компьютеров.
+    Принимает сообщения JSON, декодирует полученное сообщение
+    и проверяет что получен словарь.
+    :param client: сокет для передачи данных.
+    :return: словарь - сообщение.
+    """
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    json_response = encoded_response.decode(ENCODING)
+    response = json.loads(json_response)
+    if isinstance(response, dict):
+        return response
+    raise TypeError
+
+
+# @log
+def send_message(sock, message):
+    """
+    Функция отправки словарей через сокет.
+    Кодирует словарь в формат JSON и отправляет через сокет.
+    :param sock: сокет для передачи
+    :param message: словарь для передачи
+    :return: ничего не возвращает
+    """
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
