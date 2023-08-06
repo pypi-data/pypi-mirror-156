f1a9c49ec042e37efe1aecb6ca7d7baf68c79d92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:25 +0000

    Add package scriptlink/0.0.2/scriptlink-0.0.2.tar.gz

---
+++ packages/scriptlink/scriptlink-0.0.2.tar.gz/scriptlink-0.0.2/scriptlink/__init__.py
@@ -0,0 +1,17 @@
+"""
+Bunch of stuff
+"""
+from .constants import *
+from .scriptlink import *
+__version__ = "1.1.0"
+__author__ = "Lopht"
+__copyright__ = """
+    Copyright (c) 2020-2022, Advasoft
+
+    Permission to use, copy, modify, and distribute this software and its
+    documentation for any purpose and without fee or royalty is hereby
+    granted, provided that the above copyright notice appear in all copies
+    and that both that copyright notice and this permission notice appear
+    in supporting documentation or portions thereof, including
+    modifications, that you make.
+"""
---
+++ packages/scriptlink/scriptlink-0.0.2.tar.gz/scriptlink-0.0.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2' 
+DESCRIPTION = 'scriptlink'
+LONG_DESCRIPTION = 'a simple interface to a set of c++ functions for manipulating screen images and simulating input'
+
+# Setting up
+setup(
+       # the name must match the folder name 'verysimplemodule'
+        name="scriptlink", 
+        version=VERSION,
+        author="Lopht",
+        author_email="<bobjozeg@yahoo.com>",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        install_requires=["numpy","cv2"],
+        
+        keywords=['python', 'scriptlink'],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Education",
+            "Programming Language :: Python :: 3",
+            "Operating System :: Microsoft :: Windows",
+        ]
+)
