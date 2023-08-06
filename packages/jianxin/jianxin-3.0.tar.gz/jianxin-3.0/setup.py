328e86dee828176e9cbe408039bf2a344ddbba9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:17 +0000

    Add package jianxin/3.0/jianxin-3.0.tar.gz

---
+++ packages/jianxin/jianxin-3.0.tar.gz/jianxin-3.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+setuptools.setup(
+    name="jianxin",  # 自定义工具包的名字
+    version="3.0",  # 版本号
+    author="liweiren",  # 作者名字
+    author_email="niuledabi@qq.com",  # 作者邮箱
+    license='MIT',  # 许可协议
+    url="https://pypi.org/",  # 项目开源地址
+    packages=setuptools.find_packages(),  #自动发现自定义工具包中的所有   包和  子包
+)
