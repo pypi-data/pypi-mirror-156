931793392356ff67da36a951b50a1a2abf247bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:35:31 +0000

    Add package basxconnect/0.4.6/basxconnect-0.4.6.tar.gz

---
+++ packages/basxconnect/basxconnect-0.4.6.tar.gz/basxconnect-0.4.6/basxconnect/core/layouts/editperson/common/contributions_tab.py
@@ -0,0 +1,47 @@
+from bread import layout
+from bread.utils import Link, ModelHref, pretty_modelname
+from django.shortcuts import get_object_or_404
+from django.utils.translation import gettext_lazy as _
+
+from basxconnect.contributions.models import Contribution
+from basxconnect.core.layouts.editperson.common import utils
+from basxconnect.core.models import Person
+
+R = layout.grid.Row
+C = layout.grid.Col
+
+
+def contributions_tab(request):
+    person = get_object_or_404(Person, pk=request.resolver_match.kwargs["pk"])
+    return layout.tabs.Tab(
+        _("Contributions"),
+        utils.grid_inside_tab(
+            R(
+                utils.tiling_col(
+                    layout.datatable.DataTable.from_queryset(
+                        person.contributions.all(),
+                        columns=[
+                            "_import.date",
+                            "date",
+                            "note",
+                            "debitaccount",
+                            "creditaccount",
+                            "amount_formatted",
+                        ],
+                        title="",
+                        primary_button=layout.button.Button.from_link(
+                            Link(
+                                href=ModelHref(
+                                    Contribution, "add", query={"person": person.id}
+                                ),
+                                label=_("Add %s") % pretty_modelname(Contribution),
+                            ),
+                            icon=layout.icon.Icon("add", size=20),
+                        ),
+                        backurl=request.get_full_path(),
+                        prevent_automatic_sortingnames=True,
+                    )
+                )
+            )
+        ),
+    )
