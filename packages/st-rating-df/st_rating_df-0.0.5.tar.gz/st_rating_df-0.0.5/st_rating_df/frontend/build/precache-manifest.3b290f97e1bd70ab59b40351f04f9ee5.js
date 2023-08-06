03393696dd254880f32ff04b611ec6fbadbfc17e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:25 +0000

    Add package st-rating-df/0.0.5/st_rating_df-0.0.5.tar.gz

---
+++ packages/st-rating-df/st_rating_df-0.0.5.tar.gz/st_rating_df-0.0.5/st_rating_df/frontend/build/precache-manifest.3b290f97e1bd70ab59b40351f04f9ee5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c838d977957ac7c3a19ca055e51a153e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b292bef346fdd529b6b8",
+    "url": "./static/js/2.77e3f2ed.chunk.js"
+  },
+  {
+    "revision": "ff9d73dc3e8582ec316cab4165756e13",
+    "url": "./static/js/2.77e3f2ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1061abf471ec1026120c",
+    "url": "./static/js/main.120dd759.chunk.js"
+  },
+  {
+    "revision": "ea80fab2391112633055",
+    "url": "./static/js/runtime-main.b44bff0a.js"
+  }
+]);
