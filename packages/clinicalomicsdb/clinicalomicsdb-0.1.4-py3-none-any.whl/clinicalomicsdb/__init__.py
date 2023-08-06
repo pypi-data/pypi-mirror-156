c6e8c6cde1f83277be0178f26425b2b690bf2e6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:24:31 +0000

    Add package clinicalomicsdb/0.1.4/clinicalomicsdb-0.1.4-py3-none-any.whl

---
+++ packages/clinicalomicsdb/clinicalomicsdb-0.1.4-py3-none-any.whl/clinicalomicsdb/__init__.py
@@ -0,0 +1,21 @@
+#   Copyright 2022 Chang In Moon changin.moon@bcm.edu
+__version__ = '0.1.4'
+
+import io
+import pandas as pd
+from .database_download import download, get_dataset_link
+from .file_download import download_text as _download_text
+from .exceptions import BaseError, BaseWarning, InvalidParameterError, NoInternetError, OldPackageVersionWarning
+
+
+def list_datasets():
+    """List all available datasets."""
+
+    dataset_list_url = "https://bcm.box.com/shared/static/hfm7swxvy4x48j9a8novny2432gh750u.csv"
+
+    try:
+        dataset_list_text = _download_text(dataset_list_url)
+    except NoInternetError:
+        raise NoInternetError("Insufficient internet to download available dataset info. Check your internet connection.") from None
+
+    return pd.read_csv(io.StringIO(dataset_list_text), header=True, index_col=0)
---
+++ packages/clinicalomicsdb/clinicalomicsdb-0.1.4-py3-none-any.whl/clinicalomicsdb/database_download.py
@@ -0,0 +1,23 @@
+# Copyright 2022 Chang In Moon changin.moon@bcm.edu
+# will implement a better structure for downloading data in future versions...
+# we want to try import a csv file that contains each study name and it's box link
+import io
+import pandas as pd
+from pyrsistent import T
+from .file_download import download_text as _download_text #add .
+from .exceptions import BaseError, BaseWarning, InvalidParameterError, NoInternetError, OldPackageVersionWarning #add .
+
+def get_dataset_link():
+    dataset_link_url = "https://bcm.box.com/shared/static/1rs6wid9em7tewjpqchnar4wlr75l3ml.csv"
+    try:
+        dataset_link_text = _download_text(dataset_link_url)
+    except NoInternetError:
+        raise NoInternetError("Insufficient internet to download available dataset info. Check your internet connection.") from None
+    return pd.read_csv(io.StringIO(dataset_link_text), header=True, index_col=0)
+
+def download(dataset):
+    dataset = dataset.lower()
+    link = get_dataset_link()
+    # if the dataset is not specified or not found in the csv file list, return an error.    
+
+    return link
