80930533b66bb9d44db4fca586192d4b06551f8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:35:31 +0000

    Add package servicefoundry/0.1.58/servicefoundry-0.1.58-py3-none-any.whl

---
+++ packages/servicefoundry/servicefoundry-0.1.58-py3-none-any.whl/servicefoundry/sfy_magic_init_pack/magic_pack.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from platform import python_version
+from typing import Dict
+
+from pydantic import BaseModel
+
+from servicefoundry.io.input_hook import InputHook
+from servicefoundry.io.output_callback import OutputCallBack
+from servicefoundry.lib.exceptions import ConfigurationException
+from servicefoundry.service_definition.definition import (
+    ServiceFoundryBuildDefinition,
+    ServiceFoundryDefinition,
+)
+from servicefoundry.sfy_init.pack import Pack
+
+
+class Parameters(BaseModel):
+    name: str
+    workspace: str
+
+
+class MagicPack(Pack):
+    def __init__(
+        self,
+        python_file: str,
+        additional_requirements: Dict[str, str],
+        parameters: Parameters,
+    ):
+        self.python_file = python_file
+        if not Path(python_file).exists():
+            raise ConfigurationException(f"Couldn't find file {self.python_file}.")
+        self.additional_requirements = additional_requirements
+        self.parameters = parameters
+
+    def get_default_service_name(self):
+        return "should-be-changed"
+
+    def get_description(self):
+        return None
+
+    def ask_questions(self, input_hook: InputHook, output_hook: OutputCallBack):
+        pass
+
+    def get_default_definition(self) -> ServiceFoundryDefinition:
+        definition = super().get_default_definition()
+        definition.service.name = self.parameters.name
+        definition.service.workspace = self.parameters.workspace
+        definition.build = ServiceFoundryBuildDefinition(
+            build_pack="sfy_build_pack_python",
+            options={"python_version": f"python:{python_version()}"},
+        )
+        return definition
