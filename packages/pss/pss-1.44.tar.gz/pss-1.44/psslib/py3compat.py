c2215b9a6ad86ec5ef635ba2964b8afc2570ba3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:02 +0000

    Add package pss/1.44/pss-1.44.tar.gz

---
+++ packages/pss/pss-1.44.tar.gz/pss-1.44/psslib/py3compat.py
@@ -0,0 +1,24 @@
+#-------------------------------------------------------------------------------
+# pss: py3compat.py
+#
+# This used to be a compatibility layer between Python 2 and 3; now Python 2
+# is no longer supported, so this is just a collection of a few utility
+# functions that may be refactored away in the future.
+#
+# Eli Bendersky (eliben@gmail.com)
+# This code is in the public domain
+#-------------------------------------------------------------------------------
+import sys
+PY3 = sys.version_info[0] == 3
+assert PY3, '''\
+Python 2 is no longer supported by pss; if you need to use Python 2,
+please download an older pss version (such as version 1.43).
+'''
+
+def tostring(b):
+    """ Convert the given bytes or string object to string
+    """
+    if isinstance(b, bytes):
+        return b.decode('utf-8')
+    else:
+        return b
