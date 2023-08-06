c278f44d26c0878e68b3e2cdc6db6da60ef5fff0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:31:12 +0000

    Add package UnicodeTokenizer/0.0.1/UnicodeTokenizer-0.0.1.tar.gz

---
+++ packages/UnicodeTokenizer/UnicodeTokenizer-0.0.1.tar.gz/UnicodeTokenizer-0.0.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from os import path
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(path.join(here, 'readme.md')) as f:
+    long_description = f.read()
+
+
+setup(
+    name="UnicodeTokenizer",
+    packages=find_packages(),
+    version='0.0.1',
+    description='UnicodeTokenizer: tokenize all Unicode text',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires='>=3.0',
+    url='https://github.com/laohur/UnicodeTokenizer',
+    keywords=['UnicodeTokenizer', 'Tokenizer', 'Unicode', 'laohur'],
+    author='laohur',
+    author_email='laohur@gmail.com',
+    license='[Anti-996 License](https: // github.com/996icu/996.ICU/blob/master/LICENSE)',
+)
