4b0ae15890e832ac4c5fc5e07fe84189c7691532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:22:28 +0000

    Add package devcloudcli/1.3.0/devcloudcli-1.3.0-py3-none-any.whl

---
+++ packages/devcloudcli/devcloudcli-1.3.0-py3-none-any.whl/src/scripts/dl-model-services/dl-model-tools/nncf/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Copyright (C) 2018-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+pip3 install nncf[tf]
+echo -e "\e[1;32mnncf is installed successfully\e[0m"
---
+++ packages/devcloudcli/devcloudcli-1.3.0-py3-none-any.whl/src/scripts/dl-model-services/inference-runtimes/onnx/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+# Copyright (C) 2018-2021 Intel Corporation
+# SPDX-License-Identifier: Apache-2.0
+
+docker pull openvino/onnxruntime_ep_ubuntu18
+echo -e "\e[1;32mThe onnx is image is pulled\e[0m"
+echo -e "\e[1;36mTo view the images : docker images\e[0m"
