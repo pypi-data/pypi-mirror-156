33e3faf59077391dae73849f6a9feb2e37460924
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:55 +0000

    Add package pulp-npm/0.1.0a4/pulp_npm-0.1.0a4-py3-none-any.whl

---
+++ packages/pulp-npm/pulp_npm-0.1.0a4-py3-none-any.whl/pulp_npm/app/__init__.py
@@ -0,0 +1,10 @@
+from pulpcore.plugin import PulpPluginAppConfig
+
+
+class PulpNpmPluginAppConfig(PulpPluginAppConfig):
+    """Entry point for the npm plugin."""
+
+    name = "pulp_npm.app"
+    label = "npm"
+    version = "0.1.0a4"
+    python_package_name = "pulp-npm"
