1a314294085957632949bd5d3effb4bafbe608be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:43:54 +0000

    Add package ParrotAnafiController/1.0.1/ParrotAnafiController-1.0.1.tar.gz

---
+++ packages/ParrotAnafiController/ParrotAnafiController-1.0.1.tar.gz/ParrotAnafiController-1.0.1/README.md
@@ -0,0 +1,19 @@
+# Parrot-Controller
+ParrotController is an Olympe wrapper for the Parrot Anafi drone.
+This API allows you to connect and interface with the Parrot Anafi;
+namely, provides movement and camera functionalities
+
+## Getting Started
+Install the library:
+1. Clone the repo
+  ```sh
+  git clone https://github.com/boredbot2/Parrot-Controller.git
+  ```
+2. Pip install
+  ```sh
+  pip install ParrotAnafiController
+  ```
+
+## Authors
+* **[Kevyn Angueira Irizarry](https://github.com/boredbot2)**
+
---
+++ packages/ParrotAnafiController/ParrotAnafiController-1.0.1.tar.gz/ParrotAnafiController-1.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+long_desc = open("README.md").read()
+required = ["cv2>=2.0.2", "requests>=2.27.1", "olympe>=0.0.1"]
+
+setuptools.setup(
+    name="ParrotAnafiController",
+    version="1.0.1",
+    author="Kevyn Angueira Irizarry",
+    author_email="kevyn.angueira@gmail.com",
+    description="Wrapper for Parrot Olympe",
+    url="https://github.com/boredbot2/Parrot-Controller",
+    key_words="Parrot Anafi, Drone, Olympe",
+    install_requires=required,
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+)
+
