7273f666a346c9fb1452df88a4dc9e4a0128efb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:53 +0000

    Add package kachery-cloud/0.1.18/kachery_cloud-0.1.18.tar.gz

---
+++ packages/kachery-cloud/kachery_cloud-0.1.18.tar.gz/kachery_cloud-0.1.18/kachery_cloud/load_file.py
@@ -0,0 +1,142 @@
+import os
+from typing import Union
+import requests
+import random
+from .get_kachery_cloud_dir import get_kachery_cloud_dir
+from ._kacherycloud_request import _kacherycloud_request
+from .store_file_local import _compute_file_hash
+from ._fs_operations import _makedirs, _chmod_file
+
+
+def load_file(uri: str, *, verbose: bool=False, local_only: bool=False) -> Union[str, None]:
+    if local_only:
+        return load_file_local(uri)
+    if uri.startswith('/'):
+        if os.path.exists(uri):
+            return uri
+        else:
+            raise Exception(f'File does not exist: {uri}')
+    if uri.startswith('sha1://'):
+        x = load_file_local(uri)
+        if x is not None:
+            return x
+        sha1 = uri.split('?')[0].split('/')[2]
+        return _load_sha1_file_from_cloud(sha1, verbose=verbose)
+        
+    assert uri.startswith('ipfs://'), f'Invalid or unsupported URI: {uri}'
+    a = uri.split('?')[0].split('/')
+    assert len(a) >= 3, f'Invalid or unsupported URI: {uri}'
+    cid = a[2]
+
+    kachery_cloud_dir = get_kachery_cloud_dir()
+    e = cid[-6:]
+    parent_dir = f'{kachery_cloud_dir}/ipfs/{e[0]}{e[1]}/{e[2]}{e[3]}/{e[4]}{e[5]}'
+    filename = f'{parent_dir}/{cid}'
+    if os.path.exists(filename):
+        return filename
+
+    payload = {
+        'type': 'findIpfsFile',
+        'cid': cid
+    }
+    response= _kacherycloud_request(payload)
+    found = response['found']
+    if found:
+        url = response['url']
+    else:
+        raise Exception(f'File not found: {uri}')
+        # url = f'https://{cid}.ipfs.dweb.link'
+        # url = f'https://cloudflare-ipfs.com/ipfs/{cid}'
+        # url = f'https://ipfs.filebase.io/ipfs/{cid}'
+
+    if verbose:
+        print(f'Loading file from kachery cloud: {uri}')    
+    if not os.path.exists(parent_dir):
+        _makedirs(parent_dir)
+    tmp_filename = f'{filename}.tmp.{_random_string(8)}'
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(tmp_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    try:
+        os.rename(tmp_filename, filename)
+        _chmod_file(filename)
+    except:
+        if not os.path.exists(filename): # maybe some other process beat us to it
+            raise Exception(f'Unexpected problem moving file {tmp_filename}')
+    return filename
+
+def _load_sha1_file_from_cloud(sha1: str, *, verbose: bool):
+    payload = {
+        'type': 'findFile',
+        'hashAlg': 'sha1',
+        'hash': sha1
+    }
+    response= _kacherycloud_request(payload)
+    found = response['found']
+    uri = f'sha1://{sha1}'
+    if found:
+        url = response['url']
+    else:
+        raise Exception(f'File not found: {uri}')
+
+    kachery_cloud_dir = get_kachery_cloud_dir()
+    e = sha1
+    parent_dir = f'{kachery_cloud_dir}/sha1/{e[0]}{e[1]}/{e[2]}{e[3]}/{e[4]}{e[5]}'
+    filename = f'{parent_dir}/{sha1}'
+    if verbose:
+        print(f'Loading file from kachery cloud: {uri}') 
+    if not os.path.exists(parent_dir):
+        _makedirs(parent_dir)
+    tmp_filename = f'{filename}.tmp.{_random_string(8)}'
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(tmp_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                f.write(chunk)
+    try:
+        os.rename(tmp_filename, filename)
+        _chmod_file(filename)
+    except:
+        if not os.path.exists(filename): # maybe some other process beat us to it
+            raise Exception(f'Unexpected problem moving file {tmp_filename}')
+    return filename
+
+def load_file_local(uri: str):
+    query = _get_query_from_uri(uri)
+    assert uri.startswith('sha1://'), f'Invalid local URI: {uri}'
+    a = uri.split('?')[0].split('/')
+    assert len(a) >= 3, f'Invalid or unsupported URI: {uri}'
+    sha1 = a[2]
+
+    kachery_cloud_dir = get_kachery_cloud_dir()
+
+    s = sha1
+    parent_dir = f'{kachery_cloud_dir}/sha1/{s[0]}{s[1]}/{s[2]}{s[3]}/{s[4]}{s[5]}'
+    filename = f'{parent_dir}/{sha1}'
+    if os.path.exists(filename):
+        return filename
+    
+    if 'location' in query:
+        location = query['location']
+        if os.path.isabs(location) and os.path.exists(location):
+            sha1_2 = _compute_file_hash(location, 'sha1')
+            if sha1_2 == sha1:
+                return location
+        raise Exception(f'Unable to find local file: {uri}')
+
+def _get_query_from_uri(uri: str):
+    a = uri.split('?')
+    ret = {}
+    if len(a) < 2: return ret
+    b = a[1].split('&')
+    for c in b:
+        d = c.split('=')
+        if len(d) == 2:
+            ret[d[0]] = d[1]
+    return ret
+
+def _random_string(num_chars: int) -> str:
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+    return ''.join(random.choice(chars) for _ in range(num_chars))
