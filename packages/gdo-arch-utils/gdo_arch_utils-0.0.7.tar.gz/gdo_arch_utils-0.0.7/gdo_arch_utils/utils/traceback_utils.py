018c457fc0b07754a305d01dc3270d367c572f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:53 +0000

    Add package gdo-arch-utils/0.0.7/gdo_arch_utils-0.0.7.tar.gz

---
+++ packages/gdo-arch-utils/gdo_arch_utils-0.0.7.tar.gz/gdo_arch_utils-0.0.7/gdo_arch_utils/utils/traceback_utils.py
@@ -0,0 +1,10 @@
+from utils.log_utils import LogUtils as log_utils
+
+logger = log_utils.get_logger()
+
+
+class TracebackUtils:
+    @staticmethod
+    def get_trace():
+        import traceback
+        traceback.print_stack()
