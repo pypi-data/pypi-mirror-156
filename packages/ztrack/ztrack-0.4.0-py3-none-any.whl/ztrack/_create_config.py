fce23b436d8d32c12d78eb74db3a9ee1d01364cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:28:17 +0000

    Add package ztrack/0.4.0/ztrack-0.4.0-py3-none-any.whl

---
+++ packages/ztrack/ztrack-0.4.0-py3-none-any.whl/ztrack/_create_config.py
@@ -0,0 +1,21 @@
+from ztrack.gui.create_config import CreateConfigWindow
+from ztrack.gui.utils.launch import launch
+from ztrack.utils.file import get_paths_for_config_creation
+
+
+def create_config(
+    inputs,
+    recursive,
+    same_config,
+    overwrite,
+    verbose,
+):
+    video_paths, save_paths = get_paths_for_config_creation(
+        inputs, recursive, same_config, overwrite
+    )
+    launch(
+        CreateConfigWindow,
+        videoPaths=video_paths,
+        savePaths=save_paths,
+        verbose=verbose,
+    )
