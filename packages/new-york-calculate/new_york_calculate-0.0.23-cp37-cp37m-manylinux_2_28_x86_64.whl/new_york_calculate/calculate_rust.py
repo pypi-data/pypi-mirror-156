69c8fd7d8c1524e1c0ea98681eb9622cd1741344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:38 +0000

    Add package new-york-calculate/0.0.23/new_york_calculate-0.0.23-cp37-cp37m-manylinux_2_28_x86_64.whl

---
+++ packages/new-york-calculate/new_york_calculate-0.0.23-cp37-cp37m-manylinux_2_28_x86_64.whl/new_york_calculate/calculate_rust.py
@@ -0,0 +1,32 @@
+from .new_york_calculate import PyCalculate
+
+
+class CalculateRust:
+    def __init__(self, intraday_results, initial_balance=3000, stake=10, gain=1.0, profit=0.5):
+        self.instance = PyCalculate(candles=intraday_results, initial_balance=initial_balance, stake=stake, gain=gain,
+                                    profit=profit)
+        self.initial_balance = initial_balance
+        self.stake = stake
+        self.gain = gain
+        self.profit = profit
+
+    def calculate(self, results):
+        result = self.instance.calculate(list(results))
+
+        return {
+            'initial_balance': self.initial_balance,
+            'min_balance': result[4],
+            'stake': self.stake,
+            'profit': self.profit,
+            'gain': self.gain,
+            'balance': result[1],
+            'wallet': result[0],
+            'base_real': result[2],
+            'base_expected': result[3],
+            'drawdown': result[5],
+            'opened_orders': result[6],
+            'executed_orders': result[7],
+            'avg_wait': result[8],
+            'score': result[9],
+            'successful_ratio': result[10],
+        }
