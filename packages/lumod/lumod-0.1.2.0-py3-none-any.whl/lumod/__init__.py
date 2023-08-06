8edbecda3faab5254c854437b6c395793386a79f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:13:47 +0000

    Add package lumod/0.1.2.0/lumod-0.1.2.0-py3-none-any.whl

---
+++ packages/lumod/lumod-0.1.2.0-py3-none-any.whl/lumod/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Lumped Models (LuMod) for hydrology
+
+Author:
+Saul Arciniega Esparza
+Hydrogeology Group, Faculty of Engineering,
+National Autonomous University of Mexico
+zaul.ae@gmail.com | sarciniegae@comunidad.unam.mx
+"""
+
+__version__ = '0.1.0'
+__author__ = 'Saul Arciniega Esparza'
+
+from lumod.tools.monte_carlo import MonteCarlo
+from lumod.tools.time_series import load_example
+from .fluxes import pet_models
+from . import models
+from . import tools
+
