8705292428c46a1069f2a5a17a64accfc8928060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:10 +0000

    Add package TPLSp/1.0.13/TPLSp-1.0.13.tar.gz

---
+++ packages/TPLSp/TPLSp-1.0.13.tar.gz/TPLSp-1.0.13/src/TPLSp/TPLS_cv.py
@@ -0,0 +1,59 @@
+import numpy as np
+from . import TPLS
+from . import TPLSinputchecker
+
+class TPLS_cv:
+    
+    def __init__(self, X, Y, CVfold, NComp = 25, W = None, nmc = 0): 
+        """ Constructor method for fitting a cross-validation T-PLS model
+            X       Numerical matrix of predictors. Typically single-trial betas where each column is a voxel and row is observation
+            Y       Variable to predict. Binary 0 and 1 in case of classification, continuous variable in case of regression
+            CVfold  Cross-validation testing fold information. Can either be a vector or a matrix, the latter being more general.
+                    Vector: n-by-1 vector. Each element is a number ranging from 1 ~ numfold to identify which testing fold eachobservation belongs to
+                    Matrix: n-by-numfold matrix. Each column indicates the testing data with 1 and training data as 0.
+                    Example: For leave-one-out CV, Vector would be 1:n, Matrix form would be eye(n)
+                    Matrix form is more general as it can have same trial be in multiple test folds
+            NComp   (Optional) Number of PLS components to compute. Default is 25.
+            W       (Optional) Observation weights. Optional input. By default, all observations have equal weight.
+                    Can either be a n-by-1 vector or a n-by-nfold matrix where each column is observation weights in that CV fold
+            nmc     (Optional) 'no mean centering'. See TPLS for more detail.
+        """
+
+        # input checking
+        TPLSinputchecker.TPLSinputchecker(X,'X','mat',None,None,1); n = X.shape[0]
+        Y = TPLSinputchecker.TPLSinputchecker(Y,'Y','colvec',None,None,1)
+        if CVfold.ndim == 1:
+            CVfold = TPLSinputchecker.TPLSinputchecker(CVfold,'CVfold','colvec')
+        else:
+            TPLSinputchecker.TPLSinputchecker(CVfold,'CVfold','mat')
+        TPLSinputchecker.TPLSinputchecker(NComp,'NComp','scalar',None,1,0,1)
+        if W is None: W = np.ones((n,1))
+        W = np.atleast_2d(W) # could be a vector, could be a matrix
+        TPLSinputchecker.TPLSinputchecker(W,'W',None,None,0);
+        TPLSinputchecker.TPLSinputchecker(nmc,'nmc','scalar')
+        self.CVfold, self.numfold = self.prepCVfold(CVfold) # convert CVfold into matrix form, if not already
+        assert(n==Y.size and n==self.CVfold.shape[0] and n==W.shape[0]),'X, Y, W, and CV fold should have same number of rows'
+        if W.shape[1] == 1: W = np.repeat(W,self.numfold,1) # convert into matrix form, if not already
+        
+        self.NComp = NComp
+        self.cvMdls = []
+        for i in range(self.numfold):
+            print('Fold #'+str(i+1))
+            train = self.CVfold[:,i] == 0
+            self.cvMdls.append(TPLS.TPLS(X[train.flatten(),:],Y[train],NComp,W[train,i],nmc))
+
+    @staticmethod
+    def prepCVfold(inCVfold):
+        """ prepare CV fold data into a matrix form, which is more generalizable """
+        if inCVfold.shape[1] == 1: # vector
+            uniqfold = np.unique(inCVfold); nfold = len(uniqfold)
+            CVfold = np.zeros((inCVfold.shape[0],nfold))
+            for i in range(nfold):
+                CVfold[:,i] = 1 * np.atleast_2d(inCVfold == uniqfold[i]).T
+        elif inCVfold.shape[1] > 1: # matrix
+            nfold = inCVfold.shape[1]; CVfold = inCVfold
+            if np.any(np.logical_and(CVfold.flatten() != 0, CVfold.flatten() != 1)):
+                raise Exception('Non-binary element in matrix form CVfold. Perhaps you meant to use vector form?')
+        else:
+            raise Exception("unexpected size of CVfold")
+        return CVfold, nfold
---
+++ packages/TPLSp/TPLSp-1.0.13.tar.gz/TPLSp-1.0.13/src/TPLSp/evalTuningParam.py
@@ -0,0 +1,103 @@
+from doctest import DONT_ACCEPT_TRUE_FOR_1
+from email.utils import decode_rfc2231
+import numpy as np
+from scipy.stats import rankdata, spearmanr, pearsonr
+import matplotlib.pyplot as plt
+from . import TPLSinputchecker
+
+class evalTuningParam:
+    
+    def __init__(self, cvmdl, perftype, X, Y, compvec, threshvec, subfold = None): 
+        """ Evaluating cross-validation performance of a TPLS_cv model at compvec and threshvec
+            cvmdl       A TPLS_cv object
+            perftype    CV performance metric type. One of LLbinary, negMSE, Pearson, Spearman, AUC, ACC.
+            X           The same X as used in TPLS_cv.
+            Y           The same Y as used in TPLS_cv.
+            compvec     Vector of number of components to test in cross-validation.
+            threshvec   Vector of threshold level [0 1] to test in cross-validation.
+            subfold     (Optional) vector of subdivision within testing fold to calculate performance. For example scan run division within subject.
+        """
+
+        # input checking
+        assert(np.isin(perftype,['LLbinary','negMSE','Pearson','Spearman','AUC','ACC'])), 'Unknown performance metric'; self.type = perftype;
+        TPLSinputchecker.TPLSinputchecker(X,'X','mat',None,None,1); n, v = X.shape
+        Y = TPLSinputchecker.TPLSinputchecker(Y,'Y','colvec',None,None,1)
+        compvec = TPLSinputchecker.TPLSinputchecker(compvec,'compvec','rowvec',cvmdl.NComp,1,0,1); compvec = np.sort(compvec); self.compval = compvec;
+        threshvec = TPLSinputchecker.TPLSinputchecker(threshvec,'threshvec','rowvec',1,0); threshvec = np.sort(threshvec); self.threshval = threshvec;
+        if subfold is None:
+            subfold = np.ones((n,1))
+        else:
+            subfold = TPLSinputchecker.TPLSinputchecker(subfold,'subfold','colvec')
+        
+        # Perform CV prediction and performance measurement
+        perfmat = np.empty((len(compvec),len(threshvec),cvmdl.numfold)); perfmat.fill(np.nan)
+        for i in range(cvmdl.numfold):
+            print('Fold #'+str(i+1))
+            testCVfold = cvmdl.CVfold[:,i] == 1
+            Ytest = Y[testCVfold]
+            testsubfold = subfold[testCVfold]
+            uniqtestsubfold = np.unique(testsubfold)
+            for j in range(len(threshvec)):
+                predmat = cvmdl.cvMdls[i].predict(compvec,threshvec[j].item(),X[testCVfold.flatten(),:])
+                smallperfmat = np.empty((len(compvec),len(uniqtestsubfold)))
+                for k in range(len(uniqtestsubfold)):
+                    subfoldsel = testsubfold == uniqtestsubfold[k]
+                    smallperfmat[:,k] = self.util_perfmetric(predmat[subfoldsel.flatten(),:],Ytest[subfoldsel],perftype)
+                perfmat[:,j,i] = np.nanmean(smallperfmat, axis=1)
+        
+        # prepare output object
+        self.perfmat = perfmat; avgperfmat = np.nanmean(perfmat, axis=2) # mean performance
+        self.perf_best = np.nanmax(avgperfmat) # best mean performance
+        row_best,col_best = np.where(avgperfmat==self.perf_best) # coordinates of best point
+        self.compval_best = compvec[row_best[0]]; self.threshval_best = threshvec[col_best[0]]; # component and threshold of best point
+        standardError = np.nanstd(perfmat[row_best[0],col_best[0]])/np.sqrt(perfmat.shape[2]); # standard error of best point
+        candidates = avgperfmat[:,np.arange(col_best[0]+1)] > (self.perf_best-standardError)
+        col_1se,row_1se = np.where(candidates.T) # coordinates of 1SE point
+        self.perf_1se = avgperfmat[row_1se[0],col_1se[0]]; # performance of 1SE point
+        self.compval_1se = compvec[row_1se[0]]; self.threshval_1se = threshvec[col_1se[0]]
+        maxroute = np.max(avgperfmat,0); maxrouteind = np.argmax(avgperfmat,0)
+        self.best_at_threshold = np.concatenate((maxroute,threshvec,compvec[maxrouteind]),axis = 1)
+
+    def plot(self):
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        meansurf = np.nanmean(self.perfmat, axis = 2)
+        X, Y = np.meshgrid(self.threshval, self.compval)
+        ax.plot_surface(X, Y, meansurf, rstride=1, cstride=1,cmap='viridis', edgecolor='none')
+        ax.set_xlabel('Proportion of Voxels Left')
+        ax.set_ylabel('Number of PLS components')
+        ax.set_zlabel(self.type)
+        ax.scatter(self.threshval_best,self.compval_best,self.perf_best,c='blue')
+        ax.scatter(self.threshval_1se,self.compval_1se,self.perf_1se,c='red')
+        ax.scatter(self.threshval,self.best_at_threshold[:,2],self.best_at_threshold[:,0],c='orange')
+        plt.show()
+
+    @staticmethod
+    def util_perfmetric(predmat,testY,perftype):
+        print(predmat.shape)
+        print(testY.shape)
+        if perftype == 'LLbinary':
+            assert(np.all(np.logical_or(testY == 0, testY==1))), 'LL binary can be only calculated for binary measures'
+            predmat[testY!=1,:] = 1 - predmat[testY!=1,:] # flip probability
+            predmat[predmat>1] = 1; predmat[predmat<=0] = np.finfo(float).tiny # take care of probability predictions outside of range
+            Perf = np.nanmean(np.log(predmat),0)
+        elif perftype == 'negMSE':
+            Perf = -np.nanmean((predmat-testY)**2,0)
+        elif perftype == 'ACC':
+            assert(np.all(np.logical_or(testY == 0, testY==1))), 'Accuracy can be only calculated for binary measures'
+            Perf = np.nanmean(1*(testY==1 & (predmat>0.5)),0)
+        elif perftype == 'AUC':
+            assert(np.all(np.logical_or(testY == 0, testY==1))), 'AUC can be only calculated for binary measures'
+            n = len(testY); num_pos = sum(testY==1); num_neg = n - num_pos
+            Perf = 0.5 * np.ones(predmat.shape[1])
+            if (num_pos > 0 & num_pos < n):
+                ranks = rankdata(predmat,axis=0); Perf = ( sum( ranks[testY==1,:] ) - num_pos * (num_pos+1)/2) / ( num_pos * num_neg)
+        elif perftype == 'Pearson':
+            Perf = np.zeros(predmat.shape[1])
+            for i in range(predmat.shape[1]):
+                Perf[i] = pearsonr(testY,predmat[:,i])[0]
+        elif perftype == 'Spearman':
+            Perf = np.zeros(predmat.shape[1])
+            for i in range(predmat.shape[1]):
+                Perf[i] = spearmanr(testY,predmat[:,i])[0]
+        return Perf
