528a88f07389b152a0d403f5c87b9d0a93ce48ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:15:19 +0000

    Add package nmeaall/1.2/nmeaall-1.2.tar.gz

---
+++ packages/nmeaall/nmeaall-1.2.tar.gz/nmeaall-1.2/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='nmeaall',
+      version='1.2',
+      description='Can access all nmea file types with formats GPGGA, GPRMC and GPGSA as well as GNSS and NAVIC data too',
+      author='VishalJain_NIOT',
+      author_email='vishaljain9516@gmail.com',
+      packages=['nmeaall'],
+      install_requires=[])
