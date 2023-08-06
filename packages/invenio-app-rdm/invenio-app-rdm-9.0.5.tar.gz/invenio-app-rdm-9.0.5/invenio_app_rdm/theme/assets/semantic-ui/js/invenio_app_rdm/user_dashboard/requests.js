bd44bd8a1855192842fa126fc567643902124f78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:12:09 +0000

    Add package invenio-app-rdm/9.0.5/invenio-app-rdm-9.0.5.tar.gz

---
+++ packages/invenio-app-rdm/invenio-app-rdm-9.0.5.tar.gz/invenio-app-rdm-9.0.5/invenio_app_rdm/theme/assets/semantic-ui/js/invenio_app_rdm/user_dashboard/requests.js
@@ -0,0 +1,611 @@
+// This file is part of InvenioRDM
+// Copyright (C) 2020-2022 CERN.
+// Copyright (C) 2020-2021 Northwestern University.
+// Copyright (C) 2021 Graz University of Technology.
+// Copyright (C) 2021 New York University.
+//
+// Invenio App RDM is free software; you can redistribute it and/or modify it
+// under the terms of the MIT License; see LICENSE file for more details.
+
+import { createSearchAppInit } from "@js/invenio_search_ui";
+import {
+  SearchAppFacets,
+  SearchAppResultsPane,
+} from "@js/invenio_search_ui/components";
+import { i18next } from "@translations/invenio_app_rdm/i18next";
+import _get from "lodash/get";
+import _truncate from "lodash/truncate";
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import { GridResponsiveSidebarColumn } from "react-invenio-forms";
+import {
+  BucketAggregation,
+  Count,
+  Pagination,
+  ResultsList,
+  ResultsPerPage,
+  SearchBar,
+  Sort,
+  withState,
+} from "react-searchkit";
+import RequestTypeLabel from "@js/invenio_requests/request/RequestTypeLabel";
+import {
+  LabelTypeInvitation,
+  LabelTypeSubmission,
+} from "@js/invenio_requests/request";
+import { RequestActionController } from "@js/invenio_requests/request/actions/RequestActionController";
+
+import {
+  Button,
+  Card,
+  Container,
+  Grid,
+  Header,
+  Icon,
+  Item,
+  Segment,
+} from "semantic-ui-react";
+import {
+  RDMBucketAggregationElement,
+  RDMRecordFacetsValues,
+  RDMRecordSearchBarElement,
+  SearchHelpLinks,
+} from "../search/components";
+import { timestampToRelativeTime } from "../utils";
+
+export const RequestsResults = ({
+  sortOptions,
+  paginationOptions,
+  currentResultsState,
+}) => {
+  const { total } = currentResultsState.data;
+  return (
+    total && (
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={16}>
+            <Segment>
+              <Grid>
+                <Grid.Row
+                  verticalAlign="middle"
+                  className="small pt-5 pb-5 highlight-background"
+                >
+                  <Grid.Column
+                    mobile={16}
+                    tablet={4}
+                    computer={4}
+                    className="mt-5 mb-5"
+                  >
+                    <Count
+                      label={() => (
+                        <>
+                          {total} {i18next.t("result(s) found")}
+                        </>
+                      )}
+                    />
+                  </Grid.Column>
+                  <Grid.Column
+                    mobile={16}
+                    tablet={12}
+                    computer={12}
+                    className="text-align-right-tablet text-align-right-computer"
+                  >
+                    {sortOptions && (
+                      <Sort
+                        values={sortOptions}
+                        label={(cmp) => (
+                          <>
+                            {i18next.t("Sort by")} {cmp}
+                          </>
+                        )}
+                      />
+                    )}
+                  </Grid.Column>
+                </Grid.Row>
+
+                <Grid.Row>
+                  <Grid.Column>
+                    <ResultsList />
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row verticalAlign="middle">
+          <Grid.Column width={4} />
+          <Grid.Column width={8} textAlign="center">
+            <Pagination
+              options={{
+                size: "mini",
+                showFirst: false,
+                showLast: false,
+              }}
+            />
+          </Grid.Column>
+          <Grid.Column textAlign="right" width={4}>
+            <ResultsPerPage
+              values={paginationOptions.resultsPerPage}
+              label={(cmp) => (
+                <>
+                  {" "}
+                  {cmp} {i18next.t("results per page")}
+                </>
+              )}
+            />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    )
+  );
+};
+
+export function RequestsResultsGridItemTemplate({ result, index }) {
+  return (
+    <Card fluid key={index} href={`/me/requests/${result.metadata.id}`}>
+      <Card.Content>
+        <Card.Header>{result.metadata.title}</Card.Header>
+        <Card.Description>
+          <div
+            dangerouslySetInnerHTML={{ __html: result.metadata.description }}
+          />
+        </Card.Description>
+      </Card.Content>
+    </Card>
+  );
+}
+
+export function RequestsResultsItemTemplate({ result, index }) {
+  const createdDate = new Date(result.created);
+  const differenceInDays = timestampToRelativeTime(createdDate.toISOString());
+
+  const createdBy = result.created_by;
+  const isCreatorUser = "user" in createdBy;
+  const isCreatorCommunity = "community" in createdBy;
+  let creatorName = "";
+  if (isCreatorUser) {
+    creatorName =
+      result.expanded?.created_by.profile?.full_name ||
+      result.expanded?.created_by.username ||
+      createdBy.user;
+  } else if (isCreatorCommunity) {
+    creatorName =
+      result.expanded?.created_by.metadata?.title || createdBy.community;
+  }
+  return (
+    <Item key={index} className="community-item">
+      <Item.Content>
+        <Item.Header>
+          {result.type && <RequestTypeLabel type={result.type} />}
+          <a className="header-link" href={`/me/requests/${result.id}`}>
+            {result.title}
+          </a>
+        </Item.Header>
+
+        <Item.Meta>
+          <div className="inline-computer rel-mr-1 rel-mt-1 rel-mb-1">
+            {i18next.t(`opened {{difference}} by {{creator}}`, {
+              difference: differenceInDays,
+              creator: creatorName,
+            })}
+          </div>
+
+          {result.receiver.community &&
+            result.expanded?.receiver.metadata.title && (
+              <div className="inline-computer">
+                <Icon className="default-margin" name="users" />
+                <span className="ml-5">
+                  {result.expanded?.receiver.metadata.title}
+                </span>
+              </div>
+            )}
+        </Item.Meta>
+      </Item.Content>
+      <div>
+        <RequestActionController request={result} />
+      </div>
+    </Item>
+  );
+}
+// FIXME: Keeping ResultsGrid.item and SearchBar.element because otherwise
+// these components in RDM result broken.
+
+export const RDMRecordResultsGridItem = ({ result, index }) => {
+  const description_stripped = _get(
+    result,
+    "ui.description_stripped",
+    "No description"
+  );
+  return (
+    <Card fluid key={index} href={`/me/requests/${result.id}`}>
+      <Card.Content>
+        <Card.Header>{result.metadata.title}</Card.Header>
+        <Card.Description>
+          {_truncate(description_stripped, { length: 200 })}
+        </Card.Description>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export class RequestStatusFilterComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      open: undefined,
+    };
+  }
+
+  componentDidMount() {
+    const { currentQueryState } = this.props;
+    const userSelectionFilters = currentQueryState.filters;
+    const openFilter = userSelectionFilters.find((obj) =>
+      obj.includes("is_open")
+    );
+    if (openFilter) {
+      this.setState({
+        open: openFilter.includes("true"),
+      });
+    }
+  }
+
+  /**
+   * Updates queryFilters based on selection and removing previous filters
+   * @param {string} OpenStatus true if open requests and false if closed requests
+   */
+  retrieveRequests = (OpenStatus) => {
+    const { currentQueryState, updateQueryState } = this.props;
+    const { open } = this.state;
+
+    if (open === OpenStatus) {
+      return;
+    }
+    this.setState({
+      open: OpenStatus,
+    });
+    currentQueryState.filters = [];
+    currentQueryState.filters.push(["is_open", OpenStatus]);
+    updateQueryState(currentQueryState);
+  };
+
+  retrieveOpenRequests = () => {
+    this.retrieveRequests(true);
+  };
+
+  retrieveClosedRequests = () => {
+    this.retrieveRequests(false);
+  };
+
+  render() {
+    const { open } = this.state;
+    return (
+      <Button.Group basic>
+        <Button
+          className="request-search-filter"
+          onClick={this.retrieveOpenRequests}
+          active={open === true}
+        >
+          {i18next.t("Open")}
+        </Button>
+        <Button
+          className="request-search-filter"
+          onClick={this.retrieveClosedRequests}
+          active={open === false}
+        >
+          {i18next.t("Closed")}
+        </Button>
+      </Button.Group>
+    );
+  }
+}
+
+RequestStatusFilterComponent.propTypes = {
+  updateQueryState: PropTypes.func.isRequired,
+  currentQueryState: PropTypes.object.isRequired,
+};
+
+export const RequestStatusFilter = withState(RequestStatusFilterComponent);
+
+export const RDMRequestsSearchLayout = (props) => {
+  const [sidebarVisible, setSidebarVisible] = React.useState(false);
+
+  return (
+    <Container>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column only="mobile tablet" mobile={3} tablet={1}>
+            <Button
+              basic
+              size="medium"
+              icon="sliders"
+              onClick={() => setSidebarVisible(true)}
+              aria-label={i18next.t("Filter results")}
+              className="rel-mb-1"
+            />
+          </Grid.Column>
+
+          <Grid.Column
+            mobile={13}
+            tablet={4}
+            computer={3}
+            floated="right"
+            className="text-align-right-mobile"
+          >
+            <RequestStatusFilter className="rel-mb-1" />
+          </Grid.Column>
+
+          <Grid.Column mobile={16} tablet={11} computer={9}>
+            <SearchBar placeholder={i18next.t("Search requests...")} />
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <GridResponsiveSidebarColumn
+            width={4}
+            open={sidebarVisible}
+            onHideClick={() => setSidebarVisible(false)}
+            children={<SearchAppFacets aggs={props.config.aggs} />}
+          />
+          <Grid.Column mobile={16} tablet={16} computer={12}>
+            <SearchAppResultsPane layoutOptions={props.config.layoutOptions} />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
+  );
+};
+
+export const RequestsFacets = ({ aggs }) => {
+  return (
+    <aside aria-label={i18next.t("filters")} id="search-filters">
+      {aggs.map((agg) => {
+        return (
+          <div className="rdm-facet-container" key={agg.title}>
+            <BucketAggregation title={agg.title} agg={agg} />
+          </div>
+        );
+      })}
+
+      <Card className="borderless facet mt-0">
+        <Card.Content>
+          <Card.Header as="h2">{i18next.t("Help")}</Card.Header>
+          <SearchHelpLinks />
+        </Card.Content>
+      </Card>
+    </aside>
+  );
+};
+
+export const RDMRequestsEmptyResults = (props) => {
+  const { queryString, userSelectionFilters } = props;
+  const is_open = userSelectionFilters.some(
+    (obj) => obj.includes("is_open") && obj.includes("true")
+  );
+  const filtersToNotReset = userSelectionFilters.find((obj) =>
+    obj.includes("is_open")
+  );
+  const elementsToReset = {
+    queryString: "",
+    page: 1,
+    filters: [filtersToNotReset],
+  };
+
+  const AllDone = () => {
+    return (
+      <>
+        <Header as="h1" icon>
+          {i18next.t("All done!")}
+          <Header.Subheader>
+            {i18next.t("You've caught up with all open requests.")}
+          </Header.Subheader>
+        </Header>
+      </>
+    );
+  };
+
+  const NoResults = () => {
+    return (
+      <>
+        <Header icon>
+          <Icon name="search" />
+          {i18next.t("No requests found!")}
+        </Header>
+        {queryString && (
+          <Button
+            primary
+            onClick={() => props.updateQueryState(elementsToReset)}
+          >
+            {i18next.t("Reset search")}
+          </Button>
+        )}
+      </>
+    );
+  };
+
+  const allRequestsDone = is_open && !queryString;
+  return (
+    <>
+      <Segment placeholder textAlign="center">
+        {allRequestsDone ? <AllDone /> : <NoResults />}
+      </Segment>
+    </>
+  );
+};
+
+export const RequestAcceptButton = ({
+  onClick,
+  requestType,
+  loading,
+  ariaAttributes,
+}) => {
+  const requestIsCommunitySubmission = requestType === "community-submission";
+  const buttonText = requestIsCommunitySubmission
+    ? i18next.t("Accept and publish")
+    : i18next.t("Accept");
+  return (
+    <Button
+      icon="checkmark"
+      content={buttonText}
+      onClick={onClick}
+      color="green"
+      loading={loading}
+      disabled={loading}
+      {...ariaAttributes}
+    />
+  );
+};
+
+export const RequestCancelButton = ({
+  onClick,
+  loading,
+  ariaAttributes,
+}) => {
+  return (
+    <Button
+      icon="cancel"
+      content={i18next.t("Cancel")}
+      onClick={onClick}
+      loading={loading}
+      disabled={loading}
+      {...ariaAttributes}
+    />
+  );
+};
+
+export const RequestDeclineButton = ({
+  onClick,
+  loading,
+  ariaAttributes,
+}) => {
+  return (
+    <Button
+      icon="cancel"
+      content={i18next.t("Decline")}
+      onClick={onClick}
+      loading={loading}
+      disabled={loading}
+      color="red"
+      {...ariaAttributes}
+    />
+  );
+};
+
+export const RequestCancelButtonModalCmp = ({
+  onClick,
+  loading,
+  updateQueryState,
+  currentQueryState,
+}) => {
+  return (
+    <Button
+      icon="cancel"
+      content={i18next.t("Cancel request")}
+      onClick={() => {
+        onClick().then(() => updateQueryState(currentQueryState));
+      }}
+      loading={loading}
+      disabled={loading}
+      color="red"
+    />
+  );
+};
+
+RequestCancelButtonModalCmp.propTypes = {
+  updateQueryState: PropTypes.func.isRequired,
+  currentQueryState: PropTypes.object.isRequired,
+};
+export const RequestCancelButtonModal = withState(RequestCancelButtonModalCmp);
+
+export const RequestDeclineButtonCmp = ({
+  onClick,
+  loading,
+  ariaAttributes,
+  updateQueryState,
+  currentQueryState,
+}) => {
+  return (
+    <Button
+      icon="cancel"
+      content={i18next.t("Decline")}
+      onClick={() => {
+        onClick().then(() => updateQueryState(currentQueryState));
+      }}
+      loading={loading}
+      disabled={loading}
+      color="red"
+      {...ariaAttributes}
+    />
+  );
+};
+
+RequestDeclineButtonCmp.propTypes = {
+  updateQueryState: PropTypes.func.isRequired,
+  currentQueryState: PropTypes.object.isRequired,
+};
+
+export const RequestDeclineButtonModal = withState(RequestDeclineButtonCmp);
+
+export const RequestAcceptButtonCmp = ({
+  onClick,
+  requestType,
+  loading,
+  ariaAttributes,
+  updateQueryState,
+  currentQueryState,
+}) => {
+  const requestIsCommunitySubmission = requestType === "community-submission";
+  const buttonText = requestIsCommunitySubmission
+    ? i18next.t("Accept and publish")
+    : i18next.t("Accept");
+  return (
+    <Button
+      icon="checkmark"
+      content={buttonText}
+      onClick={() => {
+        onClick().then(() => updateQueryState(currentQueryState));
+      }}
+      loading={loading}
+      disabled={loading}
+      color="green"
+      {...ariaAttributes}
+    />
+  );
+};
+
+RequestAcceptButtonCmp.propTypes = {
+  updateQueryState: PropTypes.func.isRequired,
+  currentQueryState: PropTypes.object.isRequired,
+};
+
+export const RequestAcceptButtonModal = withState(RequestAcceptButtonCmp);
+
+export const RDMRequestsEmptyResultsWithState = withState(
+  RDMRequestsEmptyResults
+);
+
+export const defaultComponents = {
+  "BucketAggregation.element": RDMBucketAggregationElement,
+  "BucketAggregationValues.element": RDMRecordFacetsValues,
+  "SearchApp.facets": RequestsFacets,
+  "ResultsList.item": RequestsResultsItemTemplate,
+  "ResultsGrid.item": RequestsResultsGridItemTemplate,
+  "SearchApp.layout": RDMRequestsSearchLayout,
+  "SearchApp.results": RequestsResults,
+  "RequestAction.button.accept": RequestAcceptButton,
+  "RequestAction.button.decline": RequestDeclineButton,
+  "RequestAction.button.cancel": RequestCancelButton,
+  "RequestActionModal.button.cancel": RequestCancelButtonModal,
+  "RequestActionModal.button.decline": RequestDeclineButtonModal,
+  "RequestActionModal.button.accept": RequestAcceptButtonModal,
+  "SearchBar.element": RDMRecordSearchBarElement,
+  "EmptyResults.element": RDMRequestsEmptyResultsWithState,
+  "RequestTypeLabel.layout.community-submission": () => (
+    <LabelTypeSubmission className="rel-mr-1" size="large" />
+  ),
+  "RequestTypeLabel.layout.community-invitation": () => (
+    <LabelTypeInvitation className="rel-mr-1" size="large" />
+  ),
+};
+
+createSearchAppInit(defaultComponents);
