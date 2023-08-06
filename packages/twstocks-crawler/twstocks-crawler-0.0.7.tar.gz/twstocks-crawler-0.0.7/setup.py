5fe3d242579c8a3c056a55fd2a621223b93e3e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:17 +0000

    Add package twstocks-crawler/0.0.7/twstocks-crawler-0.0.7.tar.gz

---
+++ packages/twstocks-crawler/twstocks-crawler-0.0.7.tar.gz/twstocks-crawler-0.0.7/setup.py
@@ -0,0 +1,34 @@
+import os
+import setuptools
+
+metadata = {}
+with open('./src/twstocks_crawler/version.py', encoding='utf-8') as f:
+    exec(f.read(), metadata)
+
+
+setuptools.setup(
+    name="twstocks-crawler",
+    version=metadata['__version__'],
+    author="Sunaley",
+    author_email="sunaley@gmail.com",
+    description="TWStock profiles crawler",
+    long_description='',
+    long_description_content_type="text/markdown",
+    url="https://github.com/sunaley/stocks",
+    install_requires=[
+        'requests',
+        'lxml',
+        'pandas',
+        'aiohttp',
+        'aiohttp-socks',
+        'charset-normalizer==2.0.0'
+    ],
+    packages=["twstocks_crawler", "twstocks_crawler.repositories"],
+    package_dir={"": "src", },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
