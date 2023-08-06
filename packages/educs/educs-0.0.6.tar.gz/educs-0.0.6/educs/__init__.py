cf05ad090317ff8635dadb43321618de9c4da1e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:10 +0000

    Add package educs/0.0.6/educs-0.0.6.tar.gz

---
+++ packages/educs/educs-0.0.6.tar.gz/educs-0.0.6/educs/__init__.py
@@ -0,0 +1,19 @@
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+
+from educs.pygame_wrapper import (setup, draw, createCanvas, line, ellipse, circle, rect, quad, arc, triangle, push, pop, getMouseX, getMouseY, getWidth, getHeight, cursor, strokeWeight, stroke, noStroke, loadImage, image, keyPressed, mouseClicked, mouseDragged, isMousedPressed, Image, frameRate, loop, noLoop)
+
+from educs.color import (color, alpha, background,
+                         fill, noFill,
+                         red, green, blue, brightness,
+                         hue, lightness, saturation, lerpColor)
+
+from educs.constants import (TWO_PI, HALF_PI, PI, QUARTER_PI, TAU, CURSOR_ARROW, CURSOR_DIAMOND, CURSOR_BROKEN_X, CURSOR_TRI_LEFT, CURSOR_TRI_RIGHT)
+
+from educs.math import (random, constrain, floor, ceil, dist, exp, lerp, ln, log10, log2, logb, mag, rerange, norm, sq, sqrt, frac, sin, cos, tan, asin, acos, atan, atan2, degrees, radians, integral)
+
+__all__ = ["setup", "draw", "createCanvas", "background", "fill", "noFill", "line", "ellipse", "circle", "rect", "triangle", "quad", "arc", "push", "pop", "getMouseX", "getMouseY", "getWidth", "getHeight", "cursor", "TWO_PI", "HALF_PI", "PI", "QUARTER_PI", "TAU", "CURSOR_ARROW", "CURSOR_DIAMOND", "CURSOR_BROKEN_X", "CURSOR_TRI_LEFT", "CURSOR_TRI_RIGHT", "strokeWeight", "stroke", "noStroke", "loadImage", "image", "keyPressed", "mouseClicked", "mouseDragged", "isMousedPressed", "Image", "frameRate", "random", "constrain", "floor", "ceil", "dist", "exp", "lerp", "ln", "log10", "log2", "logb", "mag", "rerange", "norm", "sq", "sqrt", "frac", "sin", "cos", "tan", "asin", "acos", "atan", "atan2", "degrees", "radians", "loop", "noLoop", "integral"]
+
+__version__ = "0.0.4"
+
+print("Thank you for using educs!\n")
---
+++ packages/educs/educs-0.0.6.tar.gz/educs-0.0.6/educs/color.py
@@ -0,0 +1,180 @@
+from pygame import Color
+from multipledispatch import dispatch
+
+def _input2Color(c, a=255):
+    color = None
+    if (type(c) == int) and (0 <= c) and (255 >= c):
+        color = Color(c, c, c)
+    elif (type(c) == float) and (0 <= c) and (255 >= c):
+        color = Color(int(c), int(c), int(c))
+    elif (type(c) == str):
+        color = Color(c)
+    elif (type(c) == tuple):
+        color = Color(c)
+    else:
+        color = Color("black")
+    color.a = a
+    return color
+
+# COLOR
+def color(r, g, b):
+    return Color(r, g, b)
+
+def alpha(c):
+    return c.a
+
+def red(c):
+    return c.r
+
+def green(c):
+    return c.g
+
+def blue(c):
+    return c.b
+
+def brightness(c):
+    return c.hsva[2]
+
+def hue(c):
+    return c.hsva[0]
+
+def lightness(c):
+    return c.hsla[2]
+
+def saturation(c):
+    return c.hsla[1]
+
+def lerpColor(c1, c2, amnt):
+    return c1.lerp(c2, amnt)
+
+
+
+@dispatch(tuple)
+def background(c):
+    if (backgroundSurf):
+        x = _input2Color(c)
+        backgroundSurf.fill(x)
+    pass
+
+@dispatch(tuple, int)
+def background(c, a):
+    if (backgroundSurf):
+        x = _input2Color(c, a)
+        backgroundSurf.fill(x)
+    pass
+    
+@dispatch(int)
+def background(c):
+    if (backgroundSurf):
+        x = _input2Color(c)
+        backgroundSurf.fill(x)
+    pass
+
+@dispatch(int, int)
+def background(c, a):
+    if (backgroundSurf):
+        x = _input2Color(c, a)
+        backgroundSurf.fill(x)
+    pass
+
+@dispatch(str)
+def background(c):
+    if (backgroundSurf):
+        x = _input2Color(c)
+        backgroundSurf.fill(x)
+    pass
+
+@dispatch(str, int)
+def background(c, a):
+    if (backgroundSurf):
+        x = _input2Color(c, a)
+        backgroundSurf.fill(x)
+    pass
+
+@dispatch(float)
+def background(c):
+    if (backgroundSurf):
+        x = _input2Color(c)
+        backgroundSurf.fill(x)
+    pass
+
+@dispatch(float, int)
+def background(c, a):
+    if (backgroundSurf):
+        x = _input2Color(c, a)
+        backgroundSurf.fill(x)
+    pass
+
+@dispatch(int, int, int)
+def background(r, g, b):
+    if (backgroundSurf):
+        x = _input2Color((r, g, b))
+        backgroundSurf.fill(x)
+    pass
+
+@dispatch(int, int, int, int)
+def background(r, g, b, a):
+    if (backgroundSurf):
+        x = _input2Color((r, g, b), a)
+        backgroundSurf.fill(x)
+    pass
+
+@dispatch(int)
+def fill(c):
+    settings["fill_color"] = _input2Color(c)
+    settings["no_fill"] = False
+    pass
+
+@dispatch(int, int)
+def fill(c, a):
+    settings["fill_color"] = _input2Color(c, a)
+    settings["no_fill"] = False
+    pass
+
+@dispatch(float)
+def fill(c):
+    settings["fill_color"] = _input2Color(c)
+    settings["no_fill"] = False
+    pass
+
+@dispatch(float, int)
+def fill(c, a):
+    settings["fill_color"] = _input2Color(c, a)
+    settings["no_fill"] = False
+    pass
+
+@dispatch(str)
+def fill(c):
+    settings["fill_color"] = _input2Color(c)
+    settings["no_fill"] = False
+    pass
+
+@dispatch(str, int)
+def fill(c, a):
+    settings["fill_color"] = _input2Color(c, a)
+    settings["no_fill"] = False
+    pass
+
+@dispatch(tuple)
+def fill(c):
+    settings["fill_color"] = _input2Color(c)
+    settings["no_fill"] = False
+    pass
+
+@dispatch(tuple, int)
+def fill(c, a):
+    settings["fill_color"] = _input2Color(c, a)
+    settings["no_fill"] = False
+    pass
+
+@dispatch(int, int, int)
+def fill(r, g, b):
+    settings["fill_color"] = _input2Color((r, g, b))
+    settings["no_fill"] = False
+    pass
+
+@dispatch(int, int, int, int)
+def fill(r, g, b, a):
+    settings["fill_color"] = _input2Color((r, g, b), a)
+    settings["no_fill"] = False
+    pass
