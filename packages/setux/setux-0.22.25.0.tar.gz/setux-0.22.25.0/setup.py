de325e705148c6195f66645a2065ac8b8db489ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:28 +0000

    Add package setux/0.22.25.0/setux-0.22.25.0.tar.gz

---
+++ packages/setux/setux-0.22.25.0.tar.gz/setux-0.22.25.0/setup.py
@@ -0,0 +1,48 @@
+from os.path import join, dirname, abspath
+
+from setuptools import setup, find_namespace_packages
+
+from setux.main import __version__
+
+curdir = abspath(dirname(__file__))
+readme = open(join(curdir, 'README.md')).read()
+
+setup(
+    name             = 'setux',
+    version          = __version__,
+    description      = 'System deployment',
+    long_description = readme,
+    long_description_content_type='text/markdown',
+    keywords         = ['utility', ],
+    url              = 'https://framagit.org/louis-riviere-xyz/setux',
+    author           = 'Louis RIVIERE',
+    author_email     = 'louis@riviere.xyz',
+    license          = 'MIT',
+    classifiers      = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        "Operating System :: POSIX :: Linux",
+        "Environment :: Console",
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Installation/Setup',
+        'Topic :: System :: Systems Administration',
+        'Topic :: Utilities',
+    ],
+    python_requires='>3.7',
+    install_requires = [
+        'setux_core>=0.22.22.0',
+        'setux_deployers>=0.22.8.1',
+        'setux_distros>=0.22.8.0',
+        'setux_logger>=0.21.50.0',
+        'setux_managers>=0.22.25.0',
+        'setux_mappings>=0.22.25.0',
+        'setux_modules>=0.22.24.0',
+        'setux_targets>=0.22.24.0',
+    ],
+    packages = find_namespace_packages(
+        include=['setux.*']
+    ),
+)
