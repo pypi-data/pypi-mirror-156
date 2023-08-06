f26146da0bf7b9db0298b29afc98d049be71b78f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:56 +0000

    Add package obs2mk/4.1.2/obs2mk-4.1.2-py3-none-any.whl

---
+++ packages/obs2mk/obs2mk-4.1.2-py3-none-any.whl/mkdocs_obsidian/common/convert_one.py
@@ -0,0 +1,91 @@
+"""Convert one file function."""
+
+import os
+import sys
+from datetime import datetime
+from pathlib import Path
+
+import frontmatter
+import yaml
+from rich.console import Console
+
+from mkdocs_obsidian.common import config as cfg
+from mkdocs_obsidian.common import conversion as convert
+from mkdocs_obsidian.common import file_checking as check
+from mkdocs_obsidian.common import github_push as gitt
+
+
+def convert_one(
+    ori: Path, configuration: cfg.Configuration, git: bool, meta: int, obsidian=False
+):
+    """Function to start the conversion of *one* specified file."""
+    file_name = os.path.basename(ori).upper()
+    console = Console()
+    try:
+        try:
+            yaml_front = frontmatter.load(ori, encoding='utf-8')
+        except UnicodeDecodeError:
+            yaml_front = frontmatter.load(ori, encoding='iso-8859-1')
+        priv = configuration.post
+        clipkey = configuration.default_folder
+        CATEGORY = configuration.category_key
+        if CATEGORY in yaml_front.keys():
+            if not yaml_front[CATEGORY]:
+                priv = check.create_folder('hidden', configuration)
+                clipkey = 'hidden'
+            else:
+                priv = check.create_folder(yaml_front[CATEGORY], configuration)
+                clipkey = yaml_front[CATEGORY]
+        contents = convert.file_convert(configuration, ori, 1)
+        checkfile = convert.file_write(
+            configuration, ori, contents, priv, 1, meta)
+        if checkfile and git:
+            commit = f'Pushed {file_name.lower()} to blog'
+            gitt.git_push(commit, configuration, obsidian, 'Push', file_name)
+            convert.clipboard(configuration, str(ori), clipkey)
+        elif checkfile and not git:
+            if not obsidian:
+                console.print(
+                    f"[{datetime.now().strftime('%H:%M:%S')}] 🎉 Successfully converted"
+                    f' [u blue bold]{file_name.lower()}[/]'
+                )
+            else:
+                print(
+                    f"[{datetime.now().strftime('%H:%M:%S')}] 🎉 Successfully converted"
+                    f' {file_name.lower()}'
+                )
+    except yaml.YAMLError:
+        if obsidian:
+            print(
+                f"Error in {file_name} : Your YAML frontmatter doesn't seem valid! Use"
+                ' https://jsonformatter.org/yaml-validator to correct it!'
+            )
+        else:
+            print(
+                f"Error in [i u]{file_name}[/]: Your YAML frontmatter doesn't seem"
+                ' valid! Use https://jsonformatter.org/yaml-validator to correct it!'
+            )
+        sys.exit(2)
+
+
+def overwrite_file(source_path: str, configuration: cfg.Configuration, test=False):
+    """Overwrite file with conversion to mkdocs."""
+    from unidecode import unidecode
+
+    filename = os.path.basename(source_path)
+    source_path = Path(source_path)
+    contents = convert.file_convert(configuration, source_path, 1, False)
+    if not test:
+        os.remove(source_path)
+    if unidecode(filename).replace('.md', '') == unidecode(
+        os.path.basename(source_path.parent)
+    ):
+        source_path = Path(str(source_path).replace(filename, 'index.md'))
+        filename = 'index.md'
+    if test:
+        source_path = Path(source_path.resolve().parent.parent,
+                           'output', 'docs', 'notes', filename)
+    with open(source_path, 'w', encoding='utf-8') as new_notes:
+        for lines in contents:
+            new_notes.write(lines)
+    return True
---
+++ packages/obs2mk/obs2mk-4.1.2-py3-none-any.whl/mkdocs_obsidian/common/metadata.py
@@ -0,0 +1,60 @@
+"""Update metadata in the file, if option is used."""
+
+import os
+import re
+import urllib.parse as url
+from pathlib import Path
+
+import frontmatter
+
+from mkdocs_obsidian.common import config as cfg
+
+
+def update_frontmatter(filepath: Path, configuration: cfg.Configuration, link=1):
+    """If link = 1, update the frontmatter with new publish URL
+    Also, update the share state if convert_one.
+    """
+    SHARE = configuration.share_key
+    CATEGORY = configuration.category_key
+    with open(filepath, 'r', encoding='utf8') as metadata:
+        meta = frontmatter.load(metadata)
+    if meta.get('tag'):
+        tag = meta.metadata.pop('tag', None)
+    elif meta.get('tags'):
+        tag = meta.metadata.pop('tags', None)
+    else:
+        tag = ''
+    folder = meta.metadata.get(CATEGORY, configuration.default_folder)
+
+    with open(filepath, 'w', encoding='utf-8') as f:
+        filename = os.path.basename(filepath)
+        filename = filename.replace('.md', '')
+        if filename == os.path.basename(folder):
+            filename = ''
+        path_url = url.quote(f'{folder}/{filename}')
+        clip = f'{configuration.weblink}{path_url}'
+        meta['link'] = clip
+        update = frontmatter.dumps(meta, sort_keys=False)
+        meta = frontmatter.loads(update)
+        if link != 1:
+            meta.metadata.pop('link', None)
+        elif link == 1 and SHARE == 1 and (not meta.get(SHARE)):
+            meta[SHARE] = 'true'
+        if tag != '':
+            meta['tag'] = tag
+        update = frontmatter.dumps(meta, sort_keys=False)
+        if re.search(r'\\U\w+', update):
+            emojiz = re.search(r'\\U\w+', update)
+            emojiz = emojiz.group().strip()
+            raw = r'{}'.format(emojiz)
+            try:
+                convert_emojiz = (
+                    raw.encode('ascii')
+                    .decode('unicode_escape')
+                    .encode('utf-16', 'surrogatepass')
+                    .decode('utf-16')
+                )
+                update = re.sub(r'"\\U\w+"', convert_emojiz, update)
+            except UnicodeEncodeError:
+                pass
+        f.write(update)
