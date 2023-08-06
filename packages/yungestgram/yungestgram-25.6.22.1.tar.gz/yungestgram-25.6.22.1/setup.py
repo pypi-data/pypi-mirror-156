cbc16c1b3311d49d01f4a6c7eafcc286d9fee9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:11 +0000

    Add package yungestgram/25.6.22.1/yungestgram-25.6.22.1.tar.gz

---
+++ packages/yungestgram/yungestgram-25.6.22.1.tar.gz/yungestgram-25.6.22.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+  name = 'yungestgram',         
+  packages = ['yungestgram'],   
+  version = '25.6.22.1',      
+  license='MIT',        
+  description = 'Telegram bot library',   
+  author = 'yungestDev',                   
+  author_email = 'yungestmonsterhahaha@gmail.com',      
+  #url = 'https://github.com/yungestdev/yungestWeb',   
+  #download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    
+  keywords = ['Telegram', 'Bot'],   
+  install_requires=[            
+        'requests',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # 3 - Alpha 2 - Beta 1 - Stable/Final
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License', 
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',      
+
+  ],
+)
