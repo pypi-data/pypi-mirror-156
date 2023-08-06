95ece422012c13a14b20fa123e0b91ae87e3a341
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:24 +0000

    Add package todocli/4.0.0/todocli-4.0.0.tar.gz

---
+++ packages/todocli/todocli-4.0.0.tar.gz/todocli-4.0.0/setup.py
@@ -0,0 +1,33 @@
+import shutil
+import os.path as op
+
+from setuptools import setup
+from setuptools.command.install import install
+
+
+setup(
+	name='todocli',
+	version='4.0.0',
+	python_requires='>=3.6',
+	packages=['todo', 'todo.bash_completion'],
+	entry_points={
+		'console_scripts': [
+			'todo = todo.todo:main'
+		]
+	},
+	include_package_data=True,
+    install_requires=[
+        'setuptools==60.9.3',
+    ],
+    extras_require={
+        'tests': {
+            'pyfakefs==4.5.5',
+        },
+    },
+	author='foobuzz',
+	author_email='foobuzz@fastmail.com',
+	description='A command line todo list manager',
+	keywords='command line todo list',
+	url='https://github.com/foobuzz/todo',
+	license='MIT'
+)
