a9f4ad1a9423af8b2c9d0b17e1a6dc63174717bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:58:20 +0000

    Add package py-ballisticcalc/0.2.3/py_ballisticcalc-0.2.3-py3-none-any.whl

---
+++ packages/py-ballisticcalc/py_ballisticcalc-0.2.3-py3-none-any.whl/py_ballisticcalc/__init__.py
@@ -0,0 +1,8 @@
+__author__ = "o-murphy"
+__copyright__ = ("",)
+
+__credits__ = ["O-Murphy"]
+__version__ = "0.2.3"
+
+__all__ = ['atmosphere', 'bmath', 'drag', 'projectile', 'py_ballistics_test', 'shot_parameters',
+           'trajectory_calculator', 'weapon', 'wind']
---
+++ packages/py-ballisticcalc/py_ballisticcalc-0.2.3-py3-none-any.whl/py_ballisticcalc/bmath/unit/pressure.py
@@ -0,0 +1,35 @@
+from ..unit.types import UnitsConvertor, Units
+
+# the value indicating that pressure value is expressed in some unit
+PressureMmHg = 40
+PressureInHg = 41
+PressureBar = 42
+PressureHP = 43
+PressurePSI = 44
+
+
+class PressureConvertor(UnitsConvertor):
+    unit_type = 'pressure'
+
+    _units = {
+        PressureMmHg: {'name': 'mmHg', 'accuracy': 0,
+                       'to': lambda v: v,
+                       'from': lambda v: v},
+        PressureInHg: {'name': 'inHg', 'accuracy': 2,
+                       'to': lambda v: v * 25.4,
+                       'from': lambda v: v / 25.4},
+        PressureBar: {'name': 'bar', 'accuracy': 2,
+                      'to': lambda v: v * 750.061683,
+                      'from': lambda v: v / 750.061683},
+        PressureHP: {'name': 'hPa', 'accuracy': 4,
+                     'to': lambda v: v * 750.061683 / 1000,
+                     'from': lambda v: v / 750.061683 * 1000},
+        PressurePSI: {'name': 'psi', 'accuracy': 4,
+                      'to': lambda v: v * 51.714924102396,
+                      'from': lambda v: v / 51.714924102396}
+    }
+
+
+class Pressure(Units):
+    """ Pressure object keeps velocity or speed values """
+    convertor = PressureConvertor
