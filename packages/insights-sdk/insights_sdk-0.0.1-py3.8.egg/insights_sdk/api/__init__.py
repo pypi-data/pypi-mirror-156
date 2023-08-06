3c69a057ad25dc59a7a2036be7623cda922b4dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:41 +0000

    Add package insights-sdk/0.0.1/insights_sdk-0.0.1-py3.8.egg

---
+++ packages/insights-sdk/insights_sdk-0.0.1-py3.8.egg/insights_sdk/api/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from insights_sdk.api.authenticate_api import AuthenticateApi
+from insights_sdk.api.boundary_api import BoundaryApi
+from insights_sdk.api.metrics_api import MetricsApi
+from insights_sdk.api.weather_api import WeatherApi
+from insights_sdk.api.yield_api import YieldApi
