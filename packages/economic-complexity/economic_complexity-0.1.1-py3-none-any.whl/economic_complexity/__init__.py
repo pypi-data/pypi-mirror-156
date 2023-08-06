579c55403aa0a775959121b70c13995356ea8838
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:58 +0000

    Add package economic-complexity/0.1.1/economic_complexity-0.1.1-py3-none-any.whl

---
+++ packages/economic-complexity/economic_complexity-0.1.1-py3-none-any.whl/economic_complexity/__init__.py
@@ -0,0 +1,23 @@
+"""Economic Complexity
+
+This module contains functions to ease the calculation of Economic Complexity values.
+"""
+
+from .rca import rca
+from .complexity import complexity
+from .product_space import distance, opportunity_gain, proximity, relatedness
+from .cross_space import cross_proximity, cross_relatedness
+
+__version_info__ = ('0', '1', '0')
+__version__ = '.'.join(__version_info__)
+
+__all__ = (
+    "rca",
+    "complexity",
+    "distance",
+    "opportunity_gain",
+    "proximity",
+    "relatedness",
+    "cross_proximity",
+    "cross_relatedness",
+)
