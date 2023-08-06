a9f535997299b42e5aa30fa1bd0d498ffb708666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:39:44 +0000

    Add package puma-hep/0.1.3/puma-hep-0.1.3.tar.gz

---
+++ packages/puma-hep/puma-hep-0.1.3.tar.gz/puma-hep-0.1.3/puma/__init__.py
@@ -0,0 +1,12 @@
+"""puma framework - Plotting UMami Api."""
+
+# flake8: noqa
+# pylint: skip-file
+
+__version__ = "0.1.3"
+
+from puma.fraction_scan import FractionScan, FractionScanPlot
+from puma.histogram import Histogram, HistogramPlot
+from puma.plot_base import PlotBase, PlotLineObject, PlotObject
+from puma.roc import Roc, RocPlot
+from puma.var_vs_eff import VarVsEff, VarVsEffPlot
