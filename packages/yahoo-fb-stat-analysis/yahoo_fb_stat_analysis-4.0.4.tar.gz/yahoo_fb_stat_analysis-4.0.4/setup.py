7b2b9372e643153bc4415165df5ab9ec3bfabecd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:11 +0000

    Add package yahoo-fb-stat-analysis/4.0.4/yahoo_fb_stat_analysis-4.0.4.tar.gz

---
+++ packages/yahoo-fb-stat-analysis/yahoo_fb_stat_analysis-4.0.4.tar.gz/yahoo_fb_stat_analysis-4.0.4/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+
+def readme():
+    with open('readMe.rst') as f:
+        return f.read()
+
+
+setup(name='yahoo_fb_stat_analysis',
+      version='4.0.4',
+      description='Python module to allow for advanced statistical analysis',
+      long_description=readme(),
+      url='https://github.com/KipFeher85/Fantasy-Baseball',
+      author='Kip Feher',
+      author_email='kipfeher85@gmail.com',
+      license='MIT',
+      packages=['yahoo_fb_stat_analysis'],
+      classifiers=[
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 3.9',
+      ],
+      install_requires=['yahoo_oauth', 'yahoo_fantasy_api', 'statsapi', 'pandas', 'datetime', 'dateutil'],
+      zip_safe=False)
