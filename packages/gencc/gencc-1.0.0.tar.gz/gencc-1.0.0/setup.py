8073e148e1aced38621ca1ae964551af1a3317ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:43 +0000

    Add package gencc/1.0.0/gencc-1.0.0.tar.gz

---
+++ packages/gencc/gencc-1.0.0.tar.gz/gencc-1.0.0/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gencc',
+    packages=find_packages(),
+    include_package_data=True,
+    version="1.0.0",
+    description='CC GENERATOR FOR BIN',
+    author='CYBER-SH',
+    author_email='admin@itzakx.ml',
+    long_description=(open("README.md","r")).read(),
+    long_description_content_type="text/markdown",
+   install_requires=['lolcat'],
+ 
+    keywords=['GENCC', 'BIN', 'CC GENERATOR', 'CRADIT CARD NUMBER', 'AKXVAU', 'DCBD04', 'DEVS COMMUNITY', 'DEV COMMUNITY', 'MOHAMMAD ALAMIN'],
+    classifiers=[
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Operating System :: OS Independent',
+            'Environment :: Console',
+    ],
+    
+    license='MIT',
+    entry_points={
+            'console_scripts': [
+                'gencc = gencc.gencc',
+                
+            ],
+    },
+    python_requires='>=3.9'
+)
