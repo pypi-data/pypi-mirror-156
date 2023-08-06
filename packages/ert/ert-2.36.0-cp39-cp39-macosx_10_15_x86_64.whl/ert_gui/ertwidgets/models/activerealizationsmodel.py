07af6ac73f4d5abf7368161833ff7950d00b7bf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:36 +0000

    Add package ert/2.36.0/ert-2.36.0-cp39-cp39-macosx_10_15_x86_64.whl

---
+++ packages/ert/ert-2.36.0-cp39-cp39-macosx_10_15_x86_64.whl/ert_gui/ertwidgets/models/activerealizationsmodel.py
@@ -0,0 +1,36 @@
+from typing import List
+
+from ert.ensemble_evaluator.activerange import ActiveRange, mask_to_rangestring
+from ert_gui.ertwidgets.models.valuemodel import ValueModel
+from ert_shared.libres_facade import LibresFacade
+
+
+class ActiveRealizationsModel(ValueModel):
+    def __init__(self, facade: LibresFacade):
+        self.facade = facade
+        ValueModel.__init__(self, self.getDefaultValue())
+        self._custom = False
+
+    def setValue(self, active_realizations):
+        if (
+            active_realizations is None
+            or active_realizations.strip() == ""
+            or active_realizations == self.getDefaultValue()
+        ):
+            self._custom = False
+            ValueModel.setValue(self, self.getDefaultValue())
+        else:
+            self._custom = True
+            ValueModel.setValue(self, active_realizations)
+
+    def setValueFromMask(self, mask):
+        self.setValue(mask_to_rangestring(mask))
+
+    def getDefaultValue(self):
+        size = self.facade.get_ensemble_size()
+        return f"0-{size-1:d}"
+
+    def getActiveRealizationsMask(self) -> List[bool]:
+        return ActiveRange(
+            rangestring=self.getValue(), length=self.facade.get_ensemble_size()
+        ).mask
