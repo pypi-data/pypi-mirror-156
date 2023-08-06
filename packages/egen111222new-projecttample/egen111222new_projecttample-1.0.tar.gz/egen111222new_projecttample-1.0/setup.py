4ded1af397ff3610c81b38133bc301e3a965d44c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:46:25 +0000

    Add package egen111222new-projecttample/1.0/egen111222new_projecttample-1.0.tar.gz

---
+++ packages/egen111222new-projecttample/egen111222new_projecttample-1.0.tar.gz/egen111222new_projecttample-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='egen111222new_projecttample',
+      version='1.0',
+      description='New Description',
+      author='Kirill',
+      author_email='egen13@ukr.net',
+      url='http://ukr.net',
+      packages=['srs'],
+     )
