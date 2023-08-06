9018ce0cfdbba2e96c752215277b781a8338ddd8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:42:43 +0000

    Add package lark067/0.6.7/lark067-0.6.7.tar.gz

---
+++ packages/lark067/lark067-0.6.7.tar.gz/lark067-0.6.7/README.md
@@ -0,0 +1,5 @@
+This package exists to make a wheel available for v0.6.7
+of [Lark](https://pypi.org/project/lark-parser/).
+
+Many thanks to the makers of Lark, for an excellent library!
+
---
+++ packages/lark067/lark067-0.6.7.tar.gz/lark067-0.6.7/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = "lark067",
+    version = '0.6.7',
+    packages = ['lark', 'lark.parsers', 'lark.tools', 'lark.grammars'],
+
+    requires = [],
+    install_requires = [],
+
+    package_data = { '': ['*.md', '*.lark'] },
+
+    test_suite = 'tests.__main__',
+
+    author = "Erez Shinan",
+    description = "a modern parsing library",
+    license = "MIT",
+    keywords = "Earley LALR parser parsing ast",
+    url = "https://github.com/skieffer/lark",
+    long_description=long_description,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: General",
+        "Topic :: Text Processing :: Linguistic",
+        "License :: OSI Approved :: MIT License",
+    ],
+
+)
+
