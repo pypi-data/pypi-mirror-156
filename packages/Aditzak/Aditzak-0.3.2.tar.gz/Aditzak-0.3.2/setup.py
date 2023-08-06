ae3492bfbdce70f9a4dc3281be1e0b7b9594e013
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:35:32 +0000

    Add package Aditzak/0.3.2/Aditzak-0.3.2.tar.gz

---
+++ packages/Aditzak/Aditzak-0.3.2.tar.gz/Aditzak-0.3.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='Aditzak',
+    version='0.3.2',    
+    description='A Python package to analyse and build Euskera/Basque verbs.',
+    url='',
+    author='',
+    author_email='aditzaksoftware@gmail.com',
+    license='BSD 2-clause',
+    packages=['Aditzak'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
