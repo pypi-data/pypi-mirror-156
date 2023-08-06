4af598278ac302bb861675f7382b0ec3b67073ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:31 +0000

    Add package st-rating-df/0.0.3/st_rating_df-0.0.3-py3-none-any.whl

---
+++ packages/st-rating-df/st_rating_df-0.0.3-py3-none-any.whl/st_rating_df/frontend/build/precache-manifest.ae032c43e3b6d2e494c92233454fa13f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9170a7a15c43f808d62b9a91e6d7ff",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9137865059828182b49c",
+    "url": "./static/js/2.8a65b07d.chunk.js"
+  },
+  {
+    "revision": "ff9d73dc3e8582ec316cab4165756e13",
+    "url": "./static/js/2.8a65b07d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "890d46dbe9018f62e68a",
+    "url": "./static/js/main.1ab1a82e.chunk.js"
+  },
+  {
+    "revision": "ea80fab2391112633055",
+    "url": "./static/js/runtime-main.b44bff0a.js"
+  }
+]);
