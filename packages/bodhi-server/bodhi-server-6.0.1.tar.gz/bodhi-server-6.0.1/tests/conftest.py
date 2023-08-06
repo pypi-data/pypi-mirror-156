3dae33f419e5d39b45f5412600a8ba062273dbd9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:19:39 +0000

    Add package bodhi-server/6.0.1/bodhi-server-6.0.1.tar.gz

---
+++ packages/bodhi-server/bodhi-server-6.0.1.tar.gz/bodhi-server-6.0.1/tests/conftest.py
@@ -0,0 +1,25 @@
+# Copyright © 2007-2019 Red Hat, Inc.
+#
+# This file is part of Bodhi.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+"""Pytest configuration."""
+
+import os
+
+
+# Set BODHI_CONFIG to our testing ini file. This is done here before bodhi itself is imported.
+if "BODHI_CONFIG" not in os.environ:
+    os.environ["BODHI_CONFIG"] = os.path.join(os.path.dirname(__file__), "testing.ini")
