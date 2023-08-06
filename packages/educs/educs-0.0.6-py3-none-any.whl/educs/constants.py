22eb27237e135ccba37b12b9b17948a4697fcd65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:25:10 +0000

    Add package educs/0.0.6/educs-0.0.6-py3-none-any.whl

---
+++ packages/educs/educs-0.0.6-py3-none-any.whl/educs/constants.py
@@ -0,0 +1,15 @@
+from math import pi, tau
+import pygame.cursors
+
+# CONSTANTS
+TWO_PI = pi*2.0
+HALF_PI = pi/2.0
+PI = pi
+QUARTER_PI = pi/4.0
+TAU = tau
+
+CURSOR_ARROW = pygame.cursors.arrow
+CURSOR_DIAMOND = pygame.cursors.diamond
+CURSOR_BROKEN_X = pygame.cursors.broken_x
+CURSOR_TRI_LEFT = pygame.cursors.tri_left
+CURSOR_TRI_RIGHT = pygame.cursors.tri_right
