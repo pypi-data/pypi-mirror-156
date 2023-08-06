e6cc4904eee2bf92ed1d43793f9d89a2c5507b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:39 +0000

    Add package djangorestframework-versioning/0.0.8/djangorestframework_versioning-0.0.8.tar.gz

---
+++ packages/djangorestframework-versioning/djangorestframework_versioning-0.0.8.tar.gz/djangorestframework_versioning-0.0.8/README.md
@@ -0,0 +1,9 @@
+# Django Rest Framework Versioning
+Work In Progress! 
+
+Todo: 
+- Make VersionedSerializer work as inline serializer 
+  - Needs to get context.request from parent probably
+- Hard(er) link between VersioningSerializer and its transforms
+- decorator to version viewset actions.
+- The holy grail: get drf's openapi schema generator to listen to all this stuff. 
---
+++ packages/djangorestframework-versioning/djangorestframework_versioning-0.0.8.tar.gz/djangorestframework_versioning-0.0.8/drf_versioning/views.py
@@ -0,0 +1,17 @@
+from rest_framework import viewsets, mixins, decorators
+from rest_framework.response import Response
+
+from drf_versioning.version import Version
+from drf_versioning.version.serializers import VersionSerializer
+
+
+class VersionViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+    serializer_class = VersionSerializer
+
+    def get_queryset(self):
+        return Version.list()
+
+    @decorators.action(methods=["GET"], detail=False)
+    def my_version(self, request, *args, **kwargs):
+        version = request.version
+        return Response(data=self.get_serializer(instance=version).data, status=200)
