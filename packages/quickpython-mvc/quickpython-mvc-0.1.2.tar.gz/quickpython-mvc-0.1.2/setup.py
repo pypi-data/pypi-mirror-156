822f9fa84877b3143111646a260d33cffa6f8742
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:48:06 +0000

    Add package quickpython-mvc/0.1.2/quickpython-mvc-0.1.2.tar.gz

---
+++ packages/quickpython-mvc/quickpython-mvc-0.1.2.tar.gz/quickpython-mvc-0.1.2/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name="quickpython-mvc",  # 这里是pip项目发布的名称
+    version="0.1.2",
+    keywords=["quickpython-mvc", "quickpython", "quick", "WEB", "database", "MVC"],  # 关键字
+    description="Python rapid development framework .",
+    long_description="Python rapid development framework ",
+    license="MIT Licence",  # 许可证
+
+    url="https://github.com/quickpython",
+    author="lo106258",
+    author_email="lo106258@gmail.com",
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["tornado", "pymysql"]
+)
