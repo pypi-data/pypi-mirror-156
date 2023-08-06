f014c0bc4da7c811a5da98569d0632d752cb045b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:39 +0000

    Add package WES-Calculation/1.7.1/WES_Calculation-1.7.1.tar.gz

---
+++ packages/WES-Calculation/WES_Calculation-1.7.1.tar.gz/WES_Calculation-1.7.1/setup.py
@@ -0,0 +1,39 @@
+'''
+Author:  BDFD
+Date: 2021-10-27 18:39:19
+LastEditTime: 2022-06-24 11:15:58
+LastEditors: BDFD
+Description: In User Settings Edit
+FilePath: \Section5.2-PyPi-WES_Calculation\setup.py
+'''
+from setuptools import setup, find_packages
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+VERSION = '1.7.1'
+DESCRIPTION = 'Mini Package for Waterfront Engineering Studio(WES) Calculation model, Currently include Greenampt, Gumbel Model Calculation'
+PACKAGE_NAME = 'WES_Calculation'
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    author="BDFD",
+    author_email="bdfd2005@gmail.com",
+    description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bdfd/5.2-PyPi-WES_Calculation",
+    project_urls={
+        "Bug Tracker": "https://github.com/bdfd/5.2-PyPi-WES_Calculation/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    python_requires=">=3.6",
+)
