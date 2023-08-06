0c58b74e8a19a3d81722a875e7b2e4b4611c4663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:20:34 +0000

    Add package waypoint-api/1.0.3/waypoint_api-1.0.3.tar.gz

---
+++ packages/waypoint-api/waypoint_api-1.0.3.tar.gz/waypoint_api-1.0.3/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="waypoint_api", # Replace with your own username
+    version="1.0.3",
+    author="Fliqqr",
+    author_email="author@example.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Fliqqr/waypoint",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'aiohttp'
+    ],
+    python_requires='>=3.10',
+)
