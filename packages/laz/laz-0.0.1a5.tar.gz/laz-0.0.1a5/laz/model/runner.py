d437af57215a618f064e6a322a32dcbdbc294be7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:44:54 +0000

    Add package laz/0.0.1a5/laz-0.0.1a5.tar.gz

---
+++ packages/laz/laz-0.0.1a5.tar.gz/laz-0.0.1a5/laz/model/runner.py
@@ -0,0 +1,42 @@
+# std
+import argparse
+import traceback
+from typing import List
+
+# internal
+from laz.utils.errors import LazRuntimeError
+from laz.model.tree import Node
+from laz.model.path import Path
+from laz.model.resolver import Resolver
+from laz.model.target import Target
+from laz.model.act import Act
+
+
+class Runner:
+
+    def __init__(self, root_node: Node, cli_args: argparse.Namespace, args: List[str]):
+        self.root_node = root_node
+        self.cli_args = cli_args
+        self.path = Path(args[0])
+        self.args = args[1:]
+
+    def resolve(self) -> List[Target]:
+        resolver = Resolver(self.root_node, self.path)
+        targets = resolver.resolve()
+        if self.cli_args.reverse:
+            targets.reverse()
+        return targets
+
+    def run(self):
+        failures = []
+        targets = self.resolve()
+        for target in targets:
+            try:
+                act = Act(target, self.args)
+                act.act()
+            except Exception as e:
+                failures.append((e, traceback.format_tb()))
+        for failure in failures:
+            print(failure)
+        if failures:
+            raise LazRuntimeError('Some actions failed')
