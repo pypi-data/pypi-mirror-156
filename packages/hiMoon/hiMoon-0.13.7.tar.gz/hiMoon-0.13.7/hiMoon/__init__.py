70b99067b8fefbc7437fb44b7d671564c9fb2cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:39:27 +0000

    Add package hiMoon/0.13.7/hiMoon-0.13.7.tar.gz

---
+++ packages/hiMoon/hiMoon-0.13.7.tar.gz/hiMoon-0.13.7/hiMoon/__init__.py
@@ -0,0 +1,48 @@
+#    Copyright 2021 Solomon M. Adams
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+__author__ = "Solomon M. Adams, PharmD, PhD"
+__copyright__ = "Copyright 2021, Solomon M. Adams"
+__license__ = "Apache v2.0"
+__version__ = "0.13.7"
+__maintainer__ = "Solomon M. Adams, PharmD, PhD"
+__email__ = "sadams2013@gmail.com"
+
+import logging as LOGGING
+
+from .config import ConfigData
+
+SPECIAL_CHROM = {
+    "23": "X",
+    "24": "Y"
+}
+
+# Initialize logger
+LOGGING.getLogger().setLevel(LOGGING.WARNING)
+
+def set_logging_info():
+    LOGGING.getLogger().setLevel(LOGGING.INFO)
+
+def get_config(config_path: str = None):
+    """
+    Set a custom config based on a path
+    Args:
+        config_path ([type]): path/to/config/file.ini
+    """
+    if config_path:
+        return(ConfigData(config_path))
+    else:
+        return(ConfigData())
+
+
