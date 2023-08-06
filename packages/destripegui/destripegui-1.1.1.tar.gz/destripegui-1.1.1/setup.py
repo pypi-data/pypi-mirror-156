984a6fd9c132e3553d0562f24efb8bad8bf49b5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:44 +0000

    Add package destripegui/1.1.1/destripegui-1.1.1.tar.gz

---
+++ packages/destripegui/destripegui-1.1.1.tar.gz/destripegui-1.1.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+VERSION = "1.1.1"
+
+with open('README.md') as file:
+    long_description = file.read()
+
+REQUIREMENTS = ['pystripe']
+
+
+# some more details
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python :: 3.7',
+]
+
+# calling the setup function 
+setup(name='destripegui',
+      version=VERSION,
+      description='A GUI for automatic pystripe destriping',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      url='https://github.com/lifecanvastechnologies',
+      author='LifeCanvas Technologies',
+      license='MIT',
+      # packages=['destripegui'],
+      data_files=[('', ['destripegui/data/config.ini', 'destripegui/data/lct.ico'])],
+      classifiers=CLASSIFIERS,
+      install_requires=REQUIREMENTS,
+      entry_points={
+        'console_scripts' : ['destripegui=destripegui.destripegui:main']
+      }
+)
