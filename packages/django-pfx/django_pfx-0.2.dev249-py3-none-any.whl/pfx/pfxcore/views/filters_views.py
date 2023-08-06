c53baa21ddb480130f774e035888b62ec5e71395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:22:12 +0000

    Add package django-pfx/0.2.dev249/django_pfx-0.2.dev249-py3-none-any.whl

---
+++ packages/django-pfx/django_pfx-0.2.dev249-py3-none-any.whl/pfx/pfxcore/views/filters_views.py
@@ -0,0 +1,107 @@
+from functools import reduce
+
+from django.db.models import Q
+from django.utils.translation import gettext_lazy as _
+
+from pfx.pfxcore.shortcuts import (
+    parse_bool,
+    parse_date,
+    parse_float,
+    parse_int,
+)
+from pfx.pfxcore.views import FieldType
+
+
+class FilterGroup():
+    def __init__(self, name, label, filters):
+        self.name = name
+        self.label = label
+        self.filters = filters
+
+    @property
+    def meta(self):
+        return dict(is_group=True, name=self.name, label=self.label, items=[
+            f.meta for f in self.filters
+        ])
+
+    def query(self, params):
+        return Q(*[f.query(params) for f in self.filters])
+
+
+class Filter():
+    def __init__(
+            self, name, label, type=None, filter_func=None, choices=None,
+            related_model=None):
+        self.name = name
+        self.label = label
+        self.type = type
+        self.filter_func = filter_func
+        self.choices = choices
+        self.related_model = related_model
+
+    @property
+    def meta(self):
+        res = dict(
+            label=_(self.label),
+            name=self.name,
+            type=self.type)
+        if self.choices:
+            res['choices'] = [
+                dict(label=_(v), value=k) for k, v in self.choices]
+        if self.related_model:
+            res['related_model'] = self.related_model
+        return res
+
+    def _parse_value(self, value):
+        if self.type == FieldType.BooleanField:
+            return parse_bool(value)
+        if self.type in (FieldType.IntegerField, FieldType.ModelObject):
+            return parse_int(value)
+        if self.type == FieldType.FloatField:
+            return parse_float(value)
+        if self.type == FieldType.DateField:
+            return parse_date(value)
+        return value
+
+    def query(self, params):
+        values = [self._parse_value(v) for v in params.getlist(self.name)]
+        if values:
+            return reduce(
+                lambda x, y: x | y, [self.filter_func(v) for v in values])
+        return Q()
+
+
+class ModelFilter(Filter):
+    def __init__(
+            self, model, name, label=None, type=None, filter_func=None,
+            choices=None, related_model=None):
+        self.model = model
+        self.field = model._meta.get_field(name)
+        super().__init__(
+            name, label or self.field.verbose_name,
+            type or FieldType.from_model_field(self.field.__class__),
+            filter_func, choices or self.field.choices, related_model or (
+                self.field.remote_field and
+                self.field.remote_field.model.__name__))
+
+    @property
+    def meta(self):
+        res = dict(
+            is_group=False, label=_(self.label), name=self.name,
+            type=self.type)
+        if self.choices:
+            res['choices'] = [
+                dict(label=_(v), value=k) for k, v in self.choices]
+        if self.related_model:
+            res['related_model'] = self.related_model
+        return res
+
+    def query(self, params):
+        values = [self._parse_value(v) for v in params.getlist(self.name)]
+        if self.filter_func and values:
+            return reduce(
+                lambda x, y: x | y, [self.filter_func(v) for v in values])
+        elif values:
+            return reduce(
+                lambda x, y: x | y, [Q(**{self.name: v}) for v in values])
+        return Q()
