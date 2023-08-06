092353fca1fe675d88b304dae04ff9d82c169712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:16 +0000

    Add package TCLR/1.4.3/TCLR-1.4.3-py3-none-any.whl

---
+++ packages/TCLR/TCLR-1.4.3-py3-none-any.whl/TCLR/__init__.py
@@ -0,0 +1,6 @@
+
+__version__ = '1.4.3'
+__description__ = 'Tree-Classifier for linear regression'
+__author__ = 'Bin Cao'
+__author_email__ = 'baco@shu.edu.cn'
+__url__ = 'https://github.com/Bin-Cao/TCLRmodel'
