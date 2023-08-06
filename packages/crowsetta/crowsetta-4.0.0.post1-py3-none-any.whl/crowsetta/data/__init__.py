ca5ad2196ba49f19392a4e1f4f73291f8488c5b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:31 +0000

    Add package crowsetta/4.0.0.post1/crowsetta-4.0.0.post1-py3-none-any.whl

---
+++ packages/crowsetta/crowsetta-4.0.0.post1-py3-none-any.whl/crowsetta/data/__init__.py
@@ -0,0 +1,17 @@
+"""Module that handles example data built into ``crowsetta``"""
+from . import (
+    birdsongrec,
+    generic,
+    notmat,
+    raven,
+    simple,
+    textgrid,
+    timit,
+)
+
+from .data import (
+    available_formats,
+    ExampleAnnotFile,
+    extract_data_files,
+    get,
+)
