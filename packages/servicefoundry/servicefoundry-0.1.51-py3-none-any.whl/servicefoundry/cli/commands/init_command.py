a87f5c674053b9fcc8a0f121d023ab1ca33261c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:44 +0000

    Add package servicefoundry/0.1.51/servicefoundry-0.1.51-py3-none-any.whl

---
+++ packages/servicefoundry/servicefoundry-0.1.51-py3-none-any.whl/servicefoundry/cli/commands/init_command.py
@@ -0,0 +1,26 @@
+import logging
+
+import rich_click as click
+
+from servicefoundry.cli.const import COMMAND_CLS
+from servicefoundry.cli.io.cli_input_hook import CliInputHook
+from servicefoundry.cli.io.rich_output_callback import RichOutputCallBack
+from servicefoundry.cli.util import handle_exception_wrapper
+from servicefoundry.sfy_init.init import init
+
+logger = logging.getLogger(__name__)
+
+
+@click.command(
+    name="init", cls=COMMAND_CLS, help="Initialize a new Service for servicefoundry"
+)
+@click.option("--from-template", is_flag=True, default=False)
+@handle_exception_wrapper
+def init_command(from_template):
+    input_hook = CliInputHook()
+    output_hook = RichOutputCallBack()
+    init(from_template, input_hook, output_hook)
+
+
+def get_init_command():
+    return init_command
