4bd3652d4047ec9bdcfecf1143709a6ffbbeaa14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:36:51 +0000

    Add package frf-nsi/0.0.1/frf-nsi-0.0.1.tar.gz

---
+++ packages/frf-nsi/frf-nsi-0.0.1.tar.gz/frf-nsi-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="frf-nsi",
+    description="Reserved Python package",
+    author="FRF",
+    version="0.0.1",
+    packages=find_packages(),
+    keywords="frf-nsi",
+    long_description="Reserved Python package",
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/",
+)
