d947a9d16b50606357c42fda5bef16681f88a8fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:31:33 +0000

    Add package edgeiq/1.9.0/edgeiq-1.9.0-cp37-none-macosx_10_9_x86_64.whl

---
+++ packages/edgeiq/edgeiq-1.9.0-cp37-none-macosx_10_9_x86_64.whl/edgeiq/_globals.pyi
@@ -0,0 +1,10 @@
+from _typeshed import Incomplete
+
+DEVICE: bool
+DEVICE_MODE: Incomplete
+DEV_HOST: bool
+DEVICE_ID: Incomplete
+PROJECT_ID: Incomplete
+ANALYTICS_CFG: Incomplete
+SYSTEM_ID: Incomplete
+MODEL_ID_LIST: Incomplete
---
+++ packages/edgeiq/edgeiq-1.9.0-cp37-none-macosx_10_9_x86_64.whl/edgeiq/object_tracking/__init__.pyi
@@ -0,0 +1,4 @@
+from .centroid import CentroidTracker as CentroidTracker
+from .correlation import CorrelationTracker as CorrelationTracker
+from .kalman import KalmanTracker as KalmanTracker
+from .object_tracking import RESULT_TYPE as RESULT_TYPE, TrackingResults as TrackingResults
