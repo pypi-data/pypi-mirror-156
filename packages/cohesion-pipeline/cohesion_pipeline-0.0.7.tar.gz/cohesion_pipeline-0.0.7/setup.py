13cab9c104b1220da167c553ba9d79d08cf6fc85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:26:45 +0000

    Add package cohesion-pipeline/0.0.7/cohesion_pipeline-0.0.7.tar.gz

---
+++ packages/cohesion-pipeline/cohesion_pipeline-0.0.7.tar.gz/cohesion_pipeline-0.0.7/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = "cohesion_pipeline",
+    version = "0.0.7",
+    author = "Cohesion Team",
+    author_email = "berdugogo@gmail.com",
+    description = "cohesion_pipeline",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = "https://github.com/Berdugo1994",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'nltk~=3.7',
+        'scikit-learn~=1.1.1',
+        'pandas~=1.3.0',
+        'transformers~=4.20.1',
+        'numpy~=1.21.0',
+        'tqdm~=4.64.0',
+        'tensorflow~=2.2.0',
+        'protobuf~=3.20.1',
+        'torch'
+    ],
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where="src"),
+    python_requires = ">=3.8"
+)
