39f8d9b22eaee271c0ac5734c2e30303cfd53bfb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:13:24 +0000

    Add package devops-toolset/2.11.0/devops_toolset-2.11.0-py3-none-any.whl

---
+++ packages/devops-toolset/devops_toolset-2.11.0-py3-none-any.whl/devops_toolset/tools/commands.py
@@ -0,0 +1,18 @@
+"""tools module commands"""
+
+from devops_toolset.core.ValueDictsBase import ValueDictsBase
+from devops_toolset.core.app import App
+
+app: App = App()
+
+
+class Commands(ValueDictsBase):
+    """Commands for the tools module."""
+
+    # Add your core literal dictionaries here
+    _commands = {
+        "git_init": "git init {path}",
+        "git_add": "git add .",
+        "git_commit_m": "git commit -m \"{message}\"",
+        "public_ip_address_service_url": "http://checkip.dyndns.org",
+    }
