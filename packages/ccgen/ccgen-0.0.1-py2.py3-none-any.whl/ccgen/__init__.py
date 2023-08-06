8d07e826148d21e9b3c71c2b7c1874b5ba8b1c63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:57 +0000

    Add package ccgen/0.0.1/ccgen-0.0.1-py2.py3-none-any.whl

---
+++ packages/ccgen/ccgen-0.0.1-py2.py3-none-any.whl/ccgen/__init__.py
@@ -0,0 +1,6 @@
+#https://facebook.com/akxvau
+#https://facebook.com/akx.the.psycho
+#https://facebook.com/dcbd04
+#https://facebook.com/toxicvirus21
+#https://github.com/akxvau
+#https://t.me/dcbd04
