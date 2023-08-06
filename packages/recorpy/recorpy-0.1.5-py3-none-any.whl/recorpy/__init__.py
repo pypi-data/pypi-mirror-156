f8adc63eacdc42bbead9ec4950fd4d9d7c9f94a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:08:09 +0000

    Add package recorpy/0.1.5/recorpy-0.1.5-py3-none-any.whl

---
+++ packages/recorpy/recorpy-0.1.5-py3-none-any.whl/recorpy/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@File    :   __init__.py
+@Time    :   2022/06/24 09:59:42
+@Author  :   Bart Ortiz 
+@Version :   0.1.4
+@Contact :   bortiz@ugr.es
+@License :   CC-BY-SA or GPL3
+@Desc    :   Init file for the recorpy package
+"""
+
+from recorpy.reorder import ReorderCorr
+
+__version__ = "0.1.5"
