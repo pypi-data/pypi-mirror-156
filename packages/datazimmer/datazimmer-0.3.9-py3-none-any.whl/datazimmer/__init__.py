b6873712b6a59c1d114723ba6b79ba48a9d15a9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:45:38 +0000

    Add package datazimmer/0.3.9/datazimmer-0.3.9-py3-none-any.whl

---
+++ packages/datazimmer/datazimmer-0.3.9-py3-none-any.whl/datazimmer/__init__.py
@@ -0,0 +1,16 @@
+"""sscu-budapest utilities for scientific data engineering"""
+# flake8: noqa
+from .metadata.datascript import (
+    AbstractEntity,
+    CompositeTypeBase,
+    Index,
+    Nullable,
+    SourceUrl,
+)
+from .metadata.scrutable import ScruTable
+from .pipeline_registry import register, register_data_loader, register_env_creator
+from .project_runtime import dump_dfs_to_tables
+from .reporting import ReportFile
+from .typer_commands import app
+
+__version__ = "0.3.9"
