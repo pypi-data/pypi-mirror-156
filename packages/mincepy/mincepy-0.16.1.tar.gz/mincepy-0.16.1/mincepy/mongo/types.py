ff5aa2eed6ade58c83860d8007c2a4599d7bf345
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:31 +0000

    Add package mincepy/0.16.1/mincepy-0.16.1.tar.gz

---
+++ packages/mincepy/mincepy-0.16.1.tar.gz/mincepy-0.16.1/mincepy/mongo/types.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import bson
+
+import mincepy
+
+# pylint: disable=invalid-name
+
+# Problem with subscripting archive, bug report here:
+# https://github.com/PyCQA/pylint/issues/2822
+_Archive = mincepy.Archive[bson.ObjectId]  # pylint: disable=unsubscriptable-object
+SnapshotId = _Archive.SnapshotId
