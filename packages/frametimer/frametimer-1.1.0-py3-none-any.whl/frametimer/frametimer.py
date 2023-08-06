fbbe76341d3e708f426683bcecea13bde958c60f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:59 +0000

    Add package frametimer/1.1.0/frametimer-1.1.0-py3-none-any.whl

---
+++ packages/frametimer/frametimer-1.1.0-py3-none-any.whl/frametimer/frametimer.py
@@ -0,0 +1,46 @@
+import sys
+import time
+
+class FrameTimer:
+	def __init__(self, target_fps=60, spin_time=None):
+		self.target_fps = target_fps
+		if spin_time is None:
+			self.spin_time = self._guess_spin_time()
+		else:
+			self.spin_time = spin_time
+		self.last_tick_time = None
+
+	def _guess_spin_time(self):
+		if sys.platform.startswith('linux'):
+			return 0.001
+		else:
+			return 0.01
+
+	def tick(self, fps=None):
+		if fps is None:
+			fps = self.target_fps
+
+		if self.last_tick_time is None:
+			self.last_tick_time = self.now()
+			return 0.0
+
+		self.accurate_sleep_until(self.last_tick_time + 1. / fps)
+
+		now = self.now()
+		dt = now - self.last_tick_time
+		self.last_tick_time = now
+		return dt
+
+	def accurate_sleep_until(self, target_time):
+		sleep_time = target_time - self.now() - self.spin_time
+		if sleep_time > 0:
+			self.sleep(sleep_time)
+
+		while self.now() < target_time:
+			pass
+
+	def sleep(self, seconds):
+		time.sleep(seconds)
+
+	def now(self):
+		return time.perf_counter()
