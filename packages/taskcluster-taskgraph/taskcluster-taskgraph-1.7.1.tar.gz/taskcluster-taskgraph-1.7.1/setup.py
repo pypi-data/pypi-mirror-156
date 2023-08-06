e20511cda6ed5222bacd53e34e1d6132dfa0608e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 00:41:19 +0000

    Add package taskcluster-taskgraph/1.7.1/taskcluster-taskgraph-1.7.1.tar.gz

---
+++ packages/taskcluster-taskgraph/taskcluster-taskgraph-1.7.1.tar.gz/taskcluster-taskgraph-1.7.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import find_packages, setup
+
+with open("requirements/base.in") as fp:
+    requirements = fp.read().splitlines()
+
+setup(
+    name="taskcluster-taskgraph",
+    version="1.7.1",
+    description="Build taskcluster taskgraphs",
+    url="https://github.com/taskcluster/taskgraph",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Software Development",
+    ],
+    entry_points={"console_scripts": ["taskgraph = taskgraph.main:main"]},
+    package_data={
+        "taskgraph": [
+            "run-task/run-task",
+            "run-task/fetch-content",
+            "run-task/hgrc",
+            "run-task/robustcheckout.py",
+        ],
+        "taskgraph.test": ["automationrelevance.json"],
+    },
+)
