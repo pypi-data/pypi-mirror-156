2936ab3bdb0dd5e80bebe32dc4ef6588299fa49a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:31 +0000

    Add package huaweicloudsdkocr/3.0.93/huaweicloudsdkocr-3.0.93-py2.py3-none-any.whl

---
+++ packages/huaweicloudsdkocr/huaweicloudsdkocr-3.0.93-py2.py3-none-any.whl/huaweicloudsdkocr/v1/region/ocr_region.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+
+import types
+import six
+
+from huaweicloudsdkcore.region.region import Region
+
+
+class OcrRegion:
+    def __init__(self):
+        pass
+
+    CN_NORTH_4 = Region(id="cn-north-4", endpoint="https://ocr.cn-north-4.myhuaweicloud.com")
+
+    CN_SOUTH_1 = Region(id="cn-south-1", endpoint="https://ocr.cn-south-1.myhuaweicloud.com")
+
+    CN_EAST_3 = Region(id="cn-east-3", endpoint="https://ocr.cn-east-3.myhuaweicloud.com")
+
+    CN_NORTH_1 = Region(id="cn-north-1", endpoint="https://ocr.cn-north-1.myhuaweicloud.com")
+
+    AP_SOUTHEAST_2 = Region(id="ap-southeast-2", endpoint="https://ocr.ap-southeast-2.myhuaweicloud.com")
+
+    AP_SOUTHEAST_1 = Region(id="ap-southeast-1", endpoint="https://ocr.ap-southeast-1.myhuaweicloud.com")
+
+    AP_SOUTHEAST_3 = Region(id="ap-southeast-3", endpoint="https://ocr.ap-southeast-3.myhuaweicloud.com")
+
+    static_fields = {
+        "cn-north-4": CN_NORTH_4,
+        "cn-south-1": CN_SOUTH_1,
+        "cn-east-3": CN_EAST_3,
+        "cn-north-1": CN_NORTH_1,
+        "ap-southeast-2": AP_SOUTHEAST_2,
+        "ap-southeast-1": AP_SOUTHEAST_1,
+        "ap-southeast-3": AP_SOUTHEAST_3,
+    }
+
+    @staticmethod
+    def value_of(region_id, static_fields=types.MappingProxyType(static_fields) if six.PY3 else static_fields):
+        if region_id is None or len(region_id) == 0:
+            raise KeyError("Unexpected empty parameter: region_id.")
+        if not static_fields.get(region_id):
+            raise KeyError("Unexpected region_id: " + region_id)
+        return static_fields.get(region_id)
+
+
