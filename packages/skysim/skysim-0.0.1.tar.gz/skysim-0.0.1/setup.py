abe234cf98b6fe7cfc59d83d41e141cd9db41765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:18:54 +0000

    Add package skysim/0.0.1/skysim-0.0.1.tar.gz

---
+++ packages/skysim/skysim-0.0.1.tar.gz/skysim-0.0.1/setup.py
@@ -0,0 +1,8 @@
+import setuptools
+setuptools.setup(
+    name="skysim",
+    version="0.0.1",
+    author="Intel#1563",
+    description="Python Implementation of SkysimAPI",
+    packages=["skysim"]
+)
