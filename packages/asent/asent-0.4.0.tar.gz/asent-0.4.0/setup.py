dc779b159700f5baf00d1b9716660c92faeab7eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:11:02 +0000

    Add package asent/0.4.0/asent-0.4.0.tar.gz

---
+++ packages/asent/asent-0.4.0.tar.gz/asent-0.4.0/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+
+with open("asent/about.py") as f:
+    v = f.read()
+    for l in v.split("\n"):
+        if l.startswith("__version__"):
+            __version__ = l.split('"')[-2]
+
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(version=__version__)
