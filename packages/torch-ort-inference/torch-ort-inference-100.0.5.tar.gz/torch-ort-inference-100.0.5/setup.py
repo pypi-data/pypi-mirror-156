9c582d6424d72245f96fb0f55b479e25e4027171
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:45 +0000

    Add package torch-ort-inference/100.0.5/torch-ort-inference-100.0.5.tar.gz

---
+++ packages/torch-ort-inference/torch-ort-inference-100.0.5.tar.gz/torch-ort-inference-100.0.5/setup.py
@@ -0,0 +1,14 @@
+import setuptools 
+setuptools.setup(
+    name="torch-ort-inference",
+    version="100.0.5",
+    author="Alon walker",
+    author_email="alonwalker@facebook.com",
+    description="torch-ort-inference of torch-ort-inference of torch-ort-inference",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
