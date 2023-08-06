c11a42efefd3d7416a9b8b74447be9736c4343b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:15:22 +0000

    Add package version-handle/0.1.2/version_handle-0.1.2-py3-none-any.whl

---
+++ packages/version-handle/version_handle-0.1.2-py3-none-any.whl/version_handle/__init__.py
@@ -0,0 +1,9 @@
+# -*-coding:utf-8-*-
+# Author: Eason.Deng
+# Github: https://github.com/holbos-deng
+# Email: 2292861292@qq.com
+# CreateDate: 2022/5/23 14:42
+# Description:
+from . import python
+from .generate import get as generate
+from .entities import Version
