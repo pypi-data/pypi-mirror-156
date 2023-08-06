93fa633ed6b5566b08a8ba8f0a0238d590f558da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:36 +0000

    Add package pytest-aoreporter/0.1.1/pytest_aoreporter-0.1.1.tar.gz

---
+++ packages/pytest-aoreporter/pytest_aoreporter-0.1.1.tar.gz/pytest_aoreporter-0.1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name="pytest_aoreporter",
+    version="0.1.1",
+    author="ancientone",
+    author_email="listeningsss@163.com",
+    description="pytest report",
+    url="https://github.com/ae86sen/pytest-aoreporter",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pytest',
+        'Jinja2',
+    ],
+    entry_points={"pytest11": ['pytest_aoreporter=pytest_aoreporter.plugin', ], },
+    include_package_data=True,
+)
