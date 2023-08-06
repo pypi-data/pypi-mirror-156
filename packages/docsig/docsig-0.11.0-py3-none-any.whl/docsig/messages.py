83eb780299c9cb5ef88fa2b28d0d5c5233ed8349
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:53 +0000

    Add package docsig/0.11.0/docsig-0.11.0-py3-none-any.whl

---
+++ packages/docsig/docsig-0.11.0-py3-none-any.whl/docsig/messages.py
@@ -0,0 +1,20 @@
+"""
+docsig.messages
+===============
+"""
+#: Exxx: Error
+#: E1xx: Docstring
+E101 = "E101: parameters out of order"
+E102 = "E102: includes parameters that do not exist"
+E103 = "E103: parameters missing"
+E104 = "E104: return statement documented for None"
+E105 = "E105: return missing from docstring"
+E106 = "E106: duplicate parameters found"
+E107 = "E107: parameter appears to be incorrectly documented"
+
+#: Hxxx: Hint
+#: H1xx: Docstring
+H101 = "hint: there appears to be a syntax error that could be causing this"
+H102 = "hint: documentation is sufficient as a getter is the value returned"
+H103 = "hint: it is possible a syntax error could be causing this"
+H104 = "hint: function is missing a docstring"
