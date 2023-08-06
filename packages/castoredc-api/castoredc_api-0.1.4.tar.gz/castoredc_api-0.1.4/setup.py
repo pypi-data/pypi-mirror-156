456e9ecd1dba70d94a6b5a31b8dfe0ff8f11bd6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:30:16 +0000

    Add package castoredc-api/0.1.4/castoredc_api-0.1.4.tar.gz

---
+++ packages/castoredc-api/castoredc_api-0.1.4.tar.gz/castoredc_api-0.1.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="castoredc_api",
+    version="0.1.4",
+    description="Python wrapper for the Castor EDC API",
+    author="Reinier van Linschoten",
+    author_email="mail@reiniervl.com",
+    maintaner="Reinier van Linschoten",
+    maintaner_email="mail@reiniervl.com",
+    url="https://github.com/reiniervlinschoten/castoredc_api",
+    packages=find_packages(include=("castoredc_api", "castoredc_api.*")),
+    install_requires=[
+        "pandas>=1.3.1",
+        "numpy>=1.21.1",
+        "openpyxl>=3.0.7",
+        "tqdm>=4.62.0",
+        "httpx>=0.19.0",
+    ],
+    tests_require=["pytest"],
+    license="MIT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
