09429ddeac76574eb452e310c2b1b1f50cf0f90f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:16:25 +0000

    Add package hkw/1.2.2/hkw-1.2.2-py3-none-any.whl

---
+++ packages/hkw/hkw-1.2.2-py3-none-any.whl/hkw/data_.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+import os
+rdata_ = os.path.join(os.path.dirname(__file__), 'rdata_', 'exam.csv')
+def hkw() :
+	ppd = pd.read_csv(rdata_)
+	lat= np.loadtxt(rdata_, delimiter = ',', skiprows = 1, dtype = 'str')
+	print(lat)
+	print(ppd)
