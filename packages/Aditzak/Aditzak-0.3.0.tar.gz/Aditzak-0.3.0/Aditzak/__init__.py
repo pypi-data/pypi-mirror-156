8876cf6c6ff6caed795e45b538f754a04219243b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:36 +0000

    Add package Aditzak/0.3.0/Aditzak-0.3.0.tar.gz

---
+++ packages/Aditzak/Aditzak-0.3.0.tar.gz/Aditzak-0.3.0/Aditzak/__init__.py
@@ -0,0 +1,2 @@
+__version__ = "0.3.0"
+__author__ = [{"Henry Cook":"henryscookolaizola@gmail.com"}, {"Alejandro Sempere":"asempere@colegioaleman.net"}, "Pablo Pérez"]
---
+++ packages/Aditzak/Aditzak-0.3.0.tar.gz/Aditzak-0.3.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='Aditzak',
+    version='0.3.0',    
+    description='A Pytohn package',
+    url='',
+    author='',
+    author_email='aditzaksoftware@gmail.com',
+    license='BSD 2-clause',
+    packages=['Aditzak'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
