0413f4c12d4a0e632359a4ee5cf17009b31e3855
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:19 +0000

    Add package swagger-gen/0.1.2/swagger-gen-0.1.2.tar.gz

---
+++ packages/swagger-gen/swagger-gen-0.1.2.tar.gz/swagger-gen-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.2'
+DESCRIPTION = 'Swagger UI for Flask apps'
+LONG_DESCRIPTION = 'Automatically generate Swagger UI documentation for a Flask app.  Batteries included.'
+
+setup(
+    name='swagger-gen',
+    python_requires='>=3.9.0',
+    version=VERSION,
+    author='Dan Leonard',
+    author_email='dcl525@gmail.com',
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    package_data={'swagger_gen': ['./resources/swagger.pkl']},
+    packages=find_packages(),
+    install_requires=['flask'],
+    keywords=['python', 'swagger-gen'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Programming Language :: Python :: 3',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+    ]
+)
