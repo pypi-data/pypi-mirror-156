361060f79e3e5cf7c1a9b098015125f92f0344fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:47:40 +0000

    Add package pytest-embrace/1.0.2/pytest-embrace-1.0.2.tar.gz

---
+++ packages/pytest-embrace/pytest-embrace-1.0.2.tar.gz/pytest-embrace-1.0.2/pytest_embrace/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.0.2"
+
+
+from .case import CaseArtifact as CaseArtifact
+from .case import CaseRunner as CaseRunner
+from .case import trickles as trickles
+from .embrace import Embrace as Embrace
