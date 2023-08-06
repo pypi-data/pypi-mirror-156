bd5b6e766cf295d3b502ee01a918f27d9ab71201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:12:48 +0000

    Add package miniworldmaker/2.10.0.1/miniworldmaker-2.10.0.1.tar.gz

---
+++ packages/miniworldmaker/miniworldmaker-2.10.0.1.tar.gz/miniworldmaker-2.10.0.1/miniworldmaker/appearances/managers/transformations_background_manager.py
@@ -0,0 +1,31 @@
+import pygame
+import miniworldmaker.appearances.managers.transformations_manager as transformations_manager
+import traceback
+
+
+class TransformationsBackgroundManager(transformations_manager.TransformationsManager):
+    def __init__(self, appearance):
+        super().__init__(appearance)
+        self.transformations_pipeline.append(("grid", self.transformation_grid, "grid"))
+
+    def get_size(self):
+        size = (self.appearance.parent.container_width, self.appearance.parent.container_height)
+        return size
+
+    def transformation_grid(self, image: pygame.Surface, appearance) -> pygame.Surface:
+        parent = self.appearance.parent
+        i = 0
+        j = 0
+        while i <= parent.height:
+            pygame.draw.line(image, appearance._grid_color, (0, i), (parent.width, i), 1)
+            i += parent.tile_size
+        while j <= parent.width:
+            pygame.draw.line(image, appearance._grid_color, (j, 0), (j, parent.height), 1)
+            j += parent.tile_size
+
+        return image
+
+    #def set_dirty(self, transformation_string, status):
+    #    super().set_dirty(transformation_string, status)
+    #    self.appearance.repaint()
+    # not needed?
