84cbe4ec3b8196e52352597da4ffd131c716d45c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:57 +0000

    Add package amplitude-data-wrapper/0.1.1/amplitude-data-wrapper-0.1.1.tar.gz

---
+++ packages/amplitude-data-wrapper/amplitude-data-wrapper-0.1.1.tar.gz/amplitude-data-wrapper-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['amplitude_data_wrapper']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['requests>=2.27.1,<3.0.0', 'tqdm>=4.62.3,<5.0.0']
+
+setup_kwargs = {
+    'name': 'amplitude-data-wrapper',
+    'version': '0.1.1',
+    'description': 'python wrapper for using the amplitude analytics and taxonomy APIs',
+    'long_description': None,
+    'author': 'Tobias McVey',
+    'author_email': 'tobias.mcvey@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
