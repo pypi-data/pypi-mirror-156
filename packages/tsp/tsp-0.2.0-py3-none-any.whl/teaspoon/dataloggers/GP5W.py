449f371486751ede6fce77ac767cbea8e8e83f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:25 +0000

    Add package tsp/0.2.0/tsp-0.2.0-py3-none-any.whl

---
+++ packages/tsp/tsp-0.2.0-py3-none-any.whl/teaspoon/dataloggers/GP5W.py
@@ -0,0 +1,63 @@
+
+import pandas as pd
+import re
+
+from .AbstractReader import AbstractReader
+
+
+class GP5W(AbstractReader):
+    DATEFMT = "%d.%m.%Y %H:%M:%S"
+    HK = re.compile("^#HK")
+
+    def read(self, file: str) -> "pd.DataFrame":
+        """Read a GP5W geoprecision file
+
+        Parameters
+        ----------
+        file : str
+            Path to an GP5W Geoprecision datalogger export
+
+        Returns
+        -------
+        pandas.DataFrame
+            A dataframe containing the data
+        """
+        self.META['raw'] = list()
+        with open(file, "r") as f:
+            for line in f:
+                if self.__is_header(line):
+                    delimiters = line.count(",")
+                    columns = line.strip().split(",")
+
+                elif self._is_observation(line):
+                    line = line.strip()
+                    line += "," * (delimiters - line.count(","))
+                    self.DATA.append(line.split(","))
+
+                else:
+                    self.META['raw'].append(line)
+
+        self.DATA = pd.DataFrame(self.DATA, columns=columns)
+        self.DATA["Time"] = pd.to_datetime(self.DATA["Time"], format=self.DATEFMT)
+        self.DATA.rename(columns={"Time":"TIME"}, inplace=True)
+
+        self.DATA = self.DATA.drop(["No"], axis=1)
+        self.DATA = self.drop_hk(self.DATA)
+
+        return self.DATA
+
+    def _is_observation(self, line: str) -> bool:
+        match = re.search(r"\d*,\d{2}\.\d{2}\.\d{4}", line)
+        return bool(match)
+
+    def __is_header(self, line: str) -> bool:
+        match = re.search("No,Time", line)
+        return bool(match)
+
+    def _is_hk(self, name: str) -> bool:
+        if self.HK.match(name):
+            return True
+        return False
+
+    def drop_hk(self, df: "pd.DataFrame") -> "pd.DataFrame":
+        return df.drop([c for c in df if self._is_hk(c)], axis=1)
