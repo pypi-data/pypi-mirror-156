c9587e657fb95a323783ca6d93abb405998d9830
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:44 +0000

    Add package shipper/1.1.1/shipper-1.1.1.tar.gz

---
+++ packages/shipper/shipper-1.1.1.tar.gz/shipper-1.1.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+setup(
+    name="shipper",
+    version='1.1.1',
+    author="jaytrairat",
+    author_email="<jay.trairat@gmail.com>",
+    description='Package shipper',
+    long_description_content_type="text/markdown",
+    long_description='',
+    packages=find_packages(),
+    install_requires=[''],
+    keywords=['jaytrairat'],
+    classifiers=["Programming Language :: Python :: 3"]
+)
